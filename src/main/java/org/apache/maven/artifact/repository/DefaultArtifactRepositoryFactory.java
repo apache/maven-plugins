cf46dce176bcf7d145ad8628c82160be250b97f4
Brett Leslie Porter <brett@apache.org>
2005-04-22 08:48:41 +0000

    move artifact repository factory into maven-artifact
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk/maven-artifact@164201 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/artifact/repository/DefaultArtifactRepositoryFactory.java
@@ -0,0 +1,52 @@
+package org.apache.maven.artifact.repository;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+
+/**
+ * @author jdcasey
+ */
+public class DefaultArtifactRepositoryFactory
+    extends AbstractLogEnabled
+    implements ArtifactRepositoryFactory
+{
+    // TODO: use settings?
+    private String globalSnapshotPolicy = null;
+
+    public ArtifactRepository createArtifactRepository( String id, String url,
+                                                        ArtifactRepositoryLayout repositoryLayout,
+                                                        String snapshotPolicy )
+    {
+        ArtifactRepository repo = null;
+
+        if ( globalSnapshotPolicy != null )
+        {
+            snapshotPolicy = globalSnapshotPolicy;
+        }
+
+        repo = new ArtifactRepository( id, url, repositoryLayout, snapshotPolicy );
+
+        return repo;
+    }
+
+    public void setGlobalSnapshotPolicy( String snapshotPolicy )
+    {
+        this.globalSnapshotPolicy = snapshotPolicy;
+    }
+}
