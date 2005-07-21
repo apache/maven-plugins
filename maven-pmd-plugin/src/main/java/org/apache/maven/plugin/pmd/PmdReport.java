a4df6b25ed8c273ffbe45356fbee6af0ae89eb8c
Brett Leslie Porter <brett@apache.org>
2005-07-21 02:20:27 +0000

    move reports into maven-plugins
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk/maven-plugins@219985 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-pmd-plugin/src/main/java/org/apache/maven/plugin/pmd/PmdReport.java
@@ -0,0 +1,242 @@
+package org.apache.maven.plugin.pmd;
+
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.PMDException;
+import net.sourceforge.pmd.Report;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.RuleSet;
+import net.sourceforge.pmd.RuleSetFactory;
+
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.doxia.sink.Sink;
+import org.codehaus.doxia.site.renderer.SiteRenderer;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.StringUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Implement the PMD report.
+ *
+ * @goal pmd
+ *
+ * @todo needs to support the multiple source roots
+ * @author Brett Porter
+ * @version $Id: PmdReport.java,v 1.3 2005/02/23 00:08:53 brett Exp $
+ */
+public class PmdReport
+    extends AbstractMavenReport
+{
+    /**
+     * @parameter expression="${project.build.directory}/site"
+     * @required
+     */
+    private String outputDirectory;
+
+    /**
+     * @parameter expression="${component.org.codehaus.doxia.site.renderer.SiteRenderer}"
+     * @required
+     * @readonly
+     */
+    private SiteRenderer siteRenderer;
+
+    /**
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getName(java.util.Locale)
+     */
+    public String getName( Locale locale )
+    {
+        return getBundle( locale ).getString( "report.pmd.name" );
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getDescription(java.util.Locale)
+     */
+    public String getDescription( Locale locale )
+    {
+        return getBundle( locale ).getString( "report.pmd.description" );
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#getOutputDirectory()
+     */
+    protected String getOutputDirectory()
+    {
+        return outputDirectory;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#getProject()
+     */
+    protected MavenProject getProject()
+    {
+        return project;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#getSiteRenderer()
+     */
+    protected SiteRenderer getSiteRenderer()
+    {
+        return siteRenderer;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#executeReport(java.util.Locale)
+     */
+    public void executeReport( Locale locale )
+        throws MavenReportException
+    {
+        Sink sink = null;
+        try
+        {
+            sink = getSink();
+        }
+        catch ( IOException e )
+        {
+            throw new MavenReportException( "Can't obtain sink for PMD report.", e );
+        }
+
+        PMD pmd = new PMD();
+        RuleContext ruleContext = new RuleContext();
+        Report report = new Report();
+        // TODO: use source roots instead
+        String sourceDirectory = getProject().getBuild().getSourceDirectory();
+        PmdReportListener reportSink = new PmdReportListener( sink, sourceDirectory, getBundle( locale ) );
+        report.addListener( reportSink );
+        ruleContext.setReport( report );
+
+        RuleSetFactory ruleSetFactory = new RuleSetFactory();
+        InputStream rulesInput = pmd.getClass().getResourceAsStream( "/rulesets/controversial.xml" );
+        RuleSet ruleSet = ruleSetFactory.createRuleSet( rulesInput );
+
+        reportSink.beginDocument();
+
+        List files;
+        try
+        {
+            files = getFilesToProcess( "**/*.java", null );
+        }
+        catch ( IOException e )
+        {
+            throw new MavenReportException( "Can't parse " + sourceDirectory, e );
+        }
+
+        for ( Iterator i = files.iterator(); i.hasNext(); )
+        {
+            File file = (File) i.next();
+            FileReader fileReader;
+            try
+            {
+                fileReader = new FileReader( file );
+            }
+            catch ( FileNotFoundException e )
+            {
+                throw new MavenReportException( "Error opening source file: " + file, e );
+            }
+
+            try
+            {
+                // TODO: lazily call beginFile in case there are no rules
+
+                reportSink.beginFile( file );
+                ruleContext.setSourceCodeFilename( file.getAbsolutePath() );
+                pmd.processFile( fileReader, ruleSet, ruleContext );
+                reportSink.endFile( file );
+            }
+            catch ( PMDException e )
+            {
+                Exception ex = e;
+                if ( e.getReason() != null )
+                {
+                    ex = e.getReason();
+                }
+                throw new MavenReportException( "Failure executing PMD for: " + file, ex );
+            }
+            finally
+            {
+                try
+                {
+                    fileReader.close();
+                }
+                catch ( IOException e )
+                {
+                    throw new MavenReportException( "Error closing source file: " + file, e );
+                }
+            }
+        }
+        reportSink.endDocument();
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getOutputName()
+     */
+    public String getOutputName()
+    {
+        return "pmd";
+    }
+
+    private List getFilesToProcess( String includes, String excludes )
+        throws IOException
+    {
+        File dir = new File( getProject().getBuild().getSourceDirectory() );
+        if ( !dir.exists() )
+        {
+            return Collections.EMPTY_LIST;
+        }
+
+        StringBuffer excludesStr = new StringBuffer();
+        if ( StringUtils.isNotEmpty( excludes ) )
+        {
+            excludesStr.append( excludes );
+        }
+        for ( int i = 0; i < DEFAULT_EXCLUDES.length; i++ )
+        {
+            if ( excludesStr.length() > 0 )
+            {
+                excludesStr.append( "," );
+            }
+            excludesStr.append( DEFAULT_EXCLUDES[i] );
+        }
+
+        return FileUtils.getFiles( dir, includes, excludesStr.toString() );
+    }
+
+    private static ResourceBundle getBundle( Locale locale )
+    {
+        return ResourceBundle.getBundle("pmd-report", locale, PmdReport.class.getClassLoader() );
+    }
+}
---
+++ maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/DependenciesReport.java
@@ -0,0 +1,370 @@
+package org.apache.maven.report.projectinfo;
+
+/*
+ * Copyright 2004-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.project.ProjectBuildingException;
+import org.apache.maven.reporting.AbstractMavenReportRenderer;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.doxia.sink.Sink;
+import org.codehaus.doxia.site.renderer.SiteRenderer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+/**
+ * Generates the Project Dependencies report.
+ * 
+ * @goal dependencies
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl </a>
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton </a>
+ * @version $Id$
+ * @plexus.component
+ */
+public class DependenciesReport
+    extends AbstractMavenReport
+{
+    /**
+     * @parameter expression="${project.build.directory}/site"
+     * @required
+     */
+    private String outputDirectory;
+
+    /**
+     * @parameter expression="${component.org.codehaus.doxia.site.renderer.SiteRenderer}"
+     * @required
+     * @readonly
+     */
+    private SiteRenderer siteRenderer;
+
+    /**
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * @parameter expression="${component.org.apache.maven.artifact.factory.ArtifactFactory}"
+     * @required
+     * @readonly
+     */
+    private ArtifactFactory artifactFactory;
+
+    /**
+     * @parameter expression="${component.org.apache.maven.project.MavenProjectBuilder}"
+     * @required
+     * @readonly
+     */
+    private MavenProjectBuilder mavenProjectBuilder;
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getName(java.util.Locale)
+     */
+    public String getName( Locale locale )
+    {
+        return getBundle( locale ).getString( "report.dependencies.name" );
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getCategoryName()
+     */
+    public String getCategoryName()
+    {
+        return CATEGORY_PROJECT_INFORMATION;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getDescription(java.util.Locale)
+     */
+    public String getDescription( Locale locale )
+    {
+        return getBundle( locale ).getString( "report.dependencies.description" );
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#getOutputDirectory()
+     */
+    protected String getOutputDirectory()
+    {
+        return outputDirectory;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#getProject()
+     */
+    protected MavenProject getProject()
+    {
+        return project;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#getSiteRenderer()
+     */
+    protected SiteRenderer getSiteRenderer()
+    {
+        return siteRenderer;
+    }
+
+    /**
+     * @see org.apache.maven.reporting.AbstractMavenReport#executeReport(java.util.Locale)
+     */
+    public void executeReport( Locale locale )
+        throws MavenReportException
+    {
+        try
+        {
+            DependenciesRenderer r = new DependenciesRenderer( getSink(), getProject(), locale, mavenProjectBuilder,
+                                                               artifactFactory );
+
+            r.render();
+        }
+        catch ( IOException e )
+        {
+            throw new MavenReportException( "Can't write the report " + getOutputName(), e );
+        }
+    }
+
+    /**
+     * @see org.apache.maven.reporting.MavenReport#getOutputName()
+     */
+    public String getOutputName()
+    {
+        return "dependencies";
+    }
+
+    static class DependenciesRenderer
+        extends AbstractMavenReportRenderer
+    {
+        private MavenProject project;
+
+        private Locale locale;
+
+        private ArtifactFactory artifactFactory;
+
+        private MavenProjectBuilder mavenProjectBuilder;
+
+        public DependenciesRenderer( Sink sink, MavenProject project, Locale locale,
+                                    MavenProjectBuilder mavenProjectBuilder, ArtifactFactory artifactFactory )
+        {
+            super( sink );
+
+            this.project = project;
+
+            this.locale = locale;
+
+            this.mavenProjectBuilder = mavenProjectBuilder;
+
+            this.artifactFactory = artifactFactory;
+        }
+
+        public String getTitle()
+        {
+            return getBundle( locale ).getString( "report.dependencies.title" );
+        }
+
+        public void renderBody()
+        {
+            // Dependencies report
+            List dependencies = project.getDependencies();
+
+            if ( ( dependencies == null ) || ( dependencies.isEmpty() ) )
+            {
+                startSection( getTitle() );
+
+                // TODO: should the report just be excluded?
+                paragraph( getBundle( locale ).getString( "report.dependencies.nolist" ) );
+
+                endSection();
+
+                return;
+            }
+
+            startSection( getTitle() );
+
+            startTable();
+
+            tableCaption( getBundle( locale ).getString( "report.dependencies.intro" ) );
+
+            String groupId = getBundle( locale ).getString( "report.dependencies.column.groupId" );
+            String artifactId = getBundle( locale ).getString( "report.dependencies.column.artifactId" );
+            String version = getBundle( locale ).getString( "report.dependencies.column.version" );
+            String description = getBundle( locale ).getString( "report.dependencies.column.description" );
+            String url = getBundle( locale ).getString( "report.dependencies.column.url" );
+
+            tableHeader( new String[] { groupId, artifactId, version, description, url } );
+
+            for ( Iterator i = dependencies.iterator(); i.hasNext(); )
+            {
+                Dependency dependency = (Dependency) i.next();
+
+                Artifact artifact = artifactFactory.createArtifact( dependency.getGroupId(),
+                                                                    dependency.getArtifactId(),
+                                                                    dependency.getVersion(), dependency.getScope(),
+                                                                    dependency.getType() );
+                MavenProject artifactProject = null;
+                try
+                {
+                    artifactProject = getMavenProjectFromRepository( artifact );
+                }
+                catch ( ProjectBuildingException e )
+                {
+                    throw new IllegalArgumentException(
+                                                        "Can't find a valid Maven project in the repository for the artifact ["
+                                                            + artifact + "]." );
+                }
+
+                tableRow( new String[] {
+                    dependency.getGroupId(),
+                    dependency.getArtifactId(),
+                    dependency.getVersion(),
+                    artifactProject.getDescription(),
+                    createLinkPatternedText( artifactProject.getUrl(), artifactProject.getUrl() ) } );
+            }
+
+            endTable();
+
+            endSection();
+
+            // Transitive dependencies
+            Set artifacts = getTransitiveDependencies( project );
+
+            startSection( getBundle( locale ).getString( "report.transitivedependencies.title" ) );
+
+            if ( artifacts.isEmpty() )
+            {
+                paragraph( getBundle( locale ).getString( "report.transitivedependencies.nolist" ) );
+            }
+            else
+            {
+                startTable();
+
+                tableCaption( getBundle( locale ).getString( "report.transitivedependencies.intro" ) );
+
+                tableHeader( new String[] { groupId, artifactId, version, description, url } );
+
+                for ( Iterator i = artifacts.iterator(); i.hasNext(); )
+                {
+                    Artifact artifact = (Artifact) i.next();
+
+                    MavenProject artifactProject = null;
+                    try
+                    {
+                        artifactProject = getMavenProjectFromRepository( artifact );
+                    }
+                    catch ( ProjectBuildingException e )
+                    {
+                        throw new IllegalArgumentException(
+                                                            "Can't find a valid Maven project in the repository for the artifact ["
+                                                                + artifact + "]." );
+                    }
+                    System.out.println( "nklj-----------------------------" );
+                    System.out.println( artifactProject.getUrl() );
+                    tableRow( new String[] {
+                        artifact.getGroupId(),
+                        artifact.getArtifactId(),
+                        artifact.getVersion(),
+                        artifactProject.getDescription(),
+                        createLinkPatternedText( artifactProject.getUrl(), artifactProject.getUrl() ) } );
+                }
+
+                endTable();
+            }
+
+            endSection();
+        }
+
+        /**
+         * Return a set of <code>Artifacts</code> which are not already
+         * present in the dependencies list.
+         * 
+         * @param project
+         *            a Maven project
+         * @return a set of transitive dependencies as artifacts
+         */
+        private Set getTransitiveDependencies( MavenProject project )
+        {
+            Set transitiveDependencies = new HashSet();
+
+            List dependencies = project.getDependencies();
+            Set artifacts = project.getArtifacts();
+
+            if ( ( dependencies == null ) || ( artifacts == null ) )
+            {
+                return transitiveDependencies;
+            }
+
+            List dependenciesAsArtifacts = new ArrayList( dependencies.size() );
+            for ( Iterator i = dependencies.iterator(); i.hasNext(); )
+            {
+                Dependency dependency = (Dependency) i.next();
+
+                Artifact artifact = artifactFactory.createArtifact( dependency.getGroupId(),
+                                                                    dependency.getArtifactId(),
+                                                                    dependency.getVersion(), dependency.getScope(),
+                                                                    dependency.getType() );
+                dependenciesAsArtifacts.add( artifact );
+            }
+
+            for ( Iterator j = artifacts.iterator(); j.hasNext(); )
+            {
+                Artifact artifact = (Artifact) j.next();
+
+                if ( !dependenciesAsArtifacts.contains( artifact ) )
+                {
+                    transitiveDependencies.add( artifact );
+                }
+            }
+
+            return transitiveDependencies;
+        }
+
+        /**
+         * Get the <code>Maven project</code> from the repository depending
+         * the <code>Artifact</code> given.
+         * 
+         * @param artifact
+         *            an artifact
+         * @return the Maven project for the given artifact
+         * @throws ProjectBuildingException
+         *             if any
+         */
+        private MavenProject getMavenProjectFromRepository( Artifact artifact )
+            throws ProjectBuildingException
+        {
+            return mavenProjectBuilder.buildFromRepository( artifact, project.getRepositories(), artifact
+                .getRepository() );
+        }
+    }
+
+    private static ResourceBundle getBundle( Locale locale )
+    {
+        return ResourceBundle.getBundle( "project-info-report", locale, DependenciesReport.class.getClassLoader() );
+    }
+}
