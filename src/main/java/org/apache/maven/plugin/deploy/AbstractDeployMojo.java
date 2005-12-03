e750218d0d2978c7e5c4ca7a3ec547a9038fdf57
Allan Q. Ramirez <aramirez@apache.org>
2005-12-03 00:09:47 +0000

    PR:MNG-1551 Added deploy-file goal which will enable users to install artifacts in their remote repository
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-deploy-plugin@351856 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/deploy/AbstractDeployMojo.java
@@ -0,0 +1,65 @@
+package org.apache.maven.plugin.deploy;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.artifact.deployer.ArtifactDeployer;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+public abstract class AbstractDeployMojo
+	extends AbstractMojo
+{
+    /**
+     * @parameter expression="${component.org.apache.maven.artifact.deployer.ArtifactDeployer}"
+     * @required
+     * @readonly
+     */
+    private ArtifactDeployer deployer;
+
+    /**
+     * @parameter expression="${localRepository}"
+     * @required
+     * @readonly
+     */
+    private ArtifactRepository localRepository;
+	
+	public abstract void execute() 
+		throws MojoExecutionException;
+	
+	/* Setters and Getters */
+
+	public ArtifactDeployer getDeployer()
+	{
+		return deployer;
+	}
+
+	public void setDeployer(ArtifactDeployer deployer)
+	{
+		this.deployer = deployer;
+	}
+
+	public ArtifactRepository getLocalRepository()
+	{
+		return localRepository;
+	}
+
+	public void setLocalRepository(ArtifactRepository localRepository)
+	{
+		this.localRepository = localRepository;
+	}
+}
