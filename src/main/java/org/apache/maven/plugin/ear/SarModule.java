e01b9510ddef346f2c81ea267fce37ca9df2035e
Stephane Nicoll <snicoll@apache.org>
2005-12-16 16:02:41 +0000

    MNG-1852: Added support of sar files.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-ear-plugin@357175 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/ear/SarModule.java
@@ -0,0 +1,56 @@
+package org.apache.maven.plugin.ear;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.codehaus.plexus.util.xml.XMLWriter;
+
+/**
+ * The {@link EarModule} implementation for a JBoss sar module.
+ *
+ * @author Stephane Nicoll <snicoll@apache.org>
+ * @author $Author: $ (last edit)
+ * @version $Revision:  $
+ */
+public class SarModule
+    extends AbstractEarModule
+{
+    protected static final String SAR_MODULE = "connector";
+
+    public SarModule()
+    {
+    }
+
+    public SarModule( Artifact a )
+    {
+        super( a );
+    }
+
+    public void appendModule( XMLWriter writer, String version )
+    {
+        writer.startElement( MODULE_ELEMENT );
+        writer.startElement( SAR_MODULE );
+        writer.writeText( getUri() );
+        writer.endElement();
+        writer.endElement();
+    }
+
+    protected String getType()
+    {
+        return "sar";
+    }
+}
