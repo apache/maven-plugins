6502b1e44d90aae63c7e2352e7427ab6786cd684
Vincent Massol <vmassol@apache.org>
2005-12-24 10:42:38 +0000

    Trying to reproduce MCLOVER-3. I haven't succeeded yet but I think it's useful to add test resources anyway to prove that it works.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@358923 13f79535-47bb-0310-9956-ffa450edef68

--- maven-clover-plugin/src/it/maven-clover-plugin-sample-simple/src/test/java/org/apache/maven/plugin/clover/samples/simple/SimpleTest.java
+++ maven-clover-plugin/src/it/maven-clover-plugin-sample-simple/src/test/java/org/apache/maven/plugin/clover/samples/simple/SimpleTest.java
@@ -16,13 +16,18 @@
 package org.apache.maven.plugin.clover.samples.simple;
 
 import junit.framework.TestCase;
+import java.util.Properties;
 
 public class SimpleTest extends TestCase
 {
-    public void testSomeMethod()
+    public void testSomeMethod() throws Exception
     {
         Simple simple = new Simple();
         simple.someMethod();
+        
+        // Verify that we can load resource files from tess when using the clover plugin
+        Properties props = new Properties();
+        props.load(getClass().getResourceAsStream("/org/apache/maven/plugin/clover/samples/simple/test.properties"));
     }
 }
  
