17e3bbf7f2ee492da7efbfce569e8b673982b1c0
Vincent Siveton <vsiveton@apache.org>
2006-03-02 12:13:52 +0000

    Added package declaration in test case (Cut and paste is evil!)
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-project-info-reports-plugin@382360 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/projects/project-info-reports-plugin-test1/src/test/java/org/apache/maven/report/projectinfo/test1/AppTest.java
+++ src/test/projects/project-info-reports-plugin-test1/src/test/java/org/apache/maven/report/projectinfo/test1/AppTest.java
@@ -1,4 +1,4 @@
-org.apache.maven.report.projectinfo.test1;
+package org.apache.maven.report.projectinfo.test1;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
--- src/test/projects/project-info-reports-plugin-test9/src/test/java/org/apache/maven/report/projectinfo/test9/AppTest.java
+++ src/test/projects/project-info-reports-plugin-test9/src/test/java/org/apache/maven/report/projectinfo/test9/AppTest.java
@@ -1,4 +1,4 @@
-org.apache.maven.report.projectinfo.test9;
+package org.apache.maven.report.projectinfo.test9;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
