294d022f4869fe5a4a592dbd716c0793ab538670
Allan Q. Ramirez <aramirez@apache.org>
2006-03-09 10:07:36 +0000

    PR: MASSEMBLY-20
    -Created a scope artifact filter for assembly that can accept provided dependencies if the scope tag in assembly descriptor is set to "provided"
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@384478 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/assembly/filter/AssemblyScopeArtifactFilter.java
@@ -0,0 +1,91 @@
+package org.apache.maven.plugin.assembly.filter;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
+
+public class AssemblyScopeArtifactFilter
+    implements ArtifactFilter
+{
+    private final boolean compileScope;
+
+    private final boolean runtimeScope;
+
+    private final boolean testScope;
+
+    private final boolean providedScope;
+
+    private final boolean systemScope;
+
+    public AssemblyScopeArtifactFilter( String scope )
+    {
+        if ( DefaultArtifact.SCOPE_COMPILE.equals( scope ) )
+        {
+            systemScope = true;
+            providedScope = true;
+            compileScope = true;
+            runtimeScope = false;
+            testScope = false;
+        }
+        else if ( DefaultArtifact.SCOPE_RUNTIME.equals( scope ) )
+        {
+            systemScope = false;
+            providedScope = false;
+            compileScope = true;
+            runtimeScope = true;
+            testScope = false;
+        }
+        else if ( DefaultArtifact.SCOPE_TEST.equals( scope ) )
+        {
+            systemScope = true;
+            providedScope = true;
+            compileScope = true;
+            runtimeScope = true;
+            testScope = true;
+        }  
+        else if( DefaultArtifact.SCOPE_PROVIDED.equals( scope ) )
+        {
+            systemScope = false;
+            providedScope = true;
+            compileScope = false;
+            runtimeScope = false;
+            testScope = false;            
+        }
+        else
+        {
+            systemScope = false;
+            providedScope = false;
+            compileScope = false;
+            runtimeScope = false;
+            testScope = false;
+        }
+    }    
+    
+    public boolean include( Artifact artifact )
+    {
+        if ( Artifact.SCOPE_COMPILE.equals( artifact.getScope() ) )
+        {
+            return compileScope;
+        }
+        else if ( Artifact.SCOPE_RUNTIME.equals( artifact.getScope() ) )
+        {
+            return runtimeScope;
+        }
+        else if ( Artifact.SCOPE_TEST.equals( artifact.getScope() ) )
+        {
+            return testScope;
+        }
+        else if ( Artifact.SCOPE_PROVIDED.equals( artifact.getScope() ) )
+        {
+            return providedScope;
+        }
+        else if ( Artifact.SCOPE_SYSTEM.equals( artifact.getScope() ) )
+        {
+            return systemScope;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
