1bdf8103af5ba8d984a018acb6ccd2caff724117
Edwin L. Punzalan <epunzalan@apache.org>
2006-03-10 10:24:47 +0000

    Reverting a clearcase parameter checkoutDirectoryName as suggested by evenisse.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-project-info-reports-plugin@384756 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/report/projectinfo/ScmReport.java
+++ src/main/java/org/apache/maven/report/projectinfo/ScmReport.java
@@ -509,9 +509,7 @@ public class ScmReport
             paragraph( i18n.getString( "project-info-report", locale, "report.scm.devaccess.clearcase.intro" ) );
 
             StringBuffer command = new StringBuffer();
-            // @todo test replacement of getModule() with directory...
-            // found on a site that the next parameter is a pathname
-            command.append( "$ cleartool checkout " ).append( checkoutDirectoryName );
+            command.append( "$ cleartool checkout " );
 
             verbatimText( command.toString() );
         }
