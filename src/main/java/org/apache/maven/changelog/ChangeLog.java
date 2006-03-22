5628e3f48cb6ca6f9313253ee0ece574df94fc26
Brett Leslie Porter <brett@apache.org>
2006-03-22 06:02:52 +0000

    Import of changelog-maven-plugin code from https://svn.codehaus.org/mojo/trunk/mojo/changelog-maven-plugin/, r1708
    Code authored by people that have an Apache CLA on file: 
    * Edwin Punzalan (Initial contribution derived from code under https://svn.apache.org/repos/asf/maven/maven-1/plugins/trunk/changelog)
    * Trygve Laugstol
    * Brett Porter
    * Emmanuel Venisse
    * Brian Fox
    
    Contains code donated under the Apache License 2.0 by:
    * Julian Wood <woodj@ucalgary.ca> - r973, handle date and time being non-sequentially parsed by the handler
    
    All code is under the Apache License v2.0
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/plugins/changelog-maven-plugin@387745 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/changelog/ChangeLog.java
@@ -0,0 +1,66 @@
+package org.apache.maven.changelog;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// java imports
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Change log task. It uses a ChangeLogGenerator and ChangeLogParser to create
+ * a Collection of ChangeLogEntry objects, which are used to produce an XML
+ * output that represents the list of changes.
+ *
+ * @author <a href="mailto:glenn@somanetworks.com">Glenn McAllister</a>
+ * @author <a href="mailto:jeff.martin@synamic.co.uk">Jeff Martin</a>
+ * @author <a href="mailto:jason@zenplex.com">Jason van Zyl</a>
+ * @author <a href="mailto:dion@multitask.com.au">dIon Gillard</a>
+ * @author <a href="mailto:bodewig@apache.org">Stefan Bodewig</a>
+ * @author <a href="mailto:peter@apache.org">Peter Donald</a>
+ * @version $Id$
+ */
+public class ChangeLog
+{
+    /**
+     * parses a previously generated changelog xml document and return its changed sets
+     *
+     * @param stream the changelog xml document
+     * @return changelog sets parsed from the xml document
+     * @throws ParserConfigurationException when instantiation of the SAX parser failed
+     * @throws SAXException                 when an error occurred while parsing the xml document
+     * @throws IOException                  when an error occurred while accessing the xml document
+     */
+    public static List loadChangedSets( InputStream stream )
+        throws ParserConfigurationException, SAXException, IOException
+    {
+        SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+
+        List changeLogSets = new ArrayList();
+
+        parser.parse( stream, new ChangeLogHandler( changeLogSets ) );
+
+        return changeLogSets;
+    }
+} // end of ChangeLog
