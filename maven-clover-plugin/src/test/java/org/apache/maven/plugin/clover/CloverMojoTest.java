5fab3756853bc1306dea3c3063ab9fd752b2224a
Vincent Massol <vmassol@apache.org>
2006-03-27 20:02:32 +0000

    MCLOVER-23: Allow custom license to be passed as JAR resource, URL or File Patch submitted by: John Allen & Vincent Massol
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@389245 13f79535-47bb-0310-9956-ffa450edef68

--- maven-clover-plugin/src/test/java/org/apache/maven/plugin/clover/CloverMojoTest.java
+++ maven-clover-plugin/src/test/java/org/apache/maven/plugin/clover/CloverMojoTest.java
@@ -1,7 +1,5 @@
-package org.apache.maven.plugin.clover;
-
 /*
- * Copyright 2005 The Apache Software Foundation.
+ * Copyright 2005-2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,22 +13,48 @@ package org.apache.maven.plugin.clover;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.maven.plugin.clover;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.clover.internal.Locator;
+import org.jmock.MockObjectTestCase;
+import org.jmock.Mock;
 
-import junit.framework.TestCase;
+import java.io.File;
 
 /**
- * Unit tests for {@link org.apache.maven.plugin.clover.CloverInstrumentMojo}.
+ * Unit tests for {@link org.apache.maven.plugin.clover.AbstractCloverMojo}.
  * 
  * @author <a href="mailto:vmassol@apache.org">Vincent Massol</a>
  * @version $Id$
  */
-public class CloverMojoTest extends TestCase
+public class CloverMojoTest extends MockObjectTestCase
 {
-	public void testExecution() throws MojoExecutionException
-	{
-//		CloverInstrumentMojo mojo = new CloverInstrumentMojo();
-        //TODO: write a test here...
-	}
+    public class TestableAbstractCloverMojo extends AbstractCloverMojo
+    {
+        public void execute() throws MojoExecutionException
+        {
+            // Voluntarily left blank
+        }
+    }
+
+    public void testRegisterLicenseFile() throws MojoExecutionException
+    {
+        TestableAbstractCloverMojo mojo = new TestableAbstractCloverMojo();
+
+        Mock mockLocator = mock( Locator.class );
+        mojo.setLocator( (Locator) mockLocator.proxy() );
+
+        // Ensure that the system property is not already set
+        System.clearProperty( "clover.license.path" );
+
+        mojo.setLicenseLocation( "build-tools/clover.license" );
+        mockLocator.expects( once() ).method( "resolveLocation" )
+            .with( eq( "build-tools/clover.license" ), eq( "clover.license" ) )
+            .will( returnValue( new File( "targetFile" ) ) );
+
+        mojo.registerLicenseFile();
+
+        assertEquals( "targetFile", System.getProperty( "clover.license.path" ) );
+    }
 }
