bf86e2a1f80f7c37a7dd98ad706f8a54ccb86562
Brett Porter <brett@apache.org>
2006-03-28 19:10:28 +0000

    put back tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-compiler-plugin@389582 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/compiler-basic-test/src/main/java/TestCompile0.java
@@ -0,0 +1,12 @@
+
+
+public class TestCompile0
+{
+
+    public TestCompile0()
+    {
+
+        System.out.println("Woo Hoo!");
+    }
+
+}
---
+++ src/test/resources/unit/compiler-includes-excludes-test/src/main/java/TestCompile4.java
@@ -0,0 +1,12 @@
+
+
+public class TestCompile4
+{
+
+    public TestCompile4()
+    {
+
+        System.out.println("Woo Hoo!");
+    }
+
+}
