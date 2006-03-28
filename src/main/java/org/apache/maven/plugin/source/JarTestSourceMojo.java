30fc624dc8e4bea58f08a265aa5a7ec9a69262f0
Edwin L. Punzalan <epunzalan@apache.org>
2006-03-28 05:58:18 +0000

    PR: MSOURCES-2 Submitted by: Maria Odea Ching
    Created a new goal: jar-test for the creating test sources jar.  Retained includeTestSources parameter in sources goal so both can be packaged in a single jar.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-source-plugin@389406 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/source/JarTestSourceMojo.java
@@ -0,0 +1,65 @@
+package org.apache.maven.plugin.source;
+
+/*
+* Copyright 2005-2006 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.archiver.jar.JarArchiver;
+
+import java.io.File;
+
+/**
+ * This plugin bundles all the generated test sources into a jar archive.
+ *
+ * @goal jar-test
+ * @phase package
+ * @execute phase="generate-sources"
+ */
+public class JarTestSourceMojo
+    extends AbstractJarSourceMojo
+{
+    /**
+     * @parameter expression="${project.build.finalName}"
+     * @required
+     */
+    private String finalName;
+
+    /**
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private File outputDirectory;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        validatePackaging();
+        File outputFile = new File( outputDirectory, finalName + "-test-sources.jar" );
+        File[] testSourceDirectories = getTestSources();
+
+        try
+        {
+            createJar( outputFile, testSourceDirectories, new JarArchiver() );
+        }
+        catch ( Exception e )
+        {
+            throw new MojoExecutionException( "Error building test source JAR", e );
+        }
+
+        attachArtifact( outputFile );
+    }
+
+}
