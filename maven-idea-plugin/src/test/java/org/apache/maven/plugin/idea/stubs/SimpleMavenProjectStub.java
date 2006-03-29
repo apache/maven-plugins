dc798222a87910a7528f51514b6eeb52be858b14
Edwin L. Punzalan <epunzalan@apache.org>
2006-03-29 02:22:17 +0000

    First try on the plugin test harness
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@389656 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-idea-plugin/src/test/java/org/apache/maven/plugin/idea/stubs/SimpleMavenProjectStub.java
@@ -0,0 +1,120 @@
+package org.apache.maven.plugin.idea.stubs;
+
+/*
+ * Copyright 2005-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugins.testing.stubs.StubMavenProject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.File;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class SimpleMavenProjectStub
+    extends StubMavenProject
+{
+    private static int usageCounter;
+
+    private List collectedProjects;
+
+    public SimpleMavenProjectStub()
+    {
+        usageCounter++;
+    }
+
+    public static int getUsageCounter()
+    {
+        return usageCounter;
+    }
+
+    public String getGroupId()
+    {
+        return "org.apache.maven.plugin.test";
+    }
+
+    public String getArtifactId()
+    {
+        return "plugin-test-" + usageCounter;
+    }
+
+    public String getVersion()
+    {
+        return String.valueOf( usageCounter );
+    }
+
+    public File getBasedir()
+    {
+        File basedir = new File( "target/test-harness/" + usageCounter );
+
+        if ( !basedir.exists() )
+        {
+            basedir.mkdirs();
+        }
+
+        return basedir;
+    }
+
+    public List getCollectedProjects()
+    {
+        if ( collectedProjects == null )
+        {
+            collectedProjects = new ArrayList();
+        }
+        return collectedProjects;
+    }
+
+    public void setCollectedProjects( List list )
+    {
+        collectedProjects = list;
+    }
+
+    public List getDependencies()
+    {
+        List dependencies = new ArrayList();
+
+        Dependency dep = new Dependency();
+        dep.setGroupId( "org.apache.maven" );
+        dep.setArtifactId( "maven-model" );
+        dep.setVersion( "2.0.1" );
+        dep.setScope( Artifact.SCOPE_COMPILE );
+        dependencies.add( dep );
+
+        return dependencies;
+    }
+
+    public Artifact getArtifact()
+    {
+        Artifact artifact = new ArtifactStub();
+
+        artifact.setGroupId( getGroupId() );
+
+        artifact.setArtifactId( getArtifactId() );
+
+        artifact.setVersion( getVersion() );
+
+        return artifact;
+    }
+
+    public List getRemoteArtifactRepositories()
+    {
+        return Collections.EMPTY_LIST;
+    }
+}
