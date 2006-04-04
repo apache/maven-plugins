914f0ad9b6c5d1668b4e98e88630b343dcf9c02e
Brett Leslie Porter <brett@apache.org>
2006-04-04 23:36:39 +0000

    [MIDEA-44] defaulting short names to false to avoid issues with IDEA 5.1 and web modules
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@391449 13f79535-47bb-0310-9956-ffa450edef68

--- maven-idea-plugin/src/main/java/org/apache/maven/plugin/idea/IdeaMojo.java
+++ maven-idea-plugin/src/main/java/org/apache/maven/plugin/idea/IdeaMojo.java
@@ -171,7 +171,7 @@ public class IdeaMojo
      * Causes the module libraries to use a short name for all dependencies. This is very convenient but has been
      * reported to cause problems with IDEA.
      *
-     * @parameter default-value="true"
+     * @parameter default-value="false"
      */
     private boolean dependenciesAsLibraries;
 
