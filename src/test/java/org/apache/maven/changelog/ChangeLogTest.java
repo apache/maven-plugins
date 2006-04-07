c0fa86faa76b9406b889e64bccaef2dc5f795fa3
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-07 07:25:21 +0000

    PR: MCHANGELOG-34
    Added tests for the changelog xml cache parser
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/plugins/maven-changelog-plugin@392208 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/changelog/ChangeLogTest.java
@@ -0,0 +1,146 @@
+package org.apache.maven.changelog;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import junit.framework.TestCase;
+import org.apache.maven.scm.command.changelog.ChangeLogSet;
+import org.apache.maven.scm.ChangeSet;
+import org.apache.maven.scm.ChangeFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class ChangeLogTest
+    extends TestCase
+{
+    public void testReadFile()
+        throws Exception
+    {
+        List changedLogs = readChangeLogXml( "min-changelog.xml" );
+
+        assertNotNull( "Test changedSets were parsed", changedLogs );
+
+        assertEquals( "Test number of changelog entries", 2, changedLogs.size() );
+
+        ChangeLogSet changelogSets = (ChangeLogSet) changedLogs.get( 0 );
+
+        assertEquals( "Test number of revisions on changelog 1", 2, changelogSets.getChangeSets().size() );
+
+        ChangeSet changeSet = (ChangeSet) changelogSets.getChangeSets().get( 0 );
+
+        assertEquals( "Test changelog 1 set 1 date/time", 239635800000L, changeSet.getDate().getTime() );
+
+        assertEquals( "Test changelog 1 set 1 author", "Edwin Punzalan", changeSet.getAuthor() );
+
+        assertEquals( "Test changelog 1 set 1 msg", "First commit msg", changeSet.getComment() );
+
+        assertEquals( "Test changelog 1 set 1 files", 1, changeSet.getFiles().size() );
+
+        ChangeFile changeFile = (ChangeFile) changeSet.getFiles().get( 0 );
+
+        assertEquals( "Test changelog 1 set 1 file 1 filename", "/path/to/file.extension", changeFile.getName() );
+
+        assertEquals( "Test changelog 1 set 1 file 1 revision", "1", changeFile.getRevision() );
+
+
+        changeSet = (ChangeSet) changelogSets.getChangeSets().get( 1 );
+
+        assertEquals( "Test changelog 1 set 2 date/time", 1109223000000L, changeSet.getDate().getTime() );
+
+        assertEquals( "Test changelog 1 set 2 author", "Edwin Punzalan", changeSet.getAuthor() );
+
+        assertEquals( "Test changelog 1 set 2 msg", "Second commit msg", changeSet.getComment() );
+
+        assertEquals( "Test changelog 1 set 2 files", 2, changeSet.getFiles().size() );
+
+        changeFile = (ChangeFile) changeSet.getFiles().get( 0 );
+
+        assertEquals( "Test changelog 1 set 2 file 1 filename", "/path/to/file.extension", changeFile.getName() );
+
+        assertEquals( "Test changelog 1 set 2 file 1 revision", "2", changeFile.getRevision() );
+
+        changeFile = (ChangeFile) changeSet.getFiles().get( 1 );
+
+        assertEquals( "Test changelog 1 set 2 file 2 filename", "/path/to/file2.extension", changeFile.getName() );
+
+        assertEquals( "Test changelog 1 set 2 file 2 revision", "2", changeFile.getRevision() );
+
+
+
+        changelogSets = (ChangeLogSet) changedLogs.get( 1 );
+
+        assertEquals( "Test number of revisions on changelog 2", 2, changelogSets.getChangeSets().size() );
+
+        changeSet = (ChangeSet) changelogSets.getChangeSets().get( 0 );
+
+        assertEquals( "Test changelog 2 set 1 date/time", 1109313900000L, changeSet.getDate().getTime() );
+
+        assertEquals( "Test changelog 2 set 1 author", "Keogh Edrich Punzalan", changeSet.getAuthor() );
+
+        assertEquals( "Test changelog 2 set 1 msg", "Third commit msg", changeSet.getComment() );
+
+        assertEquals( "Test changelog 2 set 1 files", 1, changeSet.getFiles().size() );
+
+        changeFile = (ChangeFile) changeSet.getFiles().get( 0 );
+
+        assertEquals( "Test changelog 2 set 1 file 1 filename", "/path/to/file.extension", changeFile.getName() );
+
+        assertEquals( "Test changelog 2 set 1 file 1 revision", "3", changeFile.getRevision() );
+
+
+        changeSet = (ChangeSet) changelogSets.getChangeSets().get( 1 );
+
+        assertEquals( "Test changelog 2 set 2 date/time", 4107159000000L, changeSet.getDate().getTime() );
+
+        assertEquals( "Test changelog 2 set 2 author", "Keogh Edrich Punzalan", changeSet.getAuthor() );
+
+        assertEquals( "Test changelog 2 set 2 msg", "Last commit msg", changeSet.getComment() );
+
+        assertEquals( "Test changelog 2 set 2 files", 2, changeSet.getFiles().size() );
+
+        changeFile = (ChangeFile) changeSet.getFiles().get( 0 );
+
+        assertEquals( "Test changelog 2 set 2 file 1 filename", "/path/to/file.extension", changeFile.getName() );
+
+        assertEquals( "Test changelog 2 set 2 file 1 revision", "4", changeFile.getRevision() );
+
+        changeFile = (ChangeFile) changeSet.getFiles().get( 1 );
+
+        assertEquals( "Test changelog 2 set 2 file 2 filename", "/path/to/file2.extension", changeFile.getName() );
+
+        assertEquals( "Test changelog 2 set 2 file 2 revision", "4", changeFile.getRevision() );
+    }
+
+    private List readChangeLogXml( String filename )
+        throws Exception
+    {
+        File inputFile = new File( getBasedir(), "src/test/changelog-xml/" + filename );
+        InputStream in = new FileInputStream( inputFile );
+
+        return ChangeLog.loadChangedSets( in );
+    }
+
+    private String getBasedir()
+    {
+        return System.getProperty( "basedir" );
+    }
+}
