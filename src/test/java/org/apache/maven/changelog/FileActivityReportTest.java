cb3a1297dd23917c01ad1b1d52914369dbe168eb
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-11 11:47:35 +0000

    PR: MCHANGELOG-34
    Added test harness for dev-activity and file-activity mojos
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/plugins/maven-changelog-plugin@393194 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/changelog/FileActivityReportTest.java
@@ -0,0 +1,107 @@
+package org.apache.maven.changelog;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.scm.manager.ScmManager;
+import org.apache.maven.changelog.stubs.ScmManagerStub;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class FileActivityReportTest
+    extends AbstractMojoTestCase
+{
+    private ScmManager scmManager;
+
+    public void testNoSource()
+        throws Exception
+    {
+        File pluginXmlFile = new File( getBasedir(),
+                                               "src/test/plugin-configs/file-activity/no-source-plugin-config.xml" );
+
+        Mojo mojo = lookupMojo( "file-activity", pluginXmlFile );
+
+        assertNotNull( "Mojo found.", mojo );
+
+        this.setVariableValueToObject( mojo, "manager", scmManager );
+
+        mojo.execute();
+
+        File outputDir = (File) getVariableValueFromObject( mojo, "outputDirectory" );
+
+        File outputHtml = new File( outputDir, "file-activity.html" );
+
+        assertTrue( "Test html generated", outputHtml.exists() );
+    }
+
+    public void testMinConfig()
+        throws Exception
+    {
+        File outputXML = new File( getBasedir(), "src/test/changelog-xml/min-changelog.xml" );
+
+        // force reuse of existing changelog cache
+        outputXML.setLastModified( System.currentTimeMillis() );
+
+        executeMojo( "min-plugin-config.xml" );
+    }
+
+    private void executeMojo( String pluginXml )
+        throws Exception
+    {
+        File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/file-activity/" + pluginXml );
+
+        Mojo mojo = lookupMojo( "file-activity", pluginXmlFile );
+
+        assertNotNull( "Mojo found.", mojo );
+
+        this.setVariableValueToObject( mojo, "manager", scmManager );
+
+        mojo.execute();
+
+        File outputXML = (File) getVariableValueFromObject( mojo, "outputXML" );
+
+        String encoding = (String) getVariableValueFromObject( mojo, "outputEncoding" );
+
+        assertTrue( "Test if changelog.xml is created", outputXML.exists() );
+
+        String changelogXml = FileUtils.fileRead( outputXML );
+
+        assertTrue( "Test for xml header", changelogXml.startsWith( "<?xml version=\"1.0\" encoding=\"" +
+                    encoding + "\"?>" ) );
+
+        assertTrue( "Test for xml footer", changelogXml.endsWith( "</changelog>" ) );
+
+        File outputDir = (File) getVariableValueFromObject( mojo, "outputDirectory" );
+
+        File outputHtml = new File( outputDir, "file-activity.html" );
+
+        assertTrue( "Test html generated", outputHtml.exists() );
+    }
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        scmManager = new ScmManagerStub();
+    }
+}
---
+++ src/test/java/org/apache/maven/changelog/stubs/DevelopersStub.java
@@ -0,0 +1,48 @@
+package org.apache.maven.changelog.stubs;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.model.Developer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class DevelopersStub
+    extends ArrayList
+{
+    public DevelopersStub()
+    {
+        super();
+
+        List developers = new ArrayList();
+
+        Developer developer = new Developer();
+        developer.setName( "Edwin Punzalan" );
+        developer.setId( "edwin" );
+        developers.add( developer );
+
+        developer = new Developer();
+        developer.setName( "Keogh Edrich Punzalan" );
+        developer.setId( "keogh" );
+        developers.add( developer );
+
+        super.addAll( developers );
+    }
+}
