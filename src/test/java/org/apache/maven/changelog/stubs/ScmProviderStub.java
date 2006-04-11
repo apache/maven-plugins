5fd95cea06e82c5ed7371247e16edee51e3ea5c0
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-11 07:45:54 +0000

    PR: MCHANGELOG-34
    Added test harness for changelog report mojo
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/plugins/maven-changelog-plugin@393154 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/changelog/stubs/ScmProviderStub.java
+++ src/test/java/org/apache/maven/changelog/stubs/ScmProviderStub.java
@@ -1,20 +1,19 @@
 package org.apache.maven.changelog.stubs;
 
 /*
- *  Copyright 2001-2006 The Apache Software Foundation.
+ * Copyright 2001-2006 The Apache Software Foundation.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import org.apache.maven.scm.ScmException;
@@ -57,32 +56,37 @@ public class ScmProviderStub
     {
     }
 
+    protected ChangeLogScmResult getChangeLogScmResult()
+    {
+        return new ChangeLogScmResultStub();
+    }
+
     public ChangeLogScmResult changeLog( ScmRepository scmRepository, ScmFileSet scmFileSet, Date date, Date date1,
                                          int i, String string )
         throws ScmException
     {
-        return null;
+        return getChangeLogScmResult();
     }
 
     public ChangeLogScmResult changeLog( ScmRepository scmRepository, ScmFileSet scmFileSet, Date date, Date date1,
                                          int i, String string, String string1 )
         throws ScmException
     {
-        return new org.apache.maven.changelog.stubs.ChangeLogScmResult();
+        return getChangeLogScmResult();
     }
 
     public ChangeLogScmResult changeLog( ScmRepository scmRepository, ScmFileSet scmFileSet, String string,
                                          String string1 )
         throws ScmException
     {
-        return null;
+        return getChangeLogScmResult();
     }
 
     public ChangeLogScmResult changeLog( ScmRepository scmRepository, ScmFileSet scmFileSet, String string,
                                          String string1, String string2 )
         throws ScmException
     {
-        return null;
+        return getChangeLogScmResult();
     }
 
     public CheckInScmResult checkIn( ScmRepository scmRepository, ScmFileSet scmFileSet, String string, String string1 )
