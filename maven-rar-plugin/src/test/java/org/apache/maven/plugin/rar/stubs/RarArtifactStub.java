ad7c4e031ef8b122f7a31e7762684f1a6558c84c
Allan Q. Ramirez <aramirez@apache.org>
2006-04-11 02:51:09 +0000

    PR: MRAR-6
    added tests using plugin testing harness
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@393141 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-rar-plugin/src/test/java/org/apache/maven/plugin/rar/stubs/RarArtifactStub.java
@@ -0,0 +1,110 @@
+package org.apache.maven.plugin.rar.stubs;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.stubs.ArtifactStub;
+
+/**
+ * @author <a href="mailto:aramirez@apache.org">Allan Ramirez</a>
+ */
+public class RarArtifactStub
+    extends ArtifactStub
+{
+    private String groupId;
+    
+    private String artifactId;
+    
+    private String version;
+    
+    private String scope;
+    
+    private boolean optional;
+    
+    private File file;
+
+    public String getArtifactId()
+    {
+        return artifactId;
+    }
+
+    public void setArtifactId( String artifactId )
+    {
+        this.artifactId = artifactId;
+    }
+
+    public File getFile()
+    {
+        return file;
+    }
+
+    public void setFile( File file )
+    {
+        this.file = file;
+    }
+
+    public String getGroupId()
+    {
+        return groupId;
+    }
+
+    public void setGroupId( String groupId )
+    {
+        this.groupId = groupId;
+    }
+
+    public boolean isOptional()
+    {
+        return optional;
+    }
+
+    public void setOptional( boolean optional )
+    {
+        this.optional = optional;
+    }
+
+    public String getScope()
+    {
+        return scope;
+    }
+
+    public void setScope( String scope )
+    {
+        this.scope = scope;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion( String version )
+    {
+        this.version = version;
+    }
+
+    public String getId()
+    {
+        return getGroupId() + ":" + getArtifactId() + ":" + getVersion();
+    }
+    
+    public String getBaseVersion()
+    {
+        return getVersion();
+    }
+}
