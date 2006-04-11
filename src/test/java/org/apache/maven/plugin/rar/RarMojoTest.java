36d17b671b81ac143d5c4cbff51107f6a909797d
Allan Q. Ramirez <aramirez@apache.org>
2006-04-11 02:51:09 +0000

    PR: MRAR-6
    added tests using plugin testing harness
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-rar-plugin@393141 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/rar/RarMojoTest.java
@@ -0,0 +1,121 @@
+package org.apache.maven.plugin.rar;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.apache.maven.plugin.rar.stubs.RarMavenProjectStub;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.codehaus.plexus.archiver.zip.ZipEntry;
+import org.codehaus.plexus.archiver.zip.ZipFile;
+
+/**
+ * @author <a href="mailto:aramirez@apache.org">Allan Ramirez</a>
+ */
+public class RarMojoTest 
+    extends AbstractMojoTestCase
+{
+    public void testRarTestEnvironment()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "target/test-classes/unit/basic-rar-test/plugin-config.xml" );
+
+        RarMojo mojo = ( RarMojo ) lookupMojo( "rar", testPom );
+        
+        assertNotNull( mojo );
+    }
+    
+    public void testBasicRar()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "target/test-classes/unit/basic-rar-test/plugin-config.xml" );
+        
+        RarMojo mojo = ( RarMojo ) lookupMojo( "rar", testPom );
+        
+        assertNotNull( mojo );
+
+        String finalName = ( String ) getVariableValueFromObject( mojo, "finalName" );
+        
+        String workDir = ( String ) getVariableValueFromObject( mojo, "workDirectory" );
+        
+        String outputDir = ( String ) getVariableValueFromObject( mojo, "outputDirectory" );
+        
+        mojo.execute();
+        
+        //check the working directory
+        File workDirectory = new File( workDir );
+        
+        assertTrue( workDirectory.exists() );
+        
+        assertTrue( workDirectory.isDirectory() );
+        
+        File[] fileNames = workDirectory.listFiles();
+        
+        assertEquals( 2, fileNames.length );
+        
+        assertEquals( "maven-artifact01-1.0-SNAPSHOT.jar", fileNames[0].getName() );
+        
+        assertEquals( "maven-artifact02-1.0-SNAPSHOT.jar", fileNames[1].getName() );
+        
+        //check the generated rar file
+        File rarFile = new File( outputDir, finalName + ".rar" );
+     
+        assertTrue( rarFile.exists() );
+
+        //expected files/directories inside the rar file
+        List expectedFiles = new ArrayList();
+        
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.properties" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.xml" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/" );
+        expectedFiles.add( "META-INF/maven/" );
+        expectedFiles.add( "META-INF/MANIFEST.MF" );
+        expectedFiles.add( "META-INF/" );
+        expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
+        
+        ZipFile rar = new ZipFile( rarFile );
+        
+        Enumeration entries = rar.getEntries();
+        
+        assertTrue( entries.hasMoreElements() );
+ 
+        while( entries.hasMoreElements() )
+        {
+            ZipEntry entry = ( ZipEntry ) entries.nextElement(); 
+            
+            if( expectedFiles.contains( entry.getName() ) )
+            {
+                expectedFiles.remove( entry.getName() );
+                assertFalse( expectedFiles.contains( entry.getName() ) );
+            }
+            else
+            {
+                fail( entry.getName() + " is not included in the expected files" );
+            }
+        }
+        assertEquals( 0, expectedFiles.size() );
+    }
+    
+    
+}
---
+++ src/test/java/org/apache/maven/plugin/rar/stubs/RarMavenProjectStub.java
@@ -0,0 +1,145 @@
+package org.apache.maven.plugin.rar.stubs;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Organization;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.PlexusTestCase;
+
+/**
+ * @author <a href="mailto:aramirez@apache.org">Allan Ramirez</a>
+ */
+public class RarMavenProjectStub
+    extends MavenProject
+{
+    private List attachedArtifacts;
+    
+    public RarMavenProjectStub()
+    {
+        super( new Model() );
+        
+        super.setGroupId( getGroupId() );
+        super.setArtifactId( getArtifactId() );
+        super.setVersion( getVersion() );
+        super.setDescription( "Test description" );
+
+        Organization org = new Organization();
+        org.setName( "organization" );
+        org.setUrl( "http://www.some.org" );
+        
+        super.setOrganization( org );
+        super.setFile( getFile() );
+        super.setPluginArtifacts( Collections.EMPTY_SET );
+        super.setReportArtifacts( Collections.EMPTY_SET );
+        super.setExtensionArtifacts( Collections.EMPTY_SET );
+        super.setArtifact( getArtifact() );
+        super.setRemoteArtifactRepositories( Collections.EMPTY_LIST );
+        super.setPluginArtifactRepositories( Collections.EMPTY_LIST );
+        super.setCollectedProjects( Collections.EMPTY_LIST );
+        super.setActiveProfiles( Collections.EMPTY_LIST );
+        
+        super.addCompileSourceRoot( getBasedir() + "/src/test/resources/unit/basic-rar-test/src/main/java" );
+        super.addTestCompileSourceRoot( getBasedir() + "/src/test/resources/unit/basic-rar-test/src/test/java" );
+        
+        super.setExecutionRoot( false );
+    }
+    
+    public String getGroupId()
+    {
+        return "org.apache.maven.test";
+    }
+    
+    public String getArtifactId()
+    {
+        return "maven-rar-test";
+    }
+    
+    public String getVersion()
+    {
+        return "1.0-SNAPSHOT";
+    }
+    
+    public File getFile()
+    {
+        return new File( getBasedir(), "src/test/resources/unit/basic-rar-test/plugin-config.xml" );
+    }
+    
+    public File getBasedir()
+    {
+        return new File( PlexusTestCase.getBasedir() );
+    }
+    
+    public Artifact getArtifact()
+    {
+        Artifact artifact = new RarArtifactStub();
+        
+        artifact.setGroupId( getGroupId() );
+        
+        artifact.setArtifactId( getArtifactId() );
+        
+        artifact.setVersion( getVersion() );
+        
+        return artifact;
+    }
+    
+    public Set getArtifacts()
+    {   
+        Set artifacts = new HashSet();
+ 
+        artifacts.add( createArtifact( "org.apache.maven.test", "maven-artifact01", "1.0-SNAPSHOT", false ) );
+        artifacts.add( createArtifact( "org.apache.maven.test", "maven-artifact02", "1.0-SNAPSHOT", false ) );
+        
+        return artifacts;
+    }
+    
+    public List getAttachedArtifacts()
+    {
+        if ( attachedArtifacts == null )
+        {
+            attachedArtifacts = new ArrayList();
+        }
+        return attachedArtifacts;
+    }    
+    
+    protected Artifact createArtifact( String groupId, String artifactId, String version, boolean optional )
+    {
+        Artifact artifact = new RarArtifactStub();
+        
+        artifact.setGroupId( groupId );
+        
+        artifact.setArtifactId( artifactId );
+        
+        artifact.setVersion( version );
+        
+        artifact.setOptional( optional );
+        
+        artifact.setFile( new File ( getBasedir() + "/src/test/remote-repo/" + artifact.getGroupId().replace( '.', '/' ) + 
+                          "/" + artifact.getArtifactId() + "/" + artifact.getVersion() + 
+                          "/" + artifact.getArtifactId() + "-" + artifact.getVersion() + ".jar" ) ) ;
+        
+        return artifact;
+    }
+}
