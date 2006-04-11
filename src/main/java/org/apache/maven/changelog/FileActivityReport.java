b852a5dd5325969a6a9aef5f9c675ba8b0d68064
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-11 10:47:40 +0000

    PR: MCHANGELOG-12
    Fixed signatures so that the other mojos overrides the extended mojo
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/plugins/maven-changelog-plugin@393178 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/changelog/FileActivityReport.java
+++ src/main/java/org/apache/maven/changelog/FileActivityReport.java
@@ -101,7 +101,7 @@ public class FileActivityReport
      * @param bundle        the resource bundle to retrieve report phrases from
      * @param sink          the report formatting tool
      */
-    protected void doGenerateReport( ChangeLogSet changeLogSets, ResourceBundle bundle, Sink sink )
+    protected void doGenerateReport( List changeLogSets, ResourceBundle bundle, Sink sink )
     {
         sink.head();
         sink.title();
@@ -115,7 +115,11 @@ public class FileActivityReport
         sink.text( bundle.getString( "report.file-activity.mainTitle" ) );
         sink.sectionTitle1_();
 
-        doChangedSets( changeLogSets, bundle, sink );
+        for ( Iterator sets = changeLogSets.iterator(); sets.hasNext(); )
+        {
+            ChangeLogSet set = (ChangeLogSet) sets.next();
+            doChangedSets( set, bundle, sink );
+        }
 
         sink.section1_();
         sink.body_();
