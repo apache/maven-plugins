2beef8f67fd1e193a74f13d48b3d5fad1f3b1c11
Allan Q. Ramirez <aramirez@apache.org>
2006-04-12 05:59:56 +0000

    PR: MRAR-6
    added new tests
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-rar-plugin@393394 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/rar/RarMojoTest.java
+++ src/test/java/org/apache/maven/plugin/rar/RarMojoTest.java
@@ -20,16 +20,17 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Iterator;
 
-import org.apache.maven.plugin.rar.stubs.RarMavenProjectStub;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.codehaus.plexus.archiver.zip.ZipEntry;
 import org.codehaus.plexus.archiver.zip.ZipFile;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * @author <a href="mailto:aramirez@apache.org">Allan Ramirez</a>
  */
-public class RarMojoTest 
+public class RarMojoTest
     extends AbstractMojoTestCase
 {
     public void testRarTestEnvironment()
@@ -39,51 +40,62 @@ public class RarMojoTest
                                  "target/test-classes/unit/basic-rar-test/plugin-config.xml" );
 
         RarMojo mojo = ( RarMojo ) lookupMojo( "rar", testPom );
-        
+
         assertNotNull( mojo );
     }
-    
+
     public void testBasicRar()
         throws Exception
     {
         File testPom = new File( getBasedir(),
                                  "target/test-classes/unit/basic-rar-test/plugin-config.xml" );
-        
+
         RarMojo mojo = ( RarMojo ) lookupMojo( "rar", testPom );
-        
+
         assertNotNull( mojo );
 
         String finalName = ( String ) getVariableValueFromObject( mojo, "finalName" );
-        
+
         String workDir = ( String ) getVariableValueFromObject( mojo, "workDirectory" );
-        
+
         String outputDir = ( String ) getVariableValueFromObject( mojo, "outputDirectory" );
-        
+
+        Boolean includeJar = ( Boolean ) getVariableValueFromObject(  mojo, "includeJar" );
+
+        assertTrue( includeJar.booleanValue() );
+
+        //include the project jar to the rar
+        File projectJar = new File( getBasedir(), "src/test/resources/unit/basic-rar-test/target/test-rar.jar" );
+
+        FileUtils.copyFileToDirectory( projectJar, new File( outputDir ) );
+
         mojo.execute();
-        
+
         //check the working directory
         File workDirectory = new File( workDir );
-        
+
         assertTrue( workDirectory.exists() );
-        
+
         assertTrue( workDirectory.isDirectory() );
-        
+
         File[] fileNames = workDirectory.listFiles();
-        
+
         assertEquals( 3, fileNames.length );
-        
+
         assertEquals( "maven-artifact01-1.0-SNAPSHOT.jar", fileNames[0].getName() );
-        
+
         assertEquals( "maven-artifact02-1.0-SNAPSHOT.jar", fileNames[1].getName() );
-        
+
+        assertEquals( "test-rar.jar", fileNames[2].getName() );
+
         //check the generated rar file
         File rarFile = new File( outputDir, finalName + ".rar" );
-     
+
         assertTrue( rarFile.exists() );
 
         //expected files/directories inside the rar file
         List expectedFiles = new ArrayList();
-        
+
         expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.properties" );
         expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.xml" );
         expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/" );
@@ -93,17 +105,168 @@ public class RarMojoTest
         expectedFiles.add( "META-INF/" );
         expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
         expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
-        
+        expectedFiles.add( "test-rar.jar" );
+
         ZipFile rar = new ZipFile( rarFile );
-        
+
         Enumeration entries = rar.getEntries();
-        
+
         assertTrue( entries.hasMoreElements() );
- 
+
+        assertEquals( 0, getSizeOfExpectedFiles( entries, expectedFiles ) );
+    }
+
+    public void testBasicRarWithDescriptor()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "target/test-classes/unit/basic-rar-with-descriptor/plugin-config.xml" );
+
+        RarMojo mojo = ( RarMojo ) lookupMojo( "rar", testPom );
+
+        assertNotNull( mojo );
+
+        String finalName = ( String ) getVariableValueFromObject( mojo, "finalName" );
+
+        String workDir = ( String ) getVariableValueFromObject( mojo, "workDirectory" );
+
+        String outputDir = ( String ) getVariableValueFromObject( mojo, "outputDirectory" );
+
+        mojo.execute();
+
+        //check the working directory
+        File workDirectory = new File( workDir );
+
+        assertTrue( workDirectory.exists() );
+
+        assertTrue( workDirectory.isDirectory() );
+
+        File[] fileNames = workDirectory.listFiles();
+
+        List expectedFiles = new ArrayList();
+        List fileList = new ArrayList();
+
+        for( int i=0; i<fileNames.length; i++ )
+        {
+            addFileToList( fileNames[i], fileList );
+        }
+
+        expectedFiles.add( "ra.xml" );
+        expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "META-INF" );
+
+        assertEquals( expectedFiles.size(), fileList.size() );
+
+        assertEquals( 0, getSizeOfExpectedFiles( fileList, expectedFiles ) );
+
+        //check the generated rar file
+        File rarFile = new File( outputDir, finalName + ".rar" );
+
+        assertTrue( rarFile.exists() );
+
+        //expected files/directories inside the rar file
+        expectedFiles = new ArrayList();
+
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.properties" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.xml" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/" );
+        expectedFiles.add( "META-INF/maven/" );
+        expectedFiles.add( "META-INF/MANIFEST.MF" );
+        expectedFiles.add( "META-INF/ra.xml" );
+        expectedFiles.add( "META-INF/" );
+        expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
+
+        ZipFile rar = new ZipFile( rarFile );
+
+        Enumeration entries = rar.getEntries();
+
+        assertTrue( entries.hasMoreElements() );
+
+        assertEquals( 0, getSizeOfExpectedFiles( entries, expectedFiles ) );
+    }
+
+    public void testBasicRarWithManifest()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "target/test-classes/unit/basic-rar-with-manifest/plugin-config.xml" );
+
+        RarMojo mojo = ( RarMojo ) lookupMojo( "rar", testPom );
+
+        assertNotNull( mojo );
+
+        String finalName = ( String ) getVariableValueFromObject( mojo, "finalName" );
+
+        String workDir = ( String ) getVariableValueFromObject( mojo, "workDirectory" );
+
+        String outputDir = ( String ) getVariableValueFromObject( mojo, "outputDirectory" );
+
+        mojo.execute();
+
+        //check the working directory
+        File workDirectory = new File( workDir );
+
+        assertTrue( workDirectory.exists() );
+
+        assertTrue( workDirectory.isDirectory() );
+
+        File[] fileNames = workDirectory.listFiles();
+
+        List expectedFiles = new ArrayList();
+        List fileList = new ArrayList();
+
+        for( int i=0; i<fileNames.length; i++ )
+        {
+            addFileToList( fileNames[i], fileList );
+        }
+
+        expectedFiles.add( "ra.xml" );
+        expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "META-INF" );
+        expectedFiles.add( "MANIFEST.MF" );
+
+        assertEquals( expectedFiles.size(), fileList.size() );
+
+        assertEquals( 0, getSizeOfExpectedFiles( fileList, expectedFiles ) );
+
+        //check the generated rar file
+        File rarFile = new File( outputDir, finalName + ".rar" );
+
+        assertTrue( rarFile.exists() );
+
+        //expected files/directories inside the rar file
+        expectedFiles = new ArrayList();
+
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.properties" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/pom.xml" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/maven-rar-test/" );
+        expectedFiles.add( "META-INF/maven/org.apache.maven.test/" );
+        expectedFiles.add( "META-INF/maven/" );
+        expectedFiles.add( "META-INF/MANIFEST.MF" );
+        expectedFiles.add( "META-INF/ra.xml" );
+        expectedFiles.add( "META-INF/" );
+        expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
+
+        ZipFile rar = new ZipFile( rarFile );
+
+        Enumeration entries = rar.getEntries();
+
+        assertTrue( entries.hasMoreElements() );
+
+        assertEquals( 0, getSizeOfExpectedFiles( entries, expectedFiles ) );
+    }
+
+    private int getSizeOfExpectedFiles( Enumeration entries, List expectedFiles )
+    {
         while( entries.hasMoreElements() )
         {
-            ZipEntry entry = ( ZipEntry ) entries.nextElement(); 
-            
+            ZipEntry entry = ( ZipEntry ) entries.nextElement();
+
             if( expectedFiles.contains( entry.getName() ) )
             {
                 expectedFiles.remove( entry.getName() );
@@ -114,8 +277,44 @@ public class RarMojoTest
                 fail( entry.getName() + " is not included in the expected files" );
             }
         }
-        assertEquals( 0, expectedFiles.size() );
+        return expectedFiles.size();
     }
-    
-    
+
+    private int getSizeOfExpectedFiles( List fileList, List expectedFiles )
+    {
+        for( Iterator iter=fileList.iterator(); iter.hasNext(); )
+        {
+            String fileName = ( String ) iter.next();
+
+            if( expectedFiles.contains(  fileName ) )
+            {
+                expectedFiles.remove( fileName );
+                assertFalse( expectedFiles.contains( fileName ) );
+            }
+            else
+            {
+                fail( fileName + " is not included in the expected files" );
+            }
+        }
+        return expectedFiles.size();
+    }
+
+    private void addFileToList( File file, List fileList )
+    {
+        if( !file.isDirectory() )
+        {
+            fileList.add( file.getName() );
+        }
+        else
+        {
+            fileList.add( file.getName() );
+
+            File[] files = file.listFiles();
+
+            for( int i=0; i<files.length; i++ )
+            {
+                addFileToList( files[i], fileList );
+            }
+        }
+    }
 }
