081b77d69eb904fef61ddcf215dbd159fbdaf0e8
Brett Porter <brett@apache.org>
2006-04-13 00:25:45 +0000

    clearer explanation of failure
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-pmd-plugin@393669 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/pmd/PmdViolationCheckMojo.java
+++ src/main/java/org/apache/maven/plugin/pmd/PmdViolationCheckMojo.java
@@ -32,6 +32,6 @@ public class PmdViolationCheckMojo
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-        executeCheck( "pmd.xml", "violation" );
+        executeCheck( "pmd.xml", "violation", "PMD violation" );
     }
 }
