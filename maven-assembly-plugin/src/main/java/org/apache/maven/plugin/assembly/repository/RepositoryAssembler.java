553164eda7d4670e01c4374eaeaca780770d879e
Jason van Zyl <jvanzyl@apache.org>
2006-04-14 16:03:01 +0000

    [MASSEMBLY-85] place the correct metadata in with the artifact
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@394129 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/repository/RepositoryAssembler.java
+++ maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/repository/RepositoryAssembler.java
@@ -1,5 +1,8 @@
 package org.apache.maven.plugin.assembly.repository;
 
+import org.apache.maven.plugins.assembly.model.Repository;
+import org.apache.maven.project.MavenProject;
+
 import java.io.File;
 import java.util.List;
 
@@ -10,10 +13,6 @@ public interface RepositoryAssembler
 {
     String ROLE = RepositoryAssembler.class.getName();
 
-    public void assemble( File outputDirectory,
-                          List artifacts,
-                          List remoteRepositories,
-                          List versionAlignments,
-                          boolean includeMetadata )
+    public void assemble( File repositoryDirectory, Repository repository, MavenProject project )                          
         throws RepositoryAssemblyException;
 }
