297390e13e495008ffe885ee71493cb5cbdda881
Jason van Zyl <jvanzyl@apache.org>
2006-04-14 16:03:01 +0000

    [MASSEMBLY-85] place the correct metadata in with the artifact
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@394129 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/assembly/repository/DefaultRepositoryAssembler.java
+++ src/main/java/org/apache/maven/plugin/assembly/repository/DefaultRepositoryAssembler.java
@@ -2,36 +2,47 @@ package org.apache.maven.plugin.assembly.repository;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.metadata.ArtifactMetadataRetrievalException;
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
 import org.apache.maven.artifact.repository.DefaultArtifactRepository;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.artifact.repository.metadata.Metadata;
 import org.apache.maven.artifact.repository.metadata.Versioning;
 import org.apache.maven.artifact.repository.metadata.io.xpp3.MetadataXpp3Writer;
-import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
 import org.apache.maven.artifact.resolver.ArtifactResolutionException;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.plugins.assembly.model.Repository;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.project.DefaultMavenProjectBuilder;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.TimeZone;
-import java.util.Date;
 import java.util.Map;
-import java.util.HashMap;
-import java.text.SimpleDateFormat;
-import java.text.DateFormat;
+import java.util.TimeZone;
+import java.lang.reflect.Field;
 
 /**
  * @author Jason van Zyl
  */
+
+// todo will need to pop the processed project cache using reflection
 public class DefaultRepositoryAssembler
     extends AbstractLogEnabled
     implements RepositoryAssembler
@@ -48,42 +59,70 @@ public class DefaultRepositoryAssembler
 
     protected ArtifactRepositoryFactory artifactRepositoryFactory;
 
-    public void assemble( File outputDirectory,
-                          List artifacts,
-                          List remoteRepositories,
-                          List versionAlignments,
-                          boolean includeMetadata )
+    protected ArtifactMetadataSource metadataSource;
+
+    protected MavenProjectBuilder projectBuilder;
+
+    private Map versionAlignmentMap;
+
+    public void assemble( File repositoryDirectory,
+                          Repository repository,
+                          MavenProject project )
         throws RepositoryAssemblyException
     {
-        Map versionAlignmentMap = createVersionAlignmentMap( versionAlignments );
+        versionAlignmentMap = createVersionAlignmentMap( repository.getVersionAlignments() );
 
-        ArtifactRepository localRepository = createLocalRepository( outputDirectory );
+        ArtifactRepository localRepository = createLocalRepository( repositoryDirectory );
 
-        for ( Iterator i = artifacts.iterator(); i.hasNext(); )
+        try
         {
-            Artifact ma = (Artifact) i.next();
+            // i have to get everything first as a filter or transformation here doesn't seem to work
+            // to align everything. If I use a filter to change the version on the fly then I get the
+            // I get JARs but no POMs, and in some directories POMs with no JARs.
 
-            String alignmentVersion = (String) versionAlignmentMap.get( ma.getGroupId() );
+            ArtifactResolutionResult result = artifactResolver.resolveTransitively( project.getDependencyArtifacts(),
+                                                                                    project.getArtifact(),
+                                                                                    project.getRemoteArtifactRepositories(),
+                                                                                    localRepository,
+                                                                                    metadataSource );
 
-            if ( alignmentVersion != null )
+            // Now that we have the graph, let's try to align it to versions that we want and remove
+            // the repository we previously populated.
+            FileUtils.deleteDirectory( repositoryDirectory );
+
+            FileUtils.mkdir( repositoryDirectory.getAbsolutePath() );
+
+            // Blow the cache in the project builder so that we get POMs again on this next download
+            invalidateProccessedProjectCache();
+
+            for ( Iterator i = result.getArtifacts().iterator(); i.hasNext(); )
             {
-                ma.setVersion( alignmentVersion );
+                Artifact a = (Artifact) i.next();
+
+                String alignedVersion = (String) versionAlignmentMap.get( a.getGroupId() );
+
+                if ( alignedVersion != null )
+                {
+                    a.setVersion( alignedVersion );
+                }
+
+                // We need to flip it back to not being resolved so we can look for it again!
+                a.setResolved( false );
+
+                artifactResolver.resolveAlways( a, project.getRemoteArtifactRepositories(), localRepository );
+
+                // The correct metadata does not get pulled down unless this is used. Not
+                // sure what the problem is.
+                metadataSource.retrieve( a, localRepository, project.getRemoteArtifactRepositories() );
             }
 
-            Artifact a = artifactFactory.createBuildArtifact( ma.getGroupId(), ma.getArtifactId(), ma.getVersion(),
-                                                              ma.getType() );
-
-            Artifact pa = artifactFactory.createProjectArtifact( ma.getGroupId(), ma.getArtifactId(), ma.getVersion() );
-
-            try
+            if ( repository.isIncludeMetadata() )
             {
-                artifactResolver.resolveAlways( a, remoteRepositories, localRepository );
-
-                artifactResolver.resolveAlways( pa, remoteRepositories, localRepository );
-
-                if ( includeMetadata )
+                for ( Iterator i = result.getArtifacts().iterator(); i.hasNext(); )
                 {
-                    File metadataFile = new File( a.getFile().getParentFile().getParent(), "maven-metadata.xml" );
+                    Artifact a = (Artifact) i.next();
+
+                    File metadataFile = new File( a.getFile().getParentFile().getParent(), "maven-metadata-central.xml" );
 
                     Metadata m = new Metadata();
 
@@ -105,21 +144,37 @@ public class DefaultRepositoryAssembler
 
                     MetadataXpp3Writer metadataWriter = new MetadataXpp3Writer();
 
-                    metadataWriter.write( new FileWriter( metadataFile ), m );
+                    Writer writer = new FileWriter( metadataFile );
+
+                    metadataWriter.write( writer, m );
+
+                    writer.close();
+
+                    File metadataFileRemote = new File( a.getFile().getParentFile().getParent(), "maven-metadata.xml" );
+
+                    FileUtils.copyFile( metadataFile, metadataFileRemote );
                 }
             }
-            catch ( ArtifactResolutionException e )
-            {
-                throw new RepositoryAssemblyException( "Error resolving artifact: " + a, e );
-            }
-            catch ( ArtifactNotFoundException e )
-            {
-                throw new RepositoryAssemblyException( "Cannot find artifact: " + a, e );
-            }
-            catch ( IOException e )
-            {
-                throw new RepositoryAssemblyException( "Error writing artifact metdata.", e );
-            }
+        }
+        catch ( ArtifactResolutionException e )
+        {
+            throw new RepositoryAssemblyException( "Error resolving artifacts.", e );
+        }
+        catch ( ArtifactMetadataRetrievalException e )
+        {
+            throw new RepositoryAssemblyException( "Error resolving artifacts.", e );
+        }
+        catch ( ArtifactNotFoundException e )
+        {
+            throw new RepositoryAssemblyException( "Error resolving artifacts.", e );
+        }
+        catch ( IOException e )
+        {
+            throw new RepositoryAssemblyException( "Error writing artifact metdata.", e );
+        }
+        catch ( Exception e )
+        {
+            throw new RepositoryAssemblyException( "Error invalidating the processed project cache.", e );
         }
     }
 
@@ -185,5 +240,21 @@ public class DefaultRepositoryAssembler
         artifactRepositoryFactory.setGlobalChecksumPolicy( globalChecksumPolicy );
 
         return localRepository;
+    }
+
+    private void invalidateProccessedProjectCache()
+        throws Exception
+    {
+        Class klass = DefaultMavenProjectBuilder.class;
+
+        Field field = klass.getDeclaredField( "processedProjectCache" );
+
+        field.setAccessible( true );
+
+        Object cache = field.get( projectBuilder );
+
+        cache.getClass().getDeclaredMethod( "clear", null ).invoke( cache, null );
+
+        field.setAccessible( false );
     }
 }
