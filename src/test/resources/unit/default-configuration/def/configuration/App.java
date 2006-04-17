12c35e0dc436703dcdc2d954535888799e48b4c4
Maria Odea B. Ching <oching@apache.org>
2006-04-17 08:57:36 +0000

    PR: MREPOSITORY-1
    Created test using testing harness plugin.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-repository-plugin@394644 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/default-configuration/def/configuration/App.java
@@ -0,0 +1,9 @@
+package def.configuration;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sample Application." );
+    }
+}
---
+++ src/test/resources/unit/no-sourcesjar/no/sourcesjar/App.java
@@ -0,0 +1,9 @@
+package no.sourcesjar;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sample Application." );
+    }
+}
