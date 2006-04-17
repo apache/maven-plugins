0fc29416df2147d422a5b6ff63295225cbb6208f
Maria Odea B. Ching <oching@apache.org>
2006-04-17 08:57:36 +0000

    PR: MREPOSITORY-1
    Created test using testing harness plugin.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@394644 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-repository-plugin/src/test/java/org/apache/maven/plugins/repository/stubs/NoLicenseFileMavenProjectStub.java
@@ -0,0 +1,73 @@
+package org.apache.maven.plugins.repository.stubs;
+
+/*
+ * Copyright 2005-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Scm;
+import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
+
+/**
+ * @author <a href="mailto:oching@exist.com">Maria Odea Ching</a>
+ */
+public class NoLicenseFileMavenProjectStub
+    extends MavenProjectStub
+{
+    private Scm scm;
+
+    private Build build;
+
+    public NoLicenseFileMavenProjectStub()
+    {
+        setGroupId( "no.license.file" );
+        setArtifactId( "no-license-file" );
+        setVersion( "1.0-SNAPSHOT" );
+        setName( "No License File Project" );
+        setUrl( "http://maven.apache.org" );
+        setPackaging( "jar" );
+        setDescription( "Sample Maven Project that has no license file." );
+
+        Scm scm = new Scm();
+        scm.setConnection( "scm:svn:http://svn.apache.org/maven/sample/trunk" );
+        setScm( scm );
+
+        Build build = new Build();
+        build.setFinalName( "no-license-file" );
+        build.setDirectory( getBasedir() + "/target/test/unit/no-license-file/target" );
+        setBuild( build );
+
+    }
+
+    public Scm getScm()
+    {
+        return scm;
+    }
+
+    public void setScm( Scm scm )
+    {
+        this.scm = scm;
+    }
+
+    public Build getBuild()
+    {
+        return build;
+    }
+
+    public void setBuild( Build build )
+    {
+        this.build = build;
+    }
+}
