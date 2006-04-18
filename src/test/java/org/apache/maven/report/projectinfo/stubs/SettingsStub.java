d4eef027159c85ba6d9cb414821e6b1c91b13e97
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-18 06:57:15 +0000

    PR: MPIR-43
    applied licensing headers
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-project-info-reports-plugin@394864 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/report/projectinfo/stubs/SettingsStub.java
+++ src/test/java/org/apache/maven/report/projectinfo/stubs/SettingsStub.java
@@ -1,7 +1,5 @@
 package org.apache.maven.report.projectinfo.stubs;
 
-import org.apache.maven.settings.Settings;
-
 /*
  * Copyright 2004-2005 The Apache Software Foundation.
  *
@@ -9,7 +7,7 @@ import org.apache.maven.settings.Settings;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-20
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +15,8 @@ import org.apache.maven.settings.Settings;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import org.apache.maven.settings.Settings;
 
 /**
  * @author Edwin Punzalan
