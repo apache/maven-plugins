8ed18ffd222bd81101aaffa3c4f5be6b083886db
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-18 06:51:44 +0000

    PR: MPIR-43
    Added default plugin test harness classes and stubs.  Tests are currently excluded due to project-info-reports not generating any files when issued on the CLI except when doing "mvn site"
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-project-info-reports-plugin@394862 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/report/projectinfo/stubs/ProjectInfoProjectStub.java
@@ -0,0 +1,48 @@
+package org.apache.maven.report.projectinfo.stubs;
+
+/*
+ * Copyright 2004-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-20
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.CiManagement;
+import org.apache.maven.model.Notifier;
+
+import java.util.Collections;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class ProjectInfoProjectStub
+    extends MavenProjectStub
+{
+    public Model getModel()
+    {
+        Model model = new Model();
+
+        CiManagement ciMngt = new CiManagement();
+        ciMngt.setSystem( "continuum" );
+        ciMngt.setUrl( "http://continumm" );
+        Notifier notifier = new Notifier();
+        notifier.setType( "notifier-type" );
+        notifier.setAddress( "notifier@email.com");
+        notifier.setConfiguration( System.getProperties() );
+        ciMngt.setNotifiers( Collections.singletonList( notifier ) );
+        model.setCiManagement( ciMngt );
+
+        return model;
+    }
+}
