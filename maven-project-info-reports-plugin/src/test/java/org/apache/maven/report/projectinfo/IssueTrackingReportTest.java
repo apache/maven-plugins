a007a2c5d1c4c95f0960f0650eb8c1b0c401bea8
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-18 06:51:44 +0000

    PR: MPIR-43
    Added default plugin test harness classes and stubs.  Tests are currently excluded due to project-info-reports not generating any files when issued on the CLI except when doing "mvn site"
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@394862 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project-info-reports-plugin/src/test/java/org/apache/maven/report/projectinfo/IssueTrackingReportTest.java
+++ maven-project-info-reports-plugin/src/test/java/org/apache/maven/report/projectinfo/IssueTrackingReportTest.java
@@ -19,12 +19,12 @@ package org.apache.maven.report.projectinfo;
 /**
  * @author Edwin Punzalan
  */
-public class DependenciesReportTest
+public class IssueTrackingReportTest
     extends AbstractProjectInfoTestCase
 {
     public void testReport()
         throws Exception
     {
-        generateReport( "dependencies", "dependencies-plugin-config.xml" );
+        generateReport( "issue-tracking", "issue-tracking-plugin-config.xml" );
     }
 }
--- maven-project-info-reports-plugin/src/test/java/org/apache/maven/report/projectinfo/TeamListReportTest.java
+++ maven-project-info-reports-plugin/src/test/java/org/apache/maven/report/projectinfo/TeamListReportTest.java
@@ -19,12 +19,12 @@ package org.apache.maven.report.projectinfo;
 /**
  * @author Edwin Punzalan
  */
-public class DependenciesReportTest
+public class TeamListReportTest
     extends AbstractProjectInfoTestCase
 {
     public void testReport()
         throws Exception
     {
-        generateReport( "dependencies", "dependencies-plugin-config.xml" );
+        generateReport( "project-team", "project-team-plugin-config.xml" );
     }
 }
