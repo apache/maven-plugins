6dc1ea08c25cb010b3c5c27fa5f4035691c31ba7
Edwin L. Punzalan <epunzalan@apache.org =  epunzalan = Edwin L. Punzalan epunzalan@apache.org@apache.org>
2006-04-19 08:18:32 +0000

    PR: MCHECKSTYLE-39
    Added test-harness tests with min configs
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@395167 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/checkstyle/stubs/MinMavenProjectStub.java
@@ -0,0 +1,72 @@
+package org.apache.maven.plugin.checkstyle.stubs;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Organization;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.codehaus.plexus.PlexusTestCase;
+
+import java.util.List;
+import java.util.Collections;
+import java.io.File;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class MinMavenProjectStub
+    extends org.apache.maven.plugin.testing.stubs.MavenProjectStub
+{
+    public List getCompileClasspathElements()
+        throws DependencyResolutionRequiredException
+    {
+        return Collections.EMPTY_LIST;
+    }
+
+    public File getBasedir()
+    {
+        return new File( PlexusTestCase.getBasedir() );
+    }
+
+    public List getReportPlugins()
+    {
+        return Collections.EMPTY_LIST;
+    }
+
+    public Organization getOrganization()
+    {
+        Organization organization = new Organization();
+
+        organization.setName( "maven-plugin-tests" );
+
+        return organization;
+    }
+
+    public String getInceptionYear()
+    {
+        return "2006";
+    }
+
+    public Build getBuild()
+    {
+        Build build = new Build();
+
+        build.setDirectory( PlexusTestCase.getBasedir() + "/target/test-harness/checkstyle/min" );
+
+        return build;
+    }
+}
