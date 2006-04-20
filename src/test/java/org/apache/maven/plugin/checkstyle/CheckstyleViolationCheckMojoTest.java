4d3689f22f048b90928d56fdfaad2c63f1cade23
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-20 08:35:28 +0000

    PR: MCHECKSTYLE-39
    Added test-harness tests for the checkstyle:check mojo
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@395532 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/checkstyle/CheckstyleViolationCheckMojoTest.java
@@ -0,0 +1,103 @@
+package org.apache.maven.plugin.checkstyle;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+import java.io.File;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class CheckstyleViolationCheckMojoTest
+    extends AbstractMojoTestCase
+{
+    public void testDefaultConfig()
+        throws Exception
+    {
+        File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/check-plugin-config.xml" );
+
+        Mojo mojo = lookupMojo( "check", pluginXmlFile );
+
+        assertNotNull( "Mojo found.", mojo );
+
+        try
+        {
+            mojo.execute();
+
+            fail( "Must throw an exception on violations" );
+        }
+        catch ( MojoFailureException e )
+        {
+            //expected
+        }
+    }
+
+    public void testInvalidFormat()
+        throws Exception
+    {
+        File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/check-plugin-config.xml" );
+
+        Mojo mojo = lookupMojo( "check", pluginXmlFile );
+
+        assertNotNull( "Mojo found.", mojo );
+
+        setVariableValueToObject( mojo, "outputFileFormat", "plain" );
+
+        try
+        {
+            mojo.execute();
+
+            fail( "Must throw an exception invalid format: plain" );
+        }
+        catch ( MojoExecutionException e )
+        {
+            //expected
+        }
+    }
+
+    public void testNoOutputFile()
+        throws Exception
+    {
+        File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/check-plugin-config.xml" );
+
+        Mojo mojo = lookupMojo( "check", pluginXmlFile );
+
+        assertNotNull( "Mojo found.", mojo );
+
+        setVariableValueToObject( mojo, "outputFile", new File( "target/NoSuchFile.xml" ) );
+
+        mojo.execute();
+    }
+
+    public void testNoFail()
+        throws Exception
+    {
+        File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/check-plugin-config.xml" );
+
+        Mojo mojo = lookupMojo( "check", pluginXmlFile );
+
+        assertNotNull( "Mojo found.", mojo );
+
+        setVariableValueToObject( mojo, "failOnViolation", Boolean.FALSE );
+
+        mojo.execute();
+    }
+}
