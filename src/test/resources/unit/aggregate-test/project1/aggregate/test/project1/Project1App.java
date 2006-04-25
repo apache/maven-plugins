eaa5a4c6cd63fb02bd56e3797d551f130360dd0f
Maria Odea B. Ching <oching@apache.org>
2006-04-25 06:38:29 +0000

    PR: MJAVADOC-63
    Created test for JavadocReportMojo and JavadocJarMojo using the test harness plugin.
    Also created unit tests for the modello generated sources Group and Tag classes.
    Cleaned up/removed the existing test projects that were used in the old unit testing.
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@396786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/aggregate-test/project1/aggregate/test/project1/Project1App.java
@@ -0,0 +1,30 @@
+package aggregate.test.project1;
+
+/**
+ * Sample class that is included in the javadoc
+ *
+ * @author Maria Odea Ching
+ */
+public class Project1App
+{
+    /**
+     * The main method
+     *
+     * @param args  an array of strings that contains the arguments
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Sample Application." );
+    }
+
+    /**
+     * Sample method that prints out the parameter string.
+     *
+     * @param str   The string value to be printed.
+     */
+    protected void sampleMethod( String str )
+    {
+        System.out.println( str );
+    }
+
+}
