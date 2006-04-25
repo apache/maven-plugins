b56f513f170c524461879030c9f78137ff041482
Maria Odea B. Ching <oching@apache.org>
2006-04-25 06:38:29 +0000

    PR: MJAVADOC-63
    Created test for JavadocReportMojo and JavadocJarMojo using the test harness plugin.
    Also created unit tests for the modello generated sources Group and Tag classes.
    Cleaned up/removed the existing test projects that were used in the old unit testing.
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@396786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/javadocjar-invalid-destdir/javadocjar/invalid/destdir/AppSample.java
@@ -0,0 +1,26 @@
+package javadocjar.invalid.destdir;
+
+/**
+ * This is a sample class with javadoc
+ *
+ * @author Maria Odea Ching
+ */
+public class AppSample
+{
+
+    /**
+     * Variable that holds the test value
+     */
+    private String str = "TEST";
+
+    /**
+     * The main method
+     *
+     * @param args  an array of strings that contains the arguments
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Another Sample Application" );
+    }  
+
+}
