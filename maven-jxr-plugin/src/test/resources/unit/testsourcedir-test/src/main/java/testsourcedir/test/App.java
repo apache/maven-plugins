c442ddea430caa0c84921e5acbf73a207cede3c3
Maria Odea B. Ching <oching@apache.org>
2006-04-27 02:56:41 +0000

    PR: MJXR-10
    Created test for jxr mojos using the test harness. Created project stub classes and sample projects used for testing.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@397371 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-jxr-plugin/src/test/resources/unit/testsourcedir-test/src/main/java/testsourcedir/test/App.java
@@ -0,0 +1,30 @@
+package testsourcedir.test;
+
+/**
+ * This is a sample class used for testing
+ *
+ * @author Maria Odea Ching
+ */
+public class App
+{
+    /**
+     * The main method
+     *
+     * @param args  an array of strings that contains the arguments
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Sample Application." );
+    }
+
+    /**
+     * Sample method
+     *
+     * @param str   the value to be displayed
+     */
+    protected void sampleMethod( String str )
+    {
+        System.out.println( str );
+    }
+
+}
---
+++ maven-jxr-plugin/src/test/resources/unit/testsourcedir-test/src/test/java/testsourcedir/test/AppTest.java
@@ -0,0 +1,44 @@
+package testsourcedir.test;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+/**
+ * This is a sample test class used for testing
+ *
+ * @author Maria Odea Ching
+ */
+public class AppTest
+    extends PlexusTestCase
+{
+    /**
+     *
+     * @throws Exception
+     */
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+    }
+
+    /**
+     * Method to test App
+     *
+     * @throws Exception
+     */
+    public void testApp()
+        throws Exception
+    {
+        System.out.println( "TEST" );
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    protected void tearDown()
+        throws Exception
+    {
+
+    }
+
+}
