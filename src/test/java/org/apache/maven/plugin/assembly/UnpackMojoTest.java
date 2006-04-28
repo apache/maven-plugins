cd0136d6c94fe92783858ac910401009f2194f1a
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-28 08:32:26 +0000

    PR: MASSEMBLY-88
    Finished test harness tests for unpack goal
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@397801 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/assembly/UnpackMojoTest.java
+++ src/test/java/org/apache/maven/plugin/assembly/UnpackMojoTest.java
@@ -18,9 +18,13 @@ package org.apache.maven.plugin.assembly;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.artifact.Artifact;
 import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
 
 import java.io.File;
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * @author Edwin Punzalan
@@ -31,8 +35,45 @@ public class UnpackMojoTest
     public void testMinConfiguration()
         throws Exception
     {
-        String pluginXml = "min-plugin-config.xml";
+        executeMojo( "min-plugin-config.xml" );
+    }
 
+    public void testMinConfigurationWithReactorProjects()
+        throws Exception
+    {
+        executeMojo( "with-reactor-projects-plugin-config.xml" );
+    }
+
+    public void testArchiverManagerException()
+        throws Exception
+    {
+        try
+        {
+            String pluginXml = "archiver-manager-exception-plugin-config.xml";
+
+            UnpackMojo mojo = (UnpackMojo) lookupMojo( "unpack", PlexusTestCase.getBasedir() +
+                                                       "/src/test/plugin-configs/unpack/" + pluginXml );
+
+            mojo.execute();
+
+            MavenProject project = (MavenProject) getVariableValueFromObject( mojo, "project" );
+            String filename = project.getArtifact().getFile().getName();
+            String dir = filename.substring( 0, filename.lastIndexOf( '.' ) );
+            File workDir = (File) getVariableValueFromObject( mojo, "workDirectory" );
+            File unpackDir = new File( workDir, dir );
+            File unpacked = new File( unpackDir, filename + ".extracted" );
+
+            assertFalse( "Test extracted project artifact", unpacked.exists() );
+        }
+        catch ( NoSuchArchiverException e )
+        {
+            fail( "Expected exception in ArchiverManager should not fail the build" );
+        }
+    }
+
+    public void executeMojo( String pluginXml )
+        throws Exception
+    {
         UnpackMojo mojo = (UnpackMojo) lookupMojo( "unpack", PlexusTestCase.getBasedir() +
                                                    "/src/test/plugin-configs/unpack/" + pluginXml );
 
@@ -46,5 +87,30 @@ public class UnpackMojoTest
         File unpacked = new File( unpackDir, filename + ".extracted" );
 
         assertTrue( "Test extracted project artifact", unpacked.exists() );
+
+        List reactorProjectList = (List) getVariableValueFromObject( mojo, "reactorProjects" );
+        for ( Iterator reactorProjects = reactorProjectList.iterator(); reactorProjects.hasNext(); )
+        {
+            MavenProject reactorProject = (MavenProject) reactorProjects.next();
+
+            filename = reactorProject.getArtifact().getFile().getName();
+            dir = filename.substring( 0, filename.lastIndexOf( '.' ) );
+            unpackDir = new File( workDir, dir );
+            unpacked = new File( unpackDir, filename + ".extracted" );
+
+            assertTrue( "Test reactor project was extracted", unpacked.exists() );
+
+            for ( Iterator artifacts = reactorProject.getArtifacts().iterator(); artifacts.hasNext(); )
+            {
+                Artifact artifact = (Artifact) artifacts.next();
+
+                filename = artifact.getFile().getName();
+                dir = filename.substring( 0, filename.lastIndexOf( '.' ) );
+                unpackDir = new File( workDir, dir );
+                unpacked = new File( unpackDir, filename + ".extracted" );
+
+                assertTrue( "Test reactor project artifact was extracted", unpacked.exists() );
+            }
+        }
     }
 }
