245f230705b5202e78a47bb8ea024d3aedf609ae
Edwin L. Punzalan <epunzalan@apache.org>
2006-04-28 06:28:18 +0000

    PR: MASSEMBLY-88
    Added test harness in pom and added one passing min configuration test for the unpack goal.
    
    Also, fixed a bug when there are more than 3 chars in an artifact file's extension.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@397782 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/assembly/stubs/ArchiverManagerStub.java
@@ -0,0 +1,55 @@
+package org.apache.maven.plugin.assembly.stubs;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.UnArchiver;
+
+/**
+ * @author Edwin Punzalan
+ */
+public class ArchiverManagerStub
+    implements ArchiverManager
+{
+    public static ArchiverStub archiverStub;
+
+    public static UnArchiverStub unArchiverStub;
+
+    public Archiver getArchiver( String string )
+        throws NoSuchArchiverException
+    {
+        if ( archiverStub == null )
+        {
+            archiverStub = new ArchiverStub();
+        }
+
+        return archiverStub;
+    }
+
+    public UnArchiver getUnArchiver( String string )
+        throws NoSuchArchiverException
+    {
+        if ( unArchiverStub == null )
+        {
+            unArchiverStub = new UnArchiverStub();
+        }
+
+        return unArchiverStub;
+    }
+}
