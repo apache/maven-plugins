bb2fe973fde0e65e29d354aa5c85edcef2d40653
Brett Leslie Porter <brett@apache.org>
2006-05-01 05:33:24 +0000

    moving branch to trunk
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@398531 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-surefire-plugin/src/it/test6/src/test/java/JunitTest.java
@@ -0,0 +1,29 @@
+import junit.framework.TestCase;
+
+/**
+ * Provided to ensure both junit and testng tests can run happily
+ * together.
+ * 
+ * @author jkuhnert
+ */
+public class JunitTest extends TestCase {
+
+	Object testObject;
+	
+	/**
+	 * Creats an object instance
+	 */
+	public void setUp()
+	{
+		testObject = new Object();
+	}
+	
+	/**
+	 * Tests that object created in setup 
+	 * isn't null.
+	 */
+	public void testJunitObject()
+	{
+		assertNotNull(testObject);
+	}
+}
