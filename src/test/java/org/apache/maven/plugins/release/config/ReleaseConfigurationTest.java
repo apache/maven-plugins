91a3c1acab017d3e3267e0fbc8e1feeeebd6cc48
Brett Leslie Porter <brett@apache.org>
2006-05-03 04:34:11 +0000

    [MRELEASE-98] configuration tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-release-plugin@399128 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugins/release/config/ReleaseConfigurationTest.java
+++ src/test/java/org/apache/maven/plugins/release/config/ReleaseConfigurationTest.java
@@ -17,9 +17,14 @@ package org.apache.maven.plugins.release.config;
  */
 
 import junit.framework.TestCase;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Scm;
+import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Settings;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * ReleaseConfiguration Tester.
@@ -46,7 +51,8 @@ public class ReleaseConfigurationTest
         releaseConfiguration.merge( new ReleaseConfiguration() );
 
         ReleaseConfiguration expectedConfiguration = createReleaseConfiguration( releaseConfiguration.getSettings(),
-                                                                                 releaseConfiguration.getWorkingDirectory() );
+                                                                                 releaseConfiguration.getWorkingDirectory(),
+                                                                                 releaseConfiguration.getReactorProjects() );
         assertEquals( "Check merge", expectedConfiguration, releaseConfiguration );
     }
 
@@ -64,7 +70,204 @@ public class ReleaseConfigurationTest
         ReleaseConfiguration expectedConfiguration = createMergeConfiguration( releaseConfiguration.getSettings(),
                                                                                releaseConfiguration.getWorkingDirectory(),
                                                                                releaseConfiguration.getCompletedPhase() );
+        expectedConfiguration.setReactorProjects( releaseConfiguration.getReactorProjects() );
         assertEquals( "Check merge", expectedConfiguration, releaseConfiguration );
+    }
+
+    public void testEquals()
+    {
+        ReleaseConfiguration originalReleaseConfiguration = createReleaseConfiguration();
+        ReleaseConfiguration releaseConfiguration = createReleaseConfiguration(
+            originalReleaseConfiguration.getSettings(), originalReleaseConfiguration.getWorkingDirectory(),
+            originalReleaseConfiguration.getReactorProjects() );
+        doEqualsAssertions( releaseConfiguration, originalReleaseConfiguration, "other", Collections.EMPTY_LIST,
+                            new File( "f" ), new Settings() );
+        originalReleaseConfiguration = createReleaseConfiguration();
+        releaseConfiguration = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                                           originalReleaseConfiguration.getWorkingDirectory(),
+                                                           originalReleaseConfiguration.getReactorProjects() );
+        doEqualsAssertions( originalReleaseConfiguration, releaseConfiguration, "other", Collections.EMPTY_LIST,
+                            new File( "f" ), new Settings() );
+
+        originalReleaseConfiguration = createReleaseConfiguration();
+        releaseConfiguration = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                                           originalReleaseConfiguration.getWorkingDirectory(),
+                                                           originalReleaseConfiguration.getReactorProjects() );
+        doEqualsAssertions( releaseConfiguration, originalReleaseConfiguration, null, null, null, null );
+        originalReleaseConfiguration = createReleaseConfiguration();
+        releaseConfiguration = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                                           originalReleaseConfiguration.getWorkingDirectory(),
+                                                           originalReleaseConfiguration.getReactorProjects() );
+        doEqualsAssertions( originalReleaseConfiguration, releaseConfiguration, null, null, null, null );
+
+        assertEquals( "test ==", releaseConfiguration, releaseConfiguration );
+        Object obj = this;
+        assertFalse( "test class instance", releaseConfiguration.equals( obj ) );
+    }
+
+    private static void doEqualsAssertions( ReleaseConfiguration releaseConfiguration,
+                                            ReleaseConfiguration originalReleaseConfiguration, String other,
+                                            List reactorProjects, File otherFile, Settings otherSettings )
+    {
+        ReleaseConfiguration config = releaseConfiguration;
+        assertEquals( "Check original comparison", config, originalReleaseConfiguration );
+
+        config.setUrl( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setUrl( originalReleaseConfiguration.getUrl() );
+
+        config.setAdditionalArguments( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setAdditionalArguments( originalReleaseConfiguration.getAdditionalArguments() );
+
+        config.setAddSchema( !originalReleaseConfiguration.isAddSchema() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setAddSchema( originalReleaseConfiguration.isAddSchema() );
+
+        config.setGenerateReleasePoms( !originalReleaseConfiguration.isAddSchema() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setGenerateReleasePoms( originalReleaseConfiguration.isGenerateReleasePoms() );
+
+        config.setUseEditMode( !originalReleaseConfiguration.isUseEditMode() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setUseEditMode( originalReleaseConfiguration.isUseEditMode() );
+
+        config.setInteractive( !originalReleaseConfiguration.isInteractive() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setInteractive( originalReleaseConfiguration.isInteractive() );
+
+        config.setCompletedPhase( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setCompletedPhase( originalReleaseConfiguration.getCompletedPhase() );
+
+        config.setPassphrase( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setPassphrase( originalReleaseConfiguration.getPassphrase() );
+
+        config.setPassword( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setPassword( originalReleaseConfiguration.getPassword() );
+
+        config.setUsername( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setUsername( originalReleaseConfiguration.getUsername() );
+
+        config.setPrivateKey( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setPrivateKey( originalReleaseConfiguration.getPrivateKey() );
+
+        config.setPomFileName( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setPomFileName( originalReleaseConfiguration.getPomFileName() );
+
+        config.setPreparationGoals( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setPreparationGoals( originalReleaseConfiguration.getPreparationGoals() );
+
+        config.setReactorProjects( reactorProjects );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setReactorProjects( originalReleaseConfiguration.getReactorProjects() );
+
+        config.setSettings( otherSettings );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setSettings( originalReleaseConfiguration.getSettings() );
+
+        config.setReleaseLabel( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setReleaseLabel( originalReleaseConfiguration.getReleaseLabel() );
+
+        config.setTagBase( other );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setTagBase( originalReleaseConfiguration.getTagBase() );
+
+        config.setWorkingDirectory( otherFile );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config.setWorkingDirectory( originalReleaseConfiguration.getWorkingDirectory() );
+
+        // sanity check the test was resetting correctly
+        assertEquals( "Check original comparison", config, originalReleaseConfiguration );
+
+        config.mapDevelopmentVersion( "groupId:artifactId", "1.0-SNAPSHOT" );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapReleaseVersion( "groupId:artifactId", "1.0" );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", new Scm() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", new Scm() );
+        originalReleaseConfiguration.mapOriginalScmInfo( "foo", new Scm() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", new Scm() );
+        originalReleaseConfiguration.mapOriginalScmInfo( "groupId:artifactId", new Scm() );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "tag" ) );
+        originalReleaseConfiguration.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "tag" ) );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", getScm( "-", "dev", "url", "tag" ) );
+        originalReleaseConfiguration.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "tag" ) );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "-", "url", "tag" ) );
+        originalReleaseConfiguration.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "tag" ) );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "-", "tag" ) );
+        originalReleaseConfiguration.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "tag" ) );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+        config = createReleaseConfiguration( originalReleaseConfiguration.getSettings(),
+                                             originalReleaseConfiguration.getWorkingDirectory(),
+                                             originalReleaseConfiguration.getReactorProjects() );
+
+        config.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "-" ) );
+        originalReleaseConfiguration.mapOriginalScmInfo( "groupId:artifactId", getScm( "conn", "dev", "url", "tag" ) );
+        assertFalse( "Check original comparison", config.equals( originalReleaseConfiguration ) );
+    }
+
+    public void testHashCode()
+    {
+        ReleaseConfiguration releaseConfiguration = createReleaseConfiguration();
+        assertEquals( "Check hash code", releaseConfiguration.hashCode(), createReleaseConfiguration(
+            releaseConfiguration.getSettings(), releaseConfiguration.getWorkingDirectory(),
+            releaseConfiguration.getReactorProjects() ).hashCode() );
+    }
+
+    private static Scm getScm( String connection, String developerConnection, String url, String tag )
+    {
+        Scm scm = new Scm();
+        scm.setConnection( connection );
+        scm.setDeveloperConnection( developerConnection );
+        scm.setTag( tag );
+        scm.setUrl( url );
+        return scm;
     }
 
     private static ReleaseConfiguration createMergeConfiguration( Settings settings, File workingDirectory,
@@ -80,6 +283,9 @@ public class ReleaseConfigurationTest
         mergeConfiguration.setTagBase( "tag-base-merge" );
         mergeConfiguration.setReleaseLabel( "tag-merge" );
         mergeConfiguration.setUsername( "username-merge" );
+        mergeConfiguration.setAdditionalArguments( "additional-arguments-merge" );
+        mergeConfiguration.setPomFileName( "pom-file-name-merge" );
+        mergeConfiguration.setPreparationGoals( "preparation-goals-merge" );
         mergeConfiguration.setWorkingDirectory( workingDirectory );
         return mergeConfiguration;
     }
@@ -88,11 +294,13 @@ public class ReleaseConfigurationTest
     {
         Settings settings = new Settings();
         File workingDirectory = new File( "." );
+        MavenProject project = new MavenProject( new Model() );
 
-        return createReleaseConfiguration( settings, workingDirectory );
+        return createReleaseConfiguration( settings, workingDirectory, Collections.singletonList( project ) );
     }
 
-    private static ReleaseConfiguration createReleaseConfiguration( Settings settings, File workingDirectory )
+    private static ReleaseConfiguration createReleaseConfiguration( Settings settings, File workingDirectory,
+                                                                    List list )
     {
         ReleaseConfiguration releaseConfiguration = new ReleaseConfiguration();
         releaseConfiguration.setUrl( "scm-url" );
@@ -105,6 +313,10 @@ public class ReleaseConfigurationTest
         releaseConfiguration.setReleaseLabel( "tag" );
         releaseConfiguration.setUsername( "username" );
         releaseConfiguration.setWorkingDirectory( workingDirectory );
+        releaseConfiguration.setAdditionalArguments( "additional-arguments" );
+        releaseConfiguration.setPomFileName( "pom-file-name" );
+        releaseConfiguration.setPreparationGoals( "preparation-goals" );
+        releaseConfiguration.setReactorProjects( list );
         return releaseConfiguration;
     }
 }
