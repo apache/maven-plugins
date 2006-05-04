ac4b1dd1b9304b6e844a23dc9f22806a387c5596
Allan Q. Ramirez <aramirez@apache.org>
2006-05-04 11:05:42 +0000

    PR: MASSEMBLY-88
    added test for directory mojo
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@399660 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/assembly/DirectoryMojoTest.java
@@ -0,0 +1,96 @@
+package org.apache.maven.plugin.assembly;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.assembly.stubs.ArchiverManagerStub;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.artifact.Artifact;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
+
+/**
+ * @author Allan Q. Ramirez
+ */
+public class DirectoryMojoTest
+    extends AbstractMojoTestCase
+{
+    public void testEnvironment()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "src/test/plugin-configs/directory/min-plugin-config.xml" );
+
+        DirectoryMojo mojo = ( DirectoryMojo ) lookupMojo( "directory", testPom );
+
+        assertNotNull( mojo );
+    }
+
+    public void testAssemblyDirectory()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "src/test/plugin-configs/directory/min-plugin-config.xml" );
+
+        DirectoryMojo mojo = ( DirectoryMojo ) lookupMojo( "directory", testPom );
+
+        assertNotNull( mojo );
+
+        mojo.execute();
+
+        Map filesArchived = ArchiverManagerStub.archiverStub.getFiles();
+
+        Set files = filesArchived.keySet();
+
+        assertEquals( 1, files.size() );
+    }
+
+
+    public void testAssemblyDirectoryWithDependencySet()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "src/test/plugin-configs/directory/dependency-set-plugin-config.xml" );
+
+        DirectoryMojo mojo = ( DirectoryMojo ) lookupMojo( "directory", testPom );
+
+        assertNotNull( mojo );
+
+        MavenProject project = ( MavenProject ) getVariableValueFromObject( mojo, "executedProject" );
+
+        Set artifacts = project.getArtifacts();
+
+        mojo.execute();
+
+        Map filesArchived = ArchiverManagerStub.archiverStub.getFiles();
+
+        Set files = filesArchived.keySet();
+
+        for( Iterator iter = artifacts.iterator(); iter.hasNext(); )
+        {
+            Artifact artifact = ( Artifact ) iter.next();
+
+            assertTrue( files.contains( artifact.getFile() ) );
+            assertTrue( artifact.getFile().getName().endsWith( ".jar" ) );
+        }
+
+        assertTrue( "Test project is in archive", files.contains( project.getArtifact().getFile() ) );
+    }
+}
