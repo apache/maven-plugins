3dea37813099176faa4e426ef171ec1809645dfc
John Dennis Casey <jdcasey@apache.org>
2006-05-05 16:42:10 +0000

    [MASSEMBLY-29] Re-adding module-source archiving alongside module-binary archiving...had to modify the binary aspect of the moduleSet into a sub-element called <binaries/>, which is a sibling of the new <sources/> sub-element.
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@400105 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/it/test.rb
+++ maven-assembly-plugin/src/it/test.rb
@@ -2,18 +2,82 @@
 
 require 'FileUtils'
 
-basedir = `pwd`.chomp
+class Tester
 
-File.open( 'integration-tests.txt' ).each_line do |testdir|
+  attr_reader :failures
+  
+  def initialize
+  
+    @failures = Array.new
+    
+  end
 
-  testdir = testdir.chomp
-
-  FileUtils.cd "#{basedir}/#{testdir}"
-
-  puts "\n\nRUNNING TEST IN #{testdir}...\n\n"
-  puts `mvn \`cat goals.txt\``
-  puts "\n\n...done\n\n"
-
-  FileUtils.cd basedir
+  def test( testdir, basedir )
+  
+    testdir = testdir.chomp
+  
+    FileUtils.cd "#{basedir}/#{testdir}"
+  
+    goals = "clean verify"
+    if File.file?( 'goals.txt' )
+    
+      goals = File.open( 'goals.txt' ).read.chomp
+      
+    end
+    
+    puts "\nRUNNING TEST: #{testdir}..."
+    
+    output = `mvn -X #{goals}`
+    
+    logfile = File.open( 'log.txt', 'w' ) { |file| file << output }
+    
+    if output =~ /BUILD SUCCESSFUL/
+    
+      puts "[SUCCESS]"
+    
+    else
+    
+    puts "#{output}\n[ERROR]\n\n"
+    @failures << testdir
+    
+    end
+  
+    FileUtils.cd basedir
+  
+  end
 
 end
+
+@basedir = `pwd`.chomp
+@tester = Tester.new
+
+if ARGV.size > 0
+
+  ARGV.each { |testdir| @tester.test( testdir, @basedir ) }
+
+else
+
+  File.open( 'integration-tests.txt' ).each_line do |testdir|
+
+    testdir = testdir.chomp
+    
+    if ( !( testdir =~ /\w*#/ ) && testdir.size > 0 )
+    
+      @tester.test( testdir, @basedir )
+    
+    end
+
+  end
+  
+end
+
+if @tester.failures.size > 0
+
+  puts "\n\nThere were #{@tester.failures.size} failed tests.\nFailures:\n"
+  
+  @tester.failures.each {|failed| puts "\n- #{failed}"}
+  
+  puts "\n\n"
+
+end
+
