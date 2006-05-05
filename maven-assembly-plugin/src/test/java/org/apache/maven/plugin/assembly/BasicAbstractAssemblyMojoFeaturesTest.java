19451fdfb2c43d4cab98a87168e260d46e57a32a
John Dennis Casey <jdcasey@apache.org>
2006-05-05 03:41:12 +0000

    Switching to old-fashioned test builds until we get a more testable design. Harness testing is burning too much time with debugging the tests.
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@399940 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/test/java/org/apache/maven/plugin/assembly/BasicAbstractAssemblyMojoFeaturesTest.java
+++ maven-assembly-plugin/src/test/java/org/apache/maven/plugin/assembly/BasicAbstractAssemblyMojoFeaturesTest.java
@@ -2,21 +2,38 @@ package org.apache.maven.plugin.assembly;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.metadata.ArtifactMetadata;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.OverConstrainedVersionException;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.model.Model;
+import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.assembly.stubs.ArchiverManagerStub;
 import org.apache.maven.plugin.assembly.stubs.ArchiverStub;
-import org.apache.maven.plugin.assembly.stubs.ArtifactStub;
+import org.apache.maven.plugin.testing.stubs.ArtifactStub;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
+import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.UnArchiver;
 import org.codehaus.plexus.archiver.manager.ArchiverManager;
 import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -42,10 +59,22 @@ public class BasicAbstractAssemblyMojoFeaturesTest
         requiredDependencies.add( "dependencies/test.jar" );
         requiredDependencies.add( "dependencies/test2.jar" );
         
-        testDependencyMapping( pluginConfig, requiredDependencies );
+        Mojo mojo = run( pluginConfig, "directory-inline" );
+        assertFilesAdded( mojo, requiredDependencies );
     }
-
-    private void testDependencyMapping( String pluginConfig, List requiredDependencies ) throws Exception
+    
+    public void testModuleSetSourceIncludedBinariesNotIncluded() throws Exception
+    {
+        String pluginConfig = "moduleSetSourceIncludedBinariesNotIncluded-pluginConfig.xml";
+        
+        Mojo mojo = run( pluginConfig, "attached" );
+        
+        List required = Collections.singletonList( "sources/module1/src/main/java/org/test/module1/App.java" );
+        
+        assertFilesAdded( mojo, required );
+    }
+    
+    private Mojo run( String pluginConfig, String mojoName ) throws Exception
     {
         String pluginConfigResource = "basicAbstractAssemblyMojoFeaturesTest/" + pluginConfig;
         
@@ -54,13 +83,20 @@ public class BasicAbstractAssemblyMojoFeaturesTest
         assertTrue( "Cannot find plugin-configuration: \'" + pluginConfigResource + "\' in context-classloader\'s classpath.", pluginConfigFile.exists() );
         
         // TODO: Need to replace this with test-only mojos...
-        DirectoryMojo mojo = (DirectoryMojo) lookupMojo( "directory", pluginConfigFile.getAbsolutePath() );
+        Mojo mojo = (Mojo) lookupMojo( mojoName, pluginConfigFile.getAbsolutePath() );
 
         FileLoggingArchiverManagerStub archiverManager = (FileLoggingArchiverManagerStub) getVariableValueFromObject( mojo, "archiverManager" );
         archiverManager.clearArchiver();
         
         mojo.execute();
 
+        return mojo;
+    }
+
+    private void assertFilesAdded( Mojo mojo, List requiredDependencies ) throws Exception
+    {
+        FileLoggingArchiverManagerStub archiverManager = (FileLoggingArchiverManagerStub) getVariableValueFromObject( mojo, "archiverManager" );
+        
         FileLoggingArchiverStub archiver = (FileLoggingArchiverStub) archiverManager.getArchiver( null );
         
         Set addedFiles = archiver.getAddedFiles();
@@ -74,7 +110,25 @@ public class BasicAbstractAssemblyMojoFeaturesTest
             assertTrue( "Required dependency path missing: \'" + targetPath + "\'", addedFiles.contains( targetPath ) );
         }
     }
+    
+    public static final class BasedirSettableMavenProjectStub extends MavenProjectStub
+    {
+        
+        File basedir;
+        
+        public File getBasedir()
+        {
+            return basedir;
+        }
+        
+        public void setBasedir( File basedir )
+        {
+            this.basedir = basedir;
+        }
 
+        
+    }
+    
     public static final class FileLoggingArchiverManagerStub
         implements ArchiverManager
     {
@@ -107,6 +161,7 @@ public class BasicAbstractAssemblyMojoFeaturesTest
     {
 
         private Set files = new LinkedHashSet();
+        private DirectoryScanner scanner = new DirectoryScanner();
 
         public void addFile( File file, String targetPath, int mode ) throws ArchiverException
         {
@@ -122,29 +177,65 @@ public class BasicAbstractAssemblyMojoFeaturesTest
         {
             return files;
         }
-
-        public void addDirectory( File arg0 ) throws ArchiverException
+        
+        public void addDirectory( File dir ) throws ArchiverException
         {
-            // TODO Auto-generated method stub
-            
+            System.out.println( "Add dir 1" );
+            addDirectory_( dir, null, null, null );
         }
 
-        public void addDirectory( File arg0, String arg1 ) throws ArchiverException
+        public void addDirectory( File dir, String prefix ) throws ArchiverException
         {
-            // TODO Auto-generated method stub
-            
+            System.out.println( "Add dir 2" );
+            addDirectory_( dir, prefix, null, null );
         }
 
-        public void addDirectory( File arg0, String[] arg1, String[] arg2 ) throws ArchiverException
+        public void addDirectory( File dir, String[] includes, String[] excludes ) throws ArchiverException
         {
-            // TODO Auto-generated method stub
-            
+            System.out.println( "Add dir 3" );
+            addDirectory_( dir, null, includes, excludes );
         }
 
-        public void addDirectory( File arg0, String arg1, String[] arg2, String[] arg3 ) throws ArchiverException
+        public void addDirectory( File dir, String prefix, String[] includes, String[] excludes ) throws ArchiverException
         {
-            // TODO Auto-generated method stub
-            
+            System.out.println( "Add dir 3" );
+            addDirectory_( dir, prefix, includes, excludes );
+        }
+        
+        private void addDirectory_( File dir, String prefix, String[] includes, String[] excludes ) throws ArchiverException
+        {
+            try
+            {
+                String include = StringUtils.join( includes, "," );
+                String exclude = StringUtils.join( excludes, "," );
+                
+                String prepend = prefix;
+                
+                if ( prepend != null && !prepend.endsWith( "/" ) )
+                {
+                    prepend += "/";
+                }
+                
+                System.out.println( "Scanning: " + dir + "\nwith includes: " + include + "\nand excludes: " + exclude + "\nand prepending dir prefix: " + prepend );
+                
+                List fileNames = FileUtils.getFileNames( dir, include, exclude, false );
+                
+                for ( Iterator it = fileNames.iterator(); it.hasNext(); )
+                {
+                    String name = (String) it.next();
+                    
+                    String fn = prepend + dir.getPath() + "/" + name;
+                    fn.replace( '\\', '/' );
+                    
+                    System.out.println( "Adding: " + fn );
+                    
+                    files.add( fn );
+                }
+            }
+            catch ( IOException e )
+            {
+                throw new ArchiverException( "Error scanning for file names.", e );
+            }
         }
 
         public void createArchive() throws ArchiverException, IOException
@@ -223,23 +314,34 @@ public class BasicAbstractAssemblyMojoFeaturesTest
         private String depOneVersion;
         private String depOneType = "jar";
         private String depOneScope = "compile";
+        private File depOneFile;
         
         private String depTwoArtifactId;
         private String depTwoGroupId;
         private String depTwoVersion;
         private String depTwoType = "jar";
         private String depTwoScope = "compile";
+        private File depTwoFile;
+        private LinkedHashSet artifacts = new LinkedHashSet();
         
         public Set getArtifacts()
         {
             Set artifacts = new LinkedHashSet();
             
-            artifacts.add( new HandlerEquippedArtifactStub( depOneGroupId, depOneArtifactId, depOneVersion, depOneType, depOneScope ) );
-            artifacts.add( new HandlerEquippedArtifactStub( depTwoGroupId, depTwoArtifactId, depTwoVersion, depTwoType, depTwoScope ) );
+            addArtifact( depOneGroupId, depOneArtifactId, depOneVersion, depOneType, depOneScope, depOneFile );
+            addArtifact( depTwoGroupId, depTwoArtifactId, depTwoVersion, depTwoType, depTwoScope, depTwoFile );
             
             return artifacts;
         }
         
+        private void addArtifact( String groupId, String artifactId, String version, String type, String scope, File file )
+        {
+            Artifact artifact = new HandlerEquippedArtifactStub( groupId, artifactId, version, type, scope );
+            artifact.setFile( file );
+            
+            artifacts .add( artifact );
+        }
+
         public Artifact getArtifact()
         {
             return new HandlerEquippedArtifactStub( groupId, artifactId, version, packaging, scope );
@@ -263,15 +365,28 @@ public class BasicAbstractAssemblyMojoFeaturesTest
         }
     }
     
-    public static final class HandlerEquippedArtifactStub extends ArtifactStub
+    public static final class HandlerEquippedArtifactStub implements Artifact
     {
 
-        private final String type;
+        private String type;
+        private String artifactId;
+        private String groupId;
+        private String version;
+        private String classifier;
+        private String scope;
+        private File file;
 
         public HandlerEquippedArtifactStub( String groupId, String artifactId, String version, String type, String scope )
         {
-            super( groupId, artifactId, version, type, scope );
+            this.groupId = groupId;
+            this.artifactId = artifactId;
+            this.version = version;
             this.type = type;
+            this.scope = scope;
+        }
+        
+        public HandlerEquippedArtifactStub()
+        {
         }
 
         public ArtifactHandler getArtifactHandler()
@@ -281,7 +396,7 @@ public class BasicAbstractAssemblyMojoFeaturesTest
 
                 public String getClassifier()
                 {
-                    return null;
+                    return classifier;
                 }
 
                 public String getDirectory()
@@ -296,7 +411,7 @@ public class BasicAbstractAssemblyMojoFeaturesTest
 
                 public String getLanguage()
                 {
-                    return null;
+                    return "java";
                 }
 
                 public String getPackaging()
@@ -318,5 +433,240 @@ public class BasicAbstractAssemblyMojoFeaturesTest
             
             return handler;
         }
+
+        public void addMetadata( ArtifactMetadata arg0 )
+        {
+            // TODO Auto-generated method stub
+            
+        }
+
+        public String getArtifactId()
+        {
+            return artifactId;
+        }
+
+        public List getAvailableVersions()
+        {
+            return Collections.singletonList( version );
+        }
+
+        public String getBaseVersion()
+        {
+            return version;
+        }
+
+        public String getClassifier()
+        {
+            return classifier;
+        }
+
+        public String getDependencyConflictId()
+        {
+            return groupId + ":" + artifactId + ":" + type + ":" + version;
+        }
+
+        public ArtifactFilter getDependencyFilter()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public List getDependencyTrail()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public String getDownloadUrl()
+        {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public File getFile()
+        {
+            return file;
+        }
+
+        public String getGroupId()
+        {
+            return groupId;
+        }
+
+        public String getId()
+        {
+            return getDependencyConflictId() + ":" + scope;
+        }
+
+        public Collection getMetadataList()
+        {
+            return null;
+        }
+
+        public ArtifactRepository getRepository()
+        {
+            return null;
+        }
+
+        public String getScope()
+        {
+            return scope;
+        }
+
+        public ArtifactVersion getSelectedVersion() throws OverConstrainedVersionException
+        {
+            return null;
+        }
+
+        public String getType()
+        {
+            return type;
+        }
+
+        public String getVersion()
+        {
+            return version;
+        }
+
+        public VersionRange getVersionRange()
+        {
+            return VersionRange.createFromVersion( version );
+        }
+
+        public boolean hasClassifier()
+        {
+            return classifier != null;
+        }
+
+        public boolean isOptional()
+        {
+            return false;
+        }
+
+        public boolean isRelease()
+        {
+            return false;
+        }
+
+        public boolean isResolved()
+        {
+            return true;
+        }
+
+        public boolean isSelectedVersionKnown() throws OverConstrainedVersionException
+        {
+            return true;
+        }
+
+        public boolean isSnapshot()
+        {
+            return false;
+        }
+
+        public void selectVersion( String arg0 )
+        {
+        }
+
+        public void setArtifactHandler( ArtifactHandler arg0 )
+        {
+        }
+
+        public void setArtifactId( String artifactId )
+        {
+            this.artifactId = artifactId;
+        }
+
+        public void setAvailableVersions( List arg0 )
+        {
+        }
+
+        public void setBaseVersion( String version )
+        {
+            this.version = version;
+        }
+
+        public void setDependencyFilter( ArtifactFilter arg0 )
+        {
+        }
+
+        public void setDependencyTrail( List arg0 )
+        {
+        }
+
+        public void setDownloadUrl( String arg0 )
+        {
+        }
+
+        public void setFile( File file )
+        {
+            this.file = file;
+            
+            if ( file.exists() )
+            {
+                Writer writer = null;
+                try
+                {
+                    writer = new FileWriter( file );
+                    writer.write( "test artifact" );
+                }
+                catch ( IOException e )
+                {
+                    IllegalArgumentException error = new IllegalArgumentException( "Cannot write test file: " + file + ". Reason: " + e.getMessage() );
+                    error.initCause( e );
+                    
+                    throw error;
+                }
+                finally
+                {
+                    IOUtil.close( writer );
+                }
+            }
+        }
+
+        public void setGroupId( String groupId )
+        {
+            this.groupId = groupId;
+        }
+
+        public void setRelease( boolean arg0 )
+        {
+        }
+
+        public void setRepository( ArtifactRepository arg0 )
+        {
+        }
+
+        public void setResolved( boolean arg0 )
+        {
+        }
+
+        public void setResolvedVersion( String version )
+        {
+            this.version = version;
+        }
+
+        public void setScope( String scope )
+        {
+            this.scope = scope;
+        }
+
+        public void setVersion( String version )
+        {
+            this.version = version;
+        }
+
+        public void setVersionRange( VersionRange arg0 )
+        {
+        }
+
+        public void updateVersion( String arg0, ArtifactRepository arg1 )
+        {
+        }
+
+        public int compareTo( Object o )
+        {
+            return 0;
+        }
+
     }
 }
