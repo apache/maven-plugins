a4ed945a2e895fac01df802f8a496218fd3eb842
Brett Leslie Porter <brett@apache.org>
2006-05-09 14:32:36 +0000

    if using a snapshot of the release plugin, prompt if it is ok to continue (fail is the default)
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-release-plugin@405443 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugins/release/phase/CheckDependencySnapshotsPhaseTest.java
+++ src/test/java/org/apache/maven/plugins/release/phase/CheckDependencySnapshotsPhaseTest.java
@@ -16,8 +16,15 @@ package org.apache.maven.plugins.release.phase;
  * limitations under the License.
  */
 
+import org.apache.maven.plugins.release.ReleaseExecutionException;
 import org.apache.maven.plugins.release.ReleaseFailureException;
 import org.apache.maven.plugins.release.config.ReleaseConfiguration;
+import org.codehaus.plexus.components.interactivity.Prompter;
+import org.codehaus.plexus.components.interactivity.PrompterException;
+import org.jmock.cglib.Mock;
+import org.jmock.core.matcher.InvokeOnceMatcher;
+import org.jmock.core.stub.ReturnStub;
+import org.jmock.core.stub.ThrowStub;
 
 /**
  * Test the dependency snapshot check phase.
@@ -59,6 +66,181 @@ public class CheckDependencySnapshotsPhaseTest
 
         // successful execution is verification enough
         assertTrue( true );
+    }
+
+    public void testSnapshotReleasePluginNonInteractive()
+        throws Exception
+    {
+        ReleaseConfiguration releaseConfiguration = createConfigurationFromProjects( "snapshot-release-plugin" );
+        releaseConfiguration.setInteractive( false );
+
+        try
+        {
+            phase.execute( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseFailureException e )
+        {
+            assertTrue( true );
+        }
+
+        try
+        {
+            phase.simulate( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseFailureException e )
+        {
+            assertTrue( true );
+        }
+    }
+
+    public void testSnapshotReleasePluginInteractiveDeclined()
+        throws Exception
+    {
+        CheckDependencySnapshotsPhase phase =
+            (CheckDependencySnapshotsPhase) lookup( ReleasePhase.ROLE, "check-dependency-snapshots" );
+
+        ReleaseConfiguration releaseConfiguration = createConfigurationFromProjects( "snapshot-release-plugin" );
+
+        Mock mockPrompter = new Mock( Prompter.class );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will( new ReturnStub( "no" ) );
+        phase.setPrompter( (Prompter) mockPrompter.proxy() );
+
+        try
+        {
+            phase.execute( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseFailureException e )
+        {
+            assertTrue( true );
+        }
+
+        mockPrompter.reset();
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will( new ReturnStub( "no" ) );
+
+        try
+        {
+            phase.simulate( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseFailureException e )
+        {
+            assertTrue( true );
+        }
+    }
+
+    public void testSnapshotReleasePluginInteractiveAccepted()
+        throws Exception
+    {
+        CheckDependencySnapshotsPhase phase =
+            (CheckDependencySnapshotsPhase) lookup( ReleasePhase.ROLE, "check-dependency-snapshots" );
+
+        ReleaseConfiguration releaseConfiguration = createConfigurationFromProjects( "snapshot-release-plugin" );
+
+        Mock mockPrompter = new Mock( Prompter.class );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will( new ReturnStub( "yes" ) );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        phase.setPrompter( (Prompter) mockPrompter.proxy() );
+
+        phase.execute( releaseConfiguration );
+
+        mockPrompter.reset();
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will( new ReturnStub( "yes" ) );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+
+        phase.simulate( releaseConfiguration );
+
+        assertTrue( true );
+    }
+
+    public void testSnapshotReleasePluginInteractiveInvalid()
+        throws Exception
+    {
+        CheckDependencySnapshotsPhase phase =
+            (CheckDependencySnapshotsPhase) lookup( ReleasePhase.ROLE, "check-dependency-snapshots" );
+
+        ReleaseConfiguration releaseConfiguration = createConfigurationFromProjects( "snapshot-release-plugin" );
+
+        Mock mockPrompter = new Mock( Prompter.class );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will( new ReturnStub( "donkey" ) );
+        phase.setPrompter( (Prompter) mockPrompter.proxy() );
+
+        try
+        {
+            phase.execute( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseFailureException e )
+        {
+            assertTrue( true );
+        }
+
+        mockPrompter.reset();
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will( new ReturnStub( "donkey" ) );
+
+        try
+        {
+            phase.simulate( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseFailureException e )
+        {
+            assertTrue( true );
+        }
+    }
+
+    public void testSnapshotReleasePluginInteractiveException()
+        throws Exception
+    {
+        CheckDependencySnapshotsPhase phase =
+            (CheckDependencySnapshotsPhase) lookup( ReleasePhase.ROLE, "check-dependency-snapshots" );
+
+        ReleaseConfiguration releaseConfiguration = createConfigurationFromProjects( "snapshot-release-plugin" );
+
+        Mock mockPrompter = new Mock( Prompter.class );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will(
+            new ThrowStub( new PrompterException( "..." ) ) );
+        phase.setPrompter( (Prompter) mockPrompter.proxy() );
+
+        try
+        {
+            phase.execute( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseExecutionException e )
+        {
+            assertEquals( "Check cause", PrompterException.class, e.getCause().getClass() );
+        }
+
+        mockPrompter.reset();
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "showMessage" );
+        mockPrompter.expects( new InvokeOnceMatcher() ).method( "prompt" ).will(
+            new ThrowStub( new PrompterException( "..." ) ) );
+
+        try
+        {
+            phase.simulate( releaseConfiguration );
+
+            fail( "Should have failed execution" );
+        }
+        catch ( ReleaseExecutionException e )
+        {
+            assertEquals( "Check cause", PrompterException.class, e.getCause().getClass() );
+        }
     }
 
     public void testSnapshotDependenciesInProjectOnlyMismatchedVersion()
