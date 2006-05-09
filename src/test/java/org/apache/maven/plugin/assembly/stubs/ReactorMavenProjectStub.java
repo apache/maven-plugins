2ae84447122b86af37f459bd2e80494125330b4c
Edwin L. Punzalan <epunzalan@apache.org>
2006-05-09 11:10:42 +0000

    PR: MASSEMBLY-88
    Added test cases for ModuleSets
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@405387 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/assembly/stubs/ReactorMavenProjectStub.java
+++ src/test/java/org/apache/maven/plugin/assembly/stubs/ReactorMavenProjectStub.java
@@ -16,13 +16,19 @@ package org.apache.maven.plugin.assembly.stubs;
  * limitations under the License.
  */
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Reporting;
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.artifact.Artifact;
+import org.codehaus.plexus.util.FileUtils;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
 /**
@@ -35,10 +41,9 @@ public class ReactorMavenProjectStub
 
     private MavenProject parent;
 
-    public Set getArtifacts()
-    {
-        return Collections.EMPTY_SET;
-    }
+    private Model model;
+
+    private File basedir;
 
     public ReactorMavenProjectStub()
     {
@@ -58,6 +63,88 @@ public class ReactorMavenProjectStub
 
         setArtifact( new ArtifactStub( getGroupId(), getArtifactId(),
                                        getVersion(), getPackaging(), Artifact.SCOPE_COMPILE ) );
+    }
+
+    public String getId()
+    {
+        return getGroupId() + ":" + getArtifactId() + ":" + getVersion() + ":" + getPackaging();
+    }
+
+    public Set getArtifacts()
+    {
+        Set artifacts = new HashSet();
+
+        artifacts.add( new ArtifactStub( "assembly", "reactor-dependency", "1.0", "jar", Artifact.SCOPE_COMPILE ) );
+
+        return artifacts;
+    }
+
+    public File getBasedir()
+    {
+        File basedir =  super.getBasedir();
+
+        if ( parent != null )
+        {
+            basedir = parent.getBasedir();
+        }
+
+        return new File( basedir, getArtifactId() );
+    }
+
+    public Reporting getReporting()
+    {
+        return model.getReporting();
+    }
+
+    public Model getModel()
+    {
+        if ( model == null )
+        {
+            model = new Model();
+
+            model.setGroupId( getGroupId() );
+
+            model.setArtifactId( getArtifactId() );
+
+            model.setVersion( getVersion() );
+
+            model.setPackaging( getPackaging() );
+
+            model.setProperties( new Properties() );
+
+            Build build = new Build();
+            build.setFinalName( getArtifactId() + "-" + getVersion() + "." + getPackaging() );
+
+            if ( parent != null )
+            {
+                build.setDirectory( parent.getBasedir().getAbsolutePath() + "/" + getArtifactId() + "/target" );
+                FileUtils.mkdir( build.getDirectory() );
+
+                build.setOutputDirectory( parent.getBasedir().getAbsolutePath() + "/" +
+                                          getArtifactId() + "/target/classes" );
+                FileUtils.mkdir( build.getOutputDirectory() );
+
+                build.setTestOutputDirectory( parent.getBasedir().getAbsolutePath() + "/" +
+                                          getArtifactId() + "/target/test-classes" );
+                FileUtils.mkdir( build.getTestOutputDirectory() );
+
+                Reporting reporting = new Reporting();
+                reporting.setOutputDirectory( parent.getBasedir().getAbsolutePath() + "/" +
+                                          getArtifactId() + "/target/site" );
+                FileUtils.mkdir( reporting.getOutputDirectory() );
+
+                model.setReporting( reporting );
+            }
+
+            model.setBuild( build );
+        }
+
+        return model;
+    }
+
+    public Build getBuild()
+    {
+        return getModel().getBuild();
     }
 
     public void setParent( MavenProject parent )
