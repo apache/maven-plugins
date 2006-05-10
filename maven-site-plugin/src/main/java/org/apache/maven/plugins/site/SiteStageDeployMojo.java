11f53829b345ee24869ad7169d33307de97b60f7
Brett Leslie Porter <brett@apache.org>
2006-05-10 13:06:59 +0000

    fix copyright
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@405750 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteStageDeployMojo.java
+++ maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteStageDeployMojo.java
@@ -1,5 +1,7 @@
+package org.apache.maven.plugins.site;
+
 /*
- * Copyright 2004-2005 The Apache Software Foundation.
+ * Copyright 2004-2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.apache.maven.plugins.site;
 
 import org.apache.maven.artifact.manager.WagonManager;
 import org.apache.maven.plugin.MojoExecutionException;
