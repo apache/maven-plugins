eb4f39b016be5ad1187cd3da8fb69e289a7edbe3
Brett Leslie Porter <brett@apache.org>
2006-05-14 21:35:43 +0000

    add missing test files
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-war-plugin@406438 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/war/stub/ProjectHelperStub.java
@@ -0,0 +1,76 @@
+package org.apache.maven.plugin.war.stub;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+
+public class ProjectHelperStub
+    implements MavenProjectHelper
+{
+    File artifactFile;
+
+    String artifactType;
+
+    String artifactClassifier;
+
+    public File getArtifactFile()
+    {
+        return artifactFile;
+    }
+
+    public String getArtifactType()
+    {
+        return artifactType;
+    }
+
+    public String getArtifactClassifier()
+    {
+        return artifactClassifier;
+    }
+
+    public void attachArtifact( MavenProject project, File artifactFile, String artifactClassifier )
+    {
+
+    }
+
+    public void attachArtifact( MavenProject project, String artifactType, File artifactFile )
+    {
+
+    }
+
+    public void attachArtifact( MavenProject project, String _artifactType, String _artifactClassifier,
+                               File _artifactFile )
+    {
+        artifactType = _artifactType;
+        artifactClassifier = _artifactClassifier;
+        artifactFile = _artifactFile;
+    }
+
+    public void addResource( MavenProject project, String resourceDirectory, List includes, List excludes )
+    {
+
+    }
+
+    public void addTestResource( MavenProject project, String resourceDirectory, List includes, List excludes )
+    {
+
+    }
+}
