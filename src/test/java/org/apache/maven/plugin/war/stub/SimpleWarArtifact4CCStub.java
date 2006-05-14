de0628f16f299dff713653d93d792210459a3b6d
Brett Leslie Porter <brett@apache.org>
2006-05-14 21:35:43 +0000

    add missing test files
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk/maven-war-plugin@406438 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/war/stub/SimpleWarArtifact4CCStub.java
@@ -0,0 +1,56 @@
+package org.apache.maven.plugin.war.stub;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.artifact.versioning.VersionRange;
+
+/**
+ * 
+ *  stub for copy constructor
+ *  to preven the copy constructor frow blowing up
+ *
+ */
+public class SimpleWarArtifact4CCStub
+    extends SimpleWarArtifactStub
+{
+    public SimpleWarArtifact4CCStub( String basedir )
+    {
+        super( basedir );
+    }
+
+    public VersionRange getVersionRange()
+    {
+        return VersionRange.createFromVersion( getVersion() );
+    }
+
+    public String getGroupId()
+    {
+        return "org.maven.plugin.test";
+    }
+
+    public String getClassifier()
+    {
+        return "testclassifier";
+    }
+
+    public ArtifactHandler getArtifactHandler()
+    {
+        return new DefaultArtifactHandler();
+    }
+}
