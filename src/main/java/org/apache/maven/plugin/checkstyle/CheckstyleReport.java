210927c67bbda6ffd936081240530008f5f5c962
Edwin L. Punzalan <epunzalan@apache.org>
2006-05-22 03:48:02 +0000

    fixed html tags for site/javadoc that cause site generation to fail
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@408559 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/checkstyle/CheckstyleReport.java
+++ src/main/java/org/apache/maven/plugin/checkstyle/CheckstyleReport.java
@@ -153,21 +153,21 @@ public class CheckstyleReport
     private String excludes;
 
     /**
-     * Specifies the location of the XML configuration to use.
-     * <p/>
-     * Potential values are a filesystem path, a URL, or a classpath
+     * <p>Specifies the location of the XML configuration to use.</p>
+     *
+     * <p>Potential values are a filesystem path, a URL, or a classpath
      * resource.  This parameter expects that the contents of the location
      * conform to the xml format (Checkstyle
      * <a href="http://checkstyle.sourceforge.net/config.html#Modules">Checker
-     * module</a>) configuration of rulesets.
-     * <p/>
-     * This parameter is resolved as resource, URL, then file.
+     * module</a>) configuration of rulesets.</p>
+     *
+     * <p>This parameter is resolved as resource, URL, then file.
      * If successfully resolved, the contents of the configuration is copied into the
      * <code>${project.build.directory}/checkstyle-configuration.xml</code>
-     * file before being passed to checkstyle as a configuration.
-     * <p/>
-     * There are 4 predefined rulesets.
-     * <p/>
+     * file before being passed to checkstyle as a configuration.</p>
+     *
+     * <p>There are 4 predefined rulesets.</p>
+     *
      * <ul>
      * <li><code>config/sun_checks.xml</code>: Sun Checks.</li>
      * <li><code>config/turbine_checks.xml</code>: Turbine Checks.</li>
@@ -190,17 +190,17 @@ public class CheckstyleReport
     private String format;
 
     /**
-     * Specifies the location of the properties file.
-     * <p/>
-     * This parameter is resolved as URL, File, then resource.
+     * <p>Specifies the location of the properties file.</p>
+     *
+     * <p>This parameter is resolved as URL, File, then resource.
      * If successfully resolved, the contents of the properties location is copied into the
      * <code>${project.build.directory}/checkstyle-checker.properties</code>
-     * file before being passed to checkstyle for loading.
-     * <p/>
-     * The contents of the <code>propertiesLocation</code> will be made
+     * file before being passed to checkstyle for loading.</p>
+     *
+     * <p>The contents of the <code>propertiesLocation</code> will be made
      * available to checkstyle for specifying values for parameters within
      * the xml configuration (specified in the <code>configLocation</code>
-     * parameter).
+     * parameter).</p>
      *
      * @parameter expression="${checkstyle.properties.location}"
      * @since 2.0-beta-2
@@ -265,12 +265,12 @@ public class CheckstyleReport
     private File useFile;
 
     /**
-     * Specifies the location of the suppressions XML file to use.
-     * <p/>
-     * This parameter is resolved as resource, URL, then file.
+     * <p>Specifies the location of the suppressions XML file to use.</p>
+     *
+     * <p>This parameter is resolved as resource, URL, then file.
      * If successfully resolved, the contents of the suppressions XML is copied into the
      * <code>${project.build.directory}/checkstyle-supressions.xml</code>
-     * file before being passed to checkstyle for loading.
+     * file before being passed to checkstyle for loading.</p>
      *
      * @parameter expression="${checkstyle.suppressions.location}"
      * @since 2.0-beta-2
@@ -312,18 +312,14 @@ public class CheckstyleReport
     private String outputFileFormat;
 
     /**
-     * <p/>
-     * Specifies the location of the package names XML to be used to configure
-     * the Checkstyle <a href="http://checkstyle.sourceforge.net/config.html#Packages">Packages</a>.
-     * </p>
-     * <p/>
-     * <p/>
-     * This parameter is resolved as resource, URL, then file.
+     * <p>Specifies the location of the package names XML to be used to configure
+     * the Checkstyle <a href="http://checkstyle.sourceforge.net/config.html#Packages">Packages</a>.</p>
+     *
+     * <p>This parameter is resolved as resource, URL, then file.
      * If resolved to a resource, or a URL, the contents of the package names
      * XML is copied into the
      * <code>${project.build.directory}/checkstyle-packagenames.xml</code>
-     * file before being passed to checkstyle for loading.
-     * </p>
+     * file before being passed to checkstyle for loading.</p>
      *
      * @parameter
      * @since 2.0-beta-2
