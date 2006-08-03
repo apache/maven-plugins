591a567e1a0115130ddd169587db56ca5816a52d
Allan Q. Ramirez <aramirez@apache.org>
2006-08-03 09:51:02 +0000

    PR: MSUREFIRE-147
    updated docs based on the comments from the dev list
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@428294 13f79535-47bb-0310-9956-ffa450edef68

--- maven-surefire-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefirePlugin.java
+++ maven-surefire-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefirePlugin.java
@@ -136,7 +136,7 @@ public class SurefirePlugin
     private File testSourceDirectory;
 
     /**
-     * Specify this parameter if you want to use the test pattern matching notation, Ant pattern matching, to select tests to run.
+     * Specify this parameter(can be a comma separated list) if you want to use the test pattern matching notation, Ant pattern matching, to select tests to run.
      * The Ant pattern will be used to create an include pattern formatted like <code>**&#47;${test}.java</code>
      * When used, the <code>includes</code> and <code>excludes</code> patterns parameters are ignored.
      *
@@ -224,8 +224,7 @@ public class SurefirePlugin
      * When forking, set this to true to redirect the unit test standard output to a file
      * (found in reportsDirectory/testName-output.txt).
      *
-     * @parameter expression="${maven.test.redirectTestOutputToFile}"
-     * @default-value="false"
+     * @parameter expression="${maven.test.redirectTestOutputToFile}" default-value="false"
      */
     private boolean redirectTestOutputToFile;
 
@@ -331,11 +330,15 @@ public class SurefirePlugin
     private boolean trimStackTrace;
 
     /**
+     * Resolves the artifacts needed.
+     *
      * @component
      */
     private ArtifactResolver artifactResolver;
 
     /**
+     * Creates the artifact
+     *
      * @component
      */
     private ArtifactFactory artifactFactory;
@@ -348,6 +351,8 @@ public class SurefirePlugin
     private List remoteRepositories;
 
     /**
+     * For retrieval of artifact's metadata.
+     *
      * @component
      */
     private ArtifactMetadataSource metadataSource;
