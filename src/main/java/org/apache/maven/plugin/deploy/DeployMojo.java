1b68ae8606507256ffa7c26d4e44d7b0eee34a52
John Dennis Casey <jdcasey@apache.org>
2006-08-21 21:58:39 +0000

    Adding alternative deployment repository parameter for the deploy:deploy mojo, and bumping the POM version to 2.3-SNAPSHOT to accommodate this new feature.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-deploy-plugin@433384 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/deploy/DeployMojo.java
+++ src/main/java/org/apache/maven/plugin/deploy/DeployMojo.java
@@ -20,16 +20,27 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.deployer.ArtifactDeploymentException;
 import org.apache.maven.artifact.metadata.ArtifactMetadata;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.artifact.ProjectArtifactMetadata;
+import org.codehaus.plexus.PlexusConstants;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.context.ContextException;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
 
 import java.io.File;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Deploys an artifact to remote repository.
- *
+ * 
  * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
  * @author <a href="mailto:jdcasey@apache.org">John Casey (refactoring only)</a>
  * @version $Id$
@@ -38,7 +49,10 @@ import java.util.List;
  */
 public class DeployMojo
     extends AbstractDeployMojo
+    implements Contextualizable
 {
+
+    private static final Pattern ALT_REPO_SYNTAX_PATTERN = Pattern.compile( "(.+)::(.+)::(.+)" );
 
     /**
      * @parameter expression="${project.artifact}"
@@ -68,6 +82,21 @@ public class DeployMojo
     private ArtifactRepository deploymentRepository;
 
     /**
+     * Specifies an alternative repository to which the project artifacts should be deployed ( other
+     * than those specified in &lt;distributionManagement&gt; ).
+     * <br/>
+     * Format: id::layout::url
+     * 
+     * @parameter expression="${altDeploymentRepository}"
+     */
+    private String altDeploymentRepository;
+    
+    /**
+     * @component
+     */
+    private ArtifactRepositoryFactory repoFactory;
+
+    /**
      * @parameter expression="${project.attachedArtifacts}
      * @required
      * @readonly
@@ -76,33 +105,33 @@ public class DeployMojo
 
     /**
      * Parameter used to update the metadata to make the artifact as release.
-     *
+     * 
      * @parameter expression="${updateReleaseInfo}" default-value="false"
      */
     private boolean updateReleaseInfo;
 
-    public void execute()
-        throws MojoExecutionException
-    {
-        if ( deploymentRepository == null )
-        {
-            String msg = "Deployment failed: repository element was not specified in the pom inside" +
-                " distributionManagement element";
-            throw new MojoExecutionException( msg );
-        }
-        
-        String protocol = deploymentRepository.getProtocol();
-        
-        if( protocol.equals( "scp" ) )
-        {
-                File sshFile = new File( System.getProperty( "user.home" ), ".ssh" );
+    /**
+     * Contextualized.
+     */
+    private PlexusContainer container;
 
-                if( !sshFile.exists() )
-                {
-                        sshFile.mkdirs();
-                }	
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        ArtifactRepository repo = getDeploymentRepository();
+
+        String protocol = repo.getProtocol();
+
+        if ( protocol.equals( "scp" ) )
+        {
+            File sshFile = new File( System.getProperty( "user.home" ), ".ssh" );
+
+            if ( !sshFile.exists() )
+            {
+                sshFile.mkdirs();
+            }
         }
-        
+
         // Deploy the POM
         boolean isPomArtifact = "pom".equals( packaging );
         if ( !isPomArtifact )
@@ -120,7 +149,7 @@ public class DeployMojo
         {
             if ( isPomArtifact )
             {
-                getDeployer().deploy( pomFile, artifact, deploymentRepository, getLocalRepository() );
+                getDeployer().deploy( pomFile, artifact, repo, getLocalRepository() );
             }
             else
             {
@@ -128,21 +157,78 @@ public class DeployMojo
                 if ( file == null )
                 {
                     throw new MojoExecutionException(
-                        "The packaging for this project did not assign a file to the build artifact" );
+                                                      "The packaging for this project did not assign a file to the build artifact" );
                 }
-                getDeployer().deploy( file, artifact, deploymentRepository, getLocalRepository() );
+                getDeployer().deploy( file, artifact, repo, getLocalRepository() );
             }
-            
+
             for ( Iterator i = attachedArtifacts.iterator(); i.hasNext(); )
-            {	
-                Artifact attached = (Artifact) i.next();
-                
-                getDeployer().deploy( attached.getFile(), attached, deploymentRepository, getLocalRepository() );
+            {
+                Artifact attached = ( Artifact ) i.next();
+
+                getDeployer().deploy( attached.getFile(), attached, repo, getLocalRepository() );
             }
         }
         catch ( ArtifactDeploymentException e )
         {
             throw new MojoExecutionException( e.getMessage(), e );
         }
+    }
+
+    private ArtifactRepository getDeploymentRepository()
+        throws MojoExecutionException, MojoFailureException
+    {
+        if ( deploymentRepository == null && altDeploymentRepository == null )
+        {
+            String msg =
+                "Deployment failed: repository element was not specified in the pom inside"
+                                + " distributionManagement element or in -DaltDeploymentRepository=id::layout::url parameter";
+
+            throw new MojoExecutionException( msg );
+        }
+
+        ArtifactRepository repo = null;
+
+        if ( altDeploymentRepository != null )
+        {
+            Matcher matcher = ALT_REPO_SYNTAX_PATTERN.matcher( altDeploymentRepository );
+
+            if ( !matcher.matches() )
+            {
+                throw new MojoFailureException( altDeploymentRepository, "Invalid syntax for repository.",
+                                                "Invalid syntax for alternative repository. Use \"id::layout::url\"." );
+            }
+            else
+            {
+                String id = matcher.group( 1 ).trim();
+                String layout = matcher.group( 2 ).trim();
+                String url = matcher.group( 3 ).trim();
+                
+                ArtifactRepositoryLayout repoLayout;
+                try
+                {
+                    repoLayout = ( ArtifactRepositoryLayout ) container.lookup( ArtifactRepositoryLayout.ROLE, layout );
+                }
+                catch ( ComponentLookupException e )
+                {
+                    throw new MojoExecutionException( "Cannot find repository layout: " + layout, e );
+                }
+                
+                repo = repoFactory.createArtifactRepository( id, url, repoLayout, null, null );
+            }
+        }
+        
+        if ( repo == null )
+        {
+            repo = deploymentRepository;
+        }
+
+        return repo;
+    }
+
+    public void contextualize( Context context )
+        throws ContextException
+    {
+        this.container = (PlexusContainer) context.get( PlexusConstants.PLEXUS_KEY );
     }
 }
