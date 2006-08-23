f9b7416ee2887c91fa36cb68e7ec44dcef7538eb
Brett Porter <brett@apache.org>
2006-08-23 03:25:10 +0000

    [MCLEAN-18] make source checkstyle compliant Submitted by: Franz Allan Valencia See
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-clean-plugin@433864 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/clean/Fileset.java
+++ src/main/java/org/apache/maven/plugin/clean/Fileset.java
@@ -18,10 +18,29 @@ package org.apache.maven.plugin.clean;
 
 import org.apache.maven.shared.model.fileset.FileSet;
 
+/**
+ * Customizes the string representation of 
+ * <code>org.apache.maven.shared.model.fileset.FileSet</code> to return the 
+ * included and excluded files from the file-set's directory. Specifically, 
+ * <code>"file-set: <I>[directory]</I> (included: <I>[included files]</I>, 
+ * excluded: <I>[excluded files]</I>)"</code>   
+ *  
+ * @see org.apache.maven.shared.model.fileset.FileSet
+ */
 public class Fileset
     extends FileSet
 {
 
+    /**
+     * Retrieves the included and excluded files from this file-set's directory.
+     * Specifically, <code>"file-set: <I>[directory]</I> (included: 
+     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>   
+     * 
+     * @return The included and excluded files from this file-set's directory.
+     * Specifically, <code>"file-set: <I>[directory]</I> (included: 
+     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>   
+     * @see java.lang.Object#toString()
+     */
     public String toString()
     {
         return "file-set: " + getDirectory() + " (included: " + getIncludes() + ", excluded: " + getExcludes() + ")";
