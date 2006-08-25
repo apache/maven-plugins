39091dcb301f95cb882ce1a4d75c9cc477255b33
Emmanuel Venisse <evenisse@apache.org>
2006-08-25 10:39:10 +0000

    Fix plexus component descriptor generation
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@436749 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/assembly/archive/archiver/SarUnArchiver.java
+++ src/main/java/org/apache/maven/plugin/assembly/archive/archiver/SarUnArchiver.java
@@ -3,7 +3,6 @@ package org.apache.maven.plugin.assembly.archive.archiver;
 import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
 
 /**
- * @plexus.component role="org.codehaus.plexus.archiver.UnArchiver" role-hint="sar"
  * @author jdcasey
  * @todo delete this class once the plexus maven plugin can merge a generated components.xml with an existing one.
  */
