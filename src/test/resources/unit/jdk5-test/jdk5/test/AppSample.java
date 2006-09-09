d934c929997a3d1e29842cf3c56418fb5dd2a680
Vincent Siveton <vsiveton@apache.org>
2006-09-09 16:06:58 +0000

    MJAVADOC-65: The javadoc plugin seems to ignore package-info.java
    o refactored to call "javadoc @options @packages" instead of "javadoc @options @files"
    o handle package names and unnamed package
    o handle @files or @argfile depending the jdk used (see javadoc Reference Guide)
    o added test case for jdk 5
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@441808 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/jdk5-test/jdk5/test/AppSample.java
@@ -0,0 +1,75 @@
+package jdk5.test;
+
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Sample class inside the package to be included in the javadoc
+ *
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ */
+public class AppSample
+{
+    /**
+     * Contains the file to be set
+     */
+    protected File file;
+
+    /**
+     * The main method
+     *
+     * @param args  an array of strings that contains the arguments
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Another Sample Application" );
+    }
+
+    /**
+     * Setter method for variable file
+     *
+     * @param file the value to be set
+     */
+    public void setFile( File file )
+    {
+        this.file = file;
+    }
+
+    /**
+     * Getter method for variable file
+     *
+     * @return a File object
+     */
+    public File getFile()
+    {
+        return file;
+    }
+
+    /**
+     * Create new file
+     *
+     * @throws java.io.IOException  thrown if an I/O error occurred during file creation
+     */
+    public void createFile()
+        throws IOException
+    {
+        File f = new File( file.getAbsolutePath() );
+        f.createNewFile();
+    }
+}
