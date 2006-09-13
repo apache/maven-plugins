825b78642fc5741e7d82478c26336baeef268901
Vincent Siveton <vsiveton@apache.org>
2006-09-13 13:28:25 +0000

    MANTLR-9: Adding Antlr report
    o refactoring to add report support
    o added test cases
    o updated documentation
    o updated dependencies
    - bumped dependencies (maven-plugin-api => 2.0.4)
    - added dependencies (maven-reporting-impl, maven-plugin-testing-harness)
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-antlr-plugin@442981 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/antlr/AntlrPlugin.java
+++ src/main/java/org/apache/maven/plugin/antlr/AntlrPlugin.java
@@ -16,16 +16,9 @@ package org.apache.maven.plugin.antlr;
  * limitations under the License.
  */
 
-import antlr.Tool;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.project.MavenProject;
+import java.util.List;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.security.Permission;
-import java.util.StringTokenizer;
+import org.apache.maven.plugin.MojoExecutionException;
 
 //----------------------------------------------------------------------
 // Don't remove this snippet
@@ -36,210 +29,31 @@ import java.util.StringTokenizer;
  *
  * @goal generate
  * @phase generate-sources
+ * @requiresDependencyResolution compile
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id: $
  */
 public class AntlrPlugin
-    extends AbstractMojo
+    extends AbstractAntlrMojo
 {
-    /**
-     * Comma separated grammar file names present in the <code>sourceDirectory</code> directory.
-     *
-     * @parameter expression="${grammars}"
-     * @required
-     */
-    private String grammars;
-
-    /**
-     * Specifies the Antlr directory containing grammar files.
-     *
-     * @parameter expression="${basedir}/src/main/antlr"
-     * @required
-     */
-    private File sourceDirectory;
-
-    /**
-     * Specifies the destination directory where Antlr should generate files.
-     *
-     * @parameter expression="${project.build.directory}/generated-sources/antlr"
-     * @required
-     */
-    private String outputDirectory;
-
-    /**
-     * The Maven Project Object
-     *
-     * @parameter expression="${project}"
-     * @required
-     */
-    private MavenProject project;
-
     /**
      * @see org.apache.maven.plugin.Mojo#execute()
      */
     public void execute()
         throws MojoExecutionException
     {
-        StringTokenizer st = new StringTokenizer( grammars, ", " );
-
-        while ( st.hasMoreTokens() )
-        {
-            String eachGrammar = st.nextToken().trim();
-
-            File grammar = new File( sourceDirectory, eachGrammar );
-
-            getLog().info( "grammar: " + grammar );
-
-            File generated = null;
-
-            try
-            {
-                generated = getGeneratedFile( grammar.getPath(), outputDirectory );
-            }
-            catch ( Exception e )
-            {
-                throw new MojoExecutionException( "Failed to get generated file", e );
-            }
-
-            if ( generated.exists() )
-            {
-                if ( generated.lastModified() > grammar.lastModified() )
-                {
-                    // it's more recent, skip.
-                    getLog().info( "The grammar is already generated" );
-                    continue;
-                }
-            }
-
-            if ( !generated.getParentFile().exists() )
-            {
-                generated.getParentFile().mkdirs();
-            }
-
-            // ----------------------------------------------------------------------
-            // @see http://www.antlr.org/doc/options.html#Command%20Line%20Options
-            // ----------------------------------------------------------------------
-
-            String[] args = new String[]{"-o", generated.getParentFile().getPath(), grammar.getPath(),};
-
-            SecurityManager oldSm = System.getSecurityManager();
-
-            System.setSecurityManager( NoExitSecurityManager.INSTANCE );
-
-            try
-            {
-                Tool.main( args );
-            }
-            catch ( SecurityException e )
-            {
-                if ( !e.getMessage().equals( "exitVM-0" ) )
-                {
-                    throw new MojoExecutionException( "Execution failed", e );
-                }
-            }
-            finally
-            {
-                System.setSecurityManager( oldSm );
-            }
-        }
-
-        if ( project != null )
-        {
-            project.addCompileSourceRoot( outputDirectory );
-        }
+        executeAntlr();
     }
-
-    // END SNIPPET: generate-sources-0
-    //  ----------------------------------------------------------------------
-    //   Don't remove this snippet
-    //  ----------------------------------------------------------------------
+// END SNIPPET: generate-sources-0
+//----------------------------------------------------------------------
+// Don't remove this snippet
+//----------------------------------------------------------------------
 
     /**
-     * @param grammar
-     * @param outputDirectory
-     * @return generated file
-     * @throws Exception
+     * @see org.apache.maven.plugin.antlr.AbstractAntlrMojo#addArgs(java.util.List)
      */
-    protected File getGeneratedFile( String grammar, String outputDirectory )
-        throws Exception
+    protected void addArgs( List arguments )
     {
-        String generatedFileName = null;
-
-        String packageName = "";
-
-        try
-        {
-            BufferedReader in = new BufferedReader( new FileReader( grammar ) );
-
-            String line;
-
-            while ( ( line = in.readLine() ) != null )
-            {
-                line = line.trim();
-
-                int extendsIndex = line.indexOf( " extends " );
-
-                if ( line.startsWith( "class " ) && extendsIndex > -1 )
-                {
-                    generatedFileName = line.substring( 6, extendsIndex ).trim();
-
-                    break;
-                }
-                else if ( line.startsWith( "package" ) )
-                {
-                    packageName = line.substring( 8 ).trim();
-                }
-            }
-
-            in.close();
-        }
-        catch ( Exception e )
-        {
-            throw new Exception( "Unable to determine generated class", e );
-        }
-        if ( generatedFileName == null )
-        {
-            return null;
-        }
-
-        File genFile = null;
-
-        if ( "".equals( packageName ) )
-        {
-            genFile = new File( outputDirectory, generatedFileName + ".java" );
-        }
-        else
-        {
-            String packagePath = packageName.replace( '.', File.separatorChar );
-
-            packagePath = packagePath.replace( ';', File.separatorChar );
-
-            genFile = new File( new File( outputDirectory, packagePath ), generatedFileName + ".java" );
-        }
-
-        return genFile;
-    }
-}
-
-class NoExitSecurityManager
-    extends SecurityManager
-{
-    static final NoExitSecurityManager INSTANCE = new NoExitSecurityManager();
-
-    private NoExitSecurityManager()
-    {
-    }
-
-    /**
-     * @see java.lang.SecurityManager#checkPermission(java.security.Permission)
-     */
-    public void checkPermission( Permission permission )
-    {
-    }
-
-    /**
-     * @see java.lang.SecurityManager#checkExit(int)
-     */
-    public void checkExit( int status )
-    {
-        throw new SecurityException( "exitVM-" + status );
+        // nop
     }
 }
