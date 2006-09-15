c9640c7d67e0212649033be103ffa58f4aff06f5
Brian E Fox <brianf@apache.org>
2006-09-15 02:59:27 +0000

    more integration tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-dependency-plugin@446487 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/dependency/utils/ArtifactStubFactory.java
@@ -0,0 +1,114 @@
+package org.apache.maven.plugin.dependency.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.codehaus.plexus.util.StringUtils;
+
+public class ArtifactStubFactory
+{
+    File workingDir;
+
+    boolean createFiles;
+
+    public ArtifactStubFactory( File workingDir, boolean createFiles )
+    {
+        this.workingDir = workingDir;
+        this.createFiles = createFiles;
+    }
+
+    public Artifact createArtifact( String groupId, String artifactId, String version )
+    {
+        return createArtifact( groupId, artifactId, version, Artifact.SCOPE_COMPILE, "jar", "" );
+    }
+
+    public Artifact createArtifact( String groupId, String artifactId, String version, String scope )
+    {
+        return createArtifact( groupId, artifactId, version, scope, "jar", "" );
+    }
+    
+    public Artifact createArtifact( String groupId, String artifactId, String version, String scope, String type,
+                                   String classifier )
+    {
+        VersionRange vr = VersionRange.createFromVersion( version );
+        return createArtifact( groupId, artifactId, vr, scope, type, classifier, false );
+    }
+
+    public Artifact createArtifact( String groupId, String artifactId, VersionRange versionRange, String scope,
+                                   String type, String classifier, boolean optional )
+    {
+        ArtifactHandler ah = new DefaultArtifactHandler();
+        Artifact artifact = new DefaultArtifact( groupId, artifactId, versionRange, scope, type, classifier, ah,
+                                                 optional );
+        if ( createFiles )
+        {
+            setArtifactFile( artifact );
+        }
+        return artifact;
+    }
+
+    void setArtifactFile( Artifact artifact )
+    {
+        String fileName = DependencyUtil.getFormattedFileName( artifact, false );
+
+        File theFile = new File( this.workingDir, fileName );
+        theFile.getParentFile().mkdirs();
+        try
+        {
+            theFile.createNewFile();
+        }
+        catch ( IOException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        artifact.setFile( theFile );
+    }
+
+    public Artifact getReleaseArtifact()
+    {
+        return createArtifact( "groupId", "release", "1.0" );
+    }
+
+    public Artifact getSnapshotArtifact()
+    {
+        return createArtifact( "groupId", "snapshot", "2.0-SNAPSHOT" );
+    }
+
+    public Set getReleaseAndSnapshotArtifacts()
+    {
+        Set set = new HashSet();
+        set.add( getReleaseArtifact() );
+        set.add( getSnapshotArtifact() );
+        return set;
+    }
+    
+    public Set getScopedArtifacts()
+    {
+     Set set = new HashSet();
+     set.add(createArtifact("g","compile","1.0",Artifact.SCOPE_COMPILE));
+     set.add(createArtifact("g","provided","1.0",Artifact.SCOPE_PROVIDED));
+     set.add(createArtifact("g","test","1.0",Artifact.SCOPE_TEST));
+     set.add(createArtifact("g","runtime","1.0",Artifact.SCOPE_RUNTIME));
+     set.add(createArtifact("g","system","1.0",Artifact.SCOPE_SYSTEM));
+     return set;
+    }
+    
+    public Set getTypedArtifacts()
+    {
+     Set set = new HashSet();
+     set.add(createArtifact("g","a","1.0",Artifact.SCOPE_COMPILE,"war",null));
+     set.add(createArtifact("g","a","1.0",Artifact.SCOPE_COMPILE,"jar",null));
+     set.add(createArtifact("g","a","1.0",Artifact.SCOPE_COMPILE,"sources",null));
+     set.add(createArtifact("g","a","1.0",Artifact.SCOPE_COMPILE,"zip",null));
+     return set;
+    }
+}
