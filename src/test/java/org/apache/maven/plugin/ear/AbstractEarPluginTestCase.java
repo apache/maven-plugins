caaeba053355b66edc6d414ee94d50006fed04a4
Stephane Nicoll <snicoll@apache.org>
2006-09-23 13:51:17 +0000

    Adding test cases with the maven embedder.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-ear-plugin@449242 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/ear/AbstractEarPluginTestCase.java
@@ -0,0 +1,179 @@
+package org.apache.maven.plugin.ear;
+
+import org.apache.maven.cli.ConsoleDownloadMonitor;
+import org.apache.maven.embedder.MavenEmbedder;
+import org.apache.maven.embedder.MavenEmbedderConsoleLogger;
+import org.apache.maven.embedder.PlexusLoggerAdapter;
+import org.apache.maven.monitor.event.DefaultEventMonitor;
+import org.apache.maven.monitor.event.EventMonitor;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.PlexusTestCase;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Stephane Nicoll <stephane.nicoll@bsb.com>
+ * @author $Author: sni $ (last edit)
+ * @version $Revision: 1.5 $
+ */
+public abstract class AbstractEarPluginTestCase
+    extends PlexusTestCase
+{
+
+    protected final String FINAL_NAME_PREFIX = "maven-ear-plugin-test-";
+
+    protected final String FINAL_NAME_SUFFIX = "-99.0";
+
+    /**
+     * The embedder.
+     */
+    protected MavenEmbedder maven;
+
+    /**
+     * Test repository directory.
+     */
+    protected File localRepositoryDir = getTestFile( "target/test-classes/m2repo" );
+
+    /**
+     * @see org.codehaus.plexus.PlexusTestCase#setUp()
+     */
+    protected void setUp()
+        throws Exception
+    {
+        this.maven = new MavenEmbedder();
+        this.maven.setClassLoader( Thread.currentThread().getContextClassLoader() );
+        this.maven.setLogger( new MavenEmbedderConsoleLogger() );
+        this.maven.setLocalRepositoryDirectory( localRepositoryDir );
+        this.maven.setOffline( true );
+        this.maven.start();
+
+        super.setUp();
+    }
+
+    /**
+     * @see org.codehaus.plexus.PlexusTestCase#tearDown()
+     */
+    protected void tearDown()
+        throws Exception
+    {
+        maven.stop();
+        super.tearDown();
+    }
+
+    /**
+     * Execute the EAR plugin for the specified project.
+     *
+     * @param projectName the name of the project
+     * @param properties  extra properties to be used by the embedder
+     * @return the base directory of the project
+     * @throws Exception if an error occured
+     */
+    protected File executeMojo( final String projectName, final Properties properties )
+        throws Exception
+    {
+        File basedir = getTestFile( "target/test-classes/projects/" + projectName );
+
+        MavenProject project = maven.readProjectWithDependencies( new File( basedir, "pom.xml" ) );
+
+        EventMonitor eventMonitor =
+            new DefaultEventMonitor( new PlexusLoggerAdapter( new MavenEmbedderConsoleLogger() ) );
+
+        this.maven.execute( project, Arrays.asList( new String[]{"org.apache.maven.plugins:maven-clean-plugin:clean",
+            "org.apache.maven.plugins:maven-ear-plugin:generate-application-xml",
+            "org.apache.maven.plugins:maven-ear-plugin:ear"} ), eventMonitor, new ConsoleDownloadMonitor(), properties,
+                                                                basedir );
+
+        assertEarArchive( basedir, projectName );
+        assertEarDirectory( basedir, projectName );
+
+        return basedir;
+    }
+
+    protected void assertEarArchive( final File baseDir, final String projectName )
+    {
+        assertTrue( "EAR archive does not exist", getEarArchive( baseDir, projectName ).exists() );
+    }
+
+    protected void assertEarDirectory( final File baseDir, final String projectName )
+    {
+        assertTrue( "EAR archive directory does not exist", getEarDirectory( baseDir, projectName ).exists() );
+    }
+
+    protected File getTargetDirectory( final File basedir )
+    {
+        return new File( basedir, "target" );
+    }
+
+    protected File getEarArchive( final File baseDir, final String projectName )
+    {
+        return new File( getTargetDirectory( baseDir ), buildFinalName( projectName ) + ".ear" );
+    }
+
+    protected File getEarDirectory( final File baseDir, final String projectName )
+    {
+        return new File( getTargetDirectory( baseDir ), buildFinalName( projectName ) );
+    }
+
+    protected String buildFinalName( final String projectName )
+    {
+        return FINAL_NAME_PREFIX + projectName + FINAL_NAME_SUFFIX;
+    }
+
+    protected void assertArchiveContent( final File baseDir, final String projectName, final String[] artifactNames )
+    {
+        File dir = getEarDirectory( baseDir, projectName );
+        final List actualFiles = buildFiles( dir );
+        assertEquals( "Artifacts mismatch " + actualFiles, artifactNames.length, actualFiles.size() );
+        for ( int i = 0; i < artifactNames.length; i++ )
+        {
+            String artifactName = artifactNames[i];
+            File expectedFile = new File(dir, artifactName);
+            assertTrue("Artifact["+artifactName+"] not found in ear archive", actualFiles.contains( expectedFile));            
+        }
+    }
+
+    protected List buildFiles( final File baseDir )
+    {
+        final List result = new ArrayList();
+        addFiles( baseDir, result );
+
+        return result;
+    }
+
+    private void addFiles( final File directory, final List files )
+    {
+        File[] result = directory.listFiles( new FilenameFilter()
+        {
+            public boolean accept( File dir, String name )
+            {
+                if ( name.equals( "META-INF" ) )
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+        } );
+
+        for ( int i = 0; i < result.length; i++ )
+        {
+            File file = result[i];
+            if ( file.isFile() )
+            {
+                files.add( file );
+            }
+            else
+            {
+                addFiles( file, files );
+            }
+        }
+    }
+}
