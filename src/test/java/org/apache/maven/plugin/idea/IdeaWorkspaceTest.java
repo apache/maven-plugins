3b1d35aec233845d9040f2dfbdfe7c430c34e28f
Dennis Lundberg <dennisl@apache.org>
2006-09-27 22:11:39 +0000

    [MIDEA-66] CVS support wrongly configured in IWS files due to upper/lowercase mismatch.
    o Add translations from SCM type in Maven SCM connection URLs to IDEA VCS name format, for all 5 version control systems supported by IDEA 5.1.
    o Add test cases.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-idea-plugin@450599 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/idea/IdeaWorkspaceTest.java
+++ src/test/java/org/apache/maven/plugin/idea/IdeaWorkspaceTest.java
@@ -33,6 +33,30 @@ public class IdeaWorkspaceTest
         executeMojo( "src/test/workspace-plugin-configs/min-plugin-config.xml" );
     }
 
+    public void testCvsScmConnectionConfig()
+        throws Exception
+    {
+        Document iwsDocument = executeMojo( "src/test/workspace-plugin-configs/cvs-connection-plugin-config.xml" );
+
+        Element component = findComponent( iwsDocument.getRootElement(), "VcsManagerConfiguration" );
+
+        Element element = findElementByNameAttribute( component, "option", "ACTIVE_VCS_NAME" );
+
+        assertEquals( "Test scm type from scm connection", "CVS", element.attributeValue( "value" ) );
+    }
+
+    public void testPerforceScmConnectionConfig()
+        throws Exception
+    {
+        Document iwsDocument = executeMojo( "src/test/workspace-plugin-configs/perforce-connection-plugin-config.xml" );
+
+        Element component = findComponent( iwsDocument.getRootElement(), "VcsManagerConfiguration" );
+
+        Element element = findElementByNameAttribute( component, "option", "ACTIVE_VCS_NAME" );
+
+        assertEquals( "Test scm type from scm connection", "Perforce", element.attributeValue( "value" ) );
+    }
+
     public void testScmConnectionConfig()
         throws Exception
     {
@@ -67,6 +91,42 @@ public class IdeaWorkspaceTest
         Element element = findElementByNameAttribute( component, "option", "ACTIVE_VCS_NAME" );
 
         assertEquals( "Test scm type from scm connection", "type", element.attributeValue( "value" ) );
+    }
+
+    public void testStarTeamScmConnectionConfig()
+        throws Exception
+    {
+        Document iwsDocument = executeMojo( "src/test/workspace-plugin-configs/starteam-connection-plugin-config.xml" );
+
+        Element component = findComponent( iwsDocument.getRootElement(), "VcsManagerConfiguration" );
+
+        Element element = findElementByNameAttribute( component, "option", "ACTIVE_VCS_NAME" );
+
+        assertEquals( "Test scm type from scm connection", "StarTeam", element.attributeValue( "value" ) );
+    }
+
+    public void testSvnScmConnectionConfig()
+        throws Exception
+    {
+        Document iwsDocument = executeMojo( "src/test/workspace-plugin-configs/svn-connection-plugin-config.xml" );
+
+        Element component = findComponent( iwsDocument.getRootElement(), "VcsManagerConfiguration" );
+
+        Element element = findElementByNameAttribute( component, "option", "ACTIVE_VCS_NAME" );
+
+        assertEquals( "Test scm type from scm connection", "svn", element.attributeValue( "value" ) );
+    }
+
+    public void testVssScmConnectionConfig()
+        throws Exception
+    {
+        Document iwsDocument = executeMojo( "src/test/workspace-plugin-configs/vss-connection-plugin-config.xml" );
+
+        Element component = findComponent( iwsDocument.getRootElement(), "VcsManagerConfiguration" );
+
+        Element element = findElementByNameAttribute( component, "option", "ACTIVE_VCS_NAME" );
+
+        assertEquals( "Test scm type from scm connection", "SourceSafe", element.attributeValue( "value" ) );
     }
 
     private Document executeMojo( String pluginXml )
