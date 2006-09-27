f961de8a900cdbf5c1d10b898fe581e4d8263a5a
Vincent Siveton <vsiveton@apache.org>
2006-09-27 11:45:10 +0000

    MJAVADOC-92: Cant find javadoc.exe on windows in specific cases
    o Updated AbstractJavadocMojo#getJavadocPath() to read "java.home" and JAVA_HOME env variable
    o Improved exception messages
    o Updated/added test cases
    o Updated prerequisites to 2.0.1 
    o Updated dependencies and using plexus-utils:1.2
    o Tested install with Maven 2.0.1 and above
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@450405 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-javadoc-plugin/src/test/resources/unit/javaHome-test/def/configuration/AppSample.java
@@ -0,0 +1,20 @@
+package def.configuration;
+
+/**
+ * @author Maria Odea Ching
+ * @since 1.4
+ * @version %I%, %G%
+ */
+public class AppSample
+{
+
+    /**
+     * The main method
+     *
+     * @param args  an array of strings that contains the arguments
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Another Sample Application" );
+    }
+}
