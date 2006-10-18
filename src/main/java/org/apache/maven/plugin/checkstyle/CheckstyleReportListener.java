1d88be773b7ddd4a0a4c4ae28e982ea34440df09
Vincent Siveton <vsiveton@apache.org>
2006-10-18 13:32:57 +0000

    MCHECKSTYLE-47: Allow running checkstyle in test sources
    o handle testSourceDirectory
    o added test case
    o updated javadoc and documentation
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@465247 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/checkstyle/CheckstyleReportListener.java
+++ src/main/java/org/apache/maven/plugin/checkstyle/CheckstyleReportListener.java
@@ -23,17 +23,22 @@ import com.puppycrawl.tools.checkstyle.api.SeverityLevel;
 import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
+ * Listener in charge of receiving events from the Checker.
+ *
  * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
  * @version $Id$
  */
 public class CheckstyleReportListener
     extends AutomaticBean
     implements AuditListener
 {
-    private File sourceDirectory;
+    private List sourceDirectories;
 
     private CheckstyleResults results;
 
@@ -43,45 +48,88 @@ public class CheckstyleReportListener
 
     private SeverityLevel severityLevel;
 
+    /**
+     * @param sourceDirectory assume that is <code>sourceDirectory</code> is a not null directory and exists
+     */
     public CheckstyleReportListener( File sourceDirectory )
     {
-        this.sourceDirectory = sourceDirectory;
+        this.sourceDirectories = new ArrayList();
+        this.sourceDirectories.add( sourceDirectory );
     }
 
+    /**
+     * @param sourceDirectory assume that is <code>sourceDirectory</code> is a not null directory and exists
+     */
+    public void addSourceDirectory( File sourceDirectory )
+    {
+        this.sourceDirectories.add( sourceDirectory );
+    }
+
+    /**
+     * @param severityLevel
+     */
     public void setSeverityLevelFilter( SeverityLevel severityLevel )
     {
         this.severityLevel = severityLevel;
     }
 
+    /**
+     * @return
+     */
     public SeverityLevel getSeverityLevelFilter()
     {
         return severityLevel;
     }
 
+    /**
+     * @see com.puppycrawl.tools.checkstyle.api.AuditListener#auditStarted(com.puppycrawl.tools.checkstyle.api.AuditEvent)
+     */
     public void auditStarted( AuditEvent event )
     {
         setResults( new CheckstyleResults() );
     }
 
+    /**
+     * @see com.puppycrawl.tools.checkstyle.api.AuditListener#auditFinished(com.puppycrawl.tools.checkstyle.api.AuditEvent)
+     */
     public void auditFinished( AuditEvent event )
     {
         //do nothing
     }
 
+    /**
+     * @see com.puppycrawl.tools.checkstyle.api.AuditListener#fileStarted(com.puppycrawl.tools.checkstyle.api.AuditEvent)
+     */
     public void fileStarted( AuditEvent event )
     {
-        currentFile = StringUtils.substring( event.getFileName(), sourceDirectory.getPath().length() + 1 );
-        currentFile = StringUtils.replace( currentFile, "\\", "/" );
+        for (Iterator it = sourceDirectories.iterator(); it.hasNext(); )
+        {
+            File sourceDirectory = (File)it.next();
 
-        events = getResults().getFileViolations( currentFile );
+            currentFile = StringUtils.substring( event.getFileName(), sourceDirectory.getPath().length() + 1 );
+            currentFile = StringUtils.replace( currentFile, "\\", "/" );
+
+            events = getResults().getFileViolations( currentFile );
+        }
+
+        if ( events == null )
+        {
+            events = new ArrayList();
+        }
     }
 
+    /**
+     * @see com.puppycrawl.tools.checkstyle.api.AuditListener#fileFinished(com.puppycrawl.tools.checkstyle.api.AuditEvent)
+     */
     public void fileFinished( AuditEvent event )
     {
         getResults().setFileViolations( currentFile, events );
         currentFile = null;
     }
 
+    /**
+     * @see com.puppycrawl.tools.checkstyle.api.AuditListener#addError(com.puppycrawl.tools.checkstyle.api.AuditEvent)
+     */
     public void addError( AuditEvent event )
     {
         if ( SeverityLevel.IGNORE.equals( event.getSeverityLevel() ) )
@@ -95,16 +143,25 @@ public class CheckstyleReportListener
         }
     }
 
+    /**
+     * @see com.puppycrawl.tools.checkstyle.api.AuditListener#addException(com.puppycrawl.tools.checkstyle.api.AuditEvent, java.lang.Throwable)
+     */
     public void addException( AuditEvent event, Throwable throwable )
     {
         //Do Nothing
     }
 
+    /**
+     * @return
+     */
     public CheckstyleResults getResults()
     {
         return results;
     }
 
+    /**
+     * @param results
+     */
     public void setResults( CheckstyleResults results )
     {
         this.results = results;
