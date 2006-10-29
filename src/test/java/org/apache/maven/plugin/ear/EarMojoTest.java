25f6402fde353b387fde4ceea9558f7e8eadd8aa
Stephane Nicoll <snicoll@apache.org>
2006-10-29 10:54:04 +0000

    - Cleaned dependencies - Added a tests page with the list of the test - Fake artifacts are now valid zip files - Updated test structure
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-ear-plugin@468875 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/ear/EarMojoTest.java
+++ src/test/java/org/apache/maven/plugin/ear/EarMojoTest.java
@@ -19,48 +19,86 @@ package org.apache.maven.plugin.ear;
 /**
  *
  * @author <a href="snicoll@apache.org">Stephane Nicoll</a>
- * @version $Id: $
+ * @version $Id$
  */
 public class EarMojoTest
     extends AbstractEarPluginTestCase
 {
 
     /**
-     * Simple project with an EJB artifact
+     * Builds an EAR with a single EJB and no configuration.
      */
-    public void testProject01()
+    public void testProject001()
         throws Exception
     {
-        doTestProject( "project-01", new String[]{"ejb-sample-one-1.0.jar"} );
+        doTestProject( "project-001", new String[]{"ejb-sample-one-1.0.jar"} );
     }
 
     /**
-     * Simple project with a bundleDir and a bundleFileName settings.
+     * Builds an EAR with a customized artifact location and a customized artifact name.
      */
-    public void testProject02()
+    public void testProject002()
         throws Exception
     {
-        doTestProject( "project-02", new String[]{"APP-INF/lib/ejb-sample-one-1.0.jar", "ejb-sample-two.jar"} );
+        doTestProject( "project-002", new String[]{"APP-INF/lib/ejb-sample-one-1.0.jar", "ejb-sample-two.jar"} );
     }
 
     /**
-     * Test the classifier functionnality
+     * Builds an EAR with a defalt bundle directory for <tt>java</tt> modules.
      */
-    public void testProject03()
+    public void testProject003()
         throws Exception
     {
-        doTestProject( "project-03", new String[]{"ejb-sample-one-1.0-classified.jar"} );
+        doTestProject( "project-003", new String[]{"APP-INF/lib/jar-sample-one-1.0.jar", "APP-INF/lib/jar-sample-two-1.0.jar"} );
     }
 
     /**
-     * Test the unpackTypes functionnality
+     * Builds an EAR with a defalt bundle directory for _java_ modules and a custom
+     * location overriding the default.
      */
+    public void testProject004()
+        throws Exception
+    {
+        doTestProject( "project-004", new String[]{"jar-sample-one-1.0.jar", "APP-INF/lib/jar-sample-two-1.0.jar"} );
+    }
+
+    /**
+     * Builds an EAR with a custom URI.
+     */
+    public void testProject005()
+        throws Exception
+    {
+        doTestProject( "project-005", new String[]{"libs/another-name.jar"} );
+    }
+
+    /**
+     * Builds an EAR with an excluded module.
+     */
+    public void testProject006()
+        throws Exception
+    {
+        doTestProject( "project-006", new String[]{"jar-sample-two-1.0.jar"} );
+    }
+
+    /**
+     * Builds an EAR with a classified artifact and no extra configuration.
+     */
+    public void testProject007()
+        throws Exception
+    {
+        doTestProject( "project-007", new String[]{"ejb-sample-one-1.0-classified.jar"} );
+    }
+
+
+
+
+    /* Need the embedder for this one since it's new code
     public void testProject04()
         throws Exception
     {
-        //doTestProject( "project-04", new String[]{"ejb-sample-one-1.0.jar", "sar-sample-one-1.0.sar"},
-        //               new boolean[]{false, true} );
-    }
+        doTestProject( "project-04", new String[]{"ejb-sample-one-1.0.jar", "sar-sample-one-1.0.sar"},
+                       new boolean[]{false, true} );
+    } */
 
 
 }
