ac6d3da52de3bb129c6f31588193737440d8df69
Stephane Nicoll <snicoll@apache.org>
2006-11-01 17:34:56 +0000

    More tests.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@470004 13f79535-47bb-0310-9956-ffa450edef68

--- maven-ear-plugin/src/test/java/org/apache/maven/plugin/ear/AbstractEarPluginTestCase.java
+++ maven-ear-plugin/src/test/java/org/apache/maven/plugin/ear/AbstractEarPluginTestCase.java
@@ -19,12 +19,16 @@ package org.apache.maven.plugin.ear;
 import junit.framework.TestCase;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
+import org.apache.maven.archiver.MavenArchiveConfiguration;
 import org.custommonkey.xmlunit.XMLAssert;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.archiver.UnArchiver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -54,7 +58,7 @@ public abstract class AbstractEarPluginTestCase
      */
     protected File localRepositoryDir = new File( getBasedir().getAbsolutePath(), "target/test-classes/m2repo" );
 
-    protected File settingsFile = new File( getBasedir().getAbsolutePath(), "target/test-classes/settings.ml" );
+    protected File settingsFile = new File( getBasedir().getAbsolutePath(), "target/test-classes/settings.xml" );
 
 
     /**
@@ -69,8 +73,9 @@ public abstract class AbstractEarPluginTestCase
         throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/projects/" + projectName );
-        // Specifying the settings here does not seem to work.
-        Verifier verifier = new Verifier( testDir.getAbsolutePath(), settingsFile.getAbsolutePath() );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath());
+        // Let's add alternate settings.xml setting so that the latest dependencies are used
+        verifier.getCliOptions().add("-s " + settingsFile.getAbsolutePath());
         verifier.localRepo = localRepositoryDir.getAbsolutePath();
         verifier.executeGoal( "package" );
         // If no error is expected make sure that error logs are free
@@ -79,9 +84,6 @@ public abstract class AbstractEarPluginTestCase
             verifier.verifyErrorFreeLog();
         }
         verifier.resetStreams();
-        assertEarArchive( testDir, projectName );
-        assertEarDirectory( testDir, projectName );
-
         return testDir;
     }
 
@@ -115,6 +117,8 @@ public abstract class AbstractEarPluginTestCase
         throws Exception
     {
         final File baseDir = executeMojo( projectName, new Properties() );
+        assertEarArchive( baseDir, projectName );
+        assertEarDirectory( baseDir, projectName );
 
         assertArchiveContent( baseDir, projectName, expectedArtifacts, artifactsDirectory );
 
