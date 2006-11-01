7ebd6125b95031e3e32e39bd8f48431e9e1dd402
Stephane Nicoll <snicoll@apache.org>
2006-11-01 17:34:56 +0000

    More tests.
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@470004 13f79535-47bb-0310-9956-ffa450edef68

--- maven-ear-plugin/src/test/java/org/apache/maven/plugin/ear/EarMojoTest.java
+++ maven-ear-plugin/src/test/java/org/apache/maven/plugin/ear/EarMojoTest.java
@@ -1,5 +1,10 @@
 package org.apache.maven.plugin.ear;
 
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.util.Properties;
+
 /*
  * Copyright 2001-2005 The Apache Software Foundation.
  *
@@ -124,16 +129,111 @@ public class EarMojoTest
         throws Exception
     {
         doTestProject( "project-011", new String[]{"ejb-sample-one-1.0.jar"} );
-    }   
+    }
 
+    /**
+     * Builds an EAR and make sure that EAR resources are bundled within the EAR.
+     */
+    public void testProject012()
+        throws Exception
+    {
+        doTestProject( "project-012", new String[]{"README.txt", "LICENSE.txt", "ejb-sample-one-1.0.jar"} );
+    }
 
-    /* Need the embedder for this one since it's new code
-   public void testProject04()
-       throws Exception
-   {
-       doTestProject( "project-04", new String[]{"ejb-sample-one-1.0.jar", "sar-sample-one-1.0.sar"},
-                      new boolean[]{false, true} );
-   } */
+    /**
+     * Builds an EAR and make sure that EAR resources in a customized resources directory are bundled within the EAR.
+     */
+    public void testProject013()
+        throws Exception
+    {
+        doTestProject( "project-013", new String[]{"README.txt", "LICENSE.txt", "ejb-sample-one-1.0.jar"} );
+    }
 
+    /**
+     * Builds an EAR and make sure that EAR resources are bundled within the EAR using includes and excludes.
+     */
+    public void testProject014()
+        throws Exception
+    {
+        doTestProject( "project-014", new String[]{"LICENSE.txt", "ejb-sample-one-1.0.jar"} );
+    }
+
+    /**
+     * Builds an EAR and make sure that default manifest is taken into account.
+     */
+    public void testProject015()
+        throws Exception
+    {
+        final File baseDir = doTestProject( "project-015", new String[]{"ejb-sample-one-1.0.jar"} );
+        final File expectedManifest = new File( baseDir, "src/main/application/META-INF/MANIFEST.MF" );
+        final File actualManifest = new File( getEarDirectory( baseDir, "project-015" ), "META-INF/MANIFEST.MF" );
+        assertTrue( "Manifest was not copied", actualManifest.exists() );
+        assertTrue( FileUtils.contentEquals( expectedManifest, actualManifest ) );
+    }
+
+    /**
+     * Builds an EAR and make sure that custom manifest is taken into account.
+     */
+    public void testProject016()
+        throws Exception
+    {
+        System.out.println( "Skipped project-016: need a way to extract the EAR archive" );
+        /*
+        final File baseDir = doTestProject( "project-016", new String[]{"ejb-sample-one-1.0.jar"} );
+        final File expectedManifest = new File(baseDir, "src/main/ear/META-INF/MANIFEST.MF");
+        // TODO: needs a way to extract the EAR archive
+        */
+    }
+
+    /**
+     * Builds an EAR and make sure that custom application.xml is taken into account.
+     */
+    public void testProject017()
+        throws Exception
+    {
+        doTestProject( "project-017", new String[]{"ejb-sample-one-1.0.jar"} );
+    }
+
+    /**
+     * Builds an EAR with a custom final name.
+     */
+    public void testProject018()
+        throws Exception
+    {
+        final File baseDir = executeMojo( "project-018", new Properties() );
+        final File expectedFile = new File( baseDir, "target/my-custom-file.ear" );
+        assertTrue( "EAR archive not found", expectedFile.exists() );
+    }
+
+    /**
+     * Builds an EAR with unpacked archives using the unpackTypes.
+     */
+    public void testProject019()
+        throws Exception
+    {
+        doTestProject( "project-019", new String[]{"ejb-sample-one-1.0.jar", "sar-sample-one-1.0.sar", "jar-sample-one-1.0.jar"},
+                       new boolean[]{false, true, true} );
+    }
+
+    /**
+     * Builds an EAR with unpacked archives using the unpack module attribute.
+     */
+    public void testProject020()
+        throws Exception
+    {
+        doTestProject( "project-020", new String[]{"ejb-sample-one-1.0.jar", "sar-sample-one-1.0.sar", "jar-sample-one-1.0.jar"},
+                       new boolean[]{true, false, false} );
+    }
+
+    /**
+     * Builds an EAR with unpacked archives using both unpackTypes and the unpack module attribute.
+     */
+    public void testProject021()
+        throws Exception
+    {
+        doTestProject( "project-021", new String[]{"ejb-sample-one-1.0.jar", "ejb-sample-two-1.0.jar",
+            "sar-sample-one-1.0.sar", "jar-sample-one-1.0.jar", "jar-sample-two-1.0.jar"},
+                       new boolean[]{false, true, false, false, true} );
+    }
 
 }
