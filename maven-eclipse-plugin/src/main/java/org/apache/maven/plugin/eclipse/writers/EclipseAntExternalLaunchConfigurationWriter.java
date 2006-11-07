7559c2aa1e95ef590d7c9c23cde05a62dd4e0c99
Kenney Westerhof <kenney@apache.org>
2006-11-07 17:09:41 +0000

    Cleanup; add license; add portions of patch from MECLIPSE-139 submitted by Jochen Kuhnle
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@472172 13f79535-47bb-0310-9956-ffa450edef68

--- maven-eclipse-plugin/src/main/java/org/apache/maven/plugin/eclipse/writers/EclipseAntExternalLaunchConfigurationWriter.java
+++ maven-eclipse-plugin/src/main/java/org/apache/maven/plugin/eclipse/writers/EclipseAntExternalLaunchConfigurationWriter.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.apache.maven.plugin.eclipse.writers;
 
 import java.util.List;
@@ -16,16 +28,6 @@ public class EclipseAntExternalLaunchConfigurationWriter
 {
     private String buildfilePath;
 
-    protected String getLaunchConfigurationType()
-    {
-        return "org.eclipse.ant.AntBuilderLaunchConfigurationType";
-    }
-
-    protected String getBuilderLocation()
-    {
-        return "${build_project}/" + buildfilePath;
-    }
-
     /**
      *
      * @param launcherName Name of the launch file, for instance 'AntBuilder.launch'
@@ -43,13 +45,10 @@ public class EclipseAntExternalLaunchConfigurationWriter
     {
         // ant specific
         writeAttribute( writer, "process_factory_id", "org.eclipse.ant.ui.remoteAntProcessFactory" );
-        writeAttribute( writer, "org.eclipse.ant.ui.DEFAULT_VM_INSTALL", false );
-        writeAttribute( writer, "org.eclipse.jdt.launching.DEFAULT_CLASSPATH", true );
-        writeAttribute( writer, "org.eclipse.debug.ui.ATTR_CONSOLE_OUTPUT_ON", false );
 
-        writer.startElement( "listAttribute" );
-        writer.addAttribute( "key", "org.eclipse.debug.core.MAPPED_RESOURCE_TYPES" );
-        writer.endElement();
+        writeAttribute( writer, "org.eclipse.ant.ui.DEFAULT_VM_INSTALL", false );
+
+        writeAttribute( writer, "org.eclipse.debug.ui.ATTR_CONSOLE_OUTPUT_ON", false );
 
         writeAttribute( writer, "org.eclipse.ant.ui.ATTR_TARGETS_UPDATED", true );
 
@@ -58,12 +57,20 @@ public class EclipseAntExternalLaunchConfigurationWriter
             "org.eclipse.jdt.launching.CLASSPATH_PROVIDER",
             "org.eclipse.ant.ui.AntClasspathProvider" );
 
-        // writeAttribute( writer, "org.eclipse.jdt.launching.PROJECT_ATTR", "" );
+        writeAttribute( writer, "org.eclipse.debug.core.MAPPED_RESOURCE_TYPES", new String[] { "1" } );
 
-        // writer.startElement( "listAttribute" );
-        // writer.addAttribute( "key", "org.eclipse.debug.core.MAPPED_RESOURCE_PATHS" );
-        // writer.endElement();
+        writeAttribute( writer, "org.eclipse.debug.core.MAPPED_RESOURCE_PATHS", new String[] { "/"
+            + config.getEclipseProjectName() + "/" + buildfilePath } );
+    }
 
+    protected String getLaunchConfigurationType()
+    {
+        return "org.eclipse.ant.AntBuilderLaunchConfigurationType";
+    }
+
+    protected String getBuilderLocation()
+    {
+        return "${build_project}/" + buildfilePath;
     }
 
     protected List getMonitoredResources()
