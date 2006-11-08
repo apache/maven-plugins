698f962f5152742d0006936bbe9cde4320688987
Dennis Lundberg <dennisl@apache.org>
2006-11-08 21:18:00 +0000

    [MCHECKSTYLE-56] Use Release 4.2 of Checkstyle.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@472649 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/checkstyle/CheckstyleReportListenerTest.java
+++ src/test/java/org/apache/maven/plugin/checkstyle/CheckstyleReportListenerTest.java
@@ -62,20 +62,20 @@ public class CheckstyleReportListenerTest
 
         AuditEvent event = new AuditEvent( this, "/source/path/file1", null );
         fireFileStarted( event );
-        LocalizedMessage message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.INFO, getClass() );
+        LocalizedMessage message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.INFO, null, getClass() );
         fireAddError( new AuditEvent( this, "/source/path/file1", message ) );
         fireFileFinished( event );
 
         event = new AuditEvent( this, "/source/path/file2", null );
         fireFileStarted( event );
-        message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.WARNING, getClass() );
+        message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.WARNING, null, getClass() );
         fireAddError( new AuditEvent( this, "/source/path/file2", message ) );
         fireAddError( new AuditEvent( this, "/source/path/file2", message ) );
         fireFileFinished( event );
 
         event = new AuditEvent( this, "/source/path/file3", null );
         fireFileStarted( event );
-        message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.ERROR, getClass() );
+        message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.ERROR, null, getClass() );
         fireAddError( new AuditEvent( this, "/source/path/file3", message ) );
         fireAddError( new AuditEvent( this, "/source/path/file3", message ) );
         fireAddError( new AuditEvent( this, "/source/path/file3", message ) );
@@ -83,7 +83,7 @@ public class CheckstyleReportListenerTest
 
         event = new AuditEvent( this, "/source/path/file4", null );
         fireFileStarted( event );
-        message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.IGNORE, getClass() );
+        message = new LocalizedMessage( 0, 0, "", "", null, SeverityLevel.IGNORE, null, getClass() );
         fireAddError( new AuditEvent( this, "/source/path/file4", message ) );
         fireAddError( new AuditEvent( this, "/source/path/file4", message ) );
         fireAddError( new AuditEvent( this, "/source/path/file4", message ) );
