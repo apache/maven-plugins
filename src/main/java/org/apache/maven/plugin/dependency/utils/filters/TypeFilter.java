5ad5fe267fd7c4a7591536fcb38cc8de7a65be81
Brian E Fox <brianf@apache.org>
2006-11-14 03:18:43 +0000

    DEP-43 applied patch from Richard van der Hoff to allow include/exclude by classifier. Also updated site docs.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-dependency-plugin@474639 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/dependency/utils/filters/TypeFilter.java
+++ src/main/java/org/apache/maven/plugin/dependency/utils/filters/TypeFilter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2005-2006 Brian Fox (brianefox@gmail.com)
+ * Copyright 2006 MX Telecom Ltd. <richardv@mxtelecom.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,182 +15,19 @@
  */
 package org.apache.maven.plugin.dependency.utils.filters;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.logging.Log;
-import org.codehaus.plexus.util.StringUtils;
 
-public class TypeFilter
-    implements ArtifactsFilter
+public class TypeFilter extends AbstractArtifactFeatureFilter
 {
-    private List includeTypes;
-
-    private List excludeTypes;
-
-    private String includeString;
-
-    private String excludeString;
-
     public TypeFilter( String include, String exclude )
     {
-        setExcludeTypes( exclude );
-        setIncludeTypes( include );
+        super(include, exclude, "Types");
     }
 
-    /**
-     * This function determines if filtering needs to be performed. Excludes are
-     * ignored if Includes are used.
-     * 
-     * @param dependencies
-     *            the set of dependencies to filter.
-     * 
-     * @return a Set of filtered dependencies.
+    /* (non-Javadoc)
+     * @see org.apache.maven.plugin.dependency.utils.filters.AbstractArtifactFeatureFilter#getArtifactFeature(org.apache.maven.artifact.Artifact)
      */
-    public Set filter( Set artifacts, Log log )
-    {
-        Set results = artifacts;
-
-        if ( this.includeTypes != null && !this.includeTypes.isEmpty() )
-        {
-            log.debug( "Including only Types: " + this.includeString );
-            results = filterIncludes( artifacts, this.includeTypes );
-        }
-        else
-        {
-            if ( this.excludeTypes != null && !this.excludeTypes.isEmpty() )
-            {
-                log.debug( "Excluding Types: " + this.excludeString );
-                results = filterExcludes( artifacts, this.excludeTypes );
-            }
-        }
-        return results;
+    protected String getArtifactFeature(Artifact artifact) {
+        return artifact.getType();
     }
-
-    /**
-     * Processes the dependencies list and includes the dependencies that match
-     * a type in the list.
-     * 
-     * @param depends
-     *            List of dependencies.
-     * @param types
-     *            List of types to include.
-     * 
-     * @return a set of filtered types.
-     */
-    private Set filterIncludes( Set artifacts, List types )
-    {
-        Set result = new HashSet();
-
-        Iterator typeIter = types.iterator();
-        while ( typeIter.hasNext() )
-        {
-            String artifactType = (String) typeIter.next();
-            Iterator iter = artifacts.iterator();
-            while ( iter.hasNext() )
-            {
-                Artifact artifact = (Artifact) iter.next();
-
-                // if the type matches the type, add to the
-                // results
-                if ( artifact.getType().equals( artifactType ) )
-                {
-                    result.add( artifact );
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Processes the dependencies list and excludes the dependencies that match
-     * a type in the list.
-     * 
-     * @param depends
-     *            List of dependencies.
-     * @param types
-     *            List of types to exclude.
-     * 
-     * @return a set of filtered types.
-     */
-    private Set filterExcludes( Set artifacts, List types )
-    {
-        Set result = new HashSet();
-
-        Iterator iter = artifacts.iterator();
-        while ( iter.hasNext() )
-        {
-            boolean exclude = false;
-            Artifact artifact = (Artifact) iter.next();
-
-            // look through all types. If no matches are found
-            // then it can be added to the results.
-            Iterator typeIter = types.iterator();
-            while ( typeIter.hasNext() )
-            {
-                String artifactType = (String) typeIter.next();
-                if ( artifact.getType().equals( artifactType ) )
-                {
-                    exclude = true;
-                    break;
-                }
-            }
-
-            if ( !exclude )
-            {
-                result.add( artifact );
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * @param includeTypes
-     *            The includeTypes to set.
-     */
-    public void setExcludeTypes( String excludeTypeString )
-    {
-        this.excludeString = excludeTypeString;
-
-        if ( StringUtils.isNotEmpty( excludeTypeString ) )
-        {
-            this.excludeTypes = Arrays.asList( StringUtils.split( excludeTypeString, "," ) );
-        }
-    }
-
-    /**
-     * @param includeTypes
-     *            The includeTypes to set.
-     */
-    public void setIncludeTypes( String includeTypeString )
-    {
-        this.includeString = includeTypeString;
-
-        if ( StringUtils.isNotEmpty( includeTypeString ) )
-        {
-            this.includeTypes = Arrays.asList( StringUtils.split( includeTypeString, "," ) );
-        }
-    }
-
-    /**
-     * @return Returns the excludeTypes.
-     */
-    public List getExcludeTypes()
-    {
-        return this.excludeTypes;
-    }
-
-    /**
-     * @return Returns the includeTypes.
-     */
-    public List getIncludeTypes()
-    {
-        return this.includeTypes;
-    }
-
 }
---
+++ src/test/java/org/apache/maven/plugin/dependency/utils/filters/TestClassifierFilter.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2005-2006 Brian Fox (brianefox@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * 
+ */
+package org.apache.maven.plugin.dependency.utils.filters;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.dependency.utils.ArtifactStubFactory;
+import org.apache.maven.plugin.dependency.utils.SilentLog;
+import org.apache.maven.plugin.logging.Log;
+
+/**
+ * @author brianf
+ * 
+ */
+public class TestClassifierFilter
+    extends TestCase
+{
+    Set artifacts = new HashSet();
+
+    Log log = new SilentLog();
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        ArtifactStubFactory factory = new ArtifactStubFactory( null, false );
+        artifacts = factory.getClassifiedArtifacts();
+    }
+
+    public void testClassifierParsing()
+    {
+        ClassifierFilter filter = new ClassifierFilter( "one,two", "three,four," );
+        List includes = filter.getIncludes();
+        List excludes = filter.getExcludes();
+
+        assertEquals( 2, includes.size() );
+        assertEquals( 2, excludes.size() );
+        assertEquals( "one", includes.get( 0 ).toString() );
+        assertEquals( "two", includes.get( 1 ).toString() );
+        assertEquals( "three", excludes.get( 0 ).toString() );
+        assertEquals( "four", excludes.get( 1 ).toString() );
+    }
+
+    public void testFiltering()
+    {
+        ClassifierFilter filter = new ClassifierFilter( "one,two", "one,three," );
+        Set result = filter.filter( artifacts, log );
+        assertEquals( 2, result.size() );
+
+        Iterator iter = result.iterator();
+        while ( iter.hasNext() )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            assertTrue( artifact.getClassifier().equals( "one" ) || artifact.getClassifier().equals( "two" ) );
+        }
+    }
+
+    public void testFiltering2()
+    {
+        ClassifierFilter filter = new ClassifierFilter( null, "one,three," );
+        Set result = filter.filter( artifacts, log );
+        assertEquals( 2, result.size() );
+
+        Iterator iter = result.iterator();
+        while ( iter.hasNext() )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            assertTrue( artifact.getClassifier().equals( "two" ) || artifact.getClassifier().equals( "four" ) );
+        }
+    }
+
+    public void testFiltering3()
+    {
+        ClassifierFilter filter = new ClassifierFilter( null, null );
+        Set result = filter.filter( artifacts, log );
+        assertEquals( 4, result.size() );
+    }
+}
