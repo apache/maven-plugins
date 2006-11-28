525f1a927a042814b474705d3075518b1846734c
Dennis Lundberg <dennisl@apache.org>
2006-11-28 22:11:16 +0000

    [MIDEA-74] Plugin doesn't handle correctly WebModule dependency scope
    o Exclude dependencies with scope set to system or test from being packaged with a webapp.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@480236 13f79535-47bb-0310-9956-ffa450edef68

--- maven-idea-plugin/src/test/java/org/apache/maven/plugin/idea/stubs/WarMavenProjectWithProvidedDependencyStub.java
+++ maven-idea-plugin/src/test/java/org/apache/maven/plugin/idea/stubs/WarMavenProjectWithProvidedDependencyStub.java
@@ -35,12 +35,16 @@ public class WarMavenProjectWithProvidedDependencyStub
         List testArtifacts = new ArrayList();
 
         Artifact artifact = createArtifact( "org.apache.maven", "maven-model", "2.0.1" );
-
         artifact.setScope( Artifact.SCOPE_PROVIDED );
-
         testArtifacts.add( artifact );
 
-        testArtifacts.add( createArtifact( "junit", "junit", "3.8.1" ) );
+        Artifact artifact2 = createArtifact("javax.sql", "jdbc-stdext", "2.0");
+        artifact2.setScope( Artifact.SCOPE_SYSTEM );
+        testArtifacts.add( artifact2 );
+
+        Artifact artifact3 = createArtifact("junit", "junit", "3.8.1");
+        artifact3.setScope( Artifact.SCOPE_TEST );
+        testArtifacts.add( artifact3 );
 
         return testArtifacts;
     }
@@ -54,6 +58,14 @@ public class WarMavenProjectWithProvidedDependencyStub
         dep.setArtifactId( "maven-model" );
         dep.setVersion( "2.0.1" );
         dep.setScope( Artifact.SCOPE_PROVIDED );
+        dependencies.add( dep );
+
+        dep = new Dependency();
+        dep.setGroupId( "javax.sql" );
+        dep.setArtifactId( "jdbc-stdext" );
+        dep.setVersion( "2.0" );
+        dep.setScope( Artifact.SCOPE_SYSTEM );
+        dep.setSystemPath( "${java.home}/lib/rt.jar" );
         dependencies.add( dep );
 
         dep = new Dependency();
