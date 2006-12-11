bcbae57c0ae37e4368cb3b075f803f671c06facc
Fabrizio Giustina <fgiust@apache.org>
2006-12-11 20:41:36 +0000

    fixing license header + some formatting (there are no changes to the code in this commit)
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-eclipse-plugin@485864 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/eclipse/InstallPluginsMojoTest.java
+++ src/test/java/org/apache/maven/plugin/eclipse/InstallPluginsMojoTest.java
@@ -1,4 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugin.eclipse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
@@ -17,14 +43,6 @@ import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
 import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
 import org.codehaus.plexus.components.interactivity.InputHandler;
 import org.easymock.MockControl;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Collections;
-
-import junit.framework.Assert;
-import junit.framework.TestCase;
 
 public class InstallPluginsMojoTest
     extends TestCase
@@ -171,9 +189,9 @@ public class InstallPluginsMojoTest
         fileManager = new TestFileManager( "InstallPluginsMojo.test.", "" );
 
         URL resource = null;
-        
+
         resource = Thread.currentThread().getContextClassLoader().getResource( "m2repo" + SOURCE_PATH );
-        
+
         if ( resource == null )
         {
             resource = Thread.currentThread().getContextClassLoader().getResource( "M2REPO" + SOURCE_PATH );
@@ -181,7 +199,8 @@ public class InstallPluginsMojoTest
 
         if ( resource == null )
         {
-            throw new IllegalStateException( "Cannot find test source jar: (m2repo|M2REPO)" + SOURCE_PATH + " in context classloader!" );
+            throw new IllegalStateException( "Cannot find test source jar: (m2repo|M2REPO)" + SOURCE_PATH
+                + " in context classloader!" );
         }
 
         sourceFile = new File( resource.getPath() );
