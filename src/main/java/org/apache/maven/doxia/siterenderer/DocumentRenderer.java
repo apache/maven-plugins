8da9f9ee11c0cd87bfb4f8e157d046eab4119bd6
Jason van Zyl <jvanzyl@apache.org>
2006-12-14 21:50:33 +0000

    o A bridge that allows all the new site plugin functionality to work with 2.0.4
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-site-plugin@487365 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/doxia/siterenderer/DocumentRenderer.java
@@ -0,0 +1,44 @@
+package org.apache.maven.doxia.siterenderer;
+
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.doxia.module.xhtml.decoration.render.RenderingContext;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+/**
+ * Renders a page.
+ *
+ * @author <a href="mailto:brett@apache.org">Brett Porter</a>
+ */
+public interface DocumentRenderer
+{
+    void renderDocument( Writer writer, Renderer renderer, SiteRenderingContext siteRenderingContext )
+        throws RendererException, FileNotFoundException, UnsupportedEncodingException;
+
+    String getOutputName();
+
+    RenderingContext getRenderingContext();
+
+    /**
+     * Whether to always overwrite the document, or only do so when it is changed.
+     * @return whether to overwrite
+     */
+    boolean isOverwrite();
+}
--- src/main/java/org/apache/maven/plugins/site/AbstractSiteRenderingMojo.java
+++ src/main/java/org/apache/maven/plugins/site/AbstractSiteRenderingMojo.java
@@ -402,7 +402,7 @@ public abstract class AbstractSiteRenderingMojo
             catch ( AbstractMethodError e )
             {
                 // the canGenerateReport() has been added just before the 2.0 release and will cause all the reporting
-                // plugins with an earlier version to fail (most of the codehaus mojo now fails)
+                // plugins with an earlier version to fail (most of the org.codehaus mojo now fails)
                 // be nice with them, output a warning and don't let them break anything
 
                 getLog().warn( "Error loading report " + report.getClass().getName() +
