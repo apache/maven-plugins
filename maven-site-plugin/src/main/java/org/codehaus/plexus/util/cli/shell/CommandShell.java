10c95428eac7b895f58dac1f2dcc3c3f173d8632
Jason van Zyl <jvanzyl@apache.org>
2006-12-15 03:15:59 +0000

    o plexus cli stuff
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@487437 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-site-plugin/src/main/java/org/codehaus/plexus/util/cli/shell/CommandShell.java
@@ -0,0 +1,36 @@
+package org.codehaus.plexus.util.cli.shell;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * <p>
+ * Implementation to call the Command.com Shell present on Windows 95, 98 and Me
+ * </p>
+ *
+ * @author <a href="mailto:carlos@apache.org">Carlos Sanchez</a>
+ * @since 1.2
+ */
+public class CommandShell
+    extends Shell
+{
+    public CommandShell()
+    {
+        setShellCommand( "command.com" );
+        setShellArgs( new String[]{"/C"} );
+    }
+
+}
