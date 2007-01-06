e9f88cec6c8d6ab57acda6f27b5506ddc82426d1
Fabrizio Giustina <fgiust@apache.org>
2007-01-06 18:52:03 +0000

    fix license headers + some formatting. No code changes
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@493538 13f79535-47bb-0310-9956-ffa450edef68

--- maven-repository-plugin/src/main/java/org/apache/maven/plugins/repository/BundleCreateMojo.java
+++ maven-repository-plugin/src/main/java/org/apache/maven/plugins/repository/BundleCreateMojo.java
@@ -1,26 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.repository;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.File;
-import java.util.List;
 
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
-import org.apache.maven.model.License;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
@@ -95,7 +95,7 @@ public class BundleCreateMojo
         {
             throw new MojoExecutionException( "At least one license must be defined." );
         }
-        
+
         // ----------------------------------------------------------------------
         //
         // ----------------------------------------------------------------------
