7bdb7a0c6631985abd7b02e5afaa0a55467e5424
Brian E Fox <brianf@apache.org>
2007-01-18 05:59:25 +0000

    added a couple it tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-dependency-plugin@497316 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/dependency/utils/TestDependencyUtil.java
+++ src/test/java/org/apache/maven/plugin/dependency/utils/TestDependencyUtil.java
@@ -32,7 +32,6 @@ import junit.framework.TestCase;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.handler.ArtifactHandler;
-import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.dependency.testUtils.stubs.DefaultArtifactHandlerStub;
