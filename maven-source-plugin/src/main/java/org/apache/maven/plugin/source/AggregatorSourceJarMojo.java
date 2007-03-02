a9ba49364ef0f1df6a2155596824ff5b3fca4b0f
Jason van Zyl <jvanzyl@apache.org>
2007-03-02 13:08:11 +0000

    o align the use of the plugin when forked versus when used in a lifecycle
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@513744 13f79535-47bb-0310-9956-ffa450edef68

--- maven-source-plugin/src/main/java/org/apache/maven/plugin/source/AggregatorSourceJarMojo.java
+++ maven-source-plugin/src/main/java/org/apache/maven/plugin/source/AggregatorSourceJarMojo.java
@@ -1,6 +1,10 @@
 package org.apache.maven.plugin.source;
 
+import org.apache.maven.plugin.MojoExecutionException;
+
 /**
+ * Aggregrate sources for all modules in a aggregator project.
+ *
  * @goal aggregate
  * @phase package
  * @aggregator
@@ -9,4 +13,13 @@ package org.apache.maven.plugin.source;
 public class AggregatorSourceJarMojo
     extends SourceJarMojo
 {
+    /** @see org.apache.maven.plugin.AbstractMojo#execute() */
+    public void execute()
+        throws MojoExecutionException
+    {
+        if ( "pom".equals( project.getPackaging() ) )
+        {
+            packageSources( reactorProjects );
+        }
+    }
 }
