840d24e834e40bd876f7e82183078a2b095ff3d3
Brian E Fox <brianf@apache.org>
2007-03-14 00:26:19 +0000

    merged in analyzer from the sandbox
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/branches/maven-dependency-plugin-w-analyzer@517948 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/dependency/AnalyzeMojo.java
@@ -0,0 +1,112 @@
+package org.apache.maven.plugin.dependency;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalysis;
+import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzer;
+import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzerException;
+
+/**
+ * 
+ * 
+ * @author <a href="mailto:markhobson@gmail.com">Mark Hobson</a>
+ * @version $Id$
+ * @goal analyze
+ * @requiresDependencyResolution test
+ * @execute phase="test-compile"
+ */
+public class AnalyzeMojo extends AbstractMojo
+{
+    // fields -----------------------------------------------------------------
+
+    /**
+     * 
+     * 
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * 
+     * 
+     * @parameter expression="${component.org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzer}"
+     * @required
+     * @readonly
+     */
+    private ProjectDependencyAnalyzer analyzer;
+
+    // Mojo methods -----------------------------------------------------------
+
+    /*
+     * @see org.apache.maven.plugin.Mojo#execute()
+     */
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        try
+        {
+            ProjectDependencyAnalysis analysis = analyzer.analyze( project );
+
+            getLog().info( "Used declared dependencies:" );
+
+            logArtifacts( analysis.getUsedDeclaredArtifacts() );
+
+            getLog().info( "Used undeclared dependencies:" );
+
+            logArtifacts( analysis.getUsedUndeclaredArtifacts() );
+
+            getLog().info( "Unused declared dependencies:" );
+
+            logArtifacts( analysis.getUnusedDeclaredArtifacts() );
+        }
+        catch ( ProjectDependencyAnalyzerException exception )
+        {
+            throw new MojoExecutionException( "Cannot analyze dependencies", exception );
+        }
+    }
+
+    // private methods --------------------------------------------------------
+
+    private void logArtifacts( Set artifacts )
+    {
+        if ( artifacts.isEmpty() )
+        {
+            getLog().info( "   None" );
+        }
+        else
+        {
+            for ( Iterator iterator = artifacts.iterator(); iterator.hasNext(); )
+            {
+                Artifact artifact = (Artifact) iterator.next();
+
+                getLog().info( "   " + artifact );
+            }
+        }
+    }
+}
