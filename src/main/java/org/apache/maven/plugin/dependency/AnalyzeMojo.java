b1fed90cee190327370cdd2c4ebba2c1a515b1e0
Brian E Fox <brianf@apache.org>
2007-03-17 05:03:28 +0000

    added check for MNG-1577 issues
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/branches/maven-dependency-plugin-w-analyzer@519239 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/dependency/AnalyzeMojo.java
+++ src/main/java/org/apache/maven/plugin/dependency/AnalyzeMojo.java
@@ -19,10 +19,17 @@ package org.apache.maven.plugin.dependency;
  * under the License.
  */
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -30,6 +37,7 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalysis;
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzer;
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzerException;
+import org.codehaus.plexus.util.StringUtils;
 
 /**
  * 
@@ -38,10 +46,11 @@ import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzerExce
  * @version $Id$
  * @goal analyze
  * @requiresDependencyResolution test
- * @execute phase="test-compile"
+ * 
  * @since 2.0-alpha-3
  */
-public class AnalyzeMojo extends AbstractMojo
+public class AnalyzeMojo
+    extends AbstractMojo
 {
     // fields -----------------------------------------------------------------
 
@@ -53,6 +62,27 @@ public class AnalyzeMojo extends AbstractMojo
      * @readonly
      */
     private MavenProject project;
+
+    /**
+     * Check for dependency / dependencyMgt conflicts
+     * 
+     * @parameter expression=${mdep.checkDepMgt}
+     */
+    private boolean checkDependencyMgt = true;
+
+    /**
+     * Check dependency conflicts
+     * 
+     * @parameter expression=${mdep.checkDependencies}
+     */
+    private boolean checkDependencies = false;
+
+    /**
+     * Fail Build on problem
+     * 
+     * @parameter expression=${mdep.failBuild}
+     */
+    private boolean failBuild = false;
 
     /**
      * 
@@ -68,7 +98,30 @@ public class AnalyzeMojo extends AbstractMojo
     /*
      * @see org.apache.maven.plugin.Mojo#execute()
      */
-    public void execute() throws MojoExecutionException, MojoFailureException
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        boolean result = false;
+        if ( this.checkDependencies )
+        {
+            checkDependencies();
+        }
+
+        if ( this.checkDependencyMgt )
+        {
+            result = checkDependencyManagement();
+        }
+
+        if ( result && this.failBuild )
+        {
+            throw new MojoExecutionException( "Found errors." );
+        }
+    }
+
+    // private methods --------------------------------------------------------
+
+    private void checkDependencies()
+        throws MojoExecutionException
     {
         try
         {
@@ -90,9 +143,70 @@ public class AnalyzeMojo extends AbstractMojo
         {
             throw new MojoExecutionException( "Cannot analyze dependencies", exception );
         }
+
     }
 
-    // private methods --------------------------------------------------------
+    private boolean checkDependencyManagement()
+        throws MojoExecutionException
+    {
+        boolean foundMismatch = false;
+
+        getLog().info( "Found Resolved Dependency / DependencyManagement mismatches:" );
+
+        List depMgtDependencies = null;
+        DependencyManagement depMgt = project.getDependencyManagement();
+        if ( depMgt != null )
+        {
+            depMgtDependencies = depMgt.getDependencies();
+        }
+
+        if ( depMgtDependencies != null && !depMgtDependencies.isEmpty() )
+        {
+            // put all the dependencies from depMgt into a map for quick lookup
+            Map map = new HashMap();
+            Iterator iter = depMgtDependencies.iterator();
+            while ( iter.hasNext() )
+            {
+                Dependency dependency = (Dependency) iter.next();
+                map.put( dependency.getManagementKey(), dependency );
+            }
+
+            Set allDependencies = project.getArtifacts();
+            iter = allDependencies.iterator();
+            while ( iter.hasNext() )
+            {
+                Artifact artifact = (Artifact) iter.next();
+               // getLog().info( "a:"+getArtifactManagementKey( artifact ) );
+                // see if this artifact matches anything in the dependencyMgt
+                // list
+                Dependency dep = (Dependency) map.get( getArtifactManagementKey( artifact ) );
+                if ( dep != null )
+                {
+                 //   getLog().info( "Compare:" + dep.getManagementKey()+" v:"+dep.getVersion()+"a:"+artifact.getVersion());
+                   // ArtifactVersion depVersion = new DefaultArtifactVersion(dep.getVersion());
+                    ArtifactVersion artifactVersion = new DefaultArtifactVersion(artifact.getVersion());
+                    
+                    if (!artifact.isSnapshot() && !dep.getVersion().equals( artifact.getVersion() ) )
+
+                    {
+                        logMismatch( artifact, dep );
+                        foundMismatch = true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            getLog().info( "   Nothing in DepMgt." );
+        }
+
+        if ( !foundMismatch )
+        {
+            getLog().info( "   None" );
+        }
+
+        return foundMismatch;
+    }
 
     private void logArtifacts( Set artifacts )
     {
@@ -109,5 +223,24 @@ public class AnalyzeMojo extends AbstractMojo
                 getLog().info( "   " + artifact );
             }
         }
+    }
+
+    private void logMismatch( Artifact artifact, Dependency dependency )
+        throws MojoExecutionException
+    {
+        if ( artifact == null || dependency == null )
+        {
+            throw new MojoExecutionException( "Invalid params: Artifact:" + artifact + " Dependency:" + dependency );
+        }
+
+        getLog().info(
+                       "   Resolved Dependency: " + dependency.getManagementKey() +":"+dependency.getVersion()+ " Dependency Management: "
+                           + getArtifactManagementKey( artifact )+":"+artifact.getVersion() );
+    }
+
+    private String getArtifactManagementKey( Artifact artifact )
+    {
+        return artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getType()
+            + ( !StringUtils.isEmpty( artifact.getClassifier() ) ? ":" + artifact.getClassifier() : "" );
     }
 }
