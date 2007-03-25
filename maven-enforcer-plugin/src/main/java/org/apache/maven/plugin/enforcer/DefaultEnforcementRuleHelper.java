462e07c16db9acd886bab6183882717bdac6cd39
Brian E Fox <brianf@apache.org>
2007-03-25 21:54:12 +0000

    added javadocs
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@522359 13f79535-47bb-0310-9956-ffa450edef68

--- maven-enforcer-plugin/src/main/java/org/apache/maven/plugin/enforcer/DefaultEnforcementRuleHelper.java
+++ maven-enforcer-plugin/src/main/java/org/apache/maven/plugin/enforcer/DefaultEnforcementRuleHelper.java
@@ -1,11 +1,38 @@
 package org.apache.maven.plugin.enforcer;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.RuntimeInformation;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
+/**
+ * Default implementation of the EnforcementRuleHelper interface. This is used
+ * to help retreive information from the session and provide usefull elements
+ * like the log.
+ * 
+ * @author <a href="mailto:brianf@apache.org">Brian Fox</a>
+ * @version $Id: AnalyzeMojo.java 522157 2007-03-25 04:34:54Z brianf $
+ */
 public class DefaultEnforcementRuleHelper
     implements EnforcementRuleHelper
 {
