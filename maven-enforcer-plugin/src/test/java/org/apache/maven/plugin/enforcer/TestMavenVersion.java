212fb5d4b07587e3a305db817f039761f2590989
Brian E Fox <brianf@apache.org>
2007-03-28 02:51:39 +0000

    more tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@523148 13f79535-47bb-0310-9956-ffa450edef68

--- maven-enforcer-plugin/src/test/java/org/apache/maven/plugin/enforcer/TestMavenVersion.java
+++ maven-enforcer-plugin/src/test/java/org/apache/maven/plugin/enforcer/TestMavenVersion.java
@@ -1,5 +1,10 @@
 package org.apache.maven.plugin.enforcer;
 
+import org.apache.commons.lang.SystemUtils;
+import org.apache.maven.plugin.logging.SystemStreamLog;
+import org.apache.maven.shared.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.shared.enforcer.rule.api.EnforcerRuleHelper;
+
 import junit.framework.TestCase;
 
 /**
@@ -9,5 +14,38 @@ import junit.framework.TestCase;
 public class TestMavenVersion
     extends TestCase
 {
+    public void testRule()
+    throws EnforcerRuleException
+{
+    
+        
+    String thisVersion = RequireJavaVersion.fixJDKVersion( SystemUtils.JAVA_VERSION_TRIMMED );
 
+    RequireJavaVersion rule = new RequireJavaVersion();
+    rule.setVersion( thisVersion );
+
+    EnforcerRuleHelper helper = new DefaultEnforcementRuleHelper( EnforcerTestUtils.getMavenSession(),
+                                                                  new SystemStreamLog() );
+
+    // test the singular version
+    rule.execute( helper );
+
+    // exclude this version
+    rule.setVersion( "(" + thisVersion );
+
+    try
+    {
+        rule.execute( helper );
+        fail( "Expected an exception." );
+    }
+    catch ( EnforcerRuleException e )
+    {
+        // expected to catch this.
+    }
+    
+    //this shouldn't crash
+    rule.setVersion( SystemUtils.JAVA_VERSION_TRIMMED );
+    rule.execute( helper );
+
+}
 }
