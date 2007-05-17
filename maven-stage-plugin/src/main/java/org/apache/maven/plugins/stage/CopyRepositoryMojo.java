d11334d8ee58044f74d89e55240959fc2ee18b5a
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-05-17 23:10:47 +0000

    Add license headers, better use of exceptions, option to set repositoryId
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@539157 13f79535-47bb-0310-9956-ffa450edef68

--- maven-stage-plugin/src/main/java/org/apache/maven/plugins/stage/CopyRepositoryMojo.java
+++ maven-stage-plugin/src/main/java/org/apache/maven/plugins/stage/CopyRepositoryMojo.java
@@ -1,7 +1,30 @@
 package org.apache.maven.plugins.stage;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.IOException;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.wagon.WagonException;
+import org.apache.maven.wagon.repository.Repository;
 
 /**
  * @author Jason van Zyl
@@ -17,6 +40,9 @@ public class CopyRepositoryMojo
     /** @parameter expression="${target}" */
     private String target;
 
+    /** @parameter expression="${repositoryId}" */
+    private String repositoryId = "target";
+
     /**
      * @parameter expression="${version}"
      * @required
@@ -31,9 +57,15 @@ public class CopyRepositoryMojo
     {
         try
         {
-            copier.copy( source, target, version );
+            Repository targetRepository = new Repository( repositoryId, target );
+            copier.copy( source, targetRepository, version );
         }
-        catch ( Exception e )
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException(
+                "Error copying repository from " + source + " to " + target, e );
+        }
+        catch ( WagonException e )
         {
             throw new MojoExecutionException(
                 "Error copying repository from " + source + " to " + target, e );
