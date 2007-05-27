abd3c8da1bbf46da356ab3549d2c18682e6078ce
Vincent Siveton <vsiveton@apache.org>
2007-05-27 13:23:55 +0000

    MPIR-43: Use the plugin testing harness and add tests for the project-info-report mojos
    o Finished Edwin's work to go ahead
    o Bumped to maven-plugin-testing-harness:1.0
    o FYI using httpunit to test the generated reports
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-project-info-reports-plugin@541986 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/report/projectinfo/AbstractProjectInfoTestCase.java
+++ src/test/java/org/apache/maven/report/projectinfo/AbstractProjectInfoTestCase.java
@@ -19,39 +19,168 @@ package org.apache.maven.report.projectinfo;
  * under the License.
  */
 
+import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.profiles.DefaultProfileManager;
+import org.apache.maven.profiles.ProfileManager;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
 import org.apache.maven.reporting.MavenReport;
+import org.codehaus.plexus.i18n.I18N;
+import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 
 /**
+ * Abstract class to test reports generation with <a href="http://www.httpunit.org/">HTTPUnit</a> framework.
+ *
  * @author Edwin Punzalan
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id $
  */
-public class AbstractProjectInfoTestCase
+public abstract class AbstractProjectInfoTestCase
     extends AbstractMojoTestCase
 {
+    /**
+     * The default locale is English.
+     */
+    protected static final Locale DEFAULT_LOCALE = Locale.ENGLISH;
+
+    /**
+     * The current project to be test.
+     */
+    private MavenProject testMavenProject;
+
+    /**
+     * The I18N plexus component.
+     */
+    private I18N i18n;
+
+    /**
+     * @see org.apache.maven.plugin.testing.AbstractMojoTestCase#setUp()
+     */
+    protected void setUp()
+        throws Exception
+    {
+        // required for mojo lookups to work
+        super.setUp();
+
+        i18n = (I18N) getContainer().lookup( I18N.ROLE );
+
+        File f = new File( getBasedir(), "target/local-repo/" );
+        f.mkdirs();
+
+        // Set the default Locale
+        Locale.setDefault( DEFAULT_LOCALE );
+    }
+
+    /**
+     * @see org.codehaus.plexus.PlexusTestCase#getCustomConfiguration()
+     */
+    protected InputStream getCustomConfiguration()
+        throws Exception
+    {
+        // Allow sub classes to have their own configuration...
+        if ( super.getConfiguration() == null )
+        {
+            String className = AbstractProjectInfoTestCase.class.getName();
+
+            String config = className.substring( className.lastIndexOf( "." ) + 1 ) + ".xml";
+
+            return AbstractProjectInfoTestCase.class.getResourceAsStream( config );
+        }
+
+        return null;
+    }
+
+    /**
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown()
+        throws Exception
+    {
+        // nop
+    }
+
+    /**
+     * Gets a trimmed String for the given key from the resource bundle defined by Plexus.
+     *
+     * @param key the key for the desired string
+     * @return the string for the given key
+     * @throws IllegalArgumentException if the parameter is empty.
+     */
+    protected String getString( String key )
+    {
+        if ( StringUtils.isEmpty( key ) )
+        {
+            throw new IllegalArgumentException( "The key cannot be empty" );
+        }
+
+        return i18n.getString( key, Locale.getDefault() ).trim();
+    }
+
+    /**
+     * Get the current Maven project
+     *
+     * @return the maven project
+     */
+    protected MavenProject getTestMavenProject()
+    {
+        return testMavenProject;
+    }
+
+    /**
+     * Get the generated report as file in the test maven project.
+     *
+     * @return the generated report as file
+     * @throws IOException if the return file doesnt exist
+     */
+    protected File getGeneratedReport(String name )
+        throws IOException
+    {
+        String outputDirectory = getBasedir() + "/target/test-harness/" + getTestMavenProject().getArtifactId();
+
+        File report = new File( outputDirectory, name );
+        if ( !report.exists() )
+        {
+            throw new IOException( "File not found. Attempted :" + report );
+        }
+
+        return report;
+    }
+
+    /**
+     * Generate the report and return the generated file
+     *
+     * @param goal
+     * @param pluginXml
+     * @return the generated HTML file
+     * @throws Exception if any
+     */
     protected File generateReport( String goal, String pluginXml )
         throws Exception
     {
         File pluginXmlFile = new File( getBasedir(), "src/test/plugin-configs/" + pluginXml );
-
         Mojo mojo = lookupMojo( goal, pluginXmlFile );
-
         assertNotNull( "Mojo found.", mojo );
 
         mojo.execute();
 
+        MavenProjectBuilder builder = (MavenProjectBuilder) lookup( MavenProjectBuilder.ROLE );
+        ProfileManager profileManager = new DefaultProfileManager( getContainer() );
+
+        ArtifactRepository localRepository = ( ArtifactRepository ) getVariableValueFromObject( mojo, "localRepository" );
+
+        testMavenProject = builder.buildWithDependencies( pluginXmlFile, localRepository, profileManager );
+
         MavenReport reportMojo = (MavenReport) mojo;
-
         File outputDir = reportMojo.getReportOutputDirectory();
-
         String filename = reportMojo.getOutputName() + ".html";
 
-        File outputHtml = new File( outputDir, filename );
-
-//        assertTrue( "Test html generated", outputHtml.exists() );
-
-        return outputHtml;
+        return new File( outputDir, filename );
     }
 }
