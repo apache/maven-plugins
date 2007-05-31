35ab986bdfbd4e1759ed92136517cf67b4509303
Dennis Lundberg <dennisl@apache.org>
2007-05-31 21:59:44 +0000

    o Refactor the creation of title and summary for the report into a couple of common methods in the parent.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-changelog-plugin@543277 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/changelog/FileActivityReport.java
+++ src/main/java/org/apache/maven/plugin/changelog/FileActivityReport.java
@@ -141,22 +141,9 @@ public class FileActivityReport
     private void doChangedSets( ChangeLogSet set, ResourceBundle bundle, Sink sink )
     {
         sink.section2();
-        sink.sectionTitle2();
-        if ( set.getStartDate() == null )
-        {
-            sink.text( bundle.getString( "report.SetRangeUnknown" ) );
-        }
-        else if ( set.getEndDate() == null )
-        {
-            sink.text( bundle.getString( "report.SetRangeSince" ) );
-        }
-        else
-        {
-            sink.text( bundle.getString( "report.SetRangeBetween" ) );
-            sink.text( " " + set.getStartDate() + " " + bundle.getString( "report.To" ) + " " + set.getEndDate() );
-        }
-        sink.sectionTitle2_();
 
+        doChangeSetTitle( set, bundle, sink );
+        
         doSummary( set, bundle, sink );
 
         sink.table();
@@ -175,29 +162,6 @@ public class FileActivityReport
         sink.table_();
 
         sink.section2_();
-    }
-
-    /**
-     * generates the report summary section of the report
-     *
-     * @param set    changed set to generate the report from
-     * @param bundle the resource bundle to retrieve report phrases from
-     * @param sink   the report formatting tool
-     */
-    private void doSummary( ChangeLogSet set, ResourceBundle bundle, Sink sink )
-    {
-        sink.paragraph();
-
-        sink.text( bundle.getString( "report.file-activity.range" ) );
-        sink.text( ": " + set.getStartDate() + " " + bundle.getString( "report.To" ) + " " + set.getEndDate() );
-
-        sink.text( ", " + bundle.getString( "report.TotalCommits" ) );
-        sink.text( ":" + set.getChangeSets().size() );
-
-        sink.text( ", " + bundle.getString( "report.file-activity.filesChanged" ) );
-        sink.text( ":" + countFilesChanged( set.getChangeSets() ) );
-
-        sink.paragraph_();
     }
 
     /**
