d56fd5260229fc686c909dbceb346cf7381e6ba7
John Dennis Casey <jdcasey@apache.org>
2007-07-14 03:30:33 +0000

    Adding the ability to specify descriptor aggregators, similar to the one for components.xml, to process other files.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@556215 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/assembly/archive/archiver/PrefixingProxyArchiver.java
+++ src/main/java/org/apache/maven/plugin/assembly/archive/archiver/PrefixingProxyArchiver.java
@@ -1,12 +1,24 @@
 package org.apache.maven.plugin.assembly.archive.archiver;
 
+import org.apache.maven.plugin.assembly.filter.ContainerDescriptorHandler;
+import org.codehaus.plexus.archiver.ArchiveFinalizer;
 import org.codehaus.plexus.archiver.ArchivedFileSet;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.FileSet;
+import org.codehaus.plexus.archiver.FinalizerEnabled;
+import org.codehaus.plexus.archiver.util.DefaultArchivedFileSet;
+import org.codehaus.plexus.archiver.util.DefaultFileSet;
+import org.codehaus.plexus.components.io.fileselectors.FileInfo;
+import org.codehaus.plexus.components.io.fileselectors.FileSelector;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class PrefixingProxyArchiver
@@ -14,77 +26,184 @@ public class PrefixingProxyArchiver
 {
 
     private Archiver delegate;
+
     private String rootPrefix;
 
-    public PrefixingProxyArchiver( String rootPrefix, Archiver delegate )
+    private FileSelector[] selectors;
+
+    public PrefixingProxyArchiver( String rootPrefix, Archiver delegate, List containerDescriptorHandlers,
+                                   List extraSelectors, List extraFinalizers )
     {
         this.rootPrefix = rootPrefix;
         this.delegate = delegate;
 
-        if ( !rootPrefix.endsWith( "/" ) )
+        if ( !"".equals( rootPrefix ) && !rootPrefix.endsWith( "/" ) )
         {
             this.rootPrefix += "/";
+        }
+
+        List selectors = new ArrayList();
+
+        boolean isFinalizerEnabled = ( delegate instanceof FinalizerEnabled );
+
+        if ( containerDescriptorHandlers != null )
+        {
+            for ( Iterator it = containerDescriptorHandlers.iterator(); it.hasNext(); )
+            {
+                ContainerDescriptorHandler handler = (ContainerDescriptorHandler) it.next();
+
+                selectors.add( handler );
+
+                if ( isFinalizerEnabled )
+                {
+                    ( (FinalizerEnabled) delegate ).addArchiveFinalizer( handler );
+                }
+            }
+        }
+
+        if ( extraSelectors != null )
+        {
+            for ( Iterator it = extraSelectors.iterator(); it.hasNext(); )
+            {
+                FileSelector selector = (FileSelector) it.next();
+                selectors.add( selector );
+            }
+        }
+
+        if ( ( extraFinalizers != null ) && isFinalizerEnabled )
+        {
+            for ( Iterator it = extraFinalizers.iterator(); it.hasNext(); )
+            {
+                ( (FinalizerEnabled) delegate ).addArchiveFinalizer( (ArchiveFinalizer) it.next() );
+            }
+        }
+
+        if ( !selectors.isEmpty() )
+        {
+            this.selectors = (FileSelector[]) selectors.toArray( new FileSelector[0] );
         }
     }
 
     public void addArchivedFileSet( File archiveFile, String prefix, String[] includes, String[] excludes )
         throws ArchiverException
     {
-        delegate.addArchivedFileSet( archiveFile, rootPrefix + prefix, includes, excludes );
+        DefaultArchivedFileSet fs = new DefaultArchivedFileSet();
+
+        fs.setArchive( archiveFile );
+        fs.setIncludes( includes );
+        fs.setExcludes( excludes );
+        fs.setPrefix( rootPrefix + prefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addArchivedFileSet( fs );
     }
 
     public void addArchivedFileSet( File archiveFile, String prefix )
         throws ArchiverException
     {
-        delegate.addArchivedFileSet( archiveFile, rootPrefix + prefix );
+        DefaultArchivedFileSet fs = new DefaultArchivedFileSet();
+
+        fs.setArchive( archiveFile );
+        fs.setPrefix( rootPrefix + prefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addArchivedFileSet( fs );
     }
 
     public void addArchivedFileSet( File archiveFile, String[] includes, String[] excludes )
         throws ArchiverException
     {
-        delegate.addArchivedFileSet( archiveFile, rootPrefix, includes, excludes );
+        DefaultArchivedFileSet fs = new DefaultArchivedFileSet();
+
+        fs.setArchive( archiveFile );
+        fs.setIncludes( includes );
+        fs.setExcludes( excludes );
+        fs.setPrefix( rootPrefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addArchivedFileSet( fs );
     }
 
     public void addArchivedFileSet( File archiveFile )
         throws ArchiverException
     {
-        delegate.addArchivedFileSet( archiveFile, rootPrefix );
+        DefaultArchivedFileSet fs = new DefaultArchivedFileSet();
+
+        fs.setArchive( archiveFile );
+        fs.setPrefix( rootPrefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addArchivedFileSet( fs );
     }
 
     public void addDirectory( File directory, String prefix, String[] includes, String[] excludes )
         throws ArchiverException
     {
-        delegate.addDirectory( directory, rootPrefix + prefix, includes, excludes );
+        DefaultFileSet fs = new DefaultFileSet();
+
+        fs.setDirectory( directory );
+        fs.setIncludes( includes );
+        fs.setExcludes( excludes );
+        fs.setPrefix( rootPrefix + prefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addFileSet( fs );
     }
 
     public void addDirectory( File directory, String prefix )
         throws ArchiverException
     {
-        delegate.addDirectory( directory, rootPrefix + prefix );
+        DefaultFileSet fs = new DefaultFileSet();
+
+        fs.setDirectory( directory );
+        fs.setPrefix( rootPrefix + prefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addFileSet( fs );
     }
 
     public void addDirectory( File directory, String[] includes, String[] excludes )
         throws ArchiverException
     {
-        delegate.addDirectory( directory, rootPrefix, includes, excludes );
+        DefaultFileSet fs = new DefaultFileSet();
+
+        fs.setDirectory( directory );
+        fs.setIncludes( includes );
+        fs.setExcludes( excludes );
+        fs.setPrefix( rootPrefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addFileSet( fs );
     }
 
     public void addDirectory( File directory )
         throws ArchiverException
     {
-        delegate.addDirectory( directory, rootPrefix );
+        DefaultFileSet fs = new DefaultFileSet();
+
+        fs.setDirectory( directory );
+        fs.setPrefix( rootPrefix );
+        fs.setFileSelectors( selectors );
+
+        delegate.addFileSet( fs );
     }
 
     public void addFile( File inputFile, String destFileName, int permissions )
         throws ArchiverException
     {
-        delegate.addFile( inputFile, rootPrefix + destFileName, permissions );
+        if ( acceptFile( inputFile ) )
+        {
+            delegate.addFile( inputFile, rootPrefix + destFileName, permissions );
+        }
     }
 
     public void addFile( File inputFile, String destFileName )
         throws ArchiverException
     {
-        delegate.addFile( inputFile, rootPrefix + destFileName );
+        if ( acceptFile( inputFile ) )
+        {
+            delegate.addFile( inputFile, rootPrefix + destFileName );
+        }
     }
 
     public void createArchive()
@@ -161,13 +280,74 @@ public class PrefixingProxyArchiver
     public void addArchivedFileSet( ArchivedFileSet fileSet )
         throws ArchiverException
     {
-        delegate.addArchivedFileSet( new PrefixedArchivedFileSet( fileSet, rootPrefix ) );
+        delegate.addArchivedFileSet( new PrefixedArchivedFileSet( fileSet, rootPrefix, selectors ) );
     }
 
     public void addFileSet( FileSet fileSet )
         throws ArchiverException
     {
-        delegate.addFileSet( fileSet );
+        delegate.addFileSet( new PrefixedFileSet( fileSet, rootPrefix, selectors ) );
+    }
+
+    private boolean acceptFile( File inputFile )
+        throws ArchiverException
+    {
+        if ( selectors != null )
+        {
+            FileInfo fileInfo = new DefaultFileInfo( inputFile );
+
+            for ( int i = 0; i < selectors.length; i++ )
+            {
+                try
+                {
+                    if ( !selectors[i].isSelected( fileInfo ) )
+                    {
+                        return false;
+                    }
+                }
+                catch ( IOException e )
+                {
+                    throw new ArchiverException( "Error processing file: " + inputFile + " using selector: "
+                                                 + selectors[i], e );
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static final class DefaultFileInfo
+        implements FileInfo
+    {
+
+        private final File inputFile;
+
+        DefaultFileInfo( File inputFile )
+        {
+            this.inputFile = inputFile;
+        }
+
+        public InputStream getContents()
+            throws IOException
+        {
+            return new FileInputStream( inputFile );
+        }
+
+        public String getName()
+        {
+            return inputFile.getName();
+        }
+
+        public boolean isDirectory()
+        {
+            return inputFile.isDirectory();
+        }
+
+        public boolean isFile()
+        {
+            return inputFile.isFile();
+        }
+
     }
 
 }
