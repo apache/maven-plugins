aeb8d6f79d86aed8b6afcfd5b1670afb1e070e1f
Brian E Fox <brianf@apache.org>
2007-07-20 00:59:13 +0000

    applied patches from Damian Bradicich for MDEP-47
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-dependency-plugin@557831 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/dependency/TestIncludeExcludeUnpackMojo.java
@@ -0,0 +1,288 @@
+package org.apache.maven.plugin.dependency;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.dependency.fromConfiguration.ArtifactItem;
+import org.apache.maven.plugin.dependency.fromConfiguration.UnpackMojo;
+import org.apache.maven.plugin.dependency.testUtils.DependencyTestUtils;
+import org.apache.maven.plugin.dependency.utils.markers.UnpackFileMarkerHandler;
+import org.apache.maven.plugin.testing.stubs.StubArtifactCollector;
+import org.apache.maven.plugin.testing.stubs.StubArtifactResolver;
+import org.apache.maven.project.MavenProject;
+
+public class TestIncludeExcludeUnpackMojo 
+	extends AbstractDependencyMojoTestCase
+{
+	private final String PACKED_FILE = "test.zip";
+	
+	private final String UNPACKED_FILE_PREFIX = "test";
+	private final String UNPACKED_FILE_SUFFIX = ".txt";
+	
+	private final String PACKED_FILE_PATH = "target/test-classes/unit/unpack-dependencies-test/" + PACKED_FILE;
+	
+	UnpackMojo mojo;
+
+    protected void setUp()
+        throws Exception
+    {
+    	// required for mojo lookups to work
+        super.setUp( "unpack", true );
+
+        File testPom = new File( getBasedir(), "target/test-classes/unit/unpack-test/plugin-config.xml" );
+        mojo = (UnpackMojo) lookupMojo( "unpack", testPom );
+        mojo.setOutputDirectory( new File( this.testDir, "outputDirectory" ) );
+        // mojo.silent = true;
+
+        // it needs to get the archivermanager
+        //stubFactory.setUnpackableFile( mojo.getArchiverManager() );
+        // i'm using one file repeatedly to archive so I can test the name
+        // programmatically.
+        stubFactory.setSrcFile( new File( getBasedir() + File.separatorChar + PACKED_FILE_PATH ) );
+        Artifact artifact = stubFactory.createArtifact( "test", "test", "1.0", Artifact.SCOPE_COMPILE, "jar", null );
+        ArtifactItem item = stubFactory.getArtifactItem( artifact );
+        ArrayList list = new ArrayList( 1 );
+        list.add( item );
+        assertNotNull( mojo );
+        assertNotNull( mojo.getProject() );
+        MavenProject project = mojo.getProject();
+
+        mojo.setFactory( DependencyTestUtils.getArtifactFactory() );
+        mojo.setResolver( new StubArtifactResolver( stubFactory, false, false ) );
+        mojo.setMarkersDirectory( new File( this.testDir, "markers" ) );
+        mojo.setArtifactCollector( new StubArtifactCollector() );
+        mojo.setArtifactItems( list );
+    }
+    
+    protected void tearDown()
+    {
+        super.tearDown();
+        
+        mojo = null;
+        System.gc();
+    }
+    
+    public void assertMarkerFiles( Collection items, boolean exist )
+    {
+        Iterator iter = items.iterator();
+        while ( iter.hasNext() )
+        {
+            assertMarkerFile( exist, (ArtifactItem) iter.next() );
+        }
+    }
+
+    public void assertMarkerFile( boolean val, ArtifactItem item )
+    {
+        UnpackFileMarkerHandler handle = new UnpackFileMarkerHandler( item, mojo.getMarkersDirectory() );
+        try
+        {
+            assertEquals( val, handle.isMarkerSet() );
+        }
+        catch ( MojoExecutionException e )
+        {
+            fail( e.getLongMessage() );
+        }
+    }
+    
+    private void assertUnpacked(boolean unpacked, String fileName)
+    {
+    	File destFile = new File( mojo.getOutputDirectory().getAbsolutePath() , fileName );    	
+    	assertEquals(unpacked, destFile.exists());
+    }
+    
+    /**
+     * This test will validate that only the 1 and 11 files get unpacked
+     * @throws Exception
+     */
+    public void testUnpackIncludesManyFiles()
+		throws Exception
+	{
+    	mojo.setIncludes("**/*1" + UNPACKED_FILE_SUFFIX);
+    	mojo.execute();
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    /**
+     * This test will verify only the 2 file gets unpacked
+     * @throws Exception
+     */
+    public void testUnpackIncludesSingleFile()
+    	throws Exception
+	{
+    	mojo.setIncludes("**/test2" + UNPACKED_FILE_SUFFIX);
+    	mojo.execute();
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    /**
+     * This test will verify all files get unpacked
+     * @throws Exception
+     */
+    public void testUnpackIncludesAllFiles()
+    	throws Exception
+	{
+    	mojo.setIncludes("**/*");
+    	mojo.execute();
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    /**
+     * This test will validate that only the 2 and 3 files get unpacked
+     * @throws Exception
+     */
+    public void testUnpackExcludesManyFiles()
+		throws Exception
+	{
+    	mojo.setExcludes("**/*1" + UNPACKED_FILE_SUFFIX);
+    	mojo.execute();
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    /**
+     * This test will verify only the 1, 11 & 3 files get unpacked
+     * @throws Exception
+     */
+    public void testUnpackExcludesSingleFile()
+    	throws Exception
+	{
+    	mojo.setExcludes("**/test2" + UNPACKED_FILE_SUFFIX);
+    	mojo.execute();
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    /**
+     * This test will verify no files get unpacked
+     * @throws Exception
+     */
+    public void testUnpackExcludesAllFiles()
+    	throws Exception
+	{
+    	mojo.setExcludes("**/*");
+    	mojo.execute();
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(false, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    public void testNoIncludeExcludes()
+    	throws Exception
+	{
+    	mojo.execute();
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX);
+    	assertUnpacked(true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX);
+	}
+    
+    public void testIncludeArtifactItemOverride()
+    	throws Exception
+    {
+    	Artifact artifact = stubFactory.createArtifact( "test", "test", "1.0", Artifact.SCOPE_COMPILE, "jar", null );
+        ArtifactItem item = stubFactory.getArtifactItem( artifact );
+        item.setIncludes("**/*");
+        ArrayList list = new ArrayList( 1 );
+        list.add( item );
+        mojo.setArtifactItems( list ); 
+        mojo.setIncludes( "**/test2" + UNPACKED_FILE_SUFFIX );
+    	mojo.execute();
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX );
+    }
+    
+    public void testExcludeArtifactItemOverride()
+	throws Exception
+	{
+		Artifact artifact = stubFactory.createArtifact( "test", "test", "1.0", Artifact.SCOPE_COMPILE, "jar", null );
+	    ArtifactItem item = stubFactory.getArtifactItem( artifact );
+	    item.setExcludes("**/*");
+	    ArrayList list = new ArrayList( 1 );
+	    list.add( item );
+	    mojo.setArtifactItems( list ); 
+	    mojo.setExcludes( "**/test2" + UNPACKED_FILE_SUFFIX );
+		mojo.execute();
+		assertUnpacked( false, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX );
+		assertUnpacked( false, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX );
+		assertUnpacked( false, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX );
+		assertUnpacked( false, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX );
+	}
+    
+    public void testIncludeArtifactItemMultipleMarker()
+    	throws Exception
+	{
+    	ArrayList list = new ArrayList();
+    	Artifact artifact = stubFactory.createArtifact( "test", "test", "1.0", Artifact.SCOPE_COMPILE, "jar", null );
+        ArtifactItem item = stubFactory.getArtifactItem( artifact );
+        item.setOverWrite("false");
+        item.setIncludes("**/test2" + UNPACKED_FILE_SUFFIX);        
+        list.add( item );
+        item = stubFactory.getArtifactItem( artifact );
+        item.setOverWrite("false");
+        item.setIncludes("**/test3" + UNPACKED_FILE_SUFFIX);        
+        list.add( item );
+        mojo.setArtifactItems( list ); 
+    	mojo.execute();
+    	assertUnpacked( false, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( false, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX );
+    	assertMarkerFiles( mojo.getArtifactItems(), true );
+	}
+    
+    public void testIncludeArtifactItemMultipleExecutions()
+    	throws Exception
+	{
+    	ArrayList list = new ArrayList();
+    	Artifact artifact = stubFactory.createArtifact( "test", "test", "1.0", Artifact.SCOPE_COMPILE, "jar", null );
+        ArtifactItem item = stubFactory.getArtifactItem( artifact );
+        item.setOverWrite("false");
+        item.setIncludes("**/test2" + UNPACKED_FILE_SUFFIX);        
+        list.add( item );
+        item = stubFactory.getArtifactItem( artifact );
+        item.setOverWrite("false");
+        item.setIncludes("**/test3" + UNPACKED_FILE_SUFFIX);        
+        list.add( item );
+        mojo.setArtifactItems( list ); 
+    	mojo.execute();
+    	assertUnpacked( false, UNPACKED_FILE_PREFIX + 1 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( false, UNPACKED_FILE_PREFIX + 11 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX );
+    	assertUnpacked( true, UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX );
+    	assertMarkerFiles( mojo.getArtifactItems(), true );
+    	    	
+    	//Now run again and make sure the extracted files haven't gotten overwritten
+    	File destFile2 = new File( mojo.getOutputDirectory().getAbsolutePath() , UNPACKED_FILE_PREFIX + 2 + UNPACKED_FILE_SUFFIX );
+    	File destFile3 = new File( mojo.getOutputDirectory().getAbsolutePath() , UNPACKED_FILE_PREFIX + 3 + UNPACKED_FILE_SUFFIX );
+    	long time = System.currentTimeMillis();
+        time = time - ( time % 1000 );
+        destFile2.setLastModified( time );
+        destFile3.setLastModified( time );
+        assertEquals( time, destFile2.lastModified() );
+        assertEquals( time, destFile3.lastModified() );
+        Thread.sleep( 100 );
+    	mojo.execute();
+    	assertEquals( time, destFile2.lastModified() );
+        assertEquals( time, destFile3.lastModified() );
+	}
+}
---
+++ src/test/java/org/apache/maven/plugin/dependency/utils/markers/TestUnpackMarkerFileHandler.java
@@ -0,0 +1,264 @@
+package org.apache.maven.plugin.dependency.utils.markers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.dependency.fromConfiguration.ArtifactItem;
+import org.apache.maven.plugin.dependency.testUtils.DependencyArtifactStubFactory;
+import org.apache.maven.plugin.dependency.testUtils.DependencyTestUtils;
+import org.apache.maven.plugin.dependency.testUtils.stubs.StubUnpackFileMarkerHandler;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.testing.SilentLog;
+
+public class TestUnpackMarkerFileHandler
+	extends AbstractMojoTestCase
+{
+	List artifactItems = new ArrayList();
+
+    Log log = new SilentLog();
+
+    File outputFolder;
+    
+    protected File testDir;
+    
+    protected DependencyArtifactStubFactory stubFactory;
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        
+        testDir = new File( getBasedir(), "target" + File.separatorChar + "unit-tests" + File.separatorChar
+                + "unpack-markers" + File.separatorChar );
+        DependencyTestUtils.removeDirectory( testDir );
+        assertFalse( testDir.exists() );
+
+        stubFactory = new DependencyArtifactStubFactory( this.testDir, false );
+        Artifact artifact = stubFactory.createArtifact( "test", "test", "1" );
+        ArtifactItem artifactItem = stubFactory.getArtifactItem( artifact );
+        artifactItems.add( stubFactory.getArtifactItem( stubFactory.createArtifact( "test", "test", "1" ) ) );
+        artifact = stubFactory.createArtifact("test2", "test2", "2");
+        artifactItem = new ArtifactItem( artifact );
+        artifactItem.setIncludes( "**/*.xml" );
+        artifactItems.add( artifactItem );
+        artifact = stubFactory.createArtifact("test3", "test3", "3");
+        artifactItem = new ArtifactItem( artifact );
+        artifactItem.setExcludes( "**/*.class" );
+        artifactItems.add( artifactItem );
+        artifact = stubFactory.createArtifact("test4", "test4", "4");
+        artifactItem = new ArtifactItem( artifact );
+        artifactItem.setIncludes( "**/*.xml" );
+        artifactItem.setExcludes( "**/*.class" );
+        artifactItems.add( artifactItem );
+
+        outputFolder = new File( "target/markers/" );
+        DependencyTestUtils.removeDirectory( this.outputFolder );
+        assertFalse( outputFolder.exists() );
+    }
+
+    protected void tearDown()
+        throws IOException
+    {
+        DependencyTestUtils.removeDirectory( this.outputFolder );
+    }
+
+    /**
+     * 
+     * Assert that default functionallity still exists
+     * 
+     */
+    
+    public void testSetMarker()
+        throws MojoExecutionException
+    {
+        UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( (ArtifactItem) artifactItems.get( 0 ),
+                                                                         this.outputFolder );
+        assertFalse( handler.isMarkerSet() );
+        handler.setMarker();
+        assertTrue( handler.isMarkerSet() );
+        handler.clearMarker();
+        assertFalse( handler.isMarkerSet() );
+
+        handler.setMarker();
+        assertTrue( handler.isMarkerSet() );
+        handler.setMarker();
+        assertTrue( handler.isMarkerSet() );
+
+        handler.clearMarker();
+        assertFalse( handler.isMarkerSet() );
+        handler.clearMarker();
+        assertFalse( handler.isMarkerSet() );
+    }
+
+    public void testMarkerFile()
+        throws MojoExecutionException, IOException
+    {
+    	UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( (ArtifactItem) artifactItems.get( 0 ),
+                                                                         this.outputFolder );
+
+        File handle = handler.getMarkerFile();
+        assertFalse( handle.exists() );
+        assertFalse( handler.isMarkerSet() );
+
+        handler.setMarker();
+        assertTrue( handler.isMarkerSet() );
+        assertTrue( handle.exists() );
+
+        handle.delete();
+        assertFalse( handler.isMarkerSet() );
+
+        handle.createNewFile();
+        assertTrue( handler.isMarkerSet() );
+
+        handler.clearMarker();
+        assertFalse( handle.exists() );
+    }
+
+    public void testMarkerTimeStamp()
+        throws MojoExecutionException, IOException, InterruptedException
+    {
+        File theFile = new File( outputFolder, "theFile.jar" );
+        outputFolder.mkdirs();
+        theFile.createNewFile();
+        ArtifactItem theArtifactItem = (ArtifactItem) artifactItems.get( 0 );
+        Artifact theArtifact = theArtifactItem.getArtifact();
+        theArtifact.setFile( theFile );
+        UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( theArtifactItem, this.outputFolder );
+        assertFalse( handler.isMarkerSet() );
+        // if the marker is not set, assume it is infinately older than the
+        // artifact.
+        assertTrue( handler.isMarkerOlder( theArtifact ) );
+        handler.setMarker();
+        assertFalse( handler.isMarkerOlder( theArtifact ) );
+
+        theFile.setLastModified( theFile.lastModified() + 60000 );
+        assertTrue( handler.isMarkerOlder( theArtifact ) );
+
+        theFile.delete();
+        handler.clearMarker();
+        assertFalse( handler.isMarkerSet() );
+    }
+
+    public void testMarkerFileException()
+    {
+        // this stub wraps the file with an object to throw exceptions
+        StubUnpackFileMarkerHandler handler = new StubUnpackFileMarkerHandler( (ArtifactItem) artifactItems.get( 0 ),
+                                                                                 this.outputFolder );
+        try
+        {
+            handler.setMarker();
+            fail( "Expected an Exception here" );
+        }
+        catch ( MojoExecutionException e )
+        {
+
+        }
+    }
+
+    public void testGetterSetter()
+    {
+        UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( null, null );
+        assertTrue( handler.getArtifactItem() == null );
+        assertTrue( handler.getArtifact() == null );
+        handler.setArtifactItem( (ArtifactItem) artifactItems.get( 0 ) );
+        assertSame( artifactItems.get( 0 ), handler.getArtifactItem() );
+        assertSame( ((ArtifactItem) artifactItems.get( 0 )).getArtifact(), handler.getArtifact() );
+
+        assertTrue( handler.getMarkerFilesDirectory() == null );
+        handler.setMarkerFilesDirectory( outputFolder );
+        assertSame( outputFolder, handler.getMarkerFilesDirectory() );
+    }
+
+    public void testNullParent()
+        throws MojoExecutionException
+    {
+        // the parent isn't set so this will create the marker in the local
+        // folder. We must clear the
+        // marker to avoid leaving test droppings in root.
+    	UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( null, null );
+        handler.setArtifactItem( (ArtifactItem) artifactItems.get( 0 ) );
+        handler.setMarker();
+        assertTrue( handler.isMarkerSet() );
+        handler.clearMarker();
+        assertFalse( handler.isMarkerSet() );
+    }
+    
+    public void testIncludesMarker()
+    	throws MojoExecutionException, IOException
+	{
+    	UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( (ArtifactItem) artifactItems.get( 1 ), outputFolder );
+    	File handle = handler.getMarkerFile();
+        assertFalse( handle.exists() );
+        assertFalse( handler.isMarkerSet() );
+
+        handler.setMarker();
+        assertTrue( handler.isMarkerSet() );
+        assertTrue( handle.exists() );
+        String hashCode = "" + ( 0 + "**/*.xml".hashCode() );
+        assertTrue( handle.getName().indexOf( hashCode ) > -1 );
+
+        handle.delete();
+        assertFalse( handler.isMarkerSet() );
+
+        handle.createNewFile();
+        assertTrue( handler.isMarkerSet() );
+
+        handler.clearMarker();
+        assertFalse( handle.exists() );
+	}
+    
+    public void testExcludesMarker()
+		throws MojoExecutionException, IOException
+	{
+		UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( (ArtifactItem) artifactItems.get( 2 ), outputFolder );
+		File handle = handler.getMarkerFile();
+	    assertFalse( handle.exists() );
+	    assertFalse( handler.isMarkerSet() );
+	
+	    handler.setMarker();
+	    assertTrue( handler.isMarkerSet() );
+	    assertTrue( handle.exists() );
+	    String hashCode = "" + ( 0 + "**/*.class".hashCode() );
+	    assertTrue( handle.getName().indexOf( hashCode ) > -1 );
+	
+	    handle.delete();
+	    assertFalse( handler.isMarkerSet() );
+	
+	    handle.createNewFile();
+	    assertTrue( handler.isMarkerSet() );
+	
+	    handler.clearMarker();
+	    assertFalse( handle.exists() );
+	}
+    
+    public void testIncludesExcludesMarker()
+		throws MojoExecutionException, IOException
+	{
+		UnpackFileMarkerHandler handler = new UnpackFileMarkerHandler( (ArtifactItem) artifactItems.get( 3 ), outputFolder );
+		File handle = handler.getMarkerFile();
+	    assertFalse( handle.exists() );
+	    assertFalse( handler.isMarkerSet() );
+	
+	    handler.setMarker();
+	    assertTrue( handler.isMarkerSet() );
+	    assertTrue( handle.exists() );
+	    String hashCode = "" + ( 0 + "**/*.class".hashCode() + "**/*.xml".hashCode() );
+	    assertTrue( handle.getName().indexOf( hashCode ) > -1 );
+	
+	    handle.delete();
+	    assertFalse( handler.isMarkerSet() );
+	
+	    handle.createNewFile();
+	    assertTrue( handler.isMarkerSet() );
+	
+	    handler.clearMarker();
+	    assertFalse( handle.exists() );
+	}
+}
+
+
