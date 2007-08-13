63ab651a07a26811a2c21e62808fade4caf4b9e6
John Dennis Casey <jdcasey@apache.org>
2007-08-13 20:10:33 +0000

    Resolve dependencies before calling the repository assembler, to prevent skipping out because project.getDependencyArtifacts() is null.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@565497 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/archive/phase/RepositoryAssemblyPhase.java
+++ maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/archive/phase/RepositoryAssemblyPhase.java
@@ -1,15 +1,21 @@
 package org.apache.maven.plugin.assembly.archive.phase;
 
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
 import org.apache.maven.plugin.assembly.AssemblerConfigurationSource;
 import org.apache.maven.plugin.assembly.archive.ArchiveCreationException;
 import org.apache.maven.plugin.assembly.archive.phase.wrappers.RepoBuilderConfigSourceWrapper;
 import org.apache.maven.plugin.assembly.archive.phase.wrappers.RepoInfoWrapper;
 import org.apache.maven.plugin.assembly.archive.task.AddDirectoryTask;
+import org.apache.maven.plugin.assembly.artifact.DependencyResolver;
 import org.apache.maven.plugin.assembly.format.AssemblyFormattingException;
 import org.apache.maven.plugin.assembly.model.Assembly;
 import org.apache.maven.plugin.assembly.model.Repository;
 import org.apache.maven.plugin.assembly.utils.AssemblyFormatUtils;
 import org.apache.maven.plugin.assembly.utils.TypeConversionUtils;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.artifact.InvalidDependencyVersionException;
 import org.apache.maven.shared.repository.RepositoryAssembler;
 import org.apache.maven.shared.repository.RepositoryAssemblyException;
 import org.apache.maven.shared.repository.RepositoryBuilderConfigSource;
@@ -19,7 +25,9 @@ import org.codehaus.plexus.logging.AbstractLogEnabled;
 
 import java.io.File;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @plexus.component role="org.apache.maven.plugin.assembly.archive.phase.AssemblyArchiverPhase"
@@ -35,15 +43,21 @@ public class RepositoryAssemblyPhase
      */
     private RepositoryAssembler repositoryAssembler;
 
+    /**
+     * @plexus.requirement
+     */
+    private DependencyResolver dependencyResolver;
+
     public RepositoryAssemblyPhase()
     {
         // used for plexus.
     }
 
     // introduced for testing.
-    public RepositoryAssemblyPhase( RepositoryAssembler repositoryAssembler )
+    public RepositoryAssemblyPhase( RepositoryAssembler repositoryAssembler, DependencyResolver resolver )
     {
         this.repositoryAssembler = repositoryAssembler;
+        dependencyResolver = resolver;
     }
 
     public void execute( Assembly assembly, Archiver archiver, AssemblerConfigurationSource configSource )
@@ -57,6 +71,8 @@ public class RepositoryAssemblyPhase
         {
             Repository repository = (Repository) i.next();
 
+            resolveDependencies(repository, configSource);
+
             File repositoryDirectory = new File( tempRoot, repository.getOutputDirectory() );
 
             if ( !repositoryDirectory.exists() )
@@ -66,7 +82,9 @@ public class RepositoryAssemblyPhase
 
             try
             {
+                getLogger().debug( "Assembling repository to: " + repositoryDirectory );
                 repositoryAssembler.buildRemoteRepository( repositoryDirectory, wrap( repository ), wrap( configSource ) );
+                getLogger().debug( "Finished assembling repository to: " + repositoryDirectory );
             }
             catch ( RepositoryAssemblyException e )
             {
@@ -86,6 +104,42 @@ public class RepositoryAssemblyPhase
 
             task.execute( archiver, configSource );
         }
+    }
+
+    private void resolveDependencies( Repository repository, AssemblerConfigurationSource configSource )
+        throws ArchiveCreationException
+    {
+        MavenProject project = configSource.getProject();
+
+        ArtifactRepository localRepository = configSource.getLocalRepository();
+
+        List additionalRemoteRepositories = configSource.getRemoteRepositories();
+
+        Set dependencyArtifacts;
+        try
+        {
+            dependencyArtifacts = dependencyResolver
+                .resolveDependencies( project, repository.getScope(), localRepository, additionalRemoteRepositories );
+
+            if ( ( dependencyArtifacts != null ) && !dependencyArtifacts.isEmpty() )
+            {
+                dependencyArtifacts = new LinkedHashSet( dependencyArtifacts );
+            }
+        }
+        catch ( ArtifactResolutionException e )
+        {
+            throw new ArchiveCreationException( "Failed to resolve dependencies for project: " + project.getId(), e );
+        }
+        catch ( ArtifactNotFoundException e )
+        {
+            throw new ArchiveCreationException( "Failed to resolve dependencies for project: " + project.getId(), e );
+        }
+        catch ( InvalidDependencyVersionException e )
+        {
+            throw new ArchiveCreationException( "Failed to resolve dependencies for project: " + project.getId(), e );
+        }
+
+        project.setDependencyArtifacts( dependencyArtifacts );
     }
 
     private RepositoryBuilderConfigSource wrap( AssemblerConfigurationSource configSource )
