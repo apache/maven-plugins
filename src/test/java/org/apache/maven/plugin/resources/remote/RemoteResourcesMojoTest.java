7a7deff6fc8ca44062eb9dd54b9fb50f1714891d
Vincent Siveton <vsiveton@apache.org>
2007-08-16 11:04:08 +0000

    MRRESOURCES-15: ClassCast Exception happens when depending on the snapshot artifacts
    o renamed remoteRepositories to repositories to be more clear
    o added remoteArtifactRepositories field and use it, if artifact is a snapshot
    o fixed test case
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-remote-resources-plugin@566658 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/resources/remote/RemoteResourcesMojoTest.java
+++ src/test/java/org/apache/maven/plugin/resources/remote/RemoteResourcesMojoTest.java
@@ -93,7 +93,7 @@ public class RemoteResourcesMojoTest
                             new String[] { "SIMPLE.txt" },
                             null );
     }
-    
+
     public void testSimpleBundles()
         throws Exception
     {
@@ -104,7 +104,7 @@ public class RemoteResourcesMojoTest
                                                                         } );
 
         setupDefaultProject( project );
-        
+
         ArtifactRepository repo = (ArtifactRepository) getVariableValueFromObject( mojo, "localRepository" );
         String path = repo.pathOf( new DefaultArtifact( "test",
                                                         "test",
@@ -113,21 +113,21 @@ public class RemoteResourcesMojoTest
                                                         "jar",
                                                         "",
                                                         new DefaultArtifactHandler() ) );
-        
+
         File file = new File( repo.getBasedir() + "/" + path + ".jar" );
         file.getParentFile().mkdirs();
         buildResourceBundle( "default-simplebundles-create",
                              new String[] { "SIMPLE.txt" },
                              file );
-                             
-        
+
+
         mojo.execute();
-        
+
         file = (File) getVariableValueFromObject( mojo, "outputDirectory" );
-        file = new File( file, "SIMPLE.txt" ); 
+        file = new File( file, "SIMPLE.txt" );
         assertTrue( file.exists() );
     }
-        
+
     public void testFilteredBundles()
         throws Exception
     {
@@ -136,9 +136,9 @@ public class RemoteResourcesMojoTest
                                                                         new String[] {
                                                                             "test:test:1.1"
                                                                         } );
-    
+
         setupDefaultProject( project );
-        
+
         ArtifactRepository repo = (ArtifactRepository) getVariableValueFromObject( mojo, "localRepository" );
         String path = repo.pathOf( new DefaultArtifact( "test",
                                                         "test",
@@ -147,22 +147,22 @@ public class RemoteResourcesMojoTest
                                                         "jar",
                                                         "",
                                                         new DefaultArtifactHandler() ) );
-        
+
         File file = new File( repo.getBasedir() + "/" + path + ".jar" );
         file.getParentFile().mkdirs();
         buildResourceBundle( "default-filterbundles-create",
                              new String[] { "FILTER.txt.vm" },
                              file );
-                             
-        
+
+
         mojo.execute();
         // executing a second time (example: forked lifecycle) should still work
         mojo.execute();
-        
+
         file = (File) getVariableValueFromObject( mojo, "outputDirectory" );
-        file = new File( file, "FILTER.txt" ); 
+        file = new File( file, "FILTER.txt" );
         assertTrue( file.exists() );
-        
+
         String data = FileUtils.fileRead( file );
         assertTrue( data.indexOf( "2007" ) != -1 );
         assertTrue( data.indexOf( "default-filterbundles" ) != -1 );
@@ -171,34 +171,34 @@ public class RemoteResourcesMojoTest
     protected void buildResourceBundle( String id,
                                        String resourceNames[],
                                        File jarName )
-    throws Exception 
+    throws Exception
     {
         final MavenProjectResourcesStub project = createTestProject( id );
 
         final File resourceDir = new File( project.getBasedir() + "/src/main/resources" );
         final BundleRemoteResourcesMojo mojo = lookupBundleMojoWithSettings( project , resourceDir );
-        
+
         setupDefaultProject( project );
-        
+
         for ( int x = 0; x < resourceNames.length; x++ )
         {
             File resource = new File( resourceDir, resourceNames[x] );
             URL source = getClass().getResource( "/" + resourceNames[x] );
-        
+
             FileUtils.copyURLToFile( source, resource );
         }
-        
+
         mojo.execute();
-        
+
         File xmlFile = new File( project.getBasedir() + "/target/classes/META-INF/maven/remote-resources.xml" );
         assertTrue( xmlFile.exists() );
-        
+
         String data = FileUtils.fileRead( xmlFile );
         for ( int x = 0; x < resourceNames.length; x++ )
         {
             assertTrue( data.indexOf( resourceNames[x] ) != -1 );
         }
-        
+
         if ( null != jarName )
         {
             JarOutputStream jar = new JarOutputStream( new FileOutputStream( jarName ) );
@@ -217,8 +217,8 @@ public class RemoteResourcesMojoTest
             jar.close();
         }
     }
-    
-    
+
+
 
     protected MavenProjectResourcesStub createTestProject( final String testName )
     throws Exception
@@ -237,7 +237,7 @@ public class RemoteResourcesMojoTest
         project.setupBuildEnvironment();
     }
 
-    
+
     protected BundleRemoteResourcesMojo lookupBundleMojo()
     throws Exception
     {
@@ -245,7 +245,7 @@ public class RemoteResourcesMojoTest
         BundleRemoteResourcesMojo mojo = (BundleRemoteResourcesMojo) lookupMojo( "bundle", pomFile );
 
         assertNotNull( mojo );
-        
+
         return mojo;
     }
     protected BundleRemoteResourcesMojo lookupBundleMojoWithDefaultSettings( final MavenProject project )
@@ -259,7 +259,7 @@ public class RemoteResourcesMojoTest
     throws Exception
     {
         final BundleRemoteResourcesMojo mojo = lookupBundleMojo();
-    
+
         setVariableValueToObject( mojo, "resourcesDirectory", resourceDir );
         setVariableValueToObject( mojo, "outputDirectory", new File( project.getBuild().getOutputDirectory() ) );
         return mojo;
@@ -283,29 +283,28 @@ public class RemoteResourcesMojoTest
     {
         return lookupProcessMojoWithSettings( project, new ArrayList( Arrays.asList( bundles ) ) );
     }
-    
+
     protected ProcessRemoteResourcesMojo lookupProcessMojoWithSettings( final MavenProject project,
                                                                  ArrayList bundles )
         throws Exception
     {
         final ProcessRemoteResourcesMojo mojo = lookupProcessMojo();
-        
+
         MavenSession session = new MavenSession( container,
                                     null, //Settings settings,
                                     null, //ArtifactRepository localRepository,
                                     null, //EventDispatcher eventDispatcher,
-                                    null, //ReactorManager reactorManager, 
+                                    null, //ReactorManager reactorManager,
                                     Arrays.asList( new String[] {"install"} ),
                                     project.getBasedir().toString(),
                                     new Properties(),
                                     Calendar.getInstance().getTime() );
 
-        
         setVariableValueToObject( mojo, "project", project );
         setVariableValueToObject( mojo, "outputDirectory", new File( project.getBuild().getOutputDirectory() ) );
         setVariableValueToObject( mojo, "resourceBundles", bundles );
         setVariableValueToObject( mojo, "mavenSession", session );
-        setVariableValueToObject( mojo, "remoteRepositories", project.getRemoteArtifactRepositories() );
+        setVariableValueToObject( mojo, "repositories", project.getRemoteArtifactRepositories() );
         setVariableValueToObject( mojo, "resources", project.getResources() );
         return mojo;
     }
