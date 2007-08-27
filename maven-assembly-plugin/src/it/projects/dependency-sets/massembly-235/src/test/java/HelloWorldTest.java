bb1140482b03d8fee065d29b7ecadc5e28908d45
John Dennis Casey <jdcasey@apache.org>
2007-08-27 14:34:01 +0000

    [MASSEMBLY-235] Adding test case to integration tests, and verifying that this problem is already fixed in 2.2-beta-2-SNAPSHOT.
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@570143 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-assembly-plugin/src/it/projects/dependency-sets/massembly-235/src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+public class HelloWorldTest extends TestCase
+{
+	public static final void testSayHello()
+	{
+		assertEquals("Hello World!", HelloWorld.sayHello("World"));
+	}
+}
