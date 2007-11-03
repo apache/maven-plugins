a786ceacd50eed7379d2515d6def7909c950b125
Brett Porter <brett@apache.org>
2007-11-03 06:04:07 +0000

    add runtime deletion of temporary files. Add deletion safeguards on tests, though the tearDown method should take care of it already, because there are some being left behind on the CI machine
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@591556 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/assembly/format/FileSetFormatter.java
+++ src/main/java/org/apache/maven/plugin/assembly/format/FileSetFormatter.java
@@ -1,14 +1,14 @@
 package org.apache.maven.plugin.assembly.format;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.apache.maven.plugin.assembly.AssemblerConfigurationSource;
 import org.apache.maven.plugin.assembly.utils.AssemblyFileUtils;
 import org.apache.maven.shared.model.fileset.FileSet;
 import org.apache.maven.shared.model.fileset.util.FileSetManager;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 public class FileSetFormatter
 {
@@ -26,8 +26,6 @@ public class FileSetFormatter
     public File formatFileSetForAssembly( File archiveBaseDir, org.apache.maven.plugin.assembly.model.FileSet set )
         throws AssemblyFormattingException, IOException
     {
-        File formattedDir = archiveBaseDir;
-
         String lineEndingHint = set.getLineEnding();
 
         String lineEnding = AssemblyFileUtils.getLineEndingCharacters( lineEndingHint );
@@ -40,11 +38,6 @@ public class FileSetFormatter
             fileSet.setDirectory( set.getDirectory() );
             fileSet.setIncludes( set.getIncludes() );
 
-            formattedDir = FileUtils.createTempFile( "fileSetFormatter.", ".tmp", configSource.getTemporaryRootDirectory() );
-
-            formattedDir.delete();
-            formattedDir.mkdirs();
-
             fileSet.setExcludes( set.getExcludes() );
             fileSet.setUseDefaultExcludes( true );
 
@@ -55,28 +48,58 @@ public class FileSetFormatter
             if ( ( files == null ) || ( files.length == 0 ) )
             {
                 logger.info( "No files selected for line-ending conversion. Skipping: " + fileSet.getDirectory() );
+            }
+            else
+            {
+                File formattedDir =
+                    FileUtils.createTempFile( "fileSetFormatter.", ".tmp", configSource.getTemporaryRootDirectory() );
 
                 formattedDir.delete();
+                formattedDir.mkdirs();
 
-                return archiveBaseDir;
-            }
+                FileFormatter fileFormatter = new FileFormatter( configSource, logger );
+                for ( int i = 0; i < files.length; i++ )
+                {
+                    String file = files[i];
 
-            FileFormatter fileFormatter = new FileFormatter( configSource, logger );
-            for ( int i = 0; i < files.length; i++ )
-            {
-                String file = files[i];
+                    File targetFile = new File( formattedDir, file );
 
-                File targetFile = new File( formattedDir, file );
+                    targetFile.getParentFile().mkdirs();
 
-                targetFile.getParentFile().mkdirs();
-
-                File sourceFile = new File( archiveBaseDir, file );
-                sourceFile = fileFormatter.format( sourceFile, set.isFiltered(), lineEndingHint, formattedDir );
-                AssemblyFileUtils.copyFile(sourceFile,targetFile);
+                    File sourceFile = new File( archiveBaseDir, file );
+                    try
+                    {
+                        sourceFile = fileFormatter.format( sourceFile, set.isFiltered(), lineEndingHint, formattedDir );
+                        AssemblyFileUtils.copyFile( sourceFile, targetFile );
+                    }
+                    catch ( AssemblyFormattingException e )
+                    {
+                        deleteDirectory( formattedDir );
+                        throw e;
+                    }
+                    catch ( IOException e )
+                    {
+                        deleteDirectory( formattedDir );
+                        throw e;
+                    }
+                }
+                return formattedDir;
             }
         }
 
-        return formattedDir;
+        return archiveBaseDir;
+    }
+
+    private static void deleteDirectory( File formattedDir )
+    {
+        try
+                        {
+                            FileUtils.deleteDirectory( formattedDir );
+        }
+        catch ( IOException e1 )
+        {
+            // ignore
+        }
     }
 
 }
