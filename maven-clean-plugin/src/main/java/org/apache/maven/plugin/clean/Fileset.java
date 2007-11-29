ab22ed094e57634e9dfb9f9a689ae2bde5e16b96
Vincent Siveton <vsiveton@apache.org>
2007-11-29 12:37:28 +0000

    o added since tag
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@599416 13f79535-47bb-0310-9956-ffa450edef68

--- maven-clean-plugin/src/main/java/org/apache/maven/plugin/clean/Fileset.java
+++ maven-clean-plugin/src/main/java/org/apache/maven/plugin/clean/Fileset.java
@@ -22,31 +22,31 @@ package org.apache.maven.plugin.clean;
 import org.apache.maven.shared.model.fileset.FileSet;
 
 /**
- * Customizes the string representation of 
- * <code>org.apache.maven.shared.model.fileset.FileSet</code> to return the 
- * included and excluded files from the file-set's directory. Specifically, 
- * <code>"file-set: <I>[directory]</I> (included: <I>[included files]</I>, 
- * excluded: <I>[excluded files]</I>)"</code>   
- *  
+ * Customizes the string representation of
+ * <code>org.apache.maven.shared.model.fileset.FileSet</code> to return the
+ * included and excluded files from the file-set's directory. Specifically,
+ * <code>"file-set: <I>[directory]</I> (included: <I>[included files]</I>,
+ * excluded: <I>[excluded files]</I>)"</code>
+ *
+ * @version $Id$
+ * @since 2.1
  * @see org.apache.maven.shared.model.fileset.FileSet
  */
 public class Fileset
     extends FileSet
 {
-
     /**
      * Retrieves the included and excluded files from this file-set's directory.
-     * Specifically, <code>"file-set: <I>[directory]</I> (included: 
-     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>   
-     * 
+     * Specifically, <code>"file-set: <I>[directory]</I> (included:
+     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>
+     *
      * @return The included and excluded files from this file-set's directory.
-     * Specifically, <code>"file-set: <I>[directory]</I> (included: 
-     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>   
+     * Specifically, <code>"file-set: <I>[directory]</I> (included:
+     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>
      * @see java.lang.Object#toString()
      */
     public String toString()
     {
         return "file-set: " + getDirectory() + " (included: " + getIncludes() + ", excluded: " + getExcludes() + ")";
     }
-
 }
