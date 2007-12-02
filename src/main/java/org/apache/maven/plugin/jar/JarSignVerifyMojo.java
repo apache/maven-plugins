2bd06d06d9bd86667bea0cba5547d3760d3d3b6f
Vincent Siveton <vsiveton@apache.org>
2007-12-02 13:34:22 +0000

    o fixed license header
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-jar-plugin@600286 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/jar/JarSignVerifyMojo.java
+++ src/main/java/org/apache/maven/plugin/jar/JarSignVerifyMojo.java
@@ -1,19 +1,22 @@
 package org.apache.maven.plugin.jar;
 
 /*
- * Copyright 2001-2006 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License" );
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import org.apache.maven.plugin.logging.Log;
@@ -210,7 +213,7 @@ public class JarSignVerifyMojo
             getLog().info( line );
         }
     }
- 
+
   // taken from JavadocReport then slightly refactored
     // should probably share with other plugins that use $JAVA_HOME/bin tools
 
@@ -329,7 +332,7 @@ public class JarSignVerifyMojo
     //
 
     protected int executeCommandLine( Commandline commandLine, InputStream inputStream,
-                                      StreamConsumer systemOut, StreamConsumer systemErr ) 
+                                      StreamConsumer systemOut, StreamConsumer systemErr )
         throws CommandLineException
     {
         return CommandLineUtils.executeCommandLine( commandLine, inputStream, systemOut, systemErr );
