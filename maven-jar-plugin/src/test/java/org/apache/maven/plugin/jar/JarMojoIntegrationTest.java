6a6779614013a644f2b75bad3a40d127597a0f77
Vincent Siveton <vsiveton@apache.org>
2007-12-02 13:34:22 +0000

    o fixed license header
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@600286 13f79535-47bb-0310-9956-ffa450edef68

--- maven-jar-plugin/src/test/java/org/apache/maven/plugin/jar/JarMojoIntegrationTest.java
+++ maven-jar-plugin/src/test/java/org/apache/maven/plugin/jar/JarMojoIntegrationTest.java
@@ -1,23 +1,26 @@
 package org.apache.maven.plugin.jar;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.File;
 import java.util.Properties;
-
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 /**
  * Test for {@link JarMojo}
@@ -31,7 +34,7 @@ public class JarMojoIntegrationTest
         return "jar";
     }
     /**
-     * Tests the normal behavior of jar-plugin. 
+     * Tests the normal behavior of jar-plugin.
      * @throws Exception
      */
     public void testMJar_30_01()
