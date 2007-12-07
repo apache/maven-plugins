78b0bed45e91425482e9c36159d03e2d0084710e
Oliver Lamy <olamy@apache.org>
2007-12-07 23:52:57 +0000

    add an it for MJAR-60
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@602276 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-jar-plugin/src/it/MJAR-60/src/main/java/myproject/HelloWorld.java
@@ -0,0 +1,16 @@
+package myproject;
+
+/**
+ * The classic Hello World App.
+ */
+public class HelloWorld {
+
+  /**
+   * Main method.
+   *
+   * @param args Not used
+   */
+  public static void main(String[] args) {
+    System.out.println("Hi!");
+  } 
+}
