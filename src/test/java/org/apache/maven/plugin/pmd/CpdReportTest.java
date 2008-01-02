fb85c5d2ea74cd2080c3b0e73d6a1d9f339b42e2
Dennis Lundberg <dennisl@apache.org>
2008-01-02 19:59:56 +0000

    o Remove unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-pmd-plugin@608202 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/pmd/CpdReportTest.java
+++ src/test/java/org/apache/maven/plugin/pmd/CpdReportTest.java
@@ -19,10 +19,8 @@ package org.apache.maven.plugin.pmd;
  * under the License.
  */
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
