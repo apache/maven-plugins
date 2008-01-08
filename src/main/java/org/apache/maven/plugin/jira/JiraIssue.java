8b809b038d07a1abeb6a1b9f016303c3a322ab7f
Dennis Lundberg <dennisl@apache.org>
2008-01-08 23:30:49 +0000

    [MCHANGES-92] JIRA Report Improvements Submitted by: Niall Pemberton Reviewed by: Dennis Lundberg
    o Add configuration of "Fix Versions" and "Types".
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-changes-plugin@610228 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/jira/JiraIssue.java
+++ src/main/java/org/apache/maven/plugin/jira/JiraIssue.java
@@ -26,13 +26,25 @@ package org.apache.maven.plugin.jira;
  */
 public class JiraIssue
 {
+    private String component;
+
+    private String fixVersion;
+
     private String key;
 
     private String link;
 
+    private String priority;
+
+    private String reporter;
+
     private String summary;
 
     private String status;
+
+    private String type;
+
+    private String version;
 
     private String resolution;
 
@@ -100,5 +112,65 @@ public class JiraIssue
     public void setAssignee( String assignee )
     {
         this.assignee = assignee;
+    }
+
+    public String getComponent()
+    {
+        return component;
+    }
+
+    public void setComponent( String component )
+    {
+        this.component = component;
+    }
+
+    public String getFixVersion()
+    {
+        return fixVersion;
+    }
+
+    public void setFixVersion( String fixVersion )
+    {
+        this.fixVersion = fixVersion;
+    }
+
+    public String getPriority()
+    {
+        return priority;
+    }
+
+    public void setPriority( String priority )
+    {
+        this.priority = priority;
+    }
+
+    public String getReporter()
+    {
+        return reporter;
+    }
+
+    public void setReporter( String reporter )
+    {
+        this.reporter = reporter;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType( String type )
+    {
+        this.type = type;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion( String version )
+    {
+        this.version = version;
     }
 }
