91880866dbdf90d911d6fd82f551076f31e2ff17
Vincent Siveton <vsiveton@apache.org>
2008-01-10 23:10:00 +0000

    o fixed license header o no code change
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@610981 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/mojos/DirectoryInlineMojo.java
+++ maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/mojos/DirectoryInlineMojo.java
@@ -1,19 +1,22 @@
 package org.apache.maven.plugin.assembly.mojos;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import org.apache.maven.project.MavenProject;
@@ -24,7 +27,8 @@ import org.apache.maven.project.MavenProject;
  * for using at the root of a multimodule project, and is the unarchived counterpart to <code>assembly:attached</code>.
  *
  * @author <a href="mailto:gscokart@users.sourceforge.net">Gilles Scokart</a>
- * 
+ * @version $Id$
+ *
  * @goal directory-inline
  * @aggregator
  */
@@ -37,7 +41,7 @@ public class DirectoryInlineMojo
      * @readonly
      */
     private MavenProject project;
-    
+
     public MavenProject getProject()
     {
         return project;
--- maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/utils/TypeConversionUtils.java
+++ maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/utils/TypeConversionUtils.java
@@ -1,51 +1,73 @@
 package org.apache.maven.plugin.assembly.utils;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.maven.plugin.assembly.format.AssemblyFormattingException;
 import org.codehaus.plexus.logging.Logger;
 
 import java.util.List;
 
+/**
+ * @version $Id$
+ */
 public final class TypeConversionUtils
 {
-    
+
     private static final int U_R = 256;
     private static final int U_W = 128;
     private static final int U_X = 64;
-    
+
     private static final int G_R = 32;
     private static final int G_W = 16;
     private static final int G_X = 8;
-    
+
     private static final int W_R = 4;
     private static final int W_W = 2;
     private static final int W_X = 1;
-    
+
     private TypeConversionUtils()
     {
     }
-    
+
     public static String[] toStringArray( List list )
     {
         String[] result = null;
-        
+
         if ( list != null && !list.isEmpty() )
         {
             result = (String[]) list.toArray( new String[0] );
         }
-        
+
         return result;
     }
-    
+
     public static int modeToInt( String mode, Logger logger )
         throws AssemblyFormattingException
     {
         try
         {
             int value = Integer.parseInt( mode, 8 );
-            
+
             // discard sanity assessment here; we're pushing ahead.
             verifyModeSanity( value, logger );
-            
+
             return value;
         }
         catch( NumberFormatException e )
@@ -59,77 +81,77 @@ public final class TypeConversionUtils
     public static boolean verifyModeSanity( int mode, Logger logger )
     {
         StringBuffer messages = new StringBuffer();
-        
+
         messages.append( "The mode: " + Integer.toString( mode, 8 ) + " contains nonsensical permissions:" );
-        
+
         boolean warn = false;
-        
+
         // read-access checks.
         if ( ( mode & U_R ) == 0 && ( mode & G_R ) == G_R )
         {
             messages.append( "\n- Group has read access, but user does not." );
             warn = true;
         }
-        
+
         if ( ( mode & U_R ) == 0 && ( mode & W_R ) == W_R )
         {
             messages.append( "\n- World has read access, but user does not." );
             warn = true;
         }
-        
+
         if ( ( mode & G_R ) == 0 && ( mode & W_R ) == W_R )
         {
             messages.append( "\n- World has read access, but group does not." );
             warn = true;
         }
         // end read-access checks.
-        
+
         // write-access checks.
         if ( ( mode & U_W ) == 0 && ( mode & G_W ) == G_W )
         {
             messages.append( "\n- Group has write access, but user does not." );
             warn = true;
         }
-        
+
         if ( ( mode & U_W ) == 0 && ( mode & W_W ) == W_W )
         {
             messages.append( "\n- World has write access, but user does not." );
             warn = true;
         }
-        
+
         if ( ( mode & G_W ) == 0 && ( mode & W_W ) == W_W )
         {
             messages.append( "\n- World has write access, but group does not." );
             warn = true;
         }
         // end write-access checks.
-        
+
         // execute-/list-access checks.
         if ( ( mode & U_X ) == 0 && ( mode & G_X ) == G_X )
         {
             messages.append( "\n- Group has execute/list access, but user does not." );
             warn = true;
         }
-        
+
         if ( ( mode & U_X ) == 0 && ( mode & W_X ) == W_X )
         {
             messages.append( "\n- World has execute/list access, but user does not." );
             warn = true;
         }
-        
+
         if ( ( mode & G_X ) == 0 && ( mode & W_X ) == W_X )
         {
             messages.append( "\n- World has execute/list access, but group does not." );
             warn = true;
         }
         // end execute-/list-access checks.
-        
+
         if ( warn )
         {
             logger.warn( messages.toString() );
         }
-        
+
         return !warn;
     }
-    
+
 }
