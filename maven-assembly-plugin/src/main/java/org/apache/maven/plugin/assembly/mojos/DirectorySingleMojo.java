e26f1d4e7b2735e49e5ababa6e4882a4f10a2e30
Vincent Siveton <vsiveton@apache.org>
2008-01-10 23:10:00 +0000

    o fixed license header o no code change
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@610981 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/mojos/DirectorySingleMojo.java
+++ maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/mojos/DirectorySingleMojo.java
@@ -1,31 +1,35 @@
 package org.apache.maven.plugin.assembly.mojos;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import org.apache.maven.project.MavenProject;
 
 /**
  * Assemble an application bundle or distribution from an assembly descriptor into the
- * target directory structure but do not generate the final archive. 
+ * target directory structure but do not generate the final archive.
  * This goal is suitable for binding to the lifecycle if you are not aggregating the content of any modules.
  * It is the unarchived counterpart to <code>assembly:single</code>.
  *
  * @author <a href="mailto:gscokart@users.sourceforge.net">Gilles Scokart</a>
- * 
+ * @version $Id$
+ *
  * @goal directory-single
  */
 public class DirectorySingleMojo
@@ -37,7 +41,7 @@ public class DirectorySingleMojo
      * @readonly
      */
     private MavenProject project;
-    
+
     public MavenProject getProject()
     {
         return project;
--- maven-assembly-plugin/src/test/java/org/apache/maven/plugin/assembly/archive/ManifestCreationFinalizerTest.java
+++ maven-assembly-plugin/src/test/java/org/apache/maven/plugin/assembly/archive/ManifestCreationFinalizerTest.java
@@ -1,5 +1,24 @@
 package org.apache.maven.plugin.assembly.archive;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.maven.archiver.MavenArchiveConfiguration;
 import org.apache.maven.model.Model;
 import org.apache.maven.plugin.assembly.testutils.MockManager;
@@ -27,7 +46,7 @@ public class ManifestCreationFinalizerTest
 {
 
     private TestFileManager fileManager = new TestFileManager( "manifest-finalizer.test.", ".jar" );
-    
+
     public void tearDown() throws IOException
     {
         fileManager.cleanUp();
@@ -61,35 +80,35 @@ public class ManifestCreationFinalizerTest
     {
         MavenProject project = new MavenProject( new Model() );
         MavenArchiveConfiguration config = new MavenArchiveConfiguration();
-        
+
         File tempDir = fileManager.createTempDir();
-        
+
         File manifestFile = fileManager.createFile( tempDir, "MANIFEST.MF", "Main-Class: Stuff" );
-        
+
         config.setManifestFile( manifestFile );
-        
+
         JarArchiver archiver = new JarArchiver();
-        
+
         archiver.setArchiveFinalizers( Collections.singletonList( new ManifestCreationFinalizer( project, config ) ) );
 
         File file = fileManager.createTempFile();
-        
+
         archiver.setDestFile( file );
-        
+
         archiver.createArchive();
-        
-        URL resource = new URL( "jar:file:" + file.getAbsolutePath() + "!/META-INF/MANIFEST.MF" );      
-        
+
+        URL resource = new URL( "jar:file:" + file.getAbsolutePath() + "!/META-INF/MANIFEST.MF" );
+
         BufferedReader reader = new BufferedReader( new InputStreamReader( resource.openStream() ) );
-        
+
         StringWriter writer = new StringWriter();
-        
+
         IOUtil.copy( reader, writer );
-        
+
         assertTrue( writer.toString().indexOf( "Main-Class: Stuff" ) > -1 );
-        
+
         // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4823678
-        ((JarURLConnection)resource.openConnection()).getJarFile().close();        
+        ((JarURLConnection)resource.openConnection()).getJarFile().close();
     }
 
     private final class MockAndControlForArchiver
