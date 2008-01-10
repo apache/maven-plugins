44e3ac22c4c5f32e60d5af59cb35aadb3c099ce7
Vincent Siveton <vsiveton@apache.org>
2008-01-10 23:10:00 +0000

    o fixed license header o no code change
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-assembly-plugin@610981 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/assembly/utils/AssemblyFileUtils.java
+++ src/main/java/org/apache/maven/plugin/assembly/utils/AssemblyFileUtils.java
@@ -1,5 +1,24 @@
 package org.apache.maven.plugin.assembly.utils;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -18,6 +37,9 @@ import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.util.IOUtil;
 
+/**
+ * @version $Id$
+ */
 public final class AssemblyFileUtils
 {
 
@@ -33,7 +55,7 @@ public final class AssemblyFileUtils
 
     public static void verifyTempDirectoryAvailability( final File tempDir, final Logger logger )
     {
-        if (!tempDir.exists()) 
+        if (!tempDir.exists())
         {
             tempDir.mkdirs();
         }
--- src/test/java/org/apache/maven/plugin/assembly/utils/TypeConversionUtilsTest.java
+++ src/test/java/org/apache/maven/plugin/assembly/utils/TypeConversionUtilsTest.java
@@ -1,5 +1,24 @@
 package org.apache.maven.plugin.assembly.utils;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.maven.plugin.assembly.format.AssemblyFormattingException;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.logging.console.ConsoleLogger;
@@ -54,36 +73,36 @@ public class TypeConversionUtilsTest
         List messages = new ArrayList( 2 );
         messages.add( "World has write access, but user does not." );
         messages.add( "World has write access, but group does not." );
-        
+
         checkFileModeSanity( "002", false, messages );
     }
-    
+
     public void testVerifyModeSanity_WarnOnNonsensicalOctalValue_020()
     {
         List messages = new ArrayList( 1 );
         messages.add( "Group has write access, but user does not." );
-        
+
         checkFileModeSanity( "020", false, messages );
     }
-    
+
     public void testVerifyModeSanity_ReturnTrueForValidOctalValue_775()
     {
         checkFileModeSanity( "775", true, Collections.EMPTY_LIST );
     }
-    
+
     private void checkFileModeSanity( String mode, boolean isSane, List messagesToCheckIfInsane )
     {
         StringOutputStream sos = new StringOutputStream();
         PrintStream ps = new PrintStream( sos );
-        
+
         PrintStream oldOut = System.out;
         PrintStream oldErr = System.err;
-        
+
         try
         {
             System.setOut( ps );
             System.setErr( ps );
-            
+
             assertEquals( "Mode sanity should be: " + isSane, isSane, TypeConversionUtils.verifyModeSanity( Integer.parseInt( mode, 8 ), new ConsoleLogger( Logger.LEVEL_WARN, "test" ) ) );
         }
         finally
@@ -91,15 +110,15 @@ public class TypeConversionUtilsTest
             System.setOut( oldOut );
             System.setErr( oldErr );
         }
-        
+
         if ( !isSane && messagesToCheckIfInsane != null && !messagesToCheckIfInsane.isEmpty() )
         {
             String message = sos.toString();
-            
+
             for ( Iterator it = messagesToCheckIfInsane.iterator(); it.hasNext(); )
             {
                 String checkMessage = (String) it.next();
-                
+
                 assertTrue( "\'" + checkMessage + "\' is not present in output.", message.indexOf( checkMessage ) > -1 );
             }
         }
