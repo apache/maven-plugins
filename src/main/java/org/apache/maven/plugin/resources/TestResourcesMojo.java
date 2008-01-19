6a33aebc5fb49d1a9569a86b32db338ca6185aff
Vincent Siveton <vsiveton@apache.org>
2008-01-19 23:52:44 +0000

    o fixed license header o no code change
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-resources-plugin@613480 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/resources/TestResourcesMojo.java
+++ src/main/java/org/apache/maven/plugin/resources/TestResourcesMojo.java
@@ -1,30 +1,32 @@
 package org.apache.maven.plugin.resources;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.util.List;
-
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.File;
 
 /**
  * Copy resources for the test source code to the test output directory.
- * 
+ *
  * @author <a href="michal.maczka@dimatics.com">Michal Maczka</a>
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  * @version $Id$
@@ -36,7 +38,7 @@ public class TestResourcesMojo
 {
     /**
      * The output directory into which to copy the resources.
-     * 
+     *
      * @parameter expression="${project.build.testOutputDirectory}"
      * @required
      */
@@ -44,7 +46,7 @@ public class TestResourcesMojo
 
     /**
      * The list of resources we want to transfer.
-     * 
+     *
      * @parameter expression="${project.testResources}"
      * @required
      */
