12665071ef9cd3d3450e6e38577ccc65bbc49a3c
Vincent Siveton <vsiveton@apache.org>
2008-01-19 23:52:44 +0000

    o fixed license header o no code change
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@613480 13f79535-47bb-0310-9956-ffa450edef68

--- maven-resources-plugin/src/test/java/org/apache/maven/plugin/resources/EnhancedPropertyUtilsTest.java
+++ maven-resources-plugin/src/test/java/org/apache/maven/plugin/resources/EnhancedPropertyUtilsTest.java
@@ -1,19 +1,22 @@
 package org.apache.maven.plugin.resources;
 
 /*
- * Copyright 2001-2006 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import java.io.File;
@@ -22,7 +25,7 @@ import java.util.Properties;
 
 /**
  * Tests {@link PropertyUtils#loadPropertyFile(File, Properties)}.
- * 
+ *
  * @author William Ferguson
  */
 public class EnhancedPropertyUtilsTest
@@ -68,7 +71,7 @@ public class EnhancedPropertyUtilsTest
 
     /**
      * Load property test case can be adjusted by modifying the enhanced.properties and enhanced_validation properties.
-     * 
+     *
      * @throws Exception
      */
     public void testBasicLoadProperty()
@@ -82,7 +85,7 @@ public class EnhancedPropertyUtilsTest
 
     /**
      * Load property test case can be adjusted by modifying the enhanced.properties and enhanced_validation properties.
-     * 
+     *
      * @throws Exception
      */
     public void testNonExistentProperty()
@@ -96,7 +99,7 @@ public class EnhancedPropertyUtilsTest
 
     /**
      * Load property test case can be adjusted by modifying the enhanced.properties and enhanced_validation properties.
-     * 
+     *
      * @throws Exception
      */
     public void testException()
