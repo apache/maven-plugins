56f9850d64f21ef4feea9ef9e6568d8b85c8200d
Vincent Siveton <vsiveton@apache.org>
2008-01-30 00:02:59 +0000

    MPIR-83: Include reports for DependencyManagement and PluginManagement Submitted by: Nick Stolwijk Reviewed by: Vincent Siveton
    o patch applied but renamed goals to be consistent
    o renamed also the values of the keys report.dependencyManagement.name and report.pluginManagement.name into "mngt" (for the side bar)
    o added @since in the javadoc
    o updated links in doc
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@616568 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/DependencyManagementReport.java
@@ -0,0 +1,79 @@
+package org.apache.maven.report.projectinfo;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Locale;
+
+import org.apache.maven.report.projectinfo.dependencies.ManagementDependencies;
+import org.apache.maven.report.projectinfo.dependencies.renderer.DependencyManagementRenderer;
+
+
+/**
+ * Generates the Project Dependency Management report.
+ *
+ * @author Nick Stolwijk
+ * @version $Id$
+ * @since 2.1
+ * @goal dependency-management
+ * @requiresDependencyResolution test
+ */
+public class DependencyManagementReport
+    extends AbstractProjectInfoReport
+{
+    // ----------------------------------------------------------------------
+    // Public methods
+    // ----------------------------------------------------------------------
+
+    /** {@inheritDoc} */
+    public String getName( Locale locale )
+    {
+        return i18n.getString( "project-info-report", locale, "report.dependencyManagement.name" );
+    }
+
+    /** {@inheritDoc} */
+    public String getDescription( Locale locale )
+    {
+        return i18n.getString( "project-info-report", locale, "report.dependencyManagement.description" );
+    }
+
+    /** {@inheritDoc} */
+    public void executeReport( Locale locale )
+    {
+
+        ManagementDependencies dependencies = new ManagementDependencies(project.getDependencyManagement().getDependencies());
+
+        DependencyManagementRenderer r = new DependencyManagementRenderer( getSink(), locale, i18n, dependencies );
+
+        r.setLog( getLog() );
+        r.render();
+    }
+
+    /** {@inheritDoc} */
+    public String getOutputName()
+    {
+        return "dependency-management";
+    }
+
+    public boolean canGenerateReport()
+    {
+
+        return project.getDependencyManagement() != null;
+    }
+}
---
+++ maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/dependencies/ManagementDependencies.java
@@ -0,0 +1,73 @@
+package org.apache.maven.report.projectinfo.dependencies;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
+
+/**
+ * @author Nick Stolwijk
+ * @version $Id:$
+ * @since 2.1
+ */
+public class ManagementDependencies
+{
+    private final List projectDependencies;
+
+    public ManagementDependencies( List projectDependencies )
+    {
+        this.projectDependencies = projectDependencies;
+    }
+
+    public boolean hasDependencies()
+    {
+        return ( projectDependencies != null ) && ( !this.projectDependencies.isEmpty() );
+    }
+
+    public List getProjectDependencies()
+    {
+        return new ArrayList( projectDependencies );
+    }
+
+    public Map getDependenciesByScope()
+    {
+        Map dependenciesByScope = new HashMap();
+        for ( Iterator i = getProjectDependencies().iterator(); i.hasNext(); )
+        {
+            Dependency dependency = (Dependency) i.next();
+            String scope = dependency.getScope() != null ? dependency.getScope() : Artifact.SCOPE_COMPILE;
+            List multiValue = (List) dependenciesByScope.get( scope );
+            if ( multiValue == null )
+            {
+                multiValue = new ArrayList();
+            }
+            multiValue.add( dependency );
+            dependenciesByScope.put( scope, multiValue );
+        }
+
+        return dependenciesByScope;
+    }
+}
