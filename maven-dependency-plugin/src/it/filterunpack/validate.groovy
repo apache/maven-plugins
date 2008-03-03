2d8a86aa894a57df3147ccfa34af8ba861cda4ee
Brian E Fox <brianf@apache.org>
2008-03-03 20:36:37 +0000

    added shitty plugin for a new IT to check the filtering during unpack
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@633266 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-dependency-plugin/src/it/filterunpack/validate.groovy
@@ -0,0 +1,21 @@
+expected = ['/META-INF/MANIFEST.MF']
+
+for (item in expected)
+{
+    def file = new File(basedir, 'target/dependency' + item)
+    if (!file.exists())
+    {
+       throw new RuntimeException("Missing "+file.name);
+    }
+}
+
+notExpected = ['/stylesheet.css']
+
+for (item in notExpected)
+{
+    def file = new File(basedir, 'target/dependency' + item)    
+    if (file.exists())
+    {
+       throw new RuntimeException("This file shouldn't be here: "+file.name);
+    }
+}
