6a87c1bc29e42fb8760e5febd6100f71b915b6f4
Dennis Lundberg <dennisl@apache.org>
2008-03-11 23:25:22 +0000

    [MSITE-211] Can't deploy site using site:deploy due to a ProxyHTTP error Submitted by: Cyrille Le Clerc Reviewed by: Dennis Lundberg
    o I modified the handling of wildcards and the associated tests.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@636147 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteDeployMojo.java
+++ maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteDeployMojo.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.manager.WagonConfigurationException;
 import org.apache.maven.artifact.manager.WagonManager;
 import org.apache.maven.model.DistributionManagement;
@@ -30,7 +31,6 @@ import org.apache.maven.model.Site;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.settings.Proxy;
 import org.apache.maven.settings.Settings;
 import org.apache.maven.wagon.CommandExecutionException;
 import org.apache.maven.wagon.CommandExecutor;
@@ -56,11 +56,11 @@ import org.codehaus.plexus.context.ContextException;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
 
 /**
- * Deploys website using scp/file protocol.
+ * Deploys the site using scp/file protocol.
  * For scp protocol, website files are packaged into zip archive,
- * then archive is transfred to remote host, nextly it is un-archived.
+ * then the archive is transfered to the remote host, next it is un-archived.
  * This method of deployment should normally be much faster
- * then making file by file copy.  For file protocol, the files are copied
+ * then making a file by file copy.  For file protocol, the files are copied
  * directly to the destination directory.
  *
  * @author <a href="mailto:michal@org.codehaus.org">Michal Maczka</a>
@@ -171,7 +171,7 @@ public class SiteDeployMojo
 
             wagon.addTransferListener( debug );
 
-            ProxyInfo proxyInfo = getProxyInfo( settings );
+            ProxyInfo proxyInfo = getProxyInfo( repository, wagonManager );
             if ( proxyInfo != null )
             {
                 wagon.connect( repository, wagonManager.getAuthenticationInfo( id ), proxyInfo );
@@ -229,27 +229,65 @@ public class SiteDeployMojo
     }
 
     /**
-     * Convenience method to map a <code>Proxy</code> object from the user system settings to a
-     * <code>ProxyInfo</code> object.
-     *
-     * @return a proxyInfo object instancied or null if no active proxy is define in the settings.xml
+     * <p>
+     * Get the <code>ProxyInfo</code> of the proxy associated with the <code>host</code>
+     * and the <code>protocol</code> of the given <code>repository</code> 
+     * </p>
+     * <p>
+     * Extract from <a href="http://java.sun.com/j2se/1.5.0/docs/guide/net/properties.html">
+     * J2SE Doc : Networking Properties - nonProxyHosts</a> : "The value can be a list of hosts, 
+     * each separated by a |, and in addition a wildcard character (*) can be used for matching"
+     * </p>
+     * <p>
+     * Defensively support for comma (",") and semi colon (";") in addition to pipe ("|") as separator.
+     * </p>
+     * 
+     * @return a ProxyInfo object instantiated or <code>null</code> if no matching proxy is found
      */
-    public static ProxyInfo getProxyInfo( Settings settings )
+    public static ProxyInfo getProxyInfo( Repository repository, WagonManager wagonManager )
     {
-        ProxyInfo proxyInfo = null;
-        if ( settings != null && settings.getActiveProxy() != null )
-        {
-            Proxy settingsProxy = settings.getActiveProxy();
+        ProxyInfo proxyInfo = wagonManager.getProxy( repository.getProtocol() );
 
-            proxyInfo = new ProxyInfo();
-            proxyInfo.setHost( settingsProxy.getHost() );
-            proxyInfo.setType( settingsProxy.getProtocol() );
-            proxyInfo.setPort( settingsProxy.getPort() );
-            proxyInfo.setNonProxyHosts( settingsProxy.getNonProxyHosts() );
-            proxyInfo.setUserName( settingsProxy.getUsername() );
-            proxyInfo.setPassword( settingsProxy.getPassword() );
+        if ( proxyInfo == null )
+        {
+            return null;
         }
 
+        String host = repository.getHost();
+        String nonProxyHostsAsString = proxyInfo.getNonProxyHosts();
+        String[] nonProxyHosts = StringUtils.split( nonProxyHostsAsString, ",;|" );
+        for ( int i = 0; i < nonProxyHosts.length; i++ )
+        {
+            String nonProxyHost = nonProxyHosts[i];
+            if ( StringUtils.contains( nonProxyHost, "*" ) )
+            {
+                // Handle wildcard at the end, beginning or middle of the nonProxyHost
+                String nonProxyHostPrefix = StringUtils.substringBefore( nonProxyHost, "*" );
+                String nonProxyHostSuffix = StringUtils.substringAfter( nonProxyHost, "*" );
+                // prefix*
+                if ( StringUtils.isNotEmpty(nonProxyHostPrefix) && host.startsWith( nonProxyHostPrefix )
+                    && StringUtils.isEmpty(nonProxyHostSuffix) )
+                {
+                    return null;
+                }
+                // *suffix
+                if ( StringUtils.isEmpty(nonProxyHostPrefix)
+                    && StringUtils.isNotEmpty(nonProxyHostSuffix) && host.endsWith( nonProxyHostSuffix ))
+                {
+                    return null;
+                }
+                // prefix*suffix
+                if ( StringUtils.isNotEmpty(nonProxyHostPrefix) && host.startsWith( nonProxyHostPrefix )
+                    && StringUtils.isNotEmpty(nonProxyHostSuffix) && host.endsWith( nonProxyHostSuffix ))
+                {
+                    return null;
+                }
+            }
+            else if ( host.equals( nonProxyHost ) )
+            {
+                return null;
+            }
+        }
         return proxyInfo;
     }
 
