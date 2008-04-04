b4ab6e9fbd4b6eccff3868c208e51ba34a98c10a
Benjamin Bentmann <bentmann@apache.org>
2008-04-04 18:54:59 +0000

    [MSHADE-28] Add full support for glob patterns in relocator exclusions
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-shade-plugin@644822 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugins/shade/relocation/SimpleRelocatorTest.java
@@ -0,0 +1,93 @@
+package org.apache.maven.plugins.shade.relocation;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+/**
+ * Test for {@link SimpleRelocator}.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class SimpleRelocatorTest
+    extends TestCase
+{
+
+    public void testCanRelocatePath()
+    {
+        SimpleRelocator relocator;
+
+        relocator = new SimpleRelocator( "org.foo", null, null );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/Class" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/Class.class" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/bar/Class" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/bar/Class.class" ) );
+        assertEquals( false, relocator.canRelocatePath( "com/foo/bar/Class" ) );
+        assertEquals( false, relocator.canRelocatePath( "com/foo/bar/Class.class" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/Foo/Class" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/Foo/Class.class" ) );
+
+        relocator =
+            new SimpleRelocator( "org.foo", null, Arrays.asList( new String[] { "org.foo.Excluded", "org.foo.public.*",
+                "org.foo.Public*Stuff" } ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/Class" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/Class.class" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/excluded" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/Excluded" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/Excluded.class" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/public/Class" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/public/Class.class" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/PrivateStuff" ) );
+        assertEquals( true, relocator.canRelocatePath( "org/foo/PrivateStuff.class" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/PublicStuff" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/PublicStuff.class" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/PublicUtilStuff" ) );
+        assertEquals( false, relocator.canRelocatePath( "org/foo/PublicUtilStuff.class" ) );
+    }
+
+    public void testCanRelocateClass()
+    {
+        SimpleRelocator relocator;
+
+        relocator = new SimpleRelocator( "org.foo", null, null );
+        assertEquals( true, relocator.canRelocateClass( "org.foo.Class" ) );
+        assertEquals( true, relocator.canRelocateClass( "org.foo.bar.Class" ) );
+        assertEquals( false, relocator.canRelocateClass( "com.foo.bar.Class" ) );
+        assertEquals( false, relocator.canRelocateClass( "org.Foo.Class" ) );
+
+        relocator =
+            new SimpleRelocator( "org.foo", null, Arrays.asList( new String[] { "org.foo.Excluded", "org.foo.public.*",
+                "org.foo.Public*Stuff" } ) );
+        assertEquals( true, relocator.canRelocateClass( "org.foo.Class" ) );
+        assertEquals( true, relocator.canRelocateClass( "org.foo.excluded" ) );
+        assertEquals( false, relocator.canRelocateClass( "org.foo.Excluded" ) );
+        assertEquals( false, relocator.canRelocateClass( "org.foo.public.Class" ) );
+        assertEquals( true, relocator.canRelocateClass( "org.foo.PrivateStuff" ) );
+        assertEquals( false, relocator.canRelocateClass( "org.foo.PublicStuff" ) );
+        assertEquals( false, relocator.canRelocateClass( "org.foo.PublicUtilStuff" ) );
+    }
+
+    public void testRelocatePath()
+    {
+        SimpleRelocator relocator;
+
+        relocator = new SimpleRelocator( "org.foo", null, null );
+        assertEquals( "hidden/org/foo/bar/Class.class", relocator.relocatePath( "org/foo/bar/Class.class" ) );
+
+        relocator = new SimpleRelocator( "org.foo", "private.stuff", null );
+        assertEquals( "private/stuff/bar/Class.class", relocator.relocatePath( "org/foo/bar/Class.class" ) );
+    }
+
+    public void testRelocateClass()
+    {
+        SimpleRelocator relocator;
+
+        relocator = new SimpleRelocator( "org.foo", null, null );
+        assertEquals( "hidden.org.foo.bar.Class", relocator.relocateClass( "org.foo.bar.Class" ) );
+
+        relocator = new SimpleRelocator( "org.foo", "private.stuff", null );
+        assertEquals( "private.stuff.bar.Class", relocator.relocateClass( "org.foo.bar.Class" ) );
+    }
+
+}
