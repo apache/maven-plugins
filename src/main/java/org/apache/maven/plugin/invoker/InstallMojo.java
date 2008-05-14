ccae628c250694e63c0a870338063d1d68ba9f1c
Benjamin Bentmann <bentmann@apache.org>
2008-05-14 20:59:56 +0000

    [MINVOKER-37] Stage Reactor Contents to IT Repo
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-invoker-plugin@656430 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/invoker/InstallMojo.java
+++ src/main/java/org/apache/maven/plugin/invoker/InstallMojo.java
@@ -21,7 +21,9 @@ package org.apache.maven.plugin.invoker;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
@@ -35,12 +37,14 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
 /**
- * Installs the project artifacts into the local repository as a preparation to run the integration tests.
+ * Installs the project artifacts into the local repository as a preparation to run the integration tests. More
+ * precisely, all artifacts of the project itself, all its locally reachable parent POMs and all its dependencies from
+ * the reactor will be installed to the local repository.
  * 
  * @goal install
  * @phase pre-integration-test
+ * @requiresDependencyResolution runtime
  * @since 1.2
- * 
  * @author Paul Gier
  * @author Benjamin Bentmann
  * @version $Id$
@@ -79,7 +83,9 @@ public class InstallMojo
 
     /**
      * The path to the local repository into which the project artifacts should be installed for the integration tests.
-     * If not set, the regular local repository will be used.
+     * If not set, the regular local repository will be used. To prevent soiling of your regular local repository with
+     * possibly broken artifacts, it is strongly recommended to use an isolated repository for the integration tests
+     * (e.g. <code>${project.build.directory}/it-repo</code>).
      * 
      * @parameter expression="${invoker.localRepositoryPath}"
      */
@@ -95,47 +101,23 @@ public class InstallMojo
     private MavenProject project;
 
     /**
+     * The set of Maven projects in the reactor build.
+     * 
+     * @parameter default-value="${reactorProjects}"
+     * @readonly
+     */
+    private Collection reactorProjects;
+
+    /**
      * Performs this mojo's tasks.
      */
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
         ArtifactRepository testRepository = createTestRepository();
-        installProjectArtifacts( testRepository );
-    }
-
-    /**
-     * Installs the main project artifact and any attached artifacts to the local repository.
-     * 
-     * @param testRepository The local repository to install the artifacts into, must not be <code>null</code>.
-     * @throws MojoExecutionException If any artifact could not be installed.
-     */
-    private void installProjectArtifacts( ArtifactRepository testRepository )
-        throws MojoExecutionException
-    {
-        try
-        {
-            // Install the pom
-            Artifact pomArtifact =
-                artifactFactory.createArtifact( project.getGroupId(), project.getArtifactId(), project.getVersion(),
-                                                null, "pom" );
-            installer.install( project.getFile(), pomArtifact, testRepository );
-
-            // Install the main project artifact
-            installer.install( project.getArtifact().getFile(), project.getArtifact(), testRepository );
-
-            // Install any attached project artifacts
-            Collection attachedArtifacts = project.getAttachedArtifacts();
-            for ( Iterator artifactIter = attachedArtifacts.iterator(); artifactIter.hasNext(); )
-            {
-                Artifact theArtifact = (Artifact) artifactIter.next();
-                installer.install( theArtifact.getFile(), theArtifact, testRepository );
-            }
-        }
-        catch ( ArtifactInstallationException e )
-        {
-            throw new MojoExecutionException( "Failed to install project artifacts", e );
-        }
+        installProjectArtifacts( project, testRepository );
+        installProjectParents( project, testRepository );
+        installProjectDependencies( project, reactorProjects, testRepository );
     }
 
     /**
@@ -166,11 +148,120 @@ public class InstallMojo
             }
             catch ( Exception e )
             {
-                throw new MojoExecutionException( "Failed to create local repository", e );
+                throw new MojoExecutionException( "Failed to create local repository: " + localRepositoryPath, e );
             }
         }
 
         return testRepository;
+    }
+
+    /**
+     * Installs the main artifact and any attached artifacts of the specified project to the local repository.
+     * 
+     * @param mvnProject The project whose artifacts should be installed, must not be <code>null</code>.
+     * @param testRepository The local repository to install the artifacts to, must not be <code>null</code>.
+     * @throws MojoExecutionException If any artifact could not be installed.
+     */
+    private void installProjectArtifacts( MavenProject mvnProject, ArtifactRepository testRepository )
+        throws MojoExecutionException
+    {
+        try
+        {
+            installProjectPom( mvnProject, testRepository );
+
+            // Install the main project artifact
+            installer.install( mvnProject.getArtifact().getFile(), mvnProject.getArtifact(), testRepository );
+
+            // Install any attached project artifacts
+            Collection attachedArtifacts = mvnProject.getAttachedArtifacts();
+            for ( Iterator artifactIter = attachedArtifacts.iterator(); artifactIter.hasNext(); )
+            {
+                Artifact theArtifact = (Artifact) artifactIter.next();
+                installer.install( theArtifact.getFile(), theArtifact, testRepository );
+            }
+        }
+        catch ( ArtifactInstallationException e )
+        {
+            throw new MojoExecutionException( "Failed to install project artifacts: " + mvnProject, e );
+        }
+    }
+
+    /**
+     * Installs the (locally reachable) parent POMs of the specified project to the local repository. The parent POMs
+     * from the reactor must be installed or the forked IT builds will fail when using a clean repository.
+     * 
+     * @param mvnProject The project whose parent POMs should be installed, must not be <code>null</code>.
+     * @param testRepository The local repository to install the POMs to, must not be <code>null</code>.
+     * @throws MojoExecutionException If any POM could not be installed.
+     */
+    private void installProjectParents( MavenProject mvnProject, ArtifactRepository testRepository )
+        throws MojoExecutionException
+    {
+        for ( MavenProject parent = mvnProject.getParent(); parent != null; parent = parent.getParent() )
+        {
+            if ( parent.getFile() == null )
+            {
+                break;
+            }
+            installProjectPom( parent, testRepository );
+        }
+    }
+
+    /**
+     * Installs the POM of the specified project to the local repository.
+     * 
+     * @param mvnProject The project whose POM should be installed, must not be <code>null</code>.
+     * @param testRepository The local repository to install the POM to, must not be <code>null</code>.
+     * @throws MojoExecutionException If the POM could not be installed.
+     */
+    private void installProjectPom( MavenProject mvnProject, ArtifactRepository testRepository )
+        throws MojoExecutionException
+    {
+        try
+        {
+            Artifact pomArtifact =
+                artifactFactory.createProjectArtifact( mvnProject.getGroupId(), mvnProject.getArtifactId(),
+                                                       mvnProject.getVersion() );
+            installer.install( mvnProject.getFile(), pomArtifact, testRepository );
+        }
+        catch ( Exception e )
+        {
+            throw new MojoExecutionException( "Failed to install POM: " + mvnProject, e );
+        }
+    }
+
+    /**
+     * Installs the dependent projects from the reactor to the local repository. The dependencies on other modules from
+     * the reactor must be installed or the forked IT builds will fail when using a clean repository.
+     * 
+     * @param mvnProject The project whose dependent projects should be installed, must not be <code>null</code>.
+     * @param reactorProjects The set of projects in the reactor build, must not be <code>null</code>.
+     * @param testRepository The local repository to install the POMs to, must not be <code>null</code>.
+     * @throws MojoExecutionException If any dependency could not be installed.
+     */
+    private void installProjectDependencies( MavenProject mvnProject, Collection reactorProjects,
+                                             ArtifactRepository testRepository )
+        throws MojoExecutionException
+    {
+        Map projects = new HashMap();
+        for ( Iterator it = reactorProjects.iterator(); it.hasNext(); )
+        {
+            MavenProject reactorProject = (MavenProject) it.next();
+            projects.put( reactorProject.getId(), reactorProject );
+        }
+
+        for ( Iterator it = mvnProject.getArtifacts().iterator(); it.hasNext(); )
+        {
+            Artifact artifact = (Artifact) it.next();
+            String id =
+                artifact.getGroupId() + ':' + artifact.getArtifactId() + ':' + artifact.getType() + ':'
+                    + artifact.getVersion();
+            MavenProject requiredProject = (MavenProject) projects.remove( id );
+            if ( requiredProject != null )
+            {
+                installProjectArtifacts( requiredProject, testRepository );
+            }
+        }
     }
 
 }
