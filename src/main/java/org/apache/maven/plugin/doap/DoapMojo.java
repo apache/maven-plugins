d6c465c68a9521e77ad49f841f8b52b554d5ce24
Vincent Siveton <vsiveton@apache.org>
2008-06-14 10:22:18 +0000

    o improved readingness of POM o added license header o improved javadoc
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-doap-plugin@667768 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/doap/DoapMojo.java
+++ src/main/java/org/apache/maven/plugin/doap/DoapMojo.java
@@ -1,19 +1,22 @@
 package org.apache.maven.plugin.doap;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import org.apache.maven.model.Developer;
@@ -34,11 +37,11 @@ import java.util.Date;
 import java.util.Iterator;
 
 /**
- * A Maven2 plugin to generate a Description of a Project (DOAP) file from the information found
- * in a POM.
+ * Generate a Description of a Project (DOAP) file from the information found in a POM.
  *
  * @author Jason van Zyl
  * @version $Id$
+ * @since 1.0
  * @goal generate
  */
 public class DoapMojo
@@ -79,6 +82,9 @@ public class DoapMojo
 
     private XMLWriter w;
 
+    /**
+     * {@inheritDoc}
+     */
     public void execute()
         throws MojoExecutionException
     {
