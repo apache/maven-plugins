b73066e0337102ce279260db164d908432514bf9
Vincent Siveton <vsiveton@apache.org>
2008-06-17 21:01:28 +0000

    o bumped to p-u 1.5.4-snap o bumped to maven 2.0.4 as prerequisite and dependencies o fixed encoding in the generated doap file o replaced ASF header in the generated doap file with a generated notice o added a new utility class with static methods
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-doap-plugin@668827 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/doap/DoapMojo.java
+++ src/main/java/org/apache/maven/plugin/doap/DoapMojo.java
@@ -19,22 +19,20 @@ package org.apache.maven.plugin.doap;
  * under the License.
  */
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Iterator;
+
 import org.apache.maven.model.Developer;
 import org.apache.maven.model.License;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.WriterFactory;
 import org.codehaus.plexus.util.xml.PrettyPrintXMLWriter;
 import org.codehaus.plexus.util.xml.XMLWriter;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Iterator;
 
 /**
  * Generate a Description of a Project (DOAP) file from the information found in a POM.
@@ -47,8 +45,6 @@ import java.util.Iterator;
 public class DoapMojo
     extends AbstractMojo
 {
-    public static final String RDF_RESOURCE = "rdf:resource";
-
     /**
      * The POM from which information will be extracted to create a DOAP file.
      *
@@ -80,8 +76,6 @@ public class DoapMojo
      */
     private String language;
 
-    private XMLWriter w;
-
     /**
      * {@inheritDoc}
      */
@@ -92,115 +86,79 @@ public class DoapMojo
         // setup pretty print xml writer
         // ----------------------------------------------------------------------------
 
-        Writer writer;
-
+        Writer w;
         try
         {
-            writer = new FileWriter( doapFile );
+            w = WriterFactory.newXmlWriter( doapFile );
         }
         catch ( IOException e )
         {
             throw new MojoExecutionException( "Error creating DOAP file.", e );
         }
 
-        PrintWriter pw = new PrintWriter( writer );
-
-        w = new PrettyPrintXMLWriter( pw );
+        XMLWriter writer = new PrettyPrintXMLWriter( w, project.getModel().getModelEncoding(), null );
 
         // ----------------------------------------------------------------------------
         // Convert POM to DOAP
         // ----------------------------------------------------------------------------
 
-        // Grab year for the license.
-        SimpleDateFormat format = new SimpleDateFormat( "yyyy" );
-        String year = format.format( new Date() );
-
-        pw.println( "<!--" );
-        pw.println( "  ~ Copyright " + year + " The Apache Software Foundation." );
-        pw.println( "  ~" );
-        pw.println( "  ~ Licensed under the Apache License, Version 2.0 (the \"License\");" );
-        pw.println( "  ~ you may not use this file except in compliance with the License." );
-        pw.println( "  ~ You may obtain a copy of the License at" );
-        pw.println( "  ~" );
-        pw.println( "  ~      http://www.apache.org/licenses/LICENSE-2.0" );
-        pw.println( "  ~" );
-        pw.println( "  ~ Unless required by applicable law or agreed to in writing, software" );
-        pw.println( "  ~ distributed under the License is distributed on an \"AS IS\" BASIS," );
-        pw.println( "  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied." );
-        pw.println( "  ~ See the License for the specific language governing permissions and" );
-        pw.println( "  ~ limitations under the License." );
-        pw.println( "--> " );
+        DoapUtil.writeHeader( writer );
 
         // Heading
-        w.startElement( "rdf:RDF" );
-        w.addAttribute( "xml:lang", "en" );
-        w.addAttribute( "xmlns", "http://usefulinc.com/ns/doap#" );
-        w.addAttribute( "xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#" );
-        w.addAttribute( "xmlns:asfext", "http://projects.apache.org/ns/asfext#" );
-        w.addAttribute( "xmlns:foaf", "http://xmlns.com/foaf/0.1/" );
+        writer.startElement( "rdf:RDF" );
+        writer.addAttribute( "xml:lang", "en" );
+        writer.addAttribute( "xmlns", "http://usefulinc.com/ns/doap#" );
+        writer.addAttribute( "xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#" );
+        writer.addAttribute( "xmlns:asfext", "http://projects.apache.org/ns/asfext#" );
+        writer.addAttribute( "xmlns:foaf", "http://xmlns.com/foaf/0.1/" );
 
         // Project
-        w.startElement( "Project" );
-        w.addAttribute( "rdf:about", "http://Maven.rdf.apache.org/" );
-        element( "created", project.getInceptionYear() );
+        writer.startElement( "Project" );
+        writer.addAttribute( "rdf:about", "http://Maven.rdf.apache.org/" );
+        DoapUtil.writeElement( writer, "created", project.getInceptionYear() );
 
         if ( project.getLicenses().size() > 0 )
         {
             //TODO: how to map to usefulinc site, or if this is necessary, the OSI page might
             //      be more appropriate.
-            rdfResourceElement( "license", ((License) project.getLicenses().get( 0 )).getUrl() );
+            DoapUtil.writeRdfResourceElement( writer, "license", ((License) project.getLicenses().get( 0 )).getUrl() );
         }
 
-        element( "name", project.getName() );
-        rdfResourceElement( "homepage", project.getUrl() );
-        rdfResourceElement( "asfext:pmc", project.getUrl() );
-        element( "shortdesc", project.getDescription() );
-        element( "description", project.getDescription() );
+        DoapUtil.writeElement( writer, "name", project.getName() );
+        DoapUtil.writeRdfResourceElement( writer, "homepage", project.getUrl() );
+        DoapUtil.writeRdfResourceElement( writer, "asfext:pmc", project.getUrl() );
+        DoapUtil.writeElement( writer, "shortdesc", project.getDescription() );
+        DoapUtil.writeElement( writer, "description", project.getDescription() );
 
         if ( project.getIssueManagement() != null )
         {
-            rdfResourceElement( "bug-database", project.getIssueManagement().getUrl() );
+            DoapUtil.writeRdfResourceElement( writer, "bug-database", project.getIssueManagement().getUrl() );
         }
-        rdfResourceElement( "mailing-list", composeUrl( project.getUrl() , "/mail-lists.html" ) );
-        rdfResourceElement( "download-page", composeUrl( project.getUrl() , "/download.html" ) );
-        element( "programming-language", language );
+        DoapUtil.writeRdfResourceElement( writer, "mailing-list", composeUrl( project.getUrl() , "/mail-lists.html" ) );
+        DoapUtil.writeRdfResourceElement( writer, "download-page", composeUrl( project.getUrl() , "/download.html" ) );
+        DoapUtil.writeElement( writer, "programming-language", language );
         //TODO: how to lookup category, map it, or just declare it.
-        rdfResourceElement( "category", "http://projects.apache.org/category/" + category );
+        DoapUtil.writeRdfResourceElement( writer, "category", "http://projects.apache.org/category/" + category );
 
         // Releases
         publishReleases();
 
         // SCM
-        publishSourceRepository();
+        publishSourceRepository( writer );
 
         // Developers
-        publishMaintainers();
+        publishMaintainers( writer );
 
-        w.endElement();
-        w.endElement();
+        writer.endElement();
+        writer.endElement();
 
-        pw.close();
-    }
-
-    private void element( String name,
-                          String value )
-    {
-        if ( value != null )
+        try
         {
-            w.startElement( name );
-            w.writeText( value );
-            w.endElement();
+            w.close();
         }
-    }
-
-    private void rdfResourceElement( String name,
-                                     String value )
-    {
-        if ( value != null )
+        catch ( IOException e )
         {
-            w.startElement( name );
-            w.addAttribute( RDF_RESOURCE, value );
-            w.endElement();
+            throw new MojoExecutionException( "Error when closing the writer.", e );
         }
     }
 
@@ -209,7 +167,7 @@ public class DoapMojo
     {
     }
 
-    private void publishSourceRepository()
+    private void publishSourceRepository( XMLWriter w )
     {
         //<repository>
         //  <SVNRepository>
@@ -225,14 +183,14 @@ public class DoapMojo
 
         w.startElement( "repository" );
         w.startElement( "SVNRepository" );
-        rdfResourceElement( "location", project.getScm().getConnection().substring( 8 ) );
-        rdfResourceElement( "browse", project.getScm().getUrl() );
+        DoapUtil.writeRdfResourceElement( w, "location", project.getScm().getConnection().substring( 8 ) );
+        DoapUtil.writeRdfResourceElement( w, "browse", project.getScm().getUrl() );
         w.endElement();
         w.endElement();
 
     }
 
-    private void publishMaintainers()
+    private void publishMaintainers( XMLWriter w )
     {
         //<maintainer>
         //  <foaf:Person>
@@ -255,7 +213,7 @@ public class DoapMojo
             w.startElement( "foaf:name" );
             w.writeText( d.getName() );
             w.endElement();
-            rdfResourceElement( "foaf:mbox", "mailto:" + d.getEmail() );
+            DoapUtil.writeRdfResourceElement( w, "foaf:mbox", "mailto:" + d.getEmail() );
             w.endElement();
             w.endElement();
         }
@@ -275,9 +233,7 @@ public class DoapMojo
         {
             return base + path.substring( 1 );
         }
-        else
-        {
-            return base + path;
-        }
+
+        return base + path;
     }
 }
