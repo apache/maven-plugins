d088924d99eef155767aa48c9ada6367658273f0
Dennis Lundberg <dennisl@apache.org>
2008-06-17 15:21:57 +0000

    o Ensure that files have proper license headers.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-repository-plugin@668709 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/repository/BundleCreateMojo.java
+++ src/main/java/org/apache/maven/plugins/repository/BundleCreateMojo.java
@@ -1,3 +1,5 @@
+package org.apache.maven.plugins.repository;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,7 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.maven.plugins.repository;
 
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
 import org.apache.maven.plugin.AbstractMojo;
