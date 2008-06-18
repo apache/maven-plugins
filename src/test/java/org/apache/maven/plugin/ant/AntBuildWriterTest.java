7d9e0ac5549137a3d064b0b1883345a0bf8f8b49
Benjamin Bentmann <bentmann@apache.org>
2008-06-18 19:43:43 +0000

    o Handled URL decoding to prepare for the day when the Maven core will use RFC-compliant URLs
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-ant-plugin@669269 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/ant/AntBuildWriterTest.java
@@ -0,0 +1,64 @@
+package org.apache.maven.plugin.ant;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests <code>AntBuildWriter</code>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class AntBuildWriterTest
+    extends TestCase
+{
+
+    public void testGetProjectRepoDirectory()
+    {
+        String basedir = new File( System.getProperty( "java.io.tmpdir" ) ).getPath();
+
+        // non-project rooted repo URLs
+        assertEquals( null, AntBuildWriter.getProjectRepoDirectory( "http://maven.apache.org/", basedir ) );
+        assertEquals( null, AntBuildWriter.getProjectRepoDirectory( "file:///just-some-test-directory", basedir ) );
+
+        // RFC-compliant URLs
+        assertEquals( "", AntBuildWriter.getProjectRepoDirectory( new File( basedir ).toURI().toString(), basedir ) );
+        assertEquals( "dir", AntBuildWriter.getProjectRepoDirectory( new File( basedir, "dir" ).toURI().toString(),
+                                                                     basedir ) );
+        assertEquals( "dir/subdir",
+                      AntBuildWriter.getProjectRepoDirectory( new File( basedir, "dir/subdir" ).toURI().toString(),
+                                                              basedir ) );
+
+        // not so strict URLs
+        assertEquals( "", AntBuildWriter.getProjectRepoDirectory( "file://" + basedir, basedir ) );
+        assertEquals( "dir", AntBuildWriter.getProjectRepoDirectory( "file://" + basedir + "/dir", basedir ) );
+        assertEquals( "dir/subdir", AntBuildWriter.getProjectRepoDirectory( "file://" + basedir + "/dir/subdir",
+                                                                            basedir ) );
+
+        // URLs with encoded characters
+        assertEquals( "some dir",
+                      AntBuildWriter.getProjectRepoDirectory( new File( basedir, "some dir" ).toURI().toString(),
+                                                              basedir ) );
+    }
+
+}
