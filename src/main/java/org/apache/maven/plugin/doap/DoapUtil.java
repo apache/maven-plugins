3f614e5e8948bdf7527492e9ca8f3f8117ef4602
Vincent Siveton <vsiveton@apache.org>
2008-06-20 13:15:54 +0000

    MDOAP-10: Supports more DOAP roles for developers/contributors
    o supports of all DOAP role tags: maintainer, developer, documenter, translator, tester, helper
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-doap-plugin@669883 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/doap/DoapUtil.java
+++ src/main/java/org/apache/maven/plugin/doap/DoapUtil.java
@@ -20,9 +20,17 @@ package org.apache.maven.plugin.doap;
  */
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
 
+import org.apache.maven.model.Contributor;
+import org.apache.maven.model.Developer;
 import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.xml.XMLWriter;
 import org.codehaus.plexus.util.xml.XmlWriterUtil;
@@ -35,6 +43,16 @@ import org.codehaus.plexus.util.xml.XmlWriterUtil;
  */
 public class DoapUtil
 {
+    /**
+     * I18N roles supported in DOAP, i.e. <code>maintainer</code>, <code>developer</code>, <code>documenter</code>,
+     * <code>translator</code>, <code>tester</code>, <code>helper</code>,
+     * <br/>
+     * <b>Note:</b> Actually, only English keys are used.
+     */
+    private static final ResourceBundle DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N = ResourceBundle.getBundle( "doap-person",
+                                                                                                        Locale.ENGLISH );
+
+    /** RDF resource attribute */
     private static final String RDF_RESOURCE = "rdf:resource";
 
     /**
@@ -105,5 +123,102 @@ public class DoapUtil
             writer.addAttribute( RDF_RESOURCE, value );
             writer.endElement();
         }
+    }
+
+    /**
+     * Filter the developers/contributors role by the keys in {@link #DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N}
+     *
+     * @param developersOrContributors list of <code>{@link Developer}/{@link Contributor}</code>
+     * @return a none null map with <code>maintainers</code>, <code>developers</code>, <code>documenters</code>,
+     * <code>translators</code>, <code>testers</code>, <code>helpers</code>, <code>unknowns</code> as keys and list of
+     * <code>{@link Developer}/{@link Contributor}</code> as value.
+     */
+    public static Map filterDevelopersOrContributorsByDoapRoles( List developersOrContributors )
+    {
+        Map returnMap = new HashMap( 7 );
+        returnMap.put( "maintainers", new ArrayList() );
+        returnMap.put( "developers", new ArrayList() );
+        returnMap.put( "documenters", new ArrayList() );
+        returnMap.put( "translators", new ArrayList() );
+        returnMap.put( "testers", new ArrayList() );
+        returnMap.put( "helpers", new ArrayList() );
+        returnMap.put( "unknowns", new ArrayList() );
+
+        if ( developersOrContributors == null || developersOrContributors.size() == 0 )
+        {
+            return returnMap;
+        }
+
+        for ( Iterator it = developersOrContributors.iterator(); it.hasNext(); )
+        {
+            Object obj = it.next();
+
+            List roles;
+            if ( Developer.class.isAssignableFrom( obj.getClass() ) )
+            {
+                Developer developer = (Developer) obj;
+                roles = developer.getRoles();
+            }
+            else
+            {
+                Contributor contributor = (Contributor) obj;
+                roles = contributor.getRoles();
+            }
+
+            if ( roles != null && roles.size() != 0 )
+            {
+                for ( Iterator it2 = roles.iterator(); it2.hasNext(); )
+                {
+                    String role = (String) it2.next();
+
+                    if ( role.toLowerCase().indexOf(
+                                                     DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N.getString( "doap.maintainer" )
+                                                         .toLowerCase() ) != -1 )
+                    {
+                        ( (List) returnMap.get( "maintainers" ) ).add( obj );
+                    }
+                    else if ( role.toLowerCase().indexOf(
+                                                           DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N
+                                                               .getString( "doap.developer" ).toLowerCase() ) != -1 )
+                    {
+                        ( (List) returnMap.get( "developers" ) ).add( obj );
+                    }
+                    else if ( role.toLowerCase().indexOf(
+                                                           DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N
+                                                               .getString( "doap.documenter" ).toLowerCase() ) != -1 )
+                    {
+                        ( (List) returnMap.get( "documenters" ) ).add( obj );
+                    }
+                    else if ( role.toLowerCase().indexOf(
+                                                           DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N
+                                                               .getString( "doap.translator" ).toLowerCase() ) != -1 )
+                    {
+                        ( (List) returnMap.get( "translators" ) ).add( obj );
+                    }
+                    else if ( role.toLowerCase().indexOf(
+                                                           DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N
+                                                               .getString( "doap.tester" ).toLowerCase() ) != -1 )
+                    {
+                        ( (List) returnMap.get( "testers" ) ).add( obj );
+                    }
+                    else if ( role.toLowerCase().indexOf(
+                                                           DEVELOPER_OR_CONTRIBUTOR_ROLES_I18N
+                                                               .getString( "doap.helper" ).toLowerCase() ) != -1 )
+                    {
+                        ( (List) returnMap.get( "helpers" ) ).add( obj );
+                    }
+                    else
+                    {
+                        ( (List) returnMap.get( "unknowns" ) ).add( obj );
+                    }
+                }
+            }
+            else
+            {
+                ( (List) returnMap.get( "unknowns" ) ).add( obj );
+            }
+        }
+
+        return returnMap;
     }
 }
