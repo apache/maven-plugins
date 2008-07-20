77696ea9fc67a2b90a74bd395e40ef85f776e994
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-07-20 18:22:55 +0000

    Dont print stack trace
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@678298 13f79535-47bb-0310-9956-ffa450edef68

--- maven-antrun-plugin/src/main/java/org/apache/maven/plugin/antrun/AntPropertyHelper.java
+++ maven-antrun-plugin/src/main/java/org/apache/maven/plugin/antrun/AntPropertyHelper.java
@@ -186,7 +186,6 @@ public class AntPropertyHelper
             {
                 log.warn( "Error evaluating expression '" + name + "'", e );
             }
-            e.printStackTrace();
         }
 
         if ( val == null )
