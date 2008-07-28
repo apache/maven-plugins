14a135fa52ca2c2e083f10400f4795357bbb209d
Oliver Lamy <olamy@apache.org>
2008-07-28 23:32:11 +0000

    [MCHANGES-86] Create a changes-validate mojo
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-changes-plugin@680563 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/changes/ChangesValidatorMojo.java
@@ -0,0 +1,130 @@
+package org.apache.maven.plugin.changes;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.changes.schema.ChangesSchemaValidator;
+import org.apache.maven.plugin.changes.schema.SchemaValidatorException;
+import org.apache.maven.plugin.changes.schema.XmlValidationHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * Goal which validate changes file
+ * 
+ * @goal changes-validate
+ * 
+ * @author <a href="mailto:olamy@apache.org">olamy</a>
+ * @since 28 juil. 2008
+ * @version $Id$
+ * @since 2.1
+ */
+public class ChangesValidatorMojo
+    extends AbstractMojo
+{
+
+    /**
+     * The path of the <code>changes.xml</code> file that will be converted into an HTML report.
+     *
+     * @parameter expression="${changes.xmlPath}" default-value="src/changes/changes.xml"
+     */
+    private File xmlPath;
+    
+    /**
+     * The changes xsd version.
+     *
+     * @parameter expression="${changes.xsdVersion}" default-value="1.0.0"
+     */
+    private String changesXsdVersion;
+    
+    /**
+     * 
+     * @component role="org.apache.maven.plugin.changes.schema.ChangesSchemaValidator" roleHint="default"
+     *  
+     */
+    private ChangesSchemaValidator changesSchemaValidator;
+    
+    /**
+     * Mojo failure if validation failed. If not and validation failed only a warning will be logged.
+     *
+     * @parameter expression="${changes.validate.failed}" default-value="false"
+     */    
+    private boolean failOnError;
+    
+    /** 
+     * @see org.apache.maven.plugin.Mojo#execute()
+     */
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+
+        if ( !xmlPath.exists() )
+        {
+            getLog().warn( "changes.xml file " + xmlPath.getAbsolutePath() + " does not exist." );
+            return;
+        }
+
+        try
+        {
+            XmlValidationHandler xmlValidationHandler = changesSchemaValidator
+                .validateXmlWithSchema( xmlPath, changesXsdVersion, failOnError );
+            boolean hasErrros = !xmlValidationHandler.getErrors().isEmpty();
+            if ( hasErrros )
+            {
+                logSchemaValidation( xmlValidationHandler.getErrors() );
+                if ( failOnError )
+                {
+                    throw new MojoExecutionException( "changes.xml file " + xmlPath.getAbsolutePath()
+                        + " is not valid see previous errors" );
+                }
+                else
+                {
+                    getLog().info( " skip previous validation errors due failOnError false " );
+                }
+            }
+        }
+        catch ( SchemaValidatorException e )
+        {
+            if ( failOnError )
+            {
+                throw new MojoExecutionException( "failed to validate changes.xml file " + xmlPath.getAbsolutePath()
+                    + " : " + e.getMessage(), e );
+            }
+        }
+    }
+
+    private void logSchemaValidation( List /*SAXException*/errors )
+    {
+        getLog().error( "failed to validate changes " + xmlPath.getAbsolutePath() );
+        getLog().error( "validation errors : " );
+        for ( Iterator iterator = errors.iterator(); iterator.hasNext(); )
+        {
+            SAXException error = (SAXException) iterator.next();
+            getLog().error( error.getMessage() );
+        }
+    }
+
+}
