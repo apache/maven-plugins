867a36ec03e29b33cb11e8aad030cb63676130a1
Dennis Lundberg <dennisl@apache.org>
2008-08-02 23:23:47 +0000

    o Remove an unused method found by PMD/CPD. An identical method exists in org/apache/maven/plugin/war/util/WarUtils.java if anyone needs it.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-war-plugin@682077 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/war/packaging/DependenciesAnalysisPackagingTask.java
+++ src/main/java/org/apache/maven/plugin/war/packaging/DependenciesAnalysisPackagingTask.java
@@ -231,27 +231,4 @@ public class DependenciesAnalysisPackagingTask
 
     }
 
-    private Artifact getArtifact( MavenProject project, Dependency dependency )
-    {
-        final Iterator it = project.getArtifacts().iterator();
-        while ( it.hasNext() )
-        {
-            Artifact artifact = (Artifact) it.next();
-            if ( artifact.getGroupId().equals( dependency.getGroupId() )
-                && artifact.getArtifactId().equals( dependency.getArtifactId() )
-                && artifact.getType().equals( dependency.getType() ) )
-            {
-                if ( artifact.getClassifier() == null && dependency.getClassifier() == null )
-                {
-                    return artifact;
-                }
-                else if ( dependency.getClassifier() != null
-                    && dependency.getClassifier().equals( artifact.getClassifier() ) )
-                {
-                    return artifact;
-                }
-            }
-        }
-        return null;
-    }
 }
