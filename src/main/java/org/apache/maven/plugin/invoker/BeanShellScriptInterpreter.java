cbdee47992a9f4f8ad3a7db41ed0cb813dbf12f9
Benjamin Bentmann <bentmann@apache.org>
2008-08-08 01:01:37 +0000

    [MINVOKER-7] Add groovy support for pre/post build hook scripts
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-invoker-plugin@683781 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/invoker/BeanShellScriptInterpreter.java
@@ -0,0 +1,110 @@
+package org.apache.maven.plugin.invoker;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import bsh.EvalError;
+import bsh.Interpreter;
+
+/**
+ * Provides a facade to evaluate BeanShell scripts.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+class BeanShellScriptInterpreter
+    implements ScriptInterpreter
+{
+
+    public Object evaluateScript( String script, List classPath, Map globalVariables, PrintStream scriptOutput )
+        throws ScriptEvaluationException
+    {
+        PrintStream origOut = System.out;
+        PrintStream origErr = System.err;
+
+        try
+        {
+            Interpreter engine = new Interpreter();
+
+            if ( scriptOutput != null )
+            {
+                System.setErr( scriptOutput );
+                System.setOut( scriptOutput );
+                engine.setErr( scriptOutput );
+                engine.setOut( scriptOutput );
+            }
+
+            if ( classPath != null && !classPath.isEmpty() )
+            {
+                for ( Iterator it = classPath.iterator(); it.hasNext(); )
+                {
+                    String path = (String) it.next();
+                    try
+                    {
+                        engine.getClassManager().addClassPath( new File( path ).toURI().toURL() );
+                    }
+                    catch ( IOException e )
+                    {
+                        throw new RuntimeException( "bad class path: " + path, e );
+                    }
+                }
+            }
+
+            if ( globalVariables != null )
+            {
+                for ( Iterator it = globalVariables.keySet().iterator(); it.hasNext(); )
+                {
+                    String variable = (String) it.next();
+                    Object value = globalVariables.get( variable );
+                    try
+                    {
+                        engine.set( variable, value );
+                    }
+                    catch ( EvalError e )
+                    {
+                        throw new ScriptEvaluationException( "Illegal global variable: " + variable + " = " + value,
+                                                             e );
+                    }
+                }
+            }
+
+            try
+            {
+                return engine.eval( script );
+            }
+            catch ( EvalError e )
+            {
+                throw new ScriptEvaluationException( "script evaluation error", e );
+            }
+        }
+        finally
+        {
+            System.setErr( origErr );
+            System.setOut( origOut );
+        }
+    }
+
+}
