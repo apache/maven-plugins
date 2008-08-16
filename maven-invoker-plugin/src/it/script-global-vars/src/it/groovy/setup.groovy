923ece461ce4a405533b1267fc09af597db846c6
Benjamin Bentmann <bentmann@apache.org>
2008-08-16 15:22:20 +0000

    o Extended integration test to check both script interpreters
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@686510 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-invoker-plugin/src/it/script-global-vars/src/it/groovy/setup.groovy
@@ -0,0 +1,27 @@
+import java.io.*
+import java.util.*
+import java.util.regex.*
+
+try
+{
+    println basedir
+    if ( !( basedir instanceof File ) )
+    {
+        println "Global script variable not defined: basedir"
+        return false
+    }
+
+    println localRepositoryPath
+    if ( !( localRepositoryPath instanceof File ) )
+    {
+        println "Global script variable not defined: localRepositoryPath"
+        return false
+    }
+}
+catch( Throwable t )
+{
+    t.printStackTrace()
+    return false
+}
+
+return true
