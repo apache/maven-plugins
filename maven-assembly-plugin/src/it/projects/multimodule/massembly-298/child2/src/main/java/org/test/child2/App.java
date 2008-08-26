54244880eece211ea76260eca285318e7f947809
John Dennis Casey <jdcasey@apache.org>
2008-08-26 22:33:51 +0000

    [MASSEMBLY-298] Add support unpackOptions/includes and unpackOptions/excludes in module binaries. Patch submitted by: Nathaniel Harward Integration test by: me (John Casey)
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@689271 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-assembly-plugin/src/it/projects/multimodule/massembly-298/child2/src/main/java/org/test/child2/App.java
@@ -0,0 +1,13 @@
+package org.test.child2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
