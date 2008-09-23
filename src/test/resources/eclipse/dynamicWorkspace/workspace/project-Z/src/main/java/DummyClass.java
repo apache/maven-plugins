d88033576609bc151acbdd43ea134dcc8d0c6bde
Barrie Treloar <baerrach@apache.org>
2008-09-23 03:45:07 +0000

    [MECLIPSE-493] Automatically locate workspace
    Eclispe Plugin
    - workspace type changes from String to File
    - getWorkspaceConfiguration() now locates the workspace if it is null.
    ReadWorkspaceLocations
    - exposed some constants needed outside of class
    - getProjectLocation()
      - now package scoped for unit testing
      - rewrote internals to read in the URI string inside ".location" file
      - now closes stream
    - readArtefact() now takes projectLocation as File instead of String
    - readAvailableJREs() returns null if no prefs file exists and a workspace was located.
      This causes the defaultJre to be used.
    TempEclipseWorkspace
    - added test fixtures used in TestCases.
    - usePathToProject changed to which projects inside the workspace should be linked in the metadata/
      This value was only null to avoid re-initializing the internal workspaces, which test fixtures resolves.
    - Removed localizedIndicator as static test fixtures are configured once at class load time and 
      regenerating these files each test run is not a large burden.  However misconfuration of the test files
      and not regenerating was a source of errors during creation of extra ITs.
    - Refactored out writeLocationFile()
    - Deleted getWorkspaceLocation() as the variable is public.
    AbstractEclipsePluginIT
    - Added testProject( File basedir ) for ITs running inside the test eclipse workspaces.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-eclipse-plugin@698086 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/eclipse/dynamicWorkspace/workspace/project-Z/src/main/java/DummyClass.java
@@ -0,0 +1,7 @@
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id: DummyClass.java 698081 2008-09-23 03:33:22Z baerrach $
+ */
+public class DummyClass
+{
+}
