3f8751d5104a82fc25fa04ef35a28f83aac4009f
Benjamin Bentmann <bentmann@apache.org>
2008-09-30 12:11:18 +0000

    [MINVOKER-68] Enable accessibility for non-public members from BeanShell scripts
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-invoker-plugin@700430 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/it/script-accessibility/src/main/java/org/MyClass.java
@@ -0,0 +1,14 @@
+package org;
+
+public class MyClass
+{
+
+    public static final String PUBLIC = "public";
+
+    protected static final String PROTECTED = "protected";
+
+    static final String PACKAGE = "package";
+
+    private static final String PRIVATE = "private";
+
+}
