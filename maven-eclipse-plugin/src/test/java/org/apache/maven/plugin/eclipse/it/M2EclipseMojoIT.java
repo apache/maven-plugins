5b822c3b24d24946e0b7b44a7559d70e0c1cf15e
Barrie Treloar <baerrach@apache.org>
2008-12-16 05:28:18 +0000

    [MECLIPSE-326] m2eclipse goal does not respect additionalBuildcommands and additionalProjectnatures defined in pom Submitted by: Holger Joest
    - Applied patch, minor change to test to have an IT suffix so it runs as an IT and not unit test.
    - Added license header to M2EclipseMojo.java
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@726962 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-eclipse-plugin/src/test/java/org/apache/maven/plugin/eclipse/it/M2EclipseMojoIT.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.plugin.eclipse.it;
+
+import java.util.Properties;
+
+/**
+ * @version $Id:$
+ */
+public class M2EclipseMojoIT
+    extends AbstractEclipsePluginIT
+{
+
+    public void testProject01()
+        throws Exception
+    {
+        testProject( "project-m2eclipse-01", new Properties(), "clean", "m2eclipse" );
+    }
+
+}
