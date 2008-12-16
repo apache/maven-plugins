9cb6b4267d7e7162bc3c7da6d5cca8ce6b5ea399
Barrie Treloar <baerrach@apache.org>
2008-12-16 05:28:18 +0000

    [MECLIPSE-326] m2eclipse goal does not respect additionalBuildcommands and additionalProjectnatures defined in pom Submitted by: Holger Joest
    - Applied patch, minor change to test to have an IT suffix so it runs as an IT and not unit test.
    - Added license header to M2EclipseMojo.java
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-eclipse-plugin@726962 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/eclipse/M2EclipseMojo.java
+++ src/main/java/org/apache/maven/plugin/eclipse/M2EclipseMojo.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugin.eclipse;
 
 import java.util.ArrayList;
@@ -29,8 +47,23 @@ public class M2EclipseMojo
         // disable normal dependency resolution; the m2eclipse plugin will handle it.
         setResolveDependencies( false );
 
-        setAdditionalProjectnatures( new ArrayList( Collections.singletonList( M2ECLIPSE_NATURE ) ) );
-        setAdditionalBuildcommands( new ArrayList( Collections.singletonList( M2ECLIPSE_BUILD_COMMAND ) ) );
+        if ( getAdditionalProjectnatures() != null )
+        {
+            getAdditionalProjectnatures().add( M2ECLIPSE_NATURE );
+        }
+        else
+        {
+            setAdditionalProjectnatures( new ArrayList( Collections.singletonList( M2ECLIPSE_NATURE ) ) );
+        }
+
+        if ( getAdditionalBuildcommands() != null )
+        {
+            getAdditionalBuildcommands().add( M2ECLIPSE_BUILD_COMMAND );
+        }
+        else
+        {
+            setAdditionalBuildcommands( new ArrayList( Collections.singletonList( M2ECLIPSE_BUILD_COMMAND ) ) );
+        }
 
         List classpathContainers = getClasspathContainers();
         if ( classpathContainers == null )
