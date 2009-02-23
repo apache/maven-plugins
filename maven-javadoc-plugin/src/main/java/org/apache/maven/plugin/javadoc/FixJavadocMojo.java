39a307f35bd632a7d4c87705f4e817f4a5dd6251
Vincent Siveton <vsiveton@apache.org>
2009-02-23 10:58:23 +0000

    MJAVADOC-226: Add a new goal to fix Javadoc tags
    o first add with it test and doc
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/plugins/trunk@746958 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-javadoc-plugin/src/main/java/org/apache/maven/plugin/javadoc/FixJavadocMojo.java
@@ -0,0 +1,51 @@
+package org.apache.maven.plugin.javadoc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+/**
+ * Fix Javadoc documentation and tags for the <code>Java code</code> for the project.
+ * See <a href="http://java.sun.com/j2se/1.5.0/docs/tooldocs/windows/javadoc.html#wheretags">Where Tags Can Be Used</a>.
+ *
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ * @since 2.5.1
+ * @goal fix
+ * @requiresDependencyResolution compile
+ * @execute phase="compile"
+ */
+public class FixJavadocMojo
+    extends AbstractFixJavadocMojo
+{
+    /**
+     * The classes of this project to compare the last release against.
+     * Used by {@link AbstractFixJavadocMojo.ClirrMojoWrapper} class.
+     *
+     * @parameter default-value="${project.build.outputDirectory}
+     */
+    private File classesDirectory;
+
+    /** {@inheritDoc} */
+    protected File getClassesDirectory()
+    {
+        return classesDirectory;
+    }
+}
