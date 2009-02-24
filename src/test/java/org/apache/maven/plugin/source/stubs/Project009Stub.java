eb7350ab06259406b2bb716460afd1a544258431
Dennis Lundberg <dennisl@apache.org>
2009-02-24 21:57:09 +0000

    [MSOURCES-39] Add an includePom option to the sources:jar goal Submitted by: Moritz Havelock Reviewed by: Dennis Lundberg
    o Applied patch with modifications
    o Added a test case
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-source-plugin@747562 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/source/stubs/Project009Stub.java
@@ -0,0 +1,120 @@
+package org.apache.maven.plugin.source.stubs;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
+import org.codehaus.plexus.util.ReaderFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Dennis Lundberg
+ */
+public class Project009Stub
+    extends MavenProjectStub
+{
+    private Build build;
+
+    private List resources;
+
+    private List testResources;
+
+    public Project009Stub()
+    {
+        MavenXpp3Reader pomReader = new MavenXpp3Reader();
+        Model model;
+
+        try
+        {
+            final File pomFile = new File( getBasedir(), "target/test-classes/unit/project-009/pom.xml" );
+            model = pomReader.read( ReaderFactory.newXmlReader( pomFile ) );
+            setModel( model );
+            setFile( pomFile );
+
+            setGroupId( model.getGroupId() );
+            setArtifactId( model.getArtifactId() );
+            setVersion( model.getVersion() );
+            setName( model.getName() );
+            setUrl( model.getUrl() );
+            setPackaging( model.getPackaging() );
+
+            Build build = new Build();
+            build.setFinalName( getArtifactId() + "-" + getVersion() );
+            build.setDirectory( getBasedir() + "/target/test/unit/project-009/target" );
+            setBuild( build );
+
+            String basedir = getBasedir().getAbsolutePath();
+            List compileSourceRoots = new ArrayList();
+            compileSourceRoots.add( basedir + "/target/test-classes/unit/project-009/src/main/java" );
+            setCompileSourceRoots( compileSourceRoots );
+
+            List testCompileSourceRoots = new ArrayList();
+            testCompileSourceRoots.add( basedir + "/target/test-classes/unit/project-009/src/test/java" );
+            setTestCompileSourceRoots( testCompileSourceRoots );
+
+            setResources( model.getBuild().getResources() );
+            setTestResources( model.getBuild().getTestResources() );
+
+            Artifact artifact =
+                new SourcePluginArtifactStub( getGroupId(), getArtifactId(), getVersion(), getPackaging(), null );
+            setArtifact( artifact );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public Build getBuild()
+    {
+        return build;
+    }
+
+    public void setBuild( Build build )
+    {
+        this.build = build;
+    }
+
+    public List getResources()
+    {
+        return resources;
+    }
+
+    public void setResources( List resources )
+    {
+        this.resources = resources;
+    }
+
+    public List getTestResources()
+    {
+        return testResources;
+    }
+
+    public void setTestResources( List testResources )
+    {
+        this.testResources = testResources;
+    }
+}
