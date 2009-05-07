32bd3b179cf0b94ad1f1052ccaaff4b8f64e96df
Vincent Siveton <vsiveton@apache.org>
2009-05-07 11:23:50 +0000

    MPDF-1: Ability to use pom properties in pdf.xml
    o added a test case
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@772617 13f79535-47bb-0310-9956-ffa450edef68

--- maven-pdf-plugin/src/test/java/org/apache/maven/plugins/pdf/PdfMojoTest.java
+++ maven-pdf-plugin/src/test/java/org/apache/maven/plugins/pdf/PdfMojoTest.java
@@ -20,8 +20,11 @@ package org.apache.maven.plugins.pdf;
  */
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.ReaderFactory;
 
 import java.io.File;
+import java.io.Reader;
 
 /**
  * @author ltheussl
@@ -95,5 +98,44 @@ public class PdfMojoTest
         assertTrue( "iText: Pdf file has no content!", pdfFile.length() > 0 );
      }
 
+    /**
+     * @throws Exception if any.
+     */
+    public void testPdfFilterMojo() throws Exception
+    {
+        File testPom = new File( getBasedir(), "/target/test-classes/unit/pdf/pom_filtering.xml" );
+        assertTrue( "testPom does not exist!", testPom.exists() );
+
+        PdfMojo mojo = (PdfMojo) lookupMojo( "pdf", testPom );
+        assertNotNull( "pdf mojo not found!", mojo );
+
+        File pdfFile = new File( getBasedir(), "/target/test-output/pdf/maven-pdf-plugin-doc-1.0-SNAPSHOT.pdf" );
+        if ( pdfFile.exists() )
+        {
+            pdfFile.delete();
+        }
+
+        mojo.execute();
+
+        assertTrue( "FO: Pdf file not created!", pdfFile.exists() );
+        assertTrue( "FO: Pdf file has no content!", pdfFile.length() > 0 );
+
+        File foFile = new File( getBasedir(), "/target/test-output/pdf/maven-pdf-plugin-doc-1.0-SNAPSHOT.fo" );
+        assertTrue( "FO: Fo file not created!", foFile.exists() );
+        assertTrue( "FO: Fo file has no content!", foFile.length() > 0 );
+
+        Reader reader = null;
+        String foContent;
+        try
+        {
+            reader = ReaderFactory.newXmlReader( foFile );
+            foContent = IOUtil.toString( reader );
+        }
+        finally
+        {
+            IOUtil.close( reader );
+        }
+        assertTrue( foContent.indexOf( ">Test filtering<" ) > 0 );
+    }
 
 }
