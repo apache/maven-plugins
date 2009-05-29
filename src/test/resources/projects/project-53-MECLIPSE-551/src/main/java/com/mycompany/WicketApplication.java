db47e3a5556c86970ea222f4ee245d79dc00cd43
Barrie Treloar <baerrach@apache.org>
2009-05-29 03:41:15 +0000

    [MECLIPSE-551] regression - Resources are excluded if resource dir equals source dir
    EclipseSourceDir
    - includes and excludes are now lists of Strings instead of a String separated by the | character.
    - added get*AsString methods which return the eclipse formatted pattern for include and exclude.
    - added unit testing.
    
    EclipsePlugin
    - Moved generation of a String for the eclipse include/exclude patterns to EclipseSourceDir.
    - In extractResourceDirs() the resources directories were added to the set without checking the result of the add() method.
      This meant that when a resource path matched a source path the resource path was silently ignored.
      Now the source directory is merged with the resource and since Eclipse doesnt have a concept of "main", "test", "resource"
      any include/exclude patterns are removed.
      
    IT project-25
    This IT also uses src/main/java as a resource directory
    - update classpath to include src/main/resources as the pom references this value
    - added dummy file to src/main/resources so that it would be included in the .classpath
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-eclipse-plugin@779829 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/projects/project-53-MECLIPSE-551/src/main/java/com/mycompany/WicketApplication.java
@@ -0,0 +1,27 @@
+package com.mycompany;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see com.mycompany.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    
+    /**
+     * Constructor
+     */
+	public WicketApplication()
+	{
+	}
+	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+}
