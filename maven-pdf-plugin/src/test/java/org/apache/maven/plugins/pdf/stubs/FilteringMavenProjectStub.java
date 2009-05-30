e732047fd5a70048de51f8f29894dfd2680b59eb
Vincent Siveton <vsiveton@apache.org>
2009-05-30 10:59:10 +0000

    MPDF-13: Copy resources from skin artifact
    o added site renderer dependency to copy resources from skin jar
    o updated test cases
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@780233 13f79535-47bb-0310-9956-ffa450edef68

--- maven-pdf-plugin/src/test/java/org/apache/maven/plugins/pdf/stubs/FilteringMavenProjectStub.java
+++ maven-pdf-plugin/src/test/java/org/apache/maven/plugins/pdf/stubs/FilteringMavenProjectStub.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.DefaultArtifactRepository;
@@ -35,15 +36,15 @@ import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
  * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
  * @version $Id$
  */
-public class DefaultMavenProjectStub
+public class FilteringMavenProjectStub
     extends MavenProjectStub
 {
-    public DefaultMavenProjectStub()
+    public FilteringMavenProjectStub()
     {
         MavenXpp3Reader pomReader = new MavenXpp3Reader();
         try
         {
-            Model model = pomReader.read( new FileReader( new File( getBasedir() + "/pom.xml" ) ) );
+            Model model = pomReader.read( new FileReader( new File( getBasedir() + "/pom_filtering.xml" ) ) );
             setModel( model );
         }
         catch ( Exception e )
@@ -59,9 +60,27 @@ public class DefaultMavenProjectStub
     }
 
     /** {@inheritDoc} */
+    public String getVersion()
+    {
+        return getModel().getVersion();
+    }
+
+    /** {@inheritDoc} */
     public File getBasedir()
     {
         return new File( super.getBasedir() + "/target/test-classes/unit/pdf/" );
+    }
+
+    /** {@inheritDoc} */
+    public List getDevelopers()
+    {
+        return getModel().getDevelopers();
+    }
+
+    /** {@inheritDoc} */
+    public Properties getProperties()
+    {
+        return getModel().getProperties();
     }
 
     /** {@inheritDoc} */
