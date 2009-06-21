fa102ee962930fd8da95e0cd4e629bd06747ffdc
Barrie Treloar <baerrach@apache.org>
2009-06-21 07:11:14 +0000

    IT overhaul. - All manually coded comparisons removed and replaced with files in the "expected" directory - this now checks a lot more than the manual tests did. - Created verification script to check that any generated files have a corresponding expected file.   If the expected file is missing then the generated file is used as a "seed" and copied into the src/test/resources for future use.   The seeded files need to be checked to make sure they are useful and then added to source control.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@786945 13f79535-47bb-0310-9956-ffa450edef68

--- maven-eclipse-plugin/src/test/java/org/apache/maven/plugin/eclipse/it/EclipsePluginIT.java
+++ maven-eclipse-plugin/src/test/java/org/apache/maven/plugin/eclipse/it/EclipsePluginIT.java
@@ -399,38 +399,6 @@ public class EclipsePluginIT
         throws Exception
     {
         testProject( "project-35" );
-
-        File basedir = getTestFile( "target/test-classes/projects/project-35" );
-
-        checkContextRoot( basedir, "multymodule-war", "multymodule-ear", "/somethingVeryDifferent" );
-
-        FileReader reader =
-            new FileReader( new File( basedir, "multymodule-war/.settings/org.eclipse.wst.common.component" ) );
-        Xpp3Dom warComponent = Xpp3DomBuilder.build( reader );
-        Xpp3Dom[] dependentModules = warComponent.getChild( "wb-module" ).getChildren( "dependent-module" );
-        assertEquals( 2, dependentModules.length );
-        for ( int index = 0; index < dependentModules.length; index++ )
-        {
-            assertEquals( "/WEB-INF/lib", dependentModules[index].getAttribute( "deploy-path" ) );
-        }
-
-        reader = new FileReader( new File( basedir, "multymodule-ear/.settings/org.eclipse.wst.common.component" ) );
-        Xpp3Dom earComponent = Xpp3DomBuilder.build( reader );
-        dependentModules = earComponent.getChild( "wb-module" ).getChildren( "dependent-module" );
-        assertEquals( 2, dependentModules.length );
-        for ( int index = 0; index < dependentModules.length; index++ )
-        {
-            if ( dependentModules[index].getAttribute( "archiveName" ).endsWith( "war" ) )
-            {
-                assertEquals( "/", dependentModules[index].getAttribute( "deploy-path" ) );
-                assertTrue( !dependentModules[index].getAttribute( "archiveName" ).startsWith( ".." ) );
-            }
-            else
-            {
-                assertEquals( "lib", dependentModules[index].getAttribute( "deploy-path" ) );
-                assertTrue( dependentModules[index].getAttribute( "archiveName" ).startsWith( ".." ) );
-            }
-        }
     }
 
     public void testProject36()
@@ -467,26 +435,48 @@ public class EclipsePluginIT
         testProject( "project-38" );
     }
 
-    public void testProject39()
+    public void testProject39_a()
         throws Exception
     {
-        checkJRESettingsWithEclipseWorkspace( "project-39", TempEclipseWorkspace.getFixtureEclipseWithDefault13(),
-                                              "", null );
-        checkJRESettingsWithEclipseWorkspace( "project-39", TempEclipseWorkspace.getFixtureEclipseWithDefault15(),
-                                              "J2SE-1.3", null );
-        checkJRESettingsWithEclipseWorkspace( "project-39", TempEclipseWorkspace.getFixtureEclipseWorkspaceWithRad7Default14(),
-                                              "J2SE-1.3", null );
+        checkJRESettingsWithEclipseWorkspace( "project-39-a", TempEclipseWorkspace.getFixtureEclipseWithDefault13(),
+                                              null );
+    }
+    
+    public void testProject39_b()
+        throws Exception
+    {
+        checkJRESettingsWithEclipseWorkspace( "project-39-b", TempEclipseWorkspace.getFixtureEclipseWithDefault15(), null );
     }
 
-    public void testProject40()
+    public void testProject39_c()
+        throws Exception
+    {
+        checkJRESettingsWithEclipseWorkspace( "project-39-c",
+                                              TempEclipseWorkspace.getFixtureEclipseWorkspaceWithRad7Default14(), null );
+    }
+
+    public void testProject40_a()
         throws Exception
     {
         String jre131 = new java.io.File( "target/test-classes/eclipse/dummyJDK/1.3.1/bin/javac" ).getCanonicalPath();
-        checkJRESettingsWithEclipseWorkspace( "project-40", TempEclipseWorkspace.getFixtureEclipseWithDefault13(),
-                                              "JVM 1.3.1", jre131 );
-        checkJRESettingsWithEclipseWorkspace( "project-40", TempEclipseWorkspace.getFixtureEclipseWithDefault15(),
-                                              "JVM 1.3.1", jre131 );
-        checkJRESettingsWithEclipseWorkspace( "project-40", TempEclipseWorkspace.getFixtureEclipseWorkspaceWithRad7Default14(), "",
+        checkJRESettingsWithEclipseWorkspace( "project-40-a", TempEclipseWorkspace.getFixtureEclipseWithDefault13(),
+                                              jre131 );
+    }
+    
+    public void testProject40_b()
+        throws Exception
+    {
+        String jre131 = new java.io.File( "target/test-classes/eclipse/dummyJDK/1.3.1/bin/javac" ).getCanonicalPath();
+        checkJRESettingsWithEclipseWorkspace( "project-40-b", TempEclipseWorkspace.getFixtureEclipseWithDefault15(),
+                                              jre131 );
+    }
+
+    public void testProject40_c()
+        throws Exception
+    {
+        String jre131 = new java.io.File( "target/test-classes/eclipse/dummyJDK/1.3.1/bin/javac" ).getCanonicalPath();
+        checkJRESettingsWithEclipseWorkspace( "project-40-c",
+                                              TempEclipseWorkspace.getFixtureEclipseWorkspaceWithRad7Default14(),
                                               jre131 );
     }
 
@@ -663,46 +653,10 @@ public class EclipsePluginIT
         executeMaven( pom, new Properties(), goals );
         // Test project
         testProject( "j2ee-simple" );
-        checkContextRoot( basedir, "servlets/servlet", "ear", "servlet" );
-
-    }
-
-    private void checkContextRoot( File basedir, String warModule, String earModule, String expectedContextRoot )
-        throws FileNotFoundException, XmlPullParserException, IOException
-    {
-        FileReader reader =
-            new FileReader( new File( basedir, warModule + "/.settings/org.eclipse.wst.common.component" ) );
-        Xpp3Dom warComponent = Xpp3DomBuilder.build( reader );
-        Xpp3Dom[] properties = warComponent.getChild( "wb-module" ).getChildren( "property" );
-        boolean contextRootAvaliable = false;
-        for ( int index = 0; index < properties.length; index++ )
-        {
-            if ( properties[index].getAttribute( "name" ).equals( "context-root" ) )
-            {
-                assertEquals( "Context root detection in org.eclipse.wst.common.component", expectedContextRoot,
-                              properties[index].getAttribute( "value" ) );
-                contextRootAvaliable = true;
-            }
-        }
-        assertTrue( "there must be a context root here", contextRootAvaliable );
-
-        reader = new FileReader( new File( basedir, earModule + "/target/eclipseEar/META-INF/application.xml" ) );
-        Xpp3Dom generatedApplicationXML = Xpp3DomBuilder.build( reader );
-
-        Xpp3Dom[] modules = generatedApplicationXML.getChildren( "module" );
-        for ( int index = 0; index < modules.length; index++ )
-        {
-            if ( modules[index].getChild( "web" ) != null )
-            {
-                assertEquals( "Context root detection in target/eclipseEar/META-INF/application.xml",
-                              expectedContextRoot,
-                              modules[index].getChild( "web" ).getChild( "context-root" ).getValue() );
-            }
-        }
     }
 
     private void checkJRESettingsWithEclipseWorkspace( String project, TempEclipseWorkspace workspace,
-                                                       String expectedJRE, String jreExec )
+                                                       String jreExec )
         throws Exception
     {
         Properties properties = new Properties();
@@ -712,29 +666,6 @@ public class EclipsePluginIT
             properties.setProperty( "maven.compiler.executable", jreExec );
         }
         testProject( project, properties, "clean", "eclipse" );
-
-        Xpp3Dom classpath =
-            Xpp3DomBuilder.build( new FileReader( getTestFile( "target/test-classes/projects/" + project
-                + "/.classpath" ) ) );
-
-        boolean foundDirectCompileAsProject = false;
-        String foundJRE = "";
-
-        Xpp3Dom[] classpathentries = classpath.getChildren( "classpathentry" );
-        for ( int index = 0; index < classpathentries.length; index++ )
-        {
-            if ( "/direct-compile".equals( classpathentries[index].getAttribute( "path" ) ) )
-            {
-                foundDirectCompileAsProject = true;
-            }
-            String path = classpathentries[index].getAttribute( "path" );
-            if ( "con".equals( classpathentries[index].getAttribute( "kind" ) ) && path.lastIndexOf( '/' ) > 0 )
-            {
-                foundJRE = path.substring( path.lastIndexOf( '/' ) + 1 );
-            }
-        }
-        assertTrue( foundDirectCompileAsProject );
-        assertEquals( expectedJRE, foundJRE );
     }
 
 }
