ecfbca77d7bf19f1d5b7948d072e381f84456e29
Vincent Siveton <vsiveton@apache.org>
2009-07-08 15:59:36 +0000

    o forgot test files for r792176
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@792201 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/fix-test/expected/src/main/java/fix/test/ClassWithNoJavadoc.java
@@ -0,0 +1,107 @@
+package fix.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Map;
+
+/**
+ * <p>ClassWithNoJavadoc class.</p>
+ *
+ * @author Administrator
+ * @version $Id: $
+ * @since 1.0
+ */
+public class ClassWithNoJavadoc
+{
+    /** Constant <code>MY_STRING_CONSTANT="value"</code> */
+    public static final String MY_STRING_CONSTANT = "value";
+
+    /** Constant <code>MY_STRING_CONSTANT2="default value"</code> */
+    public static final String MY_STRING_CONSTANT2 = "default" + " value";
+
+    /** Constant <code>MY_INT_CONSTANT=1</code> */
+    public static final int MY_INT_CONSTANT = 1;
+
+    /** Constant <code>EOL="System.getProperty( line.separator )"</code> */
+    public static final String EOL = System.getProperty( "line.separator" );
+
+    // take care of identifier
+    private static final String MY_PRIVATE_STRING_CONSTANT = "";
+
+    // QDOX-155
+    /** Constant <code>SEPARATOR=','</code> */
+    public static final char SEPARATOR = ',';
+
+    // QDOX-156
+    /** Constant <code>TEST1="test1"</code> */
+    public static final String TEST1 = "test1";
+
+    /**
+     * <p>Constructor for ClassWithNoJavadoc.</p>
+     */
+    public ClassWithNoJavadoc()
+    {
+    }
+
+    // take care of primitive
+    /**
+     * <p>missingJavadocTagsForPrimitives</p>
+     *
+     * @param i a int.
+     * @param b a byte.
+     * @param f a float.
+     * @param c a char.
+     * @param s a short.
+     * @param l a long.
+     * @param d a double.
+     * @param bb a boolean.
+     */
+    public void missingJavadocTagsForPrimitives( int i, byte b, float f, char c, short s, long l, double d, boolean bb )
+    {
+    }
+
+    // take care of object
+    /**
+     * <p>missingJavadocTagsForObjects</p>
+     *
+     * @param s a {@link java.lang.String} object.
+     * @param m a {@link java.util.Map} object.
+     */
+    public void missingJavadocTagsForObjects( String s, Map m )
+    {
+    }
+
+    // no Javadoc needed
+    private void privateMethod( String str )
+    {
+    }
+
+    //No javadoc for inner class.
+    public class InnerClass
+    {
+        public InnerClass()
+        {
+        }
+
+        public void nothing()
+        {
+        }
+    }
+}
---
+++ src/test/resources/unit/fix-test/expected/src/main/java/fix/test/InterfaceWithJavadoc.java
@@ -0,0 +1,73 @@
+package fix.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * To add default interface tags.
+ *
+ * @author Administrator
+ * @version $Id: $
+ * @since 1.0
+ */
+public interface InterfaceWithJavadoc
+{
+    /** default comment */
+    String MY_STRING_CONSTANT = "value";
+
+    /**
+     * To add default method tags.
+     *
+     * @param aString a {@link java.lang.String} object.
+     */
+    public void method1( String aString );
+
+    /**
+     * To take care of identifier.
+     *
+     * @param aString a {@link java.lang.String} object.
+     */
+    void method2( String aString );
+
+    // one single comment
+    /**
+     * To take care of single comments.
+     *
+     * @param aString a string
+     * @return null
+     */
+    // other single comment
+    public String method3( String aString );
+
+    /**
+     * Nothing.
+     *
+     * @param aString a {@link java.lang.String} object.
+     * @return a {@link java.lang.String} object.
+     */
+    public String method4( String aString );
+
+    /**
+     * Nothing.
+     *
+     * @param aString a {@link java.lang.String} object.
+     * @return a {@link java.lang.String} object.
+     */
+    public String method5( String aString );
+}
