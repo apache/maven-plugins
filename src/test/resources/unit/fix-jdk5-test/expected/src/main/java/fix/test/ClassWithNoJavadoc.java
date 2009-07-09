039d29497d4b15c3a480890b6dd9b2f0c0cd8388
Vincent Siveton <vsiveton@apache.org>
2009-07-09 12:09:12 +0000

    o added jdk5 and jdk6 tests cases for fix plugin
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@792518 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/resources/unit/fix-jdk5-test/expected/src/main/java/fix/test/ClassWithNoJavadoc.java
@@ -0,0 +1,73 @@
+package fix.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>ClassWithNoJavadoc class.</p>
+ *
+ * @author <a href="mailto:vsiveton@apache.org">vsiveton@apache.org</a>
+ * @version $Id: $
+ * @since 1.0
+ */
+public class ClassWithNoJavadoc
+    implements InterfaceWithNoJavadoc
+{
+    /**
+     * <p>Constructor for ClassWithNoJavadoc.</p>
+     */
+    public ClassWithNoJavadoc()
+    {
+    }
+
+    /**
+     * <p>Constructor for ClassWithNoJavadoc.</p>
+     *
+     * @param list a {@link java.util.List} object.
+     */
+    public ClassWithNoJavadoc( List<String> list )
+    {
+    }
+
+    // QDOX-155
+    /**
+     * <p>withGenericParameter</p>
+     *
+     * @param request a T object.
+     * @return a {@link java.util.List} object.
+     * @throws java.lang.Exception if any.
+     */
+    public <T extends String> List<String> withGenericParameter( T request )
+        throws Exception
+    {
+        return null;
+    }
+
+    // ----------------------------------------------------------------------
+    // Inheritance
+    // ----------------------------------------------------------------------
+
+    /** {@inheritDoc} */
+    public void withGenericParameters( List<String> aList, Map<Map<String, List<String>>, List<String>> aMap )
+    {
+    }
+}
---
+++ src/test/resources/unit/fix-jdk6-test/expected/src/main/java/fix/test/ClassWithJavadoc.java
@@ -0,0 +1,57 @@
+package fix.test;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.List;
+
+/**
+ * To add default class tags.
+ *
+ * @author <a href="mailto:vsiveton@apache.org">vsiveton@apache.org</a>
+ * @version $Id: $
+ * @since 1.0
+ */
+public class ClassWithJavadoc
+    implements InterfaceWithJavadoc
+{
+    /**
+     * Constructor with generic param.
+     *
+     * @param list a {@link java.util.List} object.
+     */
+    public ClassWithJavadoc( List<String> list )
+    {
+    }
+
+    // ----------------------------------------------------------------------
+    // Inheritance
+    // ----------------------------------------------------------------------
+
+    /** {@inheritDoc} */
+    @Override
+    public void withGenericParameters( List<String> aList )
+    {
+    }
+
+    /** {@inheritDoc} */
+    public void withGenericParameters2( List<String> aList )
+    {
+    }
+}
