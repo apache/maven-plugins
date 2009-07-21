45822206c235f6936d4a97563733aac6e5623ee9
Vincent Siveton <vsiveton@apache.org>
2009-07-21 12:24:14 +0000

    MJAVADOC-181: Javadoc report not generated for multi-module project if run from parent level.
    o added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@796271 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-javadoc-plugin/src/it/MJAVADOC-181/library/module-b/src/main/java/bbb/App.java
@@ -0,0 +1,24 @@
+package bbb;
+
+/**
+ * Hello world class.
+ *
+ * @.foo    A 1st custom javadoc tag.
+ * @author  Me
+ * @version 1st
+ * @.bar    A 2nd custom javadoc tag.
+ * @see     String#startsWith(String)
+ */
+public class App
+{
+    /**
+     * Hello world method.
+     *
+     * @.foo    A 1st custom javadoc tag.
+     * @see     String#endsWith(String)
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
