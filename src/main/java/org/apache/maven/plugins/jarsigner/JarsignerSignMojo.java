0fc9b5ea8576b31c94d5bf5900b3a72d79ad653e
Benjamin Bentmann <bentmann@apache.org>
2009-08-09 17:00:30 +0000

    [MJARSIGNER-5] Allow to unsign JARs before re-signing
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-jarsigner-plugin@802558 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/jarsigner/JarsignerSignMojo.java
+++ src/main/java/org/apache/maven/plugins/jarsigner/JarsignerSignMojo.java
@@ -19,8 +19,19 @@ package org.apache.maven.plugins.jarsigner;
  * under the License.
  */
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.cli.Commandline;
 
@@ -31,6 +42,7 @@ import org.codehaus.plexus.util.cli.Commandline;
  * @version $Id$
  * @goal sign
  * @phase package
+ * @since 1.0
  */
 public class JarsignerSignMojo
     extends AbstractJarsignerMojo
@@ -99,6 +111,15 @@ public class JarsignerSignMojo
      * @required
      */
     private String alias;
+
+    /**
+     * Indicates whether existing signatures should be removed from the processed JAR files prior to signing them. If
+     * enabled, the resulting JAR will appear as being signed only once.
+     * 
+     * @parameter expression="${jarsigner.removeExistingSignatures}" default-value="false"
+     * @since 1.1
+     */
+    private boolean removeExistingSignatures;
 
     protected Commandline getCommandline( final File archive, final Commandline commandLine )
     {
@@ -173,6 +194,110 @@ public class JarsignerSignMojo
         }
 
         return commandLineInfo;
+    }
+
+    protected void preProcessArchive( final File archive )
+        throws MojoExecutionException
+    {
+        if ( removeExistingSignatures )
+        {
+            unsignArchive( archive );
+        }
+    }
+
+    /**
+     * Removes any existing signatures from the specified JAR file. We will stream from the input JAR directly to the
+     * output JAR to retain as much metadata from the original JAR as possible.
+     * 
+     * @param jarFile The JAR file to unsign, must not be <code>null</code>.
+     * @throws MojoExecutionException If the unsigning failed.
+     */
+    private void unsignArchive( final File jarFile )
+        throws MojoExecutionException
+    {
+        if ( getLog().isDebugEnabled() )
+        {
+            getLog().debug( "Unsigning " + jarFile );
+        }
+
+        File unsignedFile = new File( jarFile.getAbsolutePath() + ".unsigned" );
+
+        ZipInputStream zis = null;
+        ZipOutputStream zos = null;
+        try
+        {
+            zis = new ZipInputStream( new BufferedInputStream( new FileInputStream( jarFile ) ) );
+            zos = new ZipOutputStream( new BufferedOutputStream( new FileOutputStream( unsignedFile ) ) );
+
+            for ( ZipEntry ze = zis.getNextEntry(); ze != null; ze = zis.getNextEntry() )
+            {
+                if ( isSignatureFile( ze.getName() ) )
+                {
+                    if ( getLog().isDebugEnabled() )
+                    {
+                        getLog().debug( "  Removing " + ze.getName() );
+                    }
+
+                    continue;
+                }
+
+                zos.putNextEntry( ze );
+
+                IOUtil.copy( zis, zos );
+            }
+
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Failed to unsign archive " + jarFile, e );
+        }
+        finally
+        {
+            IOUtil.close( zis );
+            IOUtil.close( zos );
+        }
+
+        try
+        {
+            FileUtils.rename( unsignedFile, jarFile );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Failed to unsign archive " + jarFile, e );
+        }
+    }
+
+    /**
+     * Checks whether the specified JAR file entry denotes a signature-related file, i.e. matches
+     * <code>META-INF/*.SF</code>, <code>META-INF/*.DSA</code> or <code>META-INF/*.RSA</code>.
+     * 
+     * @param entryName The name of the JAR file entry to check, must not be <code>null</code>.
+     * @return <code>true</code> if the entry is related to a signature, <code>false</code> otherwise.
+     */
+    private boolean isSignatureFile( String entryName )
+    {
+        if ( entryName.regionMatches( true, 0, "META-INF", 0, 8 ) )
+        {
+            entryName = entryName.replace( '\\', '/' );
+
+            if ( entryName.indexOf( '/' ) == 8 && entryName.lastIndexOf( '/' ) == 8 )
+            {
+                if ( entryName.regionMatches( true, entryName.length() - 3, ".SF", 0, 3 ) )
+                {
+                    return true;
+                }
+                if ( entryName.regionMatches( true, entryName.length() - 4, ".DSA", 0, 4 ) )
+                {
+                    return true;
+                }
+                if ( entryName.regionMatches( true, entryName.length() - 4, ".RSA", 0, 4 ) )
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
