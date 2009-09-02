630c2ff5a0c292ba450a838dc692090abae47c99
Benjamin Bentmann <bentmann@apache.org>
2009-09-02 17:19:27 +0000

    o Formatted code
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@810620 13f79535-47bb-0310-9956-ffa450edef68

--- maven-gpg-plugin/src/main/java/org/apache/maven/plugin/gpg/SignAndDeployFileMojo.java
+++ maven-gpg-plugin/src/main/java/org/apache/maven/plugin/gpg/SignAndDeployFileMojo.java
@@ -22,10 +22,7 @@ package org.apache.maven.plugin.gpg;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.maven.artifact.Artifact;
@@ -40,7 +37,6 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.deploy.DeployFileMojo;
-import org.apache.maven.project.artifact.AttachedArtifact;
 import org.apache.maven.settings.Settings;
 
 /**
@@ -51,25 +47,27 @@ import org.apache.maven.settings.Settings;
  * @requiresProject false
  * @since 1.0-beta-4
  */
-public class SignAndDeployFileMojo extends DeployFileMojo {
+public class SignAndDeployFileMojo
+    extends DeployFileMojo
+{
+
     /**
      * The passphrase to use when signing.
-     *
+     * 
      * @parameter expression="${gpg.passphrase}"
      */
     private String passphrase;
 
     /**
-     * The "name" of the key to sign with.  Passed to gpg as --local-user.
+     * The "name" of the key to sign with. Passed to gpg as --local-user.
      * 
      * @parameter expression="${gpg.keyname}"
      */
     private String keyname;
 
-
     /**
-     * Passes --use-agent or --no-use-agent to gpg.   If using an agent,
-     * the password is optional as the agent will provide it.
+     * Passes --use-agent or --no-use-agent to gpg. If using an agent, the password is optional as the agent will
+     * provide it.
      * 
      * @parameter expression="${gpg.useagent}" default-value="false"
      * @required
@@ -78,188 +76,193 @@ public class SignAndDeployFileMojo extends DeployFileMojo {
 
     /**
      * The directory where to store signature files.
-     *
+     * 
      * @parameter expression="${gpg.asc.directory}"
      */
     private File ascDirectory;
-    
-    
+
     /**
      * @parameter expression="${settings}"
      * @required
      * @readonly
      */
     protected Settings settings;
-    
-    
+
     /**
      * Maven ArtifactHandlerManager
-     *
+     * 
      * @component
      * @required
      * @readonly
      */
     private ArtifactHandlerManager artifactHandlerManager;
-    
-    
-    /* stuff I need to copy since the plugin:plugin doesn't support inheritance outside
-     * the current jar
+
+    /*
+     * stuff I need to copy since the plugin:plugin doesn't support inheritance outside the current jar
      */
     /**
      * @component
      */
     private ArtifactDeployer deployer;
+
     /**
      * @parameter expression="${localRepository}"
      * @required
      * @readonly
      */
     private ArtifactRepository localRepository;
+
     /**
-     * GroupId of the artifact to be deployed.  Retrieved from POM file if specified.
-     *
+     * GroupId of the artifact to be deployed. Retrieved from POM file if specified.
+     * 
      * @parameter expression="${groupId}"
      */
     private String groupId;
+
     /**
-     * ArtifactId of the artifact to be deployed.  Retrieved from POM file if specified.
-     *
+     * ArtifactId of the artifact to be deployed. Retrieved from POM file if specified.
+     * 
      * @parameter expression="${artifactId}"
      */
     private String artifactId;
+
     /**
-     * Version of the artifact to be deployed.  Retrieved from POM file if specified.
-     *
+     * Version of the artifact to be deployed. Retrieved from POM file if specified.
+     * 
      * @parameter expression="${version}"
      */
     private String version;
+
     /**
-     * Type of the artifact to be deployed.  Retrieved from POM file if specified.
-     *
+     * Type of the artifact to be deployed. Retrieved from POM file if specified.
+     * 
      * @parameter expression="${packaging}"
      */
     private String packaging;
+
     /**
      * Description passed to a generated POM file (in case of generatePom=true)
-     *
+     * 
      * @parameter expression="${generatePom.description}"
      */
     private String description;
+
     /**
      * File to be deployed.
-     *
+     * 
      * @parameter expression="${file}"
      * @required
      */
     private File file;
+
     /**
-     * Server Id to map on the &lt;id&gt; under &lt;server&gt; section of settings.xml
-     * In most cases, this parameter will be required for authentication.
-     *
+     * Server Id to map on the &lt;id&gt; under &lt;server&gt; section of settings.xml In most cases, this parameter
+     * will be required for authentication.
+     * 
      * @parameter expression="${repositoryId}" default-value="remote-repository"
      * @required
      */
     private String repositoryId;
+
     /**
-     * The type of remote repository layout to deploy to. Try <i>legacy</i> for 
-     * a Maven 1.x-style repository layout.
+     * The type of remote repository layout to deploy to. Try <i>legacy</i> for a Maven 1.x-style repository layout.
      * 
      * @parameter expression="${repositoryLayout}" default-value="default"
      * @required
      */
     private String repositoryLayout;
+
     /**
      * Map that contains the layouts
-     *
+     * 
      * @component role="org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout"
      */
     private Map repositoryLayouts;
+
     /**
      * URL where the artifact will be deployed. <br/>
      * ie ( file://C:\m2-repo or scp://host.com/path/to/repo )
-     *
+     * 
      * @parameter expression="${url}"
      * @required
      */
     private String url;
+
     /**
      * Component used to create an artifact
-     *
+     * 
      * @component
      */
     private ArtifactFactory artifactFactory;
+
     /**
      * Component used to create a repository
-     *
+     * 
      * @component
      */
     private ArtifactRepositoryFactory repositoryFactory;
+
     /**
-     * Location of an existing POM file to be deployed alongside the main
-     * artifact, given by the ${file} parameter.
+     * Location of an existing POM file to be deployed alongside the main artifact, given by the ${file} parameter.
      * 
      * @parameter expression="${pomFile}"
      */
     private File pomFile;
+
     /**
-     * Upload a POM for this artifact.  Will generate a default POM if none is
-     * supplied with the pomFile argument.
-     *
+     * Upload a POM for this artifact. Will generate a default POM if none is supplied with the pomFile argument.
+     * 
      * @parameter expression="${generatePom}" default-value="true"
      */
     private boolean generatePom;
+
     /**
      * Add classifier to the artifact
-     *
+     * 
      * @parameter expression="${classifier}";
      */
     private String classifier;
+
     /**
      * Whether to deploy snapshots with a unique version or not.
-     *
+     * 
      * @parameter expression="${uniqueVersion}" default-value="true"
      */
     private boolean uniqueVersion;
-    
-    
-    
-    
+
     private final GpgSigner signer = new GpgSigner();
-    
-    
-    
+
     public void execute()
-    throws MojoExecutionException
+        throws MojoExecutionException
     {
         ArtifactHandler handler = new DefaultArtifactHandler( "asc" );
         Map map = new HashMap();
         map.put( "asc", handler );
         artifactHandlerManager.addHandlers( map );
-        
-        
+
         copyToParent();
         ArtifactDeployer deployer = getDeployer();
         signer.setInteractive( settings.isInteractiveMode() );
         signer.setKeyName( keyname );
         signer.setUseAgent( useAgent );
-        signer.setOutputDirectory(ascDirectory);
-        signer.setBaseDirectory(new File("foo").getAbsoluteFile().getParentFile().getAbsoluteFile());
-        
+        signer.setOutputDirectory( ascDirectory );
+        signer.setBaseDirectory( new File( "foo" ).getAbsoluteFile().getParentFile().getAbsoluteFile() );
+
         setDeployer( new SignedArtifactDeployer( deployer, passphrase ) );
         super.execute();
     }
-    
-    /* this sucks.  The plugin:plugin won't find properties in parent classes unless 
-     * they exist in the same compilation unit.  Thus, we need to declare our own and 
-     * copy them to the parent.  HOWEVER, the DeployFileMojo doesn't have public setters.
-     * Thus, we need to do crappy field copies.
+
+    /*
+     * this sucks. The plugin:plugin won't find properties in parent classes unless they exist in the same compilation
+     * unit. Thus, we need to declare our own and copy them to the parent. HOWEVER, the DeployFileMojo doesn't have
+     * public setters. Thus, we need to do crappy field copies.
      */
     private void copyToParent()
-    throws MojoExecutionException 
+        throws MojoExecutionException
     {
         this.setDeployer( deployer );
         this.setLocalRepository( localRepository );
-        
+
         setDeployFileMojoField( "groupId", groupId );
         setDeployFileMojoField( "artifactId", artifactId );
         setDeployFileMojoField( "version", version );
@@ -277,16 +280,17 @@ public class SignAndDeployFileMojo extends DeployFileMojo {
         setDeployFileMojoField( "classifier", classifier );
         setDeployFileMojoField( "uniqueVersion", Boolean.valueOf( uniqueVersion ) );
     }
-    
+
     private void setDeployFileMojoField( String name, Object value )
-    throws MojoExecutionException 
+        throws MojoExecutionException
     {
-        try {
+        try
+        {
             Field f = DeployFileMojo.class.getDeclaredField( name );
             f.setAccessible( true );
             f.set( this, value );
         }
-        catch (Exception e)
+        catch ( Exception e )
         {
             throw new MojoExecutionException( "Could not set field " + name, e );
         }
@@ -303,12 +307,14 @@ public class SignAndDeployFileMojo extends DeployFileMojo {
         this.localRepository = localRepository;
         super.setLocalRepository( localRepository );
     }
-    
-    private class SignedArtifactDeployer implements ArtifactDeployer 
+
+    private class SignedArtifactDeployer
+        implements ArtifactDeployer
     {
         ArtifactDeployer deployer;
+
         String pass;
-        
+
         public SignedArtifactDeployer( ArtifactDeployer dep, String passphrase )
             throws MojoExecutionException
         {
@@ -320,58 +326,51 @@ public class SignAndDeployFileMojo extends DeployFileMojo {
                 {
                     throw new MojoExecutionException( "Cannot obtain passphrase in batch mode" );
                 }
-                try 
+                try
                 {
                     pass = signer.getPassphrase( null );
                 }
-                catch (IOException e) 
+                catch ( IOException e )
                 {
                     throw new MojoExecutionException( "Exception reading password", e );
                 }
             }
         }
 
-        public void deploy( File source, Artifact artifact,
-                           ArtifactRepository deploymentRepository,
-                           ArtifactRepository localRepository ) 
-        throws ArtifactDeploymentException 
+        public void deploy( File source, Artifact artifact, ArtifactRepository deploymentRepository,
+                            ArtifactRepository localRepository )
+            throws ArtifactDeploymentException
         {
             try
             {
                 File fileSig = signer.generateSignatureForArtifact( source, pass );
                 ArtifactMetadata metadata = new AscArtifactMetadata( artifact, fileSig, false );
                 artifact.addMetadata( metadata );
-                
-                if ( !generatePom
-                    && pomFile != null )
+
+                if ( !generatePom && pomFile != null )
                 {
                     fileSig = signer.generateSignatureForArtifact( pomFile, pass );
                     metadata = new AscArtifactMetadata( artifact, fileSig, true );
                     artifact.addMetadata( metadata );
                 }
-                
+
                 deployer.deploy( source, artifact, deploymentRepository, localRepository );
             }
-            catch (MojoExecutionException e )
+            catch ( MojoExecutionException e )
             {
-                throw new ArtifactDeploymentException( e.getMessage(), e);
+                throw new ArtifactDeploymentException( e.getMessage(), e );
             }
-            
+
         }
 
-        public void deploy( String basedir, String finalName,
-                            Artifact artifact,
-                            ArtifactRepository deploymentRepository,
-                            ArtifactRepository localRepository ) 
-        throws ArtifactDeploymentException 
+        public void deploy( String basedir, String finalName, Artifact artifact,
+                            ArtifactRepository deploymentRepository, ArtifactRepository localRepository )
+            throws ArtifactDeploymentException
         {
             String extension = artifact.getArtifactHandler().getExtension();
             File source = new File( basedir, finalName + "." + extension );
             deploy( source, artifact, deploymentRepository, localRepository );
         }
     }
-    
-    
-    
 
 }
