e26c3a775dd8ae0bea0074a04ea7cfaf0087d0f6
Vincent Siveton <vsiveton@apache.org>
2009-09-10 12:28:40 +0000

    MJAVADOC-259: Resource copying from javadoc dir is incomplete
    o allow the copy of src/main/javadoc/resources to apidocs/resources
    o updated doc
    o added an IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@813404 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/it/MJAVADOC-259/src/main/java/com/mycompany/myapp/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.myapp;
+
+/**
+ * Hello world!
+ *
+ * <img src="doc-files/maven-feather.png" alt="Maven"/>
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
