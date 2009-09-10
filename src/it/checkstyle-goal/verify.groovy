af89751bb62d9eb640fbb259b1016f273a1cf0aa
Olivier Lamy <olamy@apache.org>
2009-09-10 19:48:26 +0000

    add an it for goal checkstyle:checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@813563 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/it/checkstyle-goal/verify.groovy
@@ -0,0 +1,40 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+assert new File(basedir, 'target/checkstyle-cachefile').exists();
+assert new File(basedir, 'target/checkstyle-checker.xml').exists();
+assert new File(basedir, 'target/checkstyle-header.txt').exists();
+assert new File(basedir, 'target/checkstyle-result.xml').exists();
+
+File rssFile = new File( basedir, 'target/site/checkstyle.rss' );
+assert rssFile.exists();
+
+def rss = new XmlParser().parse( rssFile );
+
+def channel = rss.channel[0]
+
+assert channel.title.text() == 'Unnamed - org.apache.maven.plugins.checkstyle:check-pass:jar:1.0-SNAPSHOT - Checkstyle report'
+
+def item = channel.item[0]
+assert item != null
+assert item.title.text().startsWith('File: 2,')
+
+
+return true;
