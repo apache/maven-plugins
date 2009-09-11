058e864db252ef19d454d77098d0687c75ab6545
John Dennis Casey <jdcasey@apache.org>
2009-09-11 17:01:22 +0000

    [MRRESOURCES-41] Adding IT for runOnlyAtExecutionRoot.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-remote-resources-plugin@813922 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/resources/remote/it/IT_GetDependencyProjects.java
@@ -0,0 +1,65 @@
+package org.apache.maven.plugin.resources.remote.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.plugin.resources.remote.it.support.TestUtils;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * @author Benjamin Bentmann
+ */
+public class IT_GetDependencyProjects
+    extends AbstractIT
+{
+
+    public void test()
+        throws IOException, URISyntaxException, VerificationException
+    {
+        File dir = TestUtils.getTestDir( "get-dependency-projects" );
+
+        Verifier verifier;
+
+        verifier = new Verifier( dir.getAbsolutePath() );
+        verifier.executeGoal( "deploy" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier = new Verifier( new File( dir, "project" ).getAbsolutePath() );
+
+        verifier.deleteArtifacts( "org.apache.maven.plugin.rresource.it.gdp" );
+
+        verifier.executeGoal( "generate-resources" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        File output = new File( dir, "project/target/maven-shared-archive-resources/DEPENDENCIES" );
+        String content = FileUtils.fileRead( output );
+
+        assertTrue( content.indexOf( "Dependency Id: org.apache.maven.plugin.rresource.it.gdp:release:1.0" ) >= 0 );
+        assertTrue( content.indexOf( "Dependency Id: org.apache.maven.plugin.rresource.it.gdp:snapshot:1.0-SNAPSHOT" ) >= 0 );
+    }
+
+}
