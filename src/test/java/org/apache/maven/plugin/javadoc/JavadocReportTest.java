06e9a8c46aca523661d37e726a1d726fd75e66f2
Vincent Siveton <vsiveton@apache.org>
2009-09-11 12:37:15 +0000

    o removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@813808 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/javadoc/JavadocReportTest.java
+++ src/test/java/org/apache/maven/plugin/javadoc/JavadocReportTest.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
