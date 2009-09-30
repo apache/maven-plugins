9f12dabe6d1787c1d72a2561f925d9423c494a77
John Dennis Casey <jdcasey@apache.org>
2009-09-30 03:15:34 +0000

    [MREPOSITORY-19] Adding more ITs.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-repository-plugin@820164 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugins/repository/it/BundlePackIT.java
+++ src/test/java/org/apache/maven/plugins/repository/it/BundlePackIT.java
@@ -1,12 +1,14 @@
 package org.apache.maven.plugins.repository.it;
 
+import static org.apache.maven.plugins.repository.it.support.IntegrationTestUtils.bootstrap;
+import static org.apache.maven.plugins.repository.it.support.IntegrationTestUtils.getCliPluginPrefix;
 import static org.apache.maven.plugins.repository.it.support.IntegrationTestUtils.getTestDir;
 import static org.apache.maven.plugins.repository.testutil.Assertions.assertZipContents;
 
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
-import org.apache.maven.plugins.repository.it.support.IntegrationTestUtils;
 import org.apache.maven.plugins.repository.testutil.Assertions;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -18,15 +20,27 @@ import java.util.Set;
 
 public class BundlePackIT
 {
+    
+    @BeforeClass
+    public static void doBootstrap()
+        throws VerificationException, IOException, URISyntaxException
+    {
+        bootstrap();
+    }
 
     @SuppressWarnings( "unchecked" )
     @Test
-    public void run()
+    public void packWithSCMInfoProvided()
         throws IOException, URISyntaxException, VerificationException
     {
         File dir = getTestDir( "bundle-pack" );
         
-        File bundleSource = new File( dir, "junit-4.5-bundle.jar" );
+        String prefix = getCliPluginPrefix();
+        String artifactId = "bundle-pack-target";
+        String groupId = "org.apache.maven.its.repository";
+        String version = "1.0";
+
+        File bundleSource = new File( dir, artifactId + "-" + version + "-bundle.jar" );
 
         if ( bundleSource.exists() )
         {
@@ -38,19 +52,63 @@ public class BundlePackIT
         verifier.setAutoclean( false );
 
         List<String> cliOptions = verifier.getCliOptions();
-        cliOptions.add( "-DgroupId=junit" );
-        cliOptions.add( "-DartifactId=junit" );
-        cliOptions.add( "-Dversion=4.5" );
-        cliOptions.add( "-DscmUrl=http://foo" );
-        cliOptions.add( "-DscmConnection=scm:svn:http://foo" );
-
-        String prefix = IntegrationTestUtils.getCliPluginPrefix();
+        cliOptions.add( "-DgroupId=" + groupId );
+        cliOptions.add( "-DartifactId=" + artifactId );
+        cliOptions.add( "-Dversion=" + version );
 
         verifier.executeGoal( prefix + "bundle-pack" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
 
         Set<String> requiredEntries = new HashSet<String>();
         requiredEntries.add( "pom.xml" );
-        requiredEntries.add( "junit-4.5.jar" );
+        requiredEntries.add( artifactId + "-" + version + ".jar" );
+        
+        if ( !verifier.isMavenDebug() )
+        {
+            bundleSource.deleteOnExit();
+        }
+
+        assertZipContents( requiredEntries, Assertions.EMPTY_ENTRY_NAMES, bundleSource );
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @Test
+    public void packWithSCMInfoMissing()
+        throws IOException, URISyntaxException, VerificationException
+    {
+        File dir = getTestDir( "bundle-pack" );
+        
+        String prefix = getCliPluginPrefix();
+        String artifactId = "bundle-pack-target-no-scm";
+        String groupId = "org.apache.maven.its.repository";
+        String version = "1.0";
+
+        File bundleSource = new File( dir, artifactId + "-" + version + "-bundle.jar" );
+
+        if ( bundleSource.exists() )
+        {
+            bundleSource.delete();
+        }
+        
+        Verifier verifier = new Verifier( dir.getAbsolutePath() );
+        
+        verifier.setAutoclean( false );
+
+        List<String> cliOptions = verifier.getCliOptions();
+        cliOptions.add( "-DgroupId=" + groupId );
+        cliOptions.add( "-DartifactId=" + artifactId );
+        cliOptions.add( "-Dversion=" + version );
+        cliOptions.add( "-DscmUrl=http://foo/" );
+        cliOptions.add( "-DscmConnection=scm:svn:http://foo/" );
+
+        verifier.executeGoal( prefix + "bundle-pack" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Set<String> requiredEntries = new HashSet<String>();
+        requiredEntries.add( "pom.xml" );
+        requiredEntries.add( artifactId + "-" + version + ".jar" );
         
         if ( !verifier.isMavenDebug() )
         {
