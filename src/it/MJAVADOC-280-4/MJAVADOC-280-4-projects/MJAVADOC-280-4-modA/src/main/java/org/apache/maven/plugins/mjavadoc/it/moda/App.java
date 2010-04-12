6f1f2a37521b0c47e6e7e4b1ce53ce7a379125ed
John Dennis Casey <jdcasey@apache.org>
2010-04-12 19:44:10 +0000

    [MJAVADOC-280] Fixing IT from previous work, adding support for javadoc-resource bundles that allow non-source javadoc configs and resources to be aggregated via dependencies, and added tests for this new support.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@933380 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/it/MJAVADOC-280-4/MJAVADOC-280-4-projects/MJAVADOC-280-4-modA/src/main/java/org/apache/maven/plugins/mjavadoc/it/moda/App.java
@@ -0,0 +1,18 @@
+package org.apache.maven.plugins.mjavadoc.it.moda;
+
+/**
+ * Hello world!
+ *
+ * @.foo    A 1st custom javadoc tag.
+ * @author  Me
+ * @version 1st
+ * @.bar    A 2nd custom javadoc tag.
+ * @see     String#startsWith(String)
+ */
+public class App
+{
+    public static void main( final String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/apache/maven/plugin/javadoc/TestResourcesBundleMojo.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.maven.plugin.javadoc;
+
+import java.io.File;
+
+/**
+ * Bundle {@link TestJavadocJar#testJavadocDirectory}, along with javadoc configuration options from 
+ * {@link AbstractJavadocMojo} such as taglet, doclet, and link information into a deployable 
+ * artifact. This artifact can then be consumed by the javadoc plugin mojos when used by the 
+ * <code>includeDependencySources</code> option, to generate javadocs that are somewhat consistent 
+ * with those generated in the original project itself.
+ *  
+ * @goal test-resource-bundle
+ * @phase package
+ * @since 2.6.2
+ */
+public class TestResourcesBundleMojo
+    extends ResourcesBundleMojo
+{
+    
+    /**
+     * Specifies the Test Javadoc resources directory to be included in the Javadoc (i.e. package.html, images...).
+     *
+     * @parameter default-value="${basedir}/src/test/javadoc" alias="javadocDirectory"
+     */
+    private File testJavadocDirectory;
+
+    @Override
+    protected String getAttachmentClassifier()
+    {
+        return TEST_JAVADOC_RESOURCES_ATTACHMENT_CLASSIFIER;
+    }
+
+    @Override
+    protected File getJavadocDirectory()
+    {
+        return testJavadocDirectory;
+    }
+    
+}
