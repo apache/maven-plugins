87543ebd63bc07021880a1e69b53f8c153869e93
Herve Boutemy <hboutemy@apache.org>
2010-06-19 21:26:11 +0000

    added a log message to show the skin used to render the site
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/branches/maven-site-plugin-3.x@956299 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/site/AbstractSiteRenderingMojo.java
+++ src/main/java/org/apache/maven/plugins/site/AbstractSiteRenderingMojo.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
@@ -290,8 +291,11 @@ public abstract class AbstractSiteRenderingMojo
         File skinFile;
         try
         {
-            skinFile = siteTool.getSkinArtifactFromRepository( localRepository, repositories, decorationModel )
-                .getFile();
+            Artifact skinArtifact =
+                siteTool.getSkinArtifactFromRepository( localRepository, repositories, decorationModel );
+            getLog().info( "Rendering site with " + skinArtifact.getId() + " skin." );
+
+            skinFile = skinArtifact.getFile();
         }
         catch ( SiteToolException e )
         {
