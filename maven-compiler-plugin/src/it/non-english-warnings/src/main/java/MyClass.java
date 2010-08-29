897435fd462ba29b57c87dd2b5928d34c94a7ca0
Benjamin Bentmann <bentmann@apache.org>
2010-08-29 19:16:21 +0000

    [MCOMPILER-109] JavacCompiler treat localized warning messages as errors
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@990633 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-compiler-plugin/src/it/non-english-warnings/src/main/java/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass
+{
+
+    public static void main( String[] args )
+    {
+        // the date constructor is deprecated and will cause a warning
+        System.out.println( new java.util.Date( 2010, 8, 29 ) );
+    }
+
+}
---
+++ maven-compiler-plugin/src/it/non-english-warnings/src/test/java/MyTest.java
@@ -0,0 +1,10 @@
+public class MyTest
+{
+
+    public static void main( String[] args )
+    {
+        // the date constructor is deprecated and will cause a warning
+        System.out.println( new java.util.Date( 2010, 8, 29 ) );
+    }
+
+}
