befae392c9ae6e244bbfd8bf2d1b497ff28e0e51
John Dennis Casey <jdcasey@apache.org>
2010-09-21 20:25:09 +0000

    [MPDF-41] with maven 3 pdf failure if reporting section is not empty ignore NoSuchMethodError to prevent failure for maven 3 users.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@999592 13f79535-47bb-0310-9956-ffa450edef68

--- maven-pdf-plugin/src/main/java/org/apache/maven/plugins/pdf/PdfMojo.java
+++ maven-pdf-plugin/src/main/java/org/apache/maven/plugins/pdf/PdfMojo.java
@@ -1134,8 +1134,7 @@ public class PdfMojo
         }
         catch ( NoSuchMethodError e )
         {
-            getLog().info( "Ignoring api call removed in maven 3, no reports are generated!" );
-            getLog().debug( e );
+            getLog().info( "ignoring not anymore existing api call in maven 3" );
             return null;
         }
     }
