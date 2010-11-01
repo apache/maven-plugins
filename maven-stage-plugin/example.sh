b4a99f883a8194c336a765f9301de7f4cd442feb
Jason van Zyl <jvanzyl@apache.org>
2010-11-01 13:12:53 +0000

    o retiring maven-stage-plugin
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1029639 13f79535-47bb-0310-9956-ffa450edef68

--- maven-stage-plugin/example.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mvn stage:copy -Dsource="http://people.apache.org/~snicoll/maven-staging/repo" -Dtarget="scp://people.apache.org/www/people.apache.org/repo/m2-ibiblio-rsync-repository" -Dversion=2.0.3
