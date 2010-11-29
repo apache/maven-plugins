275e3e469eabbe6191acb999ff7a49a9ccda69a3
Vincent Siveton <vsiveton@apache.org>
2010-11-29 11:36:59 +0000

    MPIR-180: Repositories which require authentication get blacklisted
    o refactor getInputStream to try to get auth from settings.xml
    o take care of SSL repo
    o added test case
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1040071 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/ProjectInfoReportUtils.java
+++ maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/ProjectInfoReportUtils.java
@@ -25,9 +25,21 @@ import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.commons.validator.UrlValidator;
 import org.apache.maven.artifact.Artifact;
@@ -37,8 +49,11 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
+import org.apache.maven.reporting.AbstractMavenReportRenderer;
 import org.apache.maven.settings.Proxy;
+import org.apache.maven.settings.Server;
 import org.apache.maven.settings.Settings;
+import org.codehaus.plexus.util.Base64;
 import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.util.StringUtils;
 
@@ -83,50 +98,82 @@ public class ProjectInfoReportUtils
     public static String getInputStream( URL url, Settings settings, String encoding )
         throws IOException
     {
+        return getInputStream( url, null, settings, encoding );
+    }
+
+    /**
+     * Get the input stream from an URL.
+     *
+     * @param url not null
+     * @param project could be null
+     * @param settings not null to handle proxy settings
+     * @param encoding the wanted encoding for the inputstream. If empty, encoding will be "ISO-8859-1".
+     * @return the inputstream found depending the wanted encoding.
+     * @throws IOException if any
+     * @since 2.3
+     */
+    public static String getInputStream( URL url, MavenProject project, Settings settings, String encoding )
+        throws IOException
+    {
         String scheme = url.getProtocol();
-        if ( !"file".equals( scheme ) )
+
+        if ( StringUtils.isEmpty( encoding ) )
         {
-            Proxy proxy = settings.getActiveProxy();
-            if ( proxy != null )
+            encoding = "ISO-8859-1";
+        }
+
+        if ( "file".equals( scheme ) )
+        {
+            InputStream in = null;
+            try
             {
-                if ( "http".equals( scheme ) || "https".equals( scheme ) )
+                URLConnection conn = url.openConnection();
+                in = conn.getInputStream();
+
+                return IOUtil.toString( in, encoding );
+            }
+            finally
+            {
+                IOUtil.close( in );
+            }
+        }
+
+        Proxy proxy = settings.getActiveProxy();
+        if ( proxy != null )
+        {
+            if ( "http".equals( scheme ) || "https".equals( scheme ) || "ftp".equals( scheme ) )
+            {
+                scheme += ".";
+            }
+            else
+            {
+                scheme = "";
+            }
+
+            String host = proxy.getHost();
+            if ( !StringUtils.isEmpty( host ) )
+            {
+                Properties p = System.getProperties();
+                p.setProperty( scheme + "proxySet", "true" );
+                p.setProperty( scheme + "proxyHost", host );
+                p.setProperty( scheme + "proxyPort", String.valueOf( proxy.getPort() ) );
+                if ( !StringUtils.isEmpty( proxy.getNonProxyHosts() ) )
                 {
-                    scheme = "http.";
-                }
-                else if ( "ftp".equals( scheme ) )
-                {
-                    scheme = "ftp.";
-                }
-                else
-                {
-                    scheme = "";
+                    p.setProperty( scheme + "nonProxyHosts", proxy.getNonProxyHosts() );
                 }
 
-                String host = proxy.getHost();
-                if ( !StringUtils.isEmpty( host ) )
+                final String userName = proxy.getUsername();
+                if ( !StringUtils.isEmpty( userName ) )
                 {
-                    Properties p = System.getProperties();
-                    p.setProperty( scheme + "proxySet", "true" );
-                    p.setProperty( scheme + "proxyHost", host );
-                    p.setProperty( scheme + "proxyPort", String.valueOf( proxy.getPort() ) );
-                    if ( !StringUtils.isEmpty( proxy.getNonProxyHosts() ) )
+                    final String pwd = StringUtils.isEmpty( proxy.getPassword() ) ? "" : proxy.getPassword();
+                    Authenticator.setDefault( new Authenticator()
                     {
-                        p.setProperty( scheme + "nonProxyHosts", proxy.getNonProxyHosts() );
-                    }
-
-                    final String userName = proxy.getUsername();
-                    if ( !StringUtils.isEmpty( userName ) )
-                    {
-                        final String pwd = StringUtils.isEmpty( proxy.getPassword() ) ? "" : proxy.getPassword();
-                        Authenticator.setDefault( new Authenticator()
+                        /** {@inheritDoc} */
+                        protected PasswordAuthentication getPasswordAuthentication()
                         {
-                            /** {@inheritDoc} */
-                            protected PasswordAuthentication getPasswordAuthentication()
-                            {
-                                return new PasswordAuthentication( userName, pwd.toCharArray() );
-                            }
-                        } );
-                    }
+                            return new PasswordAuthentication( userName, pwd.toCharArray() );
+                        }
+                    } );
                 }
             }
         }
@@ -134,15 +181,9 @@ public class ProjectInfoReportUtils
         InputStream in = null;
         try
         {
-            URLConnection conn = url.openConnection();
-            conn.setConnectTimeout( TIMEOUT );
-            conn.setReadTimeout( TIMEOUT );
+            URLConnection conn = getURLConnection( url, project, settings );
             in = conn.getInputStream();
 
-            if ( encoding == null )
-            {
-                return IOUtil.toString( in, "ISO-8859-1" );
-            }
             return IOUtil.toString( in, encoding );
         }
         finally
@@ -171,8 +212,9 @@ public class ProjectInfoReportUtils
         Artifact copyArtifact = ArtifactUtils.copyArtifact( artifact );
         if ( !"pom".equals( copyArtifact.getType() ) )
         {
-            copyArtifact = factory.createProjectArtifact( copyArtifact.getGroupId(), copyArtifact.getArtifactId(),
-                                                             copyArtifact.getVersion(), copyArtifact.getScope() );
+            copyArtifact =
+                factory.createProjectArtifact( copyArtifact.getGroupId(), copyArtifact.getArtifactId(),
+                                               copyArtifact.getVersion(), copyArtifact.getScope() );
         }
         try
         {
@@ -223,5 +265,100 @@ public class ProjectInfoReportUtils
 
         return URL_VALIDATOR.isValid( url );
     }
-}
 
+    /**
+     * @param url not null
+     * @param project not null
+     * @param settings not null
+     * @return the url connection with auth if required. Don't check the certificate if SSL scheme.
+     * @throws IOException if any
+     */
+    private static URLConnection getURLConnection( URL url, MavenProject project, Settings settings )
+        throws IOException
+    {
+        URLConnection conn = url.openConnection();
+        conn.setConnectTimeout( TIMEOUT );
+        conn.setReadTimeout( TIMEOUT );
+
+        // conn authorization
+        if ( settings.getServers() != null
+            && !settings.getServers().isEmpty()
+            && project != null
+            && project.getDistributionManagement() != null
+            && ( project.getDistributionManagement().getRepository() != null || project.getDistributionManagement().getSnapshotRepository() != null )
+            && ( StringUtils.isNotEmpty( project.getDistributionManagement().getRepository().getUrl() ) || StringUtils.isNotEmpty( project.getDistributionManagement().getSnapshotRepository().getUrl() ) ) )
+        {
+            Server server = null;
+            if ( url.toString().contains( project.getDistributionManagement().getRepository().getUrl() ) )
+            {
+                server = settings.getServer( project.getDistributionManagement().getRepository().getId() );
+            }
+            if ( server == null
+                && url.toString().contains( project.getDistributionManagement().getSnapshotRepository().getUrl() ) )
+            {
+                server = settings.getServer( project.getDistributionManagement().getRepository().getId() );
+            }
+
+            if ( server != null && StringUtils.isNotEmpty( server.getUsername() )
+                && StringUtils.isNotEmpty( server.getPassword() ) )
+            {
+                String up = server.getUsername().trim() + ":" + server.getPassword().trim();
+                String upEncoded = new String( Base64.encodeBase64Chunked( up.getBytes() ) ).trim();
+
+                conn.setRequestProperty( "Authorization", "Basic " + upEncoded );
+            }
+        }
+
+        if ( HttpsURLConnection.class.isAssignableFrom( conn.getClass() ) )
+        {
+            HostnameVerifier hostnameverifier = new HostnameVerifier()
+            {
+                /** {@inheritDoc} */
+                public boolean verify( String urlHostName, SSLSession session )
+                {
+                    return true;
+                }
+            };
+            ( (HttpsURLConnection) conn ).setHostnameVerifier( hostnameverifier );
+
+            TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager()
+            {
+                /** {@inheritDoc} */
+                public void checkClientTrusted( final X509Certificate[] chain, final String authType )
+                {
+                }
+
+                /** {@inheritDoc} */
+                public void checkServerTrusted( final X509Certificate[] chain, final String authType )
+                {
+                }
+
+                /** {@inheritDoc} */
+                public X509Certificate[] getAcceptedIssuers()
+                {
+                    return null;
+                }
+            } };
+
+            try
+            {
+                SSLContext sslContext = SSLContext.getInstance( "SSL" );
+                sslContext.init( null, trustAllCerts, new SecureRandom() );
+
+                SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+                ( (HttpsURLConnection) conn ).setSSLSocketFactory( sslSocketFactory );
+            }
+            catch ( NoSuchAlgorithmException e1 )
+            {
+                // ignore
+            }
+            catch ( KeyManagementException e )
+            {
+                // ignore
+            }
+        }
+
+        return conn;
+    }
+}
