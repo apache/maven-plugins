9454d2ff45b82eaf1f04f6b1b96122dd0fcbd95b
Dennis Lundberg <dennisl@apache.org>
2010-12-16 23:22:01 +0000

    [MCHANGES-140] Create a changes check mojo Submitted by: Justin Edelson Reviewed by: Dennis Lundberg
    I have modified the original patch to use a date format instead of a special token to recognize an invalid release date. A test case and documentation was also added.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1050224 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-changes-plugin/src/main/java/org/apache/maven/plugin/changes/ChangesCheckMojo.java
@@ -0,0 +1,121 @@
+package org.apache.maven.plugin.changes;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.changes.model.Release;
+
+/**
+ * Goal which checks that the changes.xml file has the necessary data to
+ * generate an announcement or a report for the current release.
+ * 
+ * @goal changes-check
+ * @author Justin Edelson
+ * @author Dennis Lundberg
+ * @since 2.4
+ */
+public class ChangesCheckMojo extends AbstractMojo
+{
+    /**
+     * The format that a correct release date should have. This value will be
+     * used as a pattern to try to create a date.
+     *
+     * @parameter expression="${changes.releaseDateFormat}" default-value="yyyy-MM-dd"
+     */
+    private String releaseDateFormat;
+
+    /**
+     * Version of the artifact.
+     *
+     * @parameter expression="${changes.version}" default-value="${project.version}"
+     * @required
+     */
+    private String version;
+
+    /**
+     * The path of the <code>changes.xml</code> file that will be checked.
+     *
+     * @parameter expression="${changes.xmlPath}" default-value="src/changes/changes.xml"
+     */
+    private File xmlPath;
+
+    private ReleaseUtils releaseUtils = new ReleaseUtils( getLog() );
+
+    /**
+     * Check that the latest release contains a valid release date.
+     *
+     * @throws MojoExecutionException
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+        if ( xmlPath.exists() )
+        {
+            ChangesXML xml = new ChangesXML( xmlPath, getLog() );
+            Release release = releaseUtils.getLatestRelease( xml.getReleaseList(), version );
+            if ( !isValidDate( release.getDateRelease(), releaseDateFormat ) )
+            {
+                throw new MojoExecutionException(
+                    "The file " + xmlPath.getAbsolutePath() + " has an invalid release date." );
+            }
+        }
+        else
+        {
+            getLog().warn( "The file " + xmlPath.getAbsolutePath() + " does not exist." );
+        }
+    }
+
+    /**
+     * Use the pattern to try to parse a Date from the given string.
+     *
+     * @param string A date as text
+     * @param pattern A pattern that can be used by {@link SimpleDateFormat}
+     * @return <code>true</code> if the string can be parsed as a date using the pattern, otherwise <code>false</code>
+     */
+    protected static boolean isValidDate( String string, String pattern )
+    {
+        if ( StringUtils.isEmpty( string ) )
+        {
+            return false;
+        }
+
+        if ( StringUtils.isEmpty( pattern ) )
+        {
+            return false;
+        }
+
+        try
+        {
+            SimpleDateFormat df = new SimpleDateFormat( pattern );
+            df.parse( string );
+            return true;
+        }
+        catch ( ParseException e )
+        {
+            return false;
+        }
+    }
+}
---
+++ maven-changes-plugin/src/test/java/org/apache/maven/plugin/changes/ChangesCheckMojoTestCase.java
@@ -0,0 +1,58 @@
+package org.apache.maven.plugin.changes;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * @author Dennis Lundberg
+ * @version $Id$
+ * @since 2.4
+ */
+public class ChangesCheckMojoTestCase
+    extends TestCase
+{
+    public void testIsValidDate() throws Exception
+    {
+        String pattern;
+
+        // null pattern
+        pattern = null;
+        assertFalse(ChangesCheckMojo.isValidDate( null, pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "", pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "2010-12-16", pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "pending", pattern ));
+
+        // empty pattern
+        pattern = "";
+        assertFalse(ChangesCheckMojo.isValidDate( null, pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "", pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "2010-12-16", pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "pending", pattern ));
+
+        // valid pattern
+        pattern = "yyyy-MM-dd";
+        assertFalse(ChangesCheckMojo.isValidDate( null, pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "", pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "2010-DD-MM", pattern ));
+        assertTrue(ChangesCheckMojo.isValidDate( "2010-12-16", pattern ));
+        assertFalse(ChangesCheckMojo.isValidDate( "pending", pattern ));
+    }
+}
