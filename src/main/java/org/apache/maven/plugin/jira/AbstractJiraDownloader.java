23cc3ca331eb7957168dcf6c1e91fdfce2e633a9
Herve Boutemy <hboutemy@apache.org>
2011-01-02 09:57:58 +0000

    [MCHANGES-144] get and store Jira response body in its direct byte form to avoid encoding issues (both when reading the content and when writing it to a file using platform encoding) and as a stream to avoid storing the full result in memory
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-changes-plugin@1054373 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/jira/AbstractJiraDownloader.java
+++ src/main/java/org/apache/maven/plugin/jira/AbstractJiraDownloader.java
@@ -36,12 +36,14 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Proxy;
 import org.apache.maven.settings.Settings;
 import org.apache.maven.wagon.proxy.ProxyInfo;
+import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Locale;
@@ -681,19 +683,25 @@ public abstract class AbstractJiraDownloader
 
             if ( gm.getStatusCode() == HttpStatus.SC_OK )
             {
-                final String strGetResponseBody = gm.getResponseBodyAsString();
+                final InputStream responseBodyStream = gm.getResponseBodyAsStream();
 
                 if ( !output.getParentFile().exists() )
                 {
                     output.getParentFile().mkdirs();
                 }
 
-                // write the reponse to file
-                PrintWriter pw = new PrintWriter( new FileWriter( output ) );
-
-                pw.print( strGetResponseBody );
-
-                pw.close();
+                // write the response to file
+                OutputStream out = null;
+                try
+                {
+                    out = new FileOutputStream( output );
+                    IOUtil.copy( responseBodyStream, out );
+                }
+                finally
+                {
+                    IOUtil.close( out );
+                    IOUtil.close( responseBodyStream );
+                }
 
                 getLog().debug( "Downloading from JIRA was successful" );
             }
