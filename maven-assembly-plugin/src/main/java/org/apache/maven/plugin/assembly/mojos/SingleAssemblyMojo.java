02d228b46b0830df8871865eea9f7bfffc131d37
Kristian Rosenvold <krosenvold@apache.org>
2011-02-07 19:59:52 +0000

    [MASSEMBLY-522] @threadSafe
    Based on manual code inspection and version check of dependencies
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1068087 13f79535-47bb-0310-9956-ffa450edef68

--- maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/mojos/SingleAssemblyMojo.java
+++ maven-assembly-plugin/src/main/java/org/apache/maven/plugin/assembly/mojos/SingleAssemblyMojo.java
@@ -31,6 +31,7 @@ import org.apache.maven.project.MavenProject;
  * @version $Id$
  * @goal single
  * @inheritByDefault false
+ * @threadSafe
  */
 public class SingleAssemblyMojo
     extends AbstractAssemblyMojo
