f74355cfb89d8cae58e4471de6bd3b460c613115
Herve Boutemy <hboutemy@apache.org>
2011-02-12 16:16:19 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-site-plugin@1070104 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/site/SiteRunMojo.java
+++ src/main/java/org/apache/maven/plugins/site/SiteRunMojo.java
@@ -25,7 +25,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
