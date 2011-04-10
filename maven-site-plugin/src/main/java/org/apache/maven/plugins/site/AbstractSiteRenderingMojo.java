c58dce043fb166bb63afd95926724f9a510f569c
Herve Boutemy <hboutemy@apache.org>
2011-04-10 15:04:30 +0000

    updated Doxia dependency
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1090814 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/main/java/org/apache/maven/plugins/site/AbstractSiteRenderingMojo.java
+++ maven-site-plugin/src/main/java/org/apache/maven/plugins/site/AbstractSiteRenderingMojo.java
@@ -438,10 +438,8 @@ public abstract class AbstractSiteRenderingMojo
     protected void populateReportItems( DecorationModel decorationModel, Locale locale,
                                         Map<String, MavenReport> reportsByOutputName )
     {
-        for ( Iterator<Menu> i = decorationModel.getMenus().iterator(); i.hasNext(); )
+        for ( Menu menu : decorationModel.getMenus() )
         {
-            Menu menu = i.next();
-
             populateItemRefs( menu.getItems(), locale, reportsByOutputName );
         }
     }
