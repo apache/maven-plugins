003c4c4f1e8dafdb2bcf1f58c91de2354ceccb1a
Kristian Rosenvold <krosenvold@apache.org>
2011-06-07 16:38:54 +0000

    [MCOMPILER-120] -Werror options support
    Invoker based IT added to test plexus-compiler feature. Based on test project by Martin Ackermann
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1133088 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-compiler-plugin/src/it/mcompiler-120/src/main/java/org/maven/test/Main.java
@@ -0,0 +1,15 @@
+package org.maven.test;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        List blah = new ArrayList();
+        blah.add("hello");
+    }
+}
