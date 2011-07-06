47605fef04b9e51d733f0e3ab3df9775bc4b4c75
Stephen Connolly <stephenc@apache.org>
2011-07-06 12:39:44 +0000

    Upgrade to plexus-archiver 2.0
    o Requires careful dancing in what dependencies you upgrade in order to ensure that the tests continue to pass.
    
    o Seems m-common-artifact-filters 1.3 causes a lot of breakage
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-dependency-plugin@1143382 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/codehaus/plexus/util/interpolation/Interpolator.java
@@ -0,0 +1,15 @@
+package org.codehaus.plexus.util.interpolation;
+
+/**
+ * COPIED FROM plexus-utils-1.5.15 TO SATISFY TESTS
+ *
+ * Interpolator interface. Based on existing RegexBasedInterpolator interface.
+ *
+ * @author cstamas
+ * @deprecated Use plexus-interpolation APIs instead.
+ * @version $Id: Interpolator.java 12174 2010-05-16 21:04:35Z rfscholte $
+ */
+public interface Interpolator
+    extends org.codehaus.plexus.interpolation.Interpolator
+{
+}
