2d962afacc279e689aa5e8236b89025e50338a46
Benson Margulies <bimargulies@apache.org>
2012-02-25 19:02:40 +0000

    Make the lifecycle idea work.
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/plugins/asf-svnpubsub-plugin@1293664 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugins/svnpubsub/SvnpubsubLifecycleMojo.java
@@ -0,0 +1,41 @@
+package org.apache.maven.plugins.svnpubsub;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * 
+ * @goal svnpubsub
+ * @execute lifecycle="svnpubsub" phase="svnpubsub-publish"
+ * @aggregate
+ */
+public class SvnpubsubLifecycleMojo extends CommonSvnpubsubMojo
+{
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        // nothing to do. This only purpose here is to set loose the lifecycle.
+        
+    }
+
+}
