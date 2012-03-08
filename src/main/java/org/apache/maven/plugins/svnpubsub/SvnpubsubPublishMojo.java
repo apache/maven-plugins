f5a21acdceb4569b0b6387b406a43e4881f9cf15
Herve Boutemy <hboutemy@apache.org>
2012-03-08 22:55:43 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/plugins/asf-svnpubsub-plugin@1298640 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/svnpubsub/SvnpubsubPublishMojo.java
+++ src/main/java/org/apache/maven/plugins/svnpubsub/SvnpubsubPublishMojo.java
@@ -29,7 +29,6 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
