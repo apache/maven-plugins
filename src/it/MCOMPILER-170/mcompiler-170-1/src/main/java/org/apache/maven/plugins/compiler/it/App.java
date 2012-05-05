ca8d41a73b77e58aaa3c9da7ab35e26651d286cc
Olivier Lamy <olamy@apache.org>
2012-05-05 22:16:38 +0000

    [MCOMPILER-170] Regression: Compiler Plugin fails when building with multiple threads (-T...). use new plexus-compiler which have various javacc reuse strategy.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-compiler-plugin@1334519 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/it/MCOMPILER-170/mcompiler-170-1/src/main/java/org/apache/maven/plugins/compiler/it/App.java
@@ -0,0 +1,32 @@
+package org.apache.maven.plugins.compiler.it;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void mainFoo( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void bar( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void foo( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void barfoo( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
