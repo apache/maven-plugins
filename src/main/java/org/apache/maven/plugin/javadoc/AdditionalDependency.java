0f6d781f985b4e5c99262aae7b38294ea5445890
Olivier Lamy <olamy@apache.org>
2012-07-30 21:35:48 +0000

    [MJAVADOC-334] Patch to make <additionalDependencies> easier and fix typo on field name Submitted by Ben Speakmon.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-javadoc-plugin@1367320 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/plugin/javadoc/AdditionalDependency.java
@@ -0,0 +1,10 @@
+package org.apache.maven.plugin.javadoc;
+
+import org.apache.maven.model.Dependency;
+
+/**
+ * Provides syntactic sugar for using the standard {@code Dependency}
+ * model as a parameter to {@link AbstractJavadocMojo}'s {@code additionalDependencies}.
+ */
+public class AdditionalDependency extends Dependency {
+}
