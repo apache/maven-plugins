3c6695667d061fb7756913d53110642ba0cbd9eb
Olivier Lamy <olamy@apache.org>
2012-08-02 18:21:29 +0000

    code formatting
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-rar-plugin@1368635 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/rar/stubs/RarMavenProjectStub.java
+++ src/test/java/org/apache/maven/plugin/rar/stubs/RarMavenProjectStub.java
@@ -19,19 +19,19 @@ package org.apache.maven.plugin.rar.stubs;
  * under the License.
  */
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Organization;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.PlexusTestCase;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author <a href="mailto:aramirez@apache.org">Allan Ramirez</a>
@@ -40,11 +40,11 @@ public class RarMavenProjectStub
     extends MavenProject
 {
     private List attachedArtifacts;
-    
+
     public RarMavenProjectStub()
     {
         super( new Model() );
-        
+
         super.setGroupId( getGroupId() );
         super.setArtifactId( getArtifactId() );
         super.setVersion( getVersion() );
@@ -53,7 +53,7 @@ public class RarMavenProjectStub
         Organization org = new Organization();
         org.setName( "organization" );
         org.setUrl( "http://www.some.org" );
-        
+
         super.setOrganization( org );
         super.setFile( getFile() );
         super.setPluginArtifacts( Collections.EMPTY_SET );
@@ -64,61 +64,61 @@ public class RarMavenProjectStub
         super.setPluginArtifactRepositories( Collections.EMPTY_LIST );
         super.setCollectedProjects( Collections.EMPTY_LIST );
         super.setActiveProfiles( Collections.EMPTY_LIST );
-        
+
         super.addCompileSourceRoot( getBasedir() + "/src/test/resources/unit/basic-rar-test/src/main/java" );
         super.addTestCompileSourceRoot( getBasedir() + "/src/test/resources/unit/basic-rar-test/src/test/java" );
-        
+
         super.setExecutionRoot( false );
     }
-    
+
     public String getGroupId()
     {
         return "org.apache.maven.test";
     }
-    
+
     public String getArtifactId()
     {
         return "maven-rar-test";
     }
-    
+
     public String getVersion()
     {
         return "1.0-SNAPSHOT";
     }
-    
+
     public File getFile()
     {
         return new File( getBasedir(), "src/test/resources/unit/basic-rar-test/plugin-config.xml" );
     }
-    
+
     public File getBasedir()
     {
         return new File( PlexusTestCase.getBasedir() );
     }
-    
+
     public Artifact getArtifact()
     {
         Artifact artifact = new RarArtifactStub();
-        
+
         artifact.setGroupId( getGroupId() );
-        
+
         artifact.setArtifactId( getArtifactId() );
-        
+
         artifact.setVersion( getVersion() );
-        
+
         return artifact;
     }
-    
+
     public Set getArtifacts()
-    {   
+    {
         Set artifacts = new HashSet();
- 
+
         artifacts.add( createArtifact( "org.apache.maven.test", "maven-artifact01", "1.0-SNAPSHOT", false ) );
         artifacts.add( createArtifact( "org.apache.maven.test", "maven-artifact02", "1.0-SNAPSHOT", false ) );
-        
+
         return artifacts;
     }
-    
+
     public List getAttachedArtifacts()
     {
         if ( attachedArtifacts == null )
@@ -126,29 +126,30 @@ public class RarMavenProjectStub
             attachedArtifacts = new ArrayList();
         }
         return attachedArtifacts;
-    }    
-    
+    }
+
     protected Artifact createArtifact( String groupId, String artifactId, String version, boolean optional )
     {
         Artifact artifact = new RarArtifactStub();
-        
+
         artifact.setGroupId( groupId );
-        
+
         artifact.setArtifactId( artifactId );
-        
+
         artifact.setVersion( version );
-        
+
         artifact.setOptional( optional );
-        
+
         artifact.setVersionRange( VersionRange.createFromVersion( "1.0" ) );
-        
-        artifact.setFile( new File ( getBasedir() + "/src/test/remote-repo/" + artifact.getGroupId().replace( '.', '/' ) + 
-                          "/" + artifact.getArtifactId() + "/" + artifact.getVersion() + 
-                          "/" + artifact.getArtifactId() + "-" + artifact.getVersion() + ".jar" ) ) ;
-        
-        artifact.setArtifactHandler(new ArtifactHandlerStub());
+
+        artifact.setFile(
+            new File( getBasedir() + "/src/test/remote-repo/" + artifact.getGroupId().replace( '.', '/' ) +
+                          "/" + artifact.getArtifactId() + "/" + artifact.getVersion() +
+                          "/" + artifact.getArtifactId() + "-" + artifact.getVersion() + ".jar" ) );
+
+        artifact.setArtifactHandler( new ArtifactHandlerStub() );
         return artifact;
     }
-    
-    
+
+
 }
