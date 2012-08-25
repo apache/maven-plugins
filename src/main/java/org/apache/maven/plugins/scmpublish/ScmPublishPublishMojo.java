73725409a049b29d15c138ab906a6c58e9d47de3
Olivier Lamy <olamy@apache.org>
2012-08-25 17:10:47 +0000

    add some debug for deleted files
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/plugins/maven-scm-publish-plugin@1377328 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/scmpublish/ScmPublishPublishMojo.java
+++ src/main/java/org/apache/maven/plugins/scmpublish/ScmPublishPublishMojo.java
@@ -272,6 +272,7 @@ public class ScmPublishPublishMojo
         ScmFileSet deletedFileSet = new ScmFileSet( checkoutDirectory, deletedList );
         try
         {
+            getLog().debug( "deleting files: " + deletedList );
             RemoveScmResult deleteResult =
                 scmProvider.remove( scmRepository, deletedFileSet, "Deleting obsolete site files." );
             if ( !deleteResult.isSuccess() )
