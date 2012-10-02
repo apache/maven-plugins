6582be6a096146bd3b7a5e39bff25844eca211b8
Robert Scholte <rfscholte@apache.org>
2012-10-02 18:25:46 +0000

    [MCOMPILER-182] Case sensitivity changes in class names not detected by compiler
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1393086 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-compiler-plugin/src/it/mcompiler-182/src/main/java/BeanA.java
@@ -0,0 +1,15 @@
+/**
+ * dumb test bean
+ */
+public class BeanA {
+
+    private int i,y;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
