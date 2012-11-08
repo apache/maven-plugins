3a842cb249e719e185dc823f3e2604a37626eb82
Anders Hammar <andham@apache.org>
2012-11-08 20:08:41 +0000

    [MCOMPILER-179] Added IT
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-compiler-plugin@1407250 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/it/mcompiler-179/src/main/java/org/maven/test/MyClass.java
@@ -0,0 +1,16 @@
+package org.maven.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyClass {
+
+    public void foo() {
+        List blah = new ArrayList();
+        blah.add("hello");
+    }
+
+    public void bar() {
+        int a = "error";
+    }
+}
