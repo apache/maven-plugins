c6a20de2cd5c74e5e32d312724c512ea867ca2cd
Olivier Lamy <olamy@apache.org>
2013-02-08 15:06:56 +0000

    [MJAR-162] skipIfEmpty not work for test-jar goal and empty directories
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-jar-plugin@1444045 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/jar/AbstractJarMojo.java
+++ src/main/java/org/apache/maven/plugin/jar/AbstractJarMojo.java
@@ -242,7 +242,7 @@ public abstract class AbstractJarMojo
     public void execute()
         throws MojoExecutionException
     {
-        if ( skipIfEmpty && !getClassesDirectory().exists() )
+        if ( skipIfEmpty && (!getClassesDirectory().exists() || getClassesDirectory().list().length < 1 ) )
         {
             getLog().info( "Skipping packaging of the test-jar" );
         }
