ffa3163e7a54c71accfba9d169763e7cb2b83819
Herve Boutemy <hboutemy@apache.org>
2013-05-11 09:31:14 +0000

    improved javadoc (then generated goal documentation)
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-dependency-plugin@1481277 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugin/dependency/TreeMojo.java
+++ src/main/java/org/apache/maven/plugin/dependency/TreeMojo.java
@@ -115,10 +115,10 @@ public class TreeMojo
 
     /**
      * If specified, this parameter will cause the dependency tree to be written using the specified format. Currently
-     * supported format are text, dot, graphml and tgf.
+     * supported format are: <code>text</code>, <code>dot</code>, <code>graphml</code> and <code>tgf</code>.
      * <p/>
      * These formats can be plotted to image files. An example of how to plot a dot file using
-     * pygraphviz can be found <a href="http://networkx.lanl.gov/pygraphviz/tutorial.html#layout-and-drawing">here</a>
+     * pygraphviz can be found <a href="http://networkx.lanl.gov/pygraphviz/tutorial.html#layout-and-drawing">here</a>.
      *
      * @since 2.2
      */
@@ -145,8 +145,8 @@ public class TreeMojo
 
     /**
      * The token set name to use when outputting the dependency tree. Possible values are <code>whitespace</code>,
-     * <code>standard</code> or <code>extended</code>, which use whitespace, standard or extended ASCII sets
-     * respectively.
+     * <code>standard</code> or <code>extended</code>, which use whitespace, standard (ie ASCII) or extended
+     * character sets respectively.
      *
      * @since 2.0-alpha-6
      */
@@ -155,8 +155,17 @@ public class TreeMojo
 
     /**
      * A comma-separated list of artifacts to filter the serialized dependency tree by, or <code>null</code> not to
-     * filter the dependency tree. The artifact syntax is defined by <code>StrictPatternIncludesArtifactFilter</code>.
-     *
+     * filter the dependency tree. The filter syntax is:
+     * 
+     * <pre>
+     * [groupId]:[artifactId]:[type]:[version]
+     * </pre>
+     * 
+     * where each pattern segment is optional and supports full and partial <code>*</code> wildcards. An empty pattern
+     * segment is treated as an implicit wildcard.
+     * <p>For example, <code>org.apache.*</code> will match all artifacts whose group id starts with
+     * <code>org.apache.</code>, and <code>:::*-SNAPSHOT</code> will match all snapshot artifacts.</p>
+     * 
      * @see StrictPatternIncludesArtifactFilter
      * @since 2.0-alpha-6
      */
@@ -165,8 +174,16 @@ public class TreeMojo
 
     /**
      * A comma-separated list of artifacts to filter from the serialized dependency tree, or <code>null</code> not to
-     * filter any artifacts from the dependency tree. The artifact syntax is defined by
-     * <code>StrictPatternExcludesArtifactFilter</code>.
+     * filter any artifacts from the dependency tree. The filter syntax is:
+     * 
+     * <pre>
+     * [groupId]:[artifactId]:[type]:[version]
+     * </pre>
+     * 
+     * where each pattern segment is optional and supports full and partial <code>*</code> wildcards. An empty pattern
+     * segment is treated as an implicit wildcard.
+     * <p>For example, <code>org.apache.*</code> will match all artifacts whose group id starts with
+     * <code>org.apache.</code>, and <code>:::*-SNAPSHOT</code> will match all snapshot artifacts.</p>
      *
      * @see StrictPatternExcludesArtifactFilter
      * @since 2.0-alpha-6
