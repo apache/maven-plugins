ddb3ea39c1c434872b84f63457127524aff165df
Kristian Rosenvold <krosenvold@apache.org>
2013-07-31 05:37:12 +0000

    And another file, svn is hard
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-install-plugin@1508731 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/plugin/install/Utils.java
@@ -0,0 +1,66 @@
+package org.apache.maven.plugin.install;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.codehaus.plexus.util.FileUtils;
+
+/**
+ * A utility class to assist testing.
+ *
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+@SuppressWarnings( "UnusedDeclaration" )
+public class Utils
+{
+
+    /**
+     * Verifies a checksum file in the local repo.
+     * 
+     * @param checksumFile The checksum file to verify, must not be <code>null</code>.
+     * @throws Exception If the verification failed.
+     */
+    public static void verifyChecksum( File checksumFile )
+        throws Exception
+    {
+        File dataFile;
+        SimpleDigester digester;
+        if ( checksumFile.getName().endsWith( ".md5" ) )
+        {
+            digester = SimpleDigester.md5();
+            dataFile = new File( checksumFile.getPath().substring( 0, checksumFile.getPath().length() - 4 ) );
+        }
+        else if ( checksumFile.getName().endsWith( ".sha1" ) )
+        {
+            digester = SimpleDigester.sha1();
+            dataFile = new File( checksumFile.getPath().substring( 0, checksumFile.getPath().length() - 5 ) );
+        }
+        else
+        {
+            throw new IllegalArgumentException( "Unsupported checksum file: " + checksumFile );
+        }
+
+        String expected = FileUtils.fileRead( checksumFile, "UTF-8" );
+        digester.verify( dataFile, expected );
+    }
+
+}
