d9dd0bc942e3fb38836fa118d40eb07ffbf13ebd
Olivier Lamy <olamy@apache.org>
2013-08-05 12:45:57 +0000

    add it with error prone compiler
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1510458 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-compiler-plugin/src/it/error-prone-compiler/src/main/java/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass
+{
+
+    public static void main(String[] args) {
+        if (args.length < 1);
+        throw new IllegalArgumentException("Missing required argument");
+    }
+
+}
