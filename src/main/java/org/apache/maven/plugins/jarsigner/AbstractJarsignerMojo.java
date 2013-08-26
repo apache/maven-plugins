8e48e45c540b3b38749b5daf522dc21eca6a13f3
Kristian Rosenvold <krosenvold@apache.org>
2013-08-26 15:49:32 +0000

    Further code analysis improvements
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-jarsigner-plugin@1517569 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/jarsigner/AbstractJarsignerMojo.java
+++ src/main/java/org/apache/maven/plugins/jarsigner/AbstractJarsignerMojo.java
@@ -40,7 +40,6 @@ import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 
