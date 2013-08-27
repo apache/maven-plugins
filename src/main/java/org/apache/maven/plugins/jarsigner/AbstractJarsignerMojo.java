c2922225da46fb2caf07b16e55be2e776ffcfac8
Kristian Rosenvold <krosenvold@apache.org>
2013-08-27 14:16:42 +0000

    Reformatted to code style
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-jarsigner-plugin@1517827 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/jarsigner/AbstractJarsignerMojo.java
+++ src/main/java/org/apache/maven/plugins/jarsigner/AbstractJarsignerMojo.java
@@ -206,18 +206,21 @@ public abstract class AbstractJarsignerMojo
                         excludes.addAll( Arrays.asList( excludeClassifiers ) );
                     }
 
-                    for (Object o : this.project.getAttachedArtifacts()) {
+                    for ( Object o : this.project.getAttachedArtifacts() )
+                    {
                         final Artifact artifact = (Artifact) o;
 
-                        if (!includes.isEmpty() && !includes.contains(artifact.getClassifier())) {
+                        if ( !includes.isEmpty() && !includes.contains( artifact.getClassifier() ) )
+                        {
                             continue;
                         }
 
-                        if (excludes.contains(artifact.getClassifier())) {
+                        if ( excludes.contains( artifact.getClassifier() ) )
+                        {
                             continue;
                         }
 
-                        processed += processArtifact(artifact) ? 1 : 0;
+                        processed += processArtifact( artifact ) ? 1 : 0;
                     }
                 }
                 else
@@ -248,16 +251,17 @@ public abstract class AbstractJarsignerMojo
                             "Failed to scan archive directory for JARs: " + e.getMessage(), e );
                     }
 
-                    for (Object jarFile1 : jarFiles) {
+                    for ( Object jarFile1 : jarFiles )
+                    {
                         File jarFile = (File) jarFile1;
 
-                        processArchive(jarFile);
+                        processArchive( jarFile );
                         processed++;
                     }
                 }
             }
 
-            getLog().info( getMessage( "processed", processed) );
+            getLog().info( getMessage( "processed", processed ) );
         }
         else
         {
@@ -398,7 +402,7 @@ public abstract class AbstractJarsignerMojo
             if ( resultCode != 0 )
             {
                 throw new MojoExecutionException(
-                    getMessage( "failure", getCommandlineInfo( commandLine ), resultCode) );
+                    getMessage( "failure", getCommandlineInfo( commandLine ), resultCode ) );
             }
 
         }
