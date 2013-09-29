6cf268079869dab1820bdf1b4c6b8c94172c2572
Dennis Lundberg <dennisl@apache.org>
2013-09-29 12:12:53 +0000

    [MSITE-665] Add back "requiresReports = true" that was mistakenly removed when applying the patch for this issue. The patch was fine, but something must have gone wrong when applying it.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1527313 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteJarMojo.java
+++ maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteJarMojo.java
@@ -46,7 +46,7 @@ import org.codehaus.plexus.archiver.jar.ManifestException;
  * @since 2.0-beta-6
  */
 // MSITE-665: requiresDependencyResolution workaround for MPLUGIN-253 
-@Mojo( name = "jar", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.TEST )
+@Mojo( name = "jar", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.TEST, requiresReports = true )
 public class SiteJarMojo
     extends SiteMojo
 {
