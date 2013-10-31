e5cea9ccf8104d8864184f22f77eaee125ab8abe
Anders Hammar <andham@apache.org>
2013-10-31 19:54:54 +0000

    [MINVOKER-122] Import information into groovy scripts of the running Maven environment
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1537603 13f79535-47bb-0310-9956-ffa450edef68

--- maven-invoker-plugin/src/main/java/org/apache/maven/plugin/invoker/AbstractInvokerMojo.java
+++ maven-invoker-plugin/src/main/java/org/apache/maven/plugin/invoker/AbstractInvokerMojo.java
@@ -1177,6 +1177,7 @@ public abstract class AbstractInvokerMojo
         {
             actualMavenVersion = SelectorUtils.getMavenVersion();
         }
+        scriptRunner.setGlobalVariable( "mavenVersion", actualMavenVersion );
         
         if ( javaHome != null )
         {
