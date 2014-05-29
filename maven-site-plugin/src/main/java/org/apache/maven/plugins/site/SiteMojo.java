2f37e9c2f0edcf56360d3a155201bb93e666506c
Herve Boutemy <hboutemy@apache.org>
2014-05-29 22:08:11 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1598428 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteMojo.java
+++ maven-site-plugin/src/main/java/org/apache/maven/plugins/site/SiteMojo.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
