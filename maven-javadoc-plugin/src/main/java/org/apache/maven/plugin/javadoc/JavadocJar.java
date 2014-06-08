121ef8080523adc730c77bfaaa8996764de63cb2
Herve Boutemy <hboutemy@apache.org>
2014-06-08 08:53:10 +0000

    [MJAVADOC-386] fixed documentation links to oracle web site Submitted by: Eric Barboni
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1601202 13f79535-47bb-0310-9956-ffa450edef68

--- maven-javadoc-plugin/src/main/java/org/apache/maven/plugin/javadoc/JavadocJar.java
+++ maven-javadoc-plugin/src/main/java/org/apache/maven/plugin/javadoc/JavadocJar.java
@@ -95,7 +95,7 @@ public class JavadocJar
 
     /**
      * Specifies the destination directory where javadoc saves the generated HTML files.
-     * See <a href="http://docs.oracle.com/javase/1.4.2/docs/tooldocs/windows/javadoc.html#d">d</a>.
+     * See <a href="http://docs.oracle.com/javase/7/docs/technotes/tools/windows/javadoc.html#d">d</a>.
      *
      * @deprecated
      */
