0c9d48747d187fcdc340b2d8883f9ab0012181d6
Herve Boutemy <hboutemy@apache.org>
2014-06-09 08:57:36 +0000

    simplified execution
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1601326 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/it/MSITE-627/src/main/java/org/apache/maven/plugins/it/MyReport.java
+++ maven-site-plugin/src/it/MSITE-627/src/main/java/org/apache/maven/plugins/it/MyReport.java
@@ -1,19 +1,22 @@
 package org.apache.maven.plugins.it;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import org.apache.maven.doxia.sink.Sink;
@@ -27,48 +30,60 @@ import java.util.Locale;
 
 /**
  * Goal which creates a sink in a report.
- *
+ * 
  * @goal test
  * @phase site
  */
-public class MyReport extends AbstractMavenReport {
+public class MyReport
+    extends AbstractMavenReport
+{
 
-    public String getOutputName() {
+    public String getOutputName()
+    {
         return "MSITE-627";
     }
 
-    public String getName(Locale locale) {
+    public String getName( Locale locale )
+    {
         return "MSITE-627";
     }
 
-    public String getDescription(Locale locale) {
+    public String getDescription( Locale locale )
+    {
         return "Test Report for MSITE-672";
     }
 
     @Override
-    protected Renderer getSiteRenderer() {
+    protected Renderer getSiteRenderer()
+    {
         return null;
     }
 
     @Override
-    protected String getOutputDirectory() {
+    protected String getOutputDirectory()
+    {
         return null;
     }
 
     @Override
-    protected MavenProject getProject() {
+    protected MavenProject getProject()
+    {
         return null;
     }
 
     @Override
-    protected void executeReport(Locale locale) throws MavenReportException {
+    protected void executeReport( Locale locale )
+        throws MavenReportException
+    {
         final Sink s = getSink();
-        final Sink sa = new SinkAdapter() {
+        final Sink sa = new SinkAdapter()
+        {
             @Override
-            public void text(String text) {                
-                s.text(text.replace("OK", "passed"));
-            }            
+            public void text( String text )
+            {
+                s.text( text.replace( "OK", "passed" ) );
+            }
         };
-        sa.text("Test OK");
+        sa.text( "Test OK" );
     }
 }
