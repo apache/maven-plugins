ff199045b27ffe4633e2b0908e9595e03b4d04d8
Herve Boutemy <hboutemy@apache.org>
2014-08-18 14:00:33 +0000

    [MCHECKSTYLE-241] added SuppressWarningsFilter (introduced in Checkstyle 5.7) to support @SuppressWarnings
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-checkstyle-plugin@1618611 13f79535-47bb-0310-9956-ffa450edef68

--- src/it/check-pass/src/main/java/org/MyClass.java
+++ src/it/check-pass/src/main/java/org/MyClass.java
@@ -23,6 +23,9 @@ package org;
  * Yada yada yada.
  */
 public class MyClass {
+    @SuppressWarnings("checkstyle:constantname")
+    private static final int testSuppressionFilter = 0;
+
     public static void main(String[] args) {
         // no op
     }
