694c61bc627476cc6ce00b80991116d40afac107
Karl Heinz Marbaise <khmarbaise@apache.org>
2014-09-06 18:58:29 +0000

    - Added rat report script.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1622907 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rat-report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FOLDERS=`find . -maxdepth 1 -type d -not -path "./.svn" -and -not -path "."`
+RAT_REPORT="mvn org.apache.rat:apache-rat-plugin:0.11:check -Drat.ignoreErrors=true -Drat.excludeSubProjects=false"
+for i in $FOLDERS; do
+  echo "Checking $i"
+  cd $i
+  $RAT_REPORT >../$i.log
+  cd ..
+done;
+# Printout the reports only first 19 lines which contain the usefull information.
+head -n19 `find . -maxdepth 3 -type f -name "rat.txt"`
+# Removing report logs.
+for i in $FOLDERS; do
+  rm $i.log
+done;
