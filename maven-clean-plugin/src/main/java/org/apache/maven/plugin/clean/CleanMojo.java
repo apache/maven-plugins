eb04bb63306a1baf4e5c0283d1d3be5e317fa6b5
Karl Heinz Marbaise <khmarbaise@apache.org>
2014-10-26 19:18:12 +0000

    [maven-release-plugin] copy for tag maven-clean-plugin-2.6.1
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/tags/maven-clean-plugin-2.6.1@1634384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-clean-plugin/src/main/java/org/apache/maven/plugin/clean/CleanMojo.java
@@ -0,0 +1,235 @@
+package org.apache.maven.plugin.clean;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Goal which cleans the build.
+ * <p/>
+ * <P>This attempts to clean a project's working directory of the files that
+ * were generated at build-time. By default, it discovers and deletes the
+ * directories configured in <code>project.build.directory</code>,
+ * <code>project.build.outputDirectory</code>,
+ * <code>project.build.testOutputDirectory</code>, and
+ * <code>project.reporting.outputDirectory</code>. </P>
+ * <p/>
+ * <P>Files outside the default may also be included in the deletion by
+ * configuring the <code>filesets</code> tag.</P>
+ *
+ * @author <a href="mailto:evenisse@maven.org">Emmanuel Venisse</a>
+ * @version $Id$
+ * @see org.apache.maven.plugin.clean.Fileset
+ * @since 2.0
+ */
+@Mojo( name = "clean", threadSafe = true )
+public class CleanMojo
+    extends AbstractMojo
+{
+
+    /**
+     * This is where build results go.
+     */
+    @Parameter( defaultValue = "${project.build.directory}", readonly = true, required = true )
+    private File directory;
+
+    /**
+     * This is where compiled classes go.
+     */
+    @Parameter( defaultValue = "${project.build.outputDirectory}", readonly = true, required = true )
+    private File outputDirectory;
+
+    /**
+     * This is where compiled test classes go.
+     */
+    @Parameter( defaultValue = "${project.build.testOutputDirectory}", readonly = true, required = true )
+    private File testOutputDirectory;
+
+    /**
+     * This is where the site plugin generates its pages.
+     *
+     * @since 2.1.1
+     */
+    @Parameter( defaultValue = "${project.build.outputDirectory}", readonly = true, required = true )
+    private File reportDirectory;
+
+    /**
+     * Sets whether the plugin runs in verbose mode. As of plugin version 2.3, the default value is derived from Maven's
+     * global debug flag (compare command line switch <code>-X</code>).
+     *
+     * @since 2.1
+     */
+    @Parameter( property = "clean.verbose" )
+    private Boolean verbose;
+
+    /**
+     * The list of file sets to delete, in addition to the default directories. For example:
+     * <pre>
+     * &lt;filesets&gt;
+     *   &lt;fileset&gt;
+     *     &lt;directory&gt;src/main/generated&lt;/directory&gt;
+     *     &lt;followSymlinks&gt;false&lt;/followSymlinks&gt;
+     *     &lt;useDefaultExcludes&gt;true&lt;/useDefaultExcludes&gt;
+     *     &lt;includes&gt;
+     *       &lt;include&gt;*.java&lt;/include&gt;
+     *     &lt;/includes&gt;
+     *     &lt;excludes&gt;
+     *       &lt;exclude&gt;Template*&lt;/exclude&gt;
+     *     &lt;/excludes&gt;
+     *   &lt;/fileset&gt;
+     * &lt;/filesets&gt;
+     * </pre>
+     *
+     * @since 2.1
+     */
+    @Parameter
+    private Fileset[] filesets;
+
+    /**
+     * Sets whether the plugin should follow symbolic links while deleting files from the default output directories of
+     * the project. Not following symlinks requires more IO operations and heap memory, regardless whether symlinks are
+     * actually present. So projects with a huge output directory that knowingly does not contain symlinks can improve
+     * performance by setting this parameter to <code>true</code>.
+     *
+     * @since 2.1
+     */
+    @Parameter( property = "clean.followSymLinks", defaultValue = "false" )
+    private boolean followSymLinks;
+
+    /**
+     * Disables the plugin execution.
+     *
+     * @since 2.2
+     */
+    @Parameter( property = "clean.skip", defaultValue = "false" )
+    private boolean skip;
+
+    /**
+     * Indicates whether the build will continue even if there are clean errors.
+     *
+     * @since 2.2
+     */
+    @Parameter( property = "maven.clean.failOnError", defaultValue = "true" )
+    private boolean failOnError;
+
+    /**
+     * Indicates whether the plugin should undertake additional attempts (after a short delay) to delete a file if the
+     * first attempt failed. This is meant to help deleting files that are temporarily locked by third-party tools like
+     * virus scanners or search indexing.
+     *
+     * @since 2.4.2
+     */
+    @Parameter( property = "maven.clean.retryOnError", defaultValue = "true" )
+    private boolean retryOnError;
+
+    /**
+     * Disables the deletion of the default output directories configured for a project. If set to <code>true</code>,
+     * only the files/directories selected via the parameter {@link #filesets} will be deleted.
+     *
+     * @since 2.3
+     */
+    @Parameter( property = "clean.excludeDefaultDirectories", defaultValue = "false" )
+    private boolean excludeDefaultDirectories;
+
+    /**
+     * Deletes file-sets in the following project build directory order: (source) directory, output directory, test
+     * directory, report directory, and then the additional file-sets.
+     *
+     * @throws MojoExecutionException When a directory failed to get deleted.
+     * @see org.apache.maven.plugin.Mojo#execute()
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+        if ( skip )
+        {
+            getLog().info( "Clean is skipped." );
+            return;
+        }
+
+        Cleaner cleaner = new Cleaner( getLog(), isVerbose() );
+
+        try
+        {
+            for ( File directory : getDirectories() )
+            {
+                if ( directory != null )
+                {
+                    cleaner.delete( directory, null, followSymLinks, failOnError, retryOnError );
+                }
+            }
+
+            if ( filesets != null )
+            {
+                for ( Fileset fileset : filesets )
+                {
+                    if ( fileset.getDirectory() == null )
+                    {
+                        throw new MojoExecutionException( "Missing base directory for " + fileset );
+                    }
+                    GlobSelector selector = new GlobSelector( fileset.getIncludes(), fileset.getExcludes(),
+                                                              fileset.isUseDefaultExcludes() );
+                    cleaner.delete( fileset.getDirectory(), selector, fileset.isFollowSymlinks(), failOnError,
+                                    retryOnError );
+                }
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Failed to clean project: " + e.getMessage(), e );
+        }
+    }
+
+    /**
+     * Indicates whether verbose output is enabled.
+     *
+     * @return <code>true</code> if verbose output is enabled, <code>false</code> otherwise.
+     */
+    private boolean isVerbose()
+    {
+        return ( verbose != null ) ? verbose : getLog().isDebugEnabled();
+    }
+
+    /**
+     * Gets the directories to clean (if any). The returned array may contain null entries.
+     *
+     * @return The directories to clean or an empty array if none, never <code>null</code>.
+     */
+    private File[] getDirectories()
+    {
+        File[] directories;
+        if ( excludeDefaultDirectories )
+        {
+            directories = new File[0];
+        }
+        else
+        {
+            directories = new File[]{ directory, outputDirectory, testOutputDirectory, reportDirectory };
+        }
+        return directories;
+    }
+
+}
---
+++ maven-clean-plugin/src/test/java/org/apache/maven/plugin/clean/CleanMojoTest.java
@@ -0,0 +1,349 @@
+package org.apache.maven.plugin.clean;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.codehaus.plexus.util.FileUtils;
+
+/**
+ * Test the clean mojo.
+ *
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ */
+public class CleanMojoTest
+    extends AbstractMojoTestCase
+{
+    /** {@inheritDoc} */
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown()
+        throws Exception
+    {
+        super.tearDown();
+    }
+
+    /**
+     * Tests the simple removal of directories
+     *
+     * @throws Exception
+     */
+    public void testBasicClean()
+        throws Exception
+    {
+        String pluginPom = getBasedir() + "/src/test/resources/unit/basic-clean-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/basic-clean-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/basic-clean-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        mojo.execute();
+
+        assertFalse( "Directory exists", checkExists( getBasedir() + "/target/test-classes/unit/"
+            + "basic-clean-test/buildDirectory" ) );
+        assertFalse( "Directory exists", checkExists( getBasedir() + "/target/test-classes/unit/basic-clean-test/"
+            + "buildOutputDirectory" ) );
+        assertFalse( "Directory exists", checkExists( getBasedir() + "/target/test-classes/unit/basic-clean-test/"
+            + "buildTestDirectory" ) );
+    }
+
+    /**
+     * Tests the removal of files and nested directories
+     *
+     * @throws Exception
+     */
+    public void testCleanNestedStructure()
+        throws Exception
+    {
+        String pluginPom = getBasedir() + "/src/test/resources/unit/nested-clean-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/nested-clean-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/nested-clean-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        mojo.execute();
+
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/nested-clean-test/target" ) );
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/nested-clean-test/target/classes" ) );
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/nested-clean-test/target/test-classes" ) );
+    }
+
+    /**
+     * Tests that no exception is thrown when all internal variables are empty and that it doesn't
+     * just remove whats there
+     *
+     * @throws Exception
+     */
+    public void testCleanEmptyDirectories()
+        throws Exception
+    {
+        String pluginPom = getBasedir() + "/src/test/resources/unit/empty-clean-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/empty-clean-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/empty-clean-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupEmptyMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        mojo.execute();
+
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/empty-clean-test/testDirectoryStructure" ) );
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/empty-clean-test/"
+            + "testDirectoryStructure/file.txt" ) );
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/empty-clean-test/"
+            + "testDirectoryStructure/outputDirectory" ) );
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/empty-clean-test/"
+            + "testDirectoryStructure/outputDirectory/file.txt" ) );
+    }
+
+    /**
+     * Tests the removal of files using fileset
+     *
+     * @throws Exception
+     */
+    public void testFilesetsClean()
+        throws Exception
+    {
+        String pluginPom = getBasedir() + "/src/test/resources/unit/fileset-clean-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/fileset-clean-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/fileset-clean-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        mojo.execute();
+
+        // fileset 1
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target" ) );
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/classes" ) );
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/test-classes" ) );
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/subdir" ) );
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/classes/file.txt" ) );
+        assertTrue( checkEmpty( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/classes" ) );
+        assertFalse( checkEmpty( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/subdir" ) );
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/target/subdir/file.txt" ) );
+
+        // fileset 2
+        assertTrue( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/"
+            + "buildOutputDirectory" ) );
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/fileset-clean-test/"
+            + "buildOutputDirectory/file.txt" ) );
+    }
+
+    /**
+     * Tests the removal of a directory as file
+     *
+     * @throws Exception
+     */
+    public void testCleanInvalidDirectory()
+        throws Exception
+    {
+        String pluginPom = getBasedir() + "/src/test/resources/unit/invalid-directory-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/invalid-directory-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/invalid-directory-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        try
+        {
+            mojo.execute();
+
+            fail( "Should fail to delete a file treated as a directory" );
+        }
+        catch ( MojoExecutionException expected )
+        {
+            assertTrue( true );
+        }
+    }
+
+    /**
+     * Tests the removal of a missing directory
+     *
+     * @throws Exception
+     */
+    public void testMissingDirectory()
+        throws Exception
+    {
+        String pluginPom = getBasedir() + "/src/test/resources/unit/missing-directory-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/missing-directory-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/missing-directory-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        mojo.execute();
+
+        assertFalse( checkExists( getBasedir() + "/target/test-classes/unit/missing-directory-test/does-not-exist" ) );
+    }
+
+    /**
+     * Test the removal of a locked file on Windows systems.
+     * <br/>
+     * Note: Unix systems doesn't lock any files.
+     *
+     * @throws Exception
+     */
+    public void testCleanLockedFile()
+        throws Exception
+    {
+        if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+        {
+            assertTrue( "Ignored this test on none Windows based systems", true );
+            return;
+        }
+
+        String pluginPom = getBasedir() + "/src/test/resources/unit/locked-file-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/locked-file-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/locked-file-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        assertNotNull( mojo );
+
+        File f = new File( getBasedir(), "target/test-classes/unit/locked-file-test/buildDirectory/file.txt" );
+        FileChannel channel = null;
+        FileLock lock = null;
+        try
+        {
+            channel = new RandomAccessFile( f, "rw" ).getChannel();
+            lock = channel.lock();
+
+            mojo.execute();
+
+            fail( "Should fail to delete a file that is locked" );
+        }
+        catch ( MojoExecutionException expected )
+        {
+            assertTrue( true );
+        }
+        finally
+        {
+            if ( lock != null )
+            {
+                lock.release();
+            }
+
+            if ( channel != null )
+            {
+                channel.close();
+            }
+        }
+    }
+
+    /**
+     * Test the removal of a locked file on Windows systems.
+     * <br/>
+     * Note: Unix systems doesn't lock any files.
+     *
+     * @throws Exception
+     */
+    public void testCleanLockedFileWithNoError()
+        throws Exception
+    {
+        if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+        {
+            assertTrue( "Ignored this test on none Windows based systems", true );
+            return;
+        }
+
+        String pluginPom = getBasedir() + "/src/test/resources/unit/locked-file-test/plugin-pom.xml";
+
+        // safety
+        FileUtils.copyDirectory( new File( getBasedir(), "src/test/resources/unit/locked-file-test" ),
+                                 new File( getBasedir(), "target/test-classes/unit/locked-file-test" ), null, "**/.svn,**/.svn/**" );
+
+        CleanMojo mojo = (CleanMojo) lookupMojo( "clean", pluginPom );
+        setVariableValueToObject( mojo, "failOnError", Boolean.FALSE );
+        assertNotNull( mojo );
+
+        File f = new File( getBasedir(), "target/test-classes/unit/locked-file-test/buildDirectory/file.txt" );
+        FileChannel channel = null;
+        FileLock lock = null;
+        try
+        {
+            channel = new RandomAccessFile( f, "rw" ).getChannel();
+            lock = channel.lock();
+
+            mojo.execute();
+
+            assertTrue( true );
+        }
+        catch ( MojoExecutionException expected )
+        {
+            fail( "Should display a warning when deleting a file that is locked" );
+        }
+        finally
+        {
+            if ( lock != null )
+            {
+                lock.release();
+            }
+
+            if ( channel != null )
+            {
+                channel.close();
+            }
+        }
+    }
+
+    /**
+     * @param dir a dir or a file
+     * @return true if a file/dir exists, false otherwise
+     */
+    private boolean checkExists( String dir )
+    {
+        return FileUtils.fileExists( new File( dir ).getAbsolutePath() );
+    }
+
+    /**
+     * @param dir a directory
+     * @return true if a dir is empty, false otherwise
+     */
+    private boolean checkEmpty( String dir )
+    {
+        return new File( dir ).listFiles().length == 0;
+    }
+}
