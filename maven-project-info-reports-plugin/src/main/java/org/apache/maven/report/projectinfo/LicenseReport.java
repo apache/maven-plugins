9f423985fff5d9d018aaa15420e0fa421d37cbc2
Michael Osipov <michaelo@apache.org>
2014-12-01 08:23:01 +0000

    Do not use Locale.ROOT for now because it is available in Java 6 and upwards.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1642645 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/LicenseReport.java
+++ maven-project-info-reports-plugin/src/main/java/org/apache/maven/report/projectinfo/LicenseReport.java
@@ -351,7 +351,8 @@ public class LicenseReport
                 String licenseContent = ProjectInfoReportUtils.getContent( licenseUrl, settings, licenseFileEncoding );
 
                 // TODO: we should check for a text/html mime type instead, and possibly use a html parser to do this a bit more cleanly/reliably.
-                String licenseContentLC = licenseContent.toLowerCase( Locale.ROOT );
+                // TODO Use Locale.ROOT as soon as we migrate to Java 6
+                String licenseContentLC = licenseContent.toLowerCase( Locale.ENGLISH );
                 int bodyStart = licenseContentLC.indexOf( "<body" );
                 int bodyEnd = licenseContentLC.indexOf( "</body>" );
 
