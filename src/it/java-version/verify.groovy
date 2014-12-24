ca93bb4180debfaeec51015ef3e8f9fcd3f4b56f
Herve Boutemy <hboutemy@apache.org>
2014-12-24 15:08:58 +0000

    [MPIR-263] improved minimum java requirement when m-compiler-p not explicitely configured: use default properties
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-project-info-reports-plugin@1647803 13f79535-47bb-0310-9956-ffa450edef68

--- src/it/java-version/verify.groovy
+++ src/it/java-version/verify.groovy
@@ -19,6 +19,6 @@
 
 assert new File( basedir, 'plugin/target/site/project-summary.html' ).text.contains( '<td>1.5</td>' )
 assert new File( basedir, 'pluginManagement/target/site/project-summary.html' ).text.contains( '<td>1.4</td>' )
-//MPIR-263: assert new File( basedir, 'property/target/site/project-summary.html' ).text.contains( '<td>1.3</td>' )
+assert new File( basedir, 'property/target/site/project-summary.html' ).text.contains( '<td>1.3</td>' )
 
 return true;
