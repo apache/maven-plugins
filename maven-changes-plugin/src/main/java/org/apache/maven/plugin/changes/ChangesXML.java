4519fe24f7b89577a15364efa749a0d26594d981
Karl Heinz Marbaise <khmarbaise@apache.org>
2015-06-16 17:28:11 +0000

    [MCHANGES-354] The plugin should fail to execute if the changes.xml file cannot be parsed  Submitted by: Gabor Szabo  Patch applied without changes.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1685874 13f79535-47bb-0310-9956-ffa450edef68

--- maven-changes-plugin/src/main/java/org/apache/maven/plugin/changes/ChangesXML.java
+++ maven-changes-plugin/src/main/java/org/apache/maven/plugin/changes/ChangesXML.java
@@ -19,17 +19,17 @@ package org.apache.maven.plugin.changes;
  * under the License.
  */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Collections;
-import java.util.List;
-
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.changes.model.Body;
 import org.apache.maven.plugins.changes.model.ChangesDocument;
 import org.apache.maven.plugins.changes.model.Properties;
 import org.apache.maven.plugins.changes.model.io.xpp3.ChangesXpp3Reader;
 import org.codehaus.plexus.util.IOUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A facade for a changes.xml file.
@@ -39,17 +39,28 @@ import org.codehaus.plexus.util.IOUtil;
 public class ChangesXML
 {
 
+    /** The list of releases in the changes.xml file. */
     private List releaseList;
 
+    /** The author in the changes.xml file. */
     private String author;
 
+    /** The title of the changes.xml file. */
     private String title;
 
+    /** The e-mail address of the author in the changes.xml file. */
     private String authorEmail;
 
+    /** The changes.xml document. */
     private ChangesDocument changesDocument;
 
-    public ChangesXML( File xmlPath, Log log )
+    /**
+     * Constructor that sets the changes.xml file and the logger.
+     * @param xmlPath the changes.xml file
+     * @param log the logger
+     * @throws ChangesXMLRuntimeException if there was a fatal error while parsing the changes.xml file
+     */
+    public ChangesXML( File xmlPath, Log log ) throws ChangesXMLRuntimeException
     {
 
         if ( xmlPath == null || !xmlPath.exists() )
@@ -98,8 +109,8 @@ public class ChangesXML
         }
         catch ( Throwable e )
         {
-            // FIXME throw an Exception ?
             log.error( "An error occurred when parsing the changes.xml file: ", e );
+            throw new ChangesXMLRuntimeException( "An error occurred when parsing the changes.xml file", e );
         }
         finally
         {
@@ -107,46 +118,82 @@ public class ChangesXML
         }
     }
 
+    /**
+     * Sets the {@link ChangesXML#author} attribute.
+     * @param author the new value of the {@link ChangesXML#author} attribute
+     */
     public void setAuthor( String author )
     {
         this.author = author;
     }
 
+    /**
+     * Returns the current value of the author attribute.
+     * @return the current value of the author attribute
+     */
     public String getAuthor()
     {
         return author;
     }
 
+    /**
+     * Sets the {@link ChangesXML#releaseList} attribute.
+     * @param releaseList the new value of the {@link ChangesXML#releaseList} attribute
+     */
     public void setReleaseList( List releaseList )
     {
         this.releaseList = releaseList;
     }
 
+    /**
+     * Returns the current value of the {@link ChangesXML#releaseList} attribute.
+     * @return the current value of the {@link ChangesXML#releaseList} attribute
+     */
     public List getReleaseList()
     {
         return releaseList == null ? Collections.EMPTY_LIST : releaseList;
     }
 
+    /**
+     * Sets the {@link ChangesXML#title} attribute.
+     * @param title the new value of the {@link ChangesXML#title} attribute
+     */
     public void setTitle( String title )
     {
         this.title = title;
     }
 
+    /**
+     * Returns the current value of the {@link ChangesXML#title} attribute.
+     * @return the current value of the {@link ChangesXML#title} attribute
+     */
     public String getTitle()
     {
         return title;
     }
 
+    /**
+     Returns the current value of the {@link ChangesXML#changesDocument} attribute.
+     * @return the current value of the {@link ChangesXML#changesDocument} attribute
+     */
     public ChangesDocument getChangesDocument()
     {
         return changesDocument;
     }
 
+    /**
+     * Returns the current value of the {@link ChangesXML#authorEmail} attribute.
+     * @return the current value of the {@link ChangesXML#authorEmail} attribute
+     */
     public String getAuthorEmail()
     {
         return authorEmail;
     }
 
+    /**
+     * Sets the {@link ChangesXML#authorEmail} attribute.
+     * @param authorEmail the new value of the {@link ChangesXML#authorEmail} attribute
+     */
     public void setAuthorEmail( String authorEmail )
     {
         this.authorEmail = authorEmail;
