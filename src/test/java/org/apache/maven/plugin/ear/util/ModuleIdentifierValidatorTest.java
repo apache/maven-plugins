9ec49a40b55b081302bcf6a83b6eeb73594041ff
Karl Heinz Marbaise <khmarbaise@apache.org>
2015-06-20 14:56:54 +0000

    Added tests.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-ear-plugin@1686633 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/ear/util/ModuleIdentifierValidatorTest.java
+++ src/test/java/org/apache/maven/plugin/ear/util/ModuleIdentifierValidatorTest.java
@@ -59,6 +59,19 @@ public class ModuleIdentifierValidatorTest
         return earModule;
     }
 
+    private EarModule createMockEarModule( String groupId, String artifactId, String classifier, String version )
+    {
+        EarModule earModule = mock( EarModule.class );
+        Artifact artifact = mock( Artifact.class );
+        when( earModule.getArtifact() ).thenReturn( artifact );
+        when( earModule.getArtifact().getGroupId() ).thenReturn( groupId );
+        when( earModule.getArtifact().getArtifactId() ).thenReturn( artifactId );
+        when( earModule.getArtifact().getClassifier() ).thenReturn( classifier );
+        when( earModule.getArtifact().getVersion() ).thenReturn( version );
+        when( earModule.getArtifact().getId() ).thenReturn( groupId + ":" + artifactId + ":" + classifier + ":" + version );
+        return earModule;
+    }
+
     @Test
     public void existDuplicateShouldResultFalseWithEmptyList()
     {
@@ -99,10 +112,44 @@ public class ModuleIdentifierValidatorTest
     }
 
     @Test
+    public void shouldNotFailCauseTheClassifiersAreDifferent()
+    {
+        earModules.add( createMockEarModule( "org.apache", "artifact-1", "first", "1.0.0" ) );
+        earModules.add( createMockEarModule( "org.apache", "artifact-2", "second", "1.0.0" ) );
+        earModules.add( createMockEarModule( "org.apache.maven", "aid-1", "first", "1.0.0" ) );
+        earModules.add( createMockEarModule( "org.apache.maven", "aid-2", "second", "1.0.0" ) );
+        earModules.add( createMockEarModule( "org.apache.maven.test", "result-artifact-a", "first", "1.0.0" ) );
+        earModules.add( createMockEarModule( "org.apache.maven.test", "result-artifact-b", "second", "1.0.0" ) );
+
+        assertFalse( miv.checkForDuplicateArtifacts().existDuplicateArtifacts() );
+
+        Map<String, List<EarModule>> result = miv.getDuplicateArtifacts();
+
+        assertTrue( result.isEmpty() );
+    }
+
+    @Test
     public void shouldFailCauseTheArtifactIdsAreIdenticalWithDifferentGroupIds()
     {
         EarModule earModule1 = createMockEarModule( "org.apache.maven.test", "result-artifact", "1.0.0" );
         EarModule earModule2 = createMockEarModule( "org.apache.maven", "result-artifact", "1.0.0" );
+        earModules.add( earModule1 );
+        earModules.add( earModule2 );
+
+        miv.checkForDuplicateArtifacts();
+        Map<String, List<EarModule>> result = miv.getDuplicateArtifacts();
+
+        assertFalse( result.isEmpty() );
+        assertEquals( 1, result.size() );
+        assertTrue( result.containsKey( "result-artifact:1.0.0" ) );
+        assertEquals( 2, result.get( "result-artifact:1.0.0" ).size() );
+    }
+
+    @Test
+    public void shouldFailCauseTheArtifactIdsAreIdenticalWithClassifiers()
+    {
+        EarModule earModule1 = createMockEarModule( "org.apache.maven.test", "result-artifact", "first", "1.0.0" );
+        EarModule earModule2 = createMockEarModule( "org.apache.maven", "result-artifact", "second", "1.0.0" );
         earModules.add( earModule1 );
         earModules.add( earModule2 );
 
