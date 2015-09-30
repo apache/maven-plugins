9c318d0c12cba4b432265907c78b1ed99f2b2583
Michael Osipov <michaelo@apache.org>
2015-09-30 19:47:29 +0000

    [MCHECKSTYLE-307] Upgrade to Checkstyle 6.11
    Final upgrade with change to IT MCHECKSTYLE-268.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1706140 13f79535-47bb-0310-9956-ffa450edef68

--- maven-checkstyle-plugin/src/it/MCHECKSTYLE-268/verify.groovy
+++ maven-checkstyle-plugin/src/it/MCHECKSTYLE-268/verify.groovy
@@ -19,4 +19,4 @@
  */
 def buildLog = new File( basedir, 'build.log' )
 
-assert buildLog.text.contains( "You have 2 Checkstyle violations." )
+assert buildLog.text.contains( "You have 3 Checkstyle violations." )
