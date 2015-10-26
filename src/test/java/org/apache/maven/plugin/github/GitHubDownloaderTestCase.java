713718b4f180f4cd119174845deebccecaa8b873
Olivier Lamy <olamy@apache.org>
2015-10-26 09:52:21 +0000

    apply maven format
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-changes-plugin@1710542 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugin/github/GitHubDownloaderTestCase.java
+++ src/test/java/org/apache/maven/plugin/github/GitHubDownloaderTestCase.java
@@ -1,20 +1,14 @@
 /*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to Elasticsearch under one or more contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Elasticsearch licenses this file to you under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package org.apache.maven.plugin.github;
@@ -27,32 +21,35 @@ import org.eclipse.egit.github.core.User;
 
 import java.io.IOException;
 
-public class GitHubDownloaderTestCase extends TestCase {
-    public void testCreateIssue() throws IOException {
+public class GitHubDownloaderTestCase
+    extends TestCase
+{
+
+    public void testCreateIssue()
+        throws IOException
+    {
 
         MavenProject mavenProject = new MavenProject();
         IssueManagement issueManagement = new IssueManagement();
-        issueManagement.setSystem("GitHub");
-        issueManagement.setUrl("https://github.com/dadoonet/spring-elasticsearch/issues/");
-        mavenProject.setIssueManagement(issueManagement);
+        issueManagement.setSystem( "GitHub" );
+        issueManagement.setUrl( "https://github.com/dadoonet/spring-elasticsearch/issues/" );
+        mavenProject.setIssueManagement( issueManagement );
 
-        GitHubDownloader gitHubDownloader = new GitHubDownloader(
-                mavenProject, "https", 80, true, false
-        );
+        GitHubDownloader gitHubDownloader = new GitHubDownloader( mavenProject, "https", 80, true, false );
 
         org.eclipse.egit.github.core.Issue githubIssue = new org.eclipse.egit.github.core.Issue();
-        githubIssue.setNumber(1);
-        githubIssue.setBody("Body");
-        githubIssue.setTitle("Title");
+        githubIssue.setNumber( 1 );
+        githubIssue.setBody( "Body" );
+        githubIssue.setTitle( "Title" );
         User user = new User();
-        githubIssue.setUser(user);
+        githubIssue.setUser( user );
 
-        Issue issue = gitHubDownloader.createIssue(githubIssue);
+        Issue issue = gitHubDownloader.createIssue( githubIssue );
 
-        assertEquals(Integer.toString(githubIssue.getNumber()), issue.getId());
-        assertEquals(Integer.toString(githubIssue.getNumber()), issue.getKey());
-        assertEquals(githubIssue.getTitle(), issue.getTitle());
-        assertEquals(githubIssue.getBody(), issue.getSummary());
-        assertEquals(issueManagement.getUrl()+githubIssue.getNumber(), issue.getLink());
+        assertEquals( Integer.toString( githubIssue.getNumber() ), issue.getId() );
+        assertEquals( Integer.toString( githubIssue.getNumber() ), issue.getKey() );
+        assertEquals( githubIssue.getTitle(), issue.getTitle() );
+        assertEquals( githubIssue.getBody(), issue.getSummary() );
+        assertEquals( issueManagement.getUrl() + githubIssue.getNumber(), issue.getLink() );
     }
 }
