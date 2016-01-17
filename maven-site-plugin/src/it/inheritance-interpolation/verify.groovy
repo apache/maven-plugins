e4ecfa9daed7f433bd0cfc9f0f664395501d23dd
Herve Boutemy <hboutemy@apache.org>
2016-01-17 23:42:57 +0000

    [MSITE-135] interpolation is done after inheritance, with child values (through DOXIASITETOOLS-118)
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1725152 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site-plugin/src/it/inheritance-interpolation/verify.groovy
+++ maven-site-plugin/src/it/inheritance-interpolation/verify.groovy
@@ -36,12 +36,8 @@ assert content.contains( '<<<http://maven.apache.org/prop-value-from-child/relat
 // Site Decoration Model inheritance+interpolation
 childContent = new File( basedir, 'child/target/site/index.html' ).text;
 
-// see DOXIASITETOOLS-118: interpolation happens too early
-// assert childContent.contains( 'project.artifactId = child' );
-assert childContent.contains( 'project.artifactId = repo-parent' );
+assert childContent.contains( 'project.artifactId = child' );
 
 reactorContent = new File( basedir, 'reactor-parent/target/site/index.html' ).text;
 
-// see DOXIASITETOOLS-118: interpolation happens too early
-// assert reactorContent.contains( 'project.artifactId = reactor-parent' );
-assert reactorContent.contains( 'project.artifactId = repo-parent' );
+assert reactorContent.contains( 'project.artifactId = reactor-parent' );
