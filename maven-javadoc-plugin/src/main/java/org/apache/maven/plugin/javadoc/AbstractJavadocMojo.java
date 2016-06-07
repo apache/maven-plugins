45c9d7210c30419c94b86134685e5c2c0ff0506d
Robert Scholte <rfscholte@apache.org>
2016-06-07 17:28:14 +0000

    [MJAVADOC-355] Encoding problem if platform encoding used to run Maven is different from default platform encoding. revert r1720609 and r1720589
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1747276 13f79535-47bb-0310-9956-ffa450edef68

--- maven-javadoc-plugin/src/main/java/org/apache/maven/plugin/javadoc/AbstractJavadocMojo.java
+++ maven-javadoc-plugin/src/main/java/org/apache/maven/plugin/javadoc/AbstractJavadocMojo.java
@@ -1984,16 +1984,12 @@ public abstract class AbstractJavadocMojo
         // Wrap Javadoc JVM args
         // ----------------------------------------------------------------------
 
-        boolean fileEncodingSeen = false;
-
         addMemoryArg( cmd, "-Xmx", this.maxmemory );
         addMemoryArg( cmd, "-Xms", this.minmemory );
         addProxyArg( cmd );
 
         if ( StringUtils.isNotEmpty( additionalJOption ) )
         {
-            fileEncodingSeen = this.additionalJOption.contains( "-J-Dfile.encoding=" );
-
             cmd.createArg().setValue( additionalJOption );
         }
 
@@ -2001,17 +1997,8 @@ public abstract class AbstractJavadocMojo
         {
             for ( String jo : additionalJOptions )
             {
-                if ( !fileEncodingSeen )
-                {
-                    fileEncodingSeen = jo.contains( "-J-Dfile.encoding=" );
-                }
                 cmd.createArg().setValue( jo );
             }
-        }
-
-        if ( !fileEncodingSeen )
-        {
-            cmd.createArg().setValue( "-J-Dfile.encoding=" + ReaderFactory.FILE_ENCODING );
         }
 
         List<String> arguments = new ArrayList<String>();
