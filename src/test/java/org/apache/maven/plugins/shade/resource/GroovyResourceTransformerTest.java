0d93c69833212b0e702a8d8866e58b7f2faf7ab8
Olivier Lamy <olamy@apache.org>
2016-11-26 11:03:39 +0000

    formatting
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-shade-plugin@1771460 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/plugins/shade/resource/GroovyResourceTransformerTest.java
+++ src/test/java/org/apache/maven/plugins/shade/resource/GroovyResourceTransformerTest.java
@@ -34,16 +34,16 @@ import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 
 /**
- * Test for {@link XmlAppendingTransformer}.
- * 
+ * Test for {@link GroovyResourceTransformer}.
+ *
  * @author Julien Viet
- * @version $Id$
  */
 public class GroovyResourceTransformerTest
     extends TestCase
 {
 
-    private static InputStream stream( Properties props ) throws Exception
+    private static InputStream stream( Properties props )
+        throws Exception
     {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         props.store( baos, null );
@@ -51,7 +51,9 @@ public class GroovyResourceTransformerTest
 
     }
 
-    private static InputStream module( String name, String version, String extensionClasses, String staticExtensionClasses ) throws Exception
+    private static InputStream module( String name, String version, String extensionClasses,
+                                       String staticExtensionClasses )
+        throws Exception
     {
         Properties desc = new Properties();
         desc.setProperty( "moduleName", name );
@@ -67,7 +69,8 @@ public class GroovyResourceTransformerTest
         return stream( desc );
     }
 
-    private static Properties transform( GroovyResourceTransformer transformer ) throws Exception
+    private static Properties transform( GroovyResourceTransformer transformer )
+        throws Exception
     {
         File tempJar = File.createTempFile( "shade.", ".jar" );
         tempJar.deleteOnExit();
@@ -86,7 +89,8 @@ public class GroovyResourceTransformerTest
         return desc;
     }
 
-    public void testFilter() throws Exception
+    public void testFilter()
+        throws Exception
     {
         GroovyResourceTransformer transformer = new GroovyResourceTransformer();
         assertTrue( transformer.canTransformResource( GroovyResourceTransformer.EXT_MODULE_NAME ) );
@@ -94,20 +98,23 @@ public class GroovyResourceTransformerTest
         assertFalse( transformer.canTransformResource( JarFile.MANIFEST_NAME ) );
     }
 
-    public void testEmpty() throws Exception
+    public void testEmpty()
+        throws Exception
     {
         GroovyResourceTransformer transformer = new GroovyResourceTransformer();
         assertFalse( transformer.hasTransformedResource() );
         assertNull( transform( transformer ) );
     }
 
-    public void testSpecifyModuleName() throws Exception
+    public void testSpecifyModuleName()
+        throws Exception
     {
         GroovyResourceTransformer transformer = new GroovyResourceTransformer();
         transformer.extModuleName = "the-module-name";
         transformer.extModuleVersion = "2.0";
         transformer.processResource( GroovyResourceTransformer.EXT_MODULE_NAME,
-            module("mod1", "1.0", "some.ext", "some.staticExt"), Collections.<Relocator>emptyList() );
+                                     module( "mod1", "1.0", "some.ext", "some.staticExt" ),
+                                     Collections.<Relocator>emptyList() );
         Properties desc = transform( transformer );
         assertEquals( "the-module-name", desc.getProperty( "moduleName" ) );
         assertEquals( "2.0", desc.getProperty( "moduleVersion" ) );
@@ -115,17 +122,20 @@ public class GroovyResourceTransformerTest
         assertEquals( "some.staticExt", desc.getProperty( "staticExtensionClasses" ) );
     }
 
-    public void testConcatenation() throws Exception
+    public void testConcatenation()
+        throws Exception
     {
         GroovyResourceTransformer transformer = new GroovyResourceTransformer();
         transformer.processResource( GroovyResourceTransformer.EXT_MODULE_NAME,
-            module( "mod1", "1.0", "some.ext1", null ), Collections.<Relocator>emptyList() );
-        transformer.processResource(GroovyResourceTransformer.EXT_MODULE_NAME,
-            module( "mod2", "1.0", null, "some.staticExt1" ), Collections.<Relocator>emptyList());
-        transformer.processResource(GroovyResourceTransformer.EXT_MODULE_NAME,
-            module( "mod3", "1.0", "", "" ), Collections.<Relocator>emptyList());
-        transformer.processResource(GroovyResourceTransformer.EXT_MODULE_NAME,
-            module( "mod4", "1.0", "some.ext2", "some.staticExt2" ), Collections.<Relocator>emptyList());
+                                     module( "mod1", "1.0", "some.ext1", null ), Collections.<Relocator>emptyList() );
+        transformer.processResource( GroovyResourceTransformer.EXT_MODULE_NAME,
+                                     module( "mod2", "1.0", null, "some.staticExt1" ),
+                                     Collections.<Relocator>emptyList() );
+        transformer.processResource( GroovyResourceTransformer.EXT_MODULE_NAME, module( "mod3", "1.0", "", "" ),
+                                     Collections.<Relocator>emptyList() );
+        transformer.processResource( GroovyResourceTransformer.EXT_MODULE_NAME,
+                                     module( "mod4", "1.0", "some.ext2", "some.staticExt2" ),
+                                     Collections.<Relocator>emptyList() );
         Properties desc = transform( transformer );
         assertEquals( "no-module-name", desc.getProperty( "moduleName" ) );
         assertEquals( "1.0", desc.getProperty( "moduleVersion" ) );
