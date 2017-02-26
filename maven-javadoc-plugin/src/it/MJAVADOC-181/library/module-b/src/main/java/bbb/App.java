db4bff573ed05a963738752d5ce5a5e223e0c8c4
Guillaume Boué <gboue@apache.org>
2017-02-26 00:55:45 +0000

    Fixing the ITs so that the project can be built with JDK 8
     * Self-enclosing elements are disallowed so <img> should be used instead of <img/>
     * Custom tags cannot start with a dot anymore (http://stackoverflow.com/q/28726294)
     * A link to a non-existent class generates an error and not a warning anymore (but only with Oracle JDK 8, not OpenJDK 8)
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1784413 13f79535-47bb-0310-9956-ffa450edef68

--- maven-javadoc-plugin/src/it/MJAVADOC-181/library/module-b/src/main/java/bbb/App.java
+++ maven-javadoc-plugin/src/it/MJAVADOC-181/library/module-b/src/main/java/bbb/App.java
@@ -22,10 +22,10 @@ package bbb;
 /**
  * Hello world class.
  *
- * @.foo    A 1st custom javadoc tag.
+ * @m.foo    A 1st custom javadoc tag.
  * @author  Me
  * @version 1st
- * @.bar    A 2nd custom javadoc tag.
+ * @m.bar    A 2nd custom javadoc tag.
  * @see     String#startsWith(String)
  */
 public class App
@@ -33,7 +33,7 @@ public class App
     /**
      * Hello world method.
      *
-     * @.foo    A 1st custom javadoc tag.
+     * @m.foo    A 1st custom javadoc tag.
      * @see     String#endsWith(String)
      */
     public static void main( String[] args )
--- maven-javadoc-plugin/src/it/dependencySource-3/MJAVADOC-280-3-modA/src/main/java/org/apache/maven/plugins/mjavadoc/it/moda/App.java
+++ maven-javadoc-plugin/src/it/dependencySource-3/MJAVADOC-280-3-modA/src/main/java/org/apache/maven/plugins/mjavadoc/it/moda/App.java
@@ -21,10 +21,10 @@ package org.apache.maven.plugins.mjavadoc.it.moda;
 
 /**
  * Hello world!
- * @.foo    A 1st custom javadoc tag.
+ * @m.foo    A 1st custom javadoc tag.
  * @author  Me
  * @version 1st
- * @.bar    A 2nd custom javadoc tag.
+ * @m.bar    A 2nd custom javadoc tag.
  * @see     String#startsWith(String)
  */
 public class App
