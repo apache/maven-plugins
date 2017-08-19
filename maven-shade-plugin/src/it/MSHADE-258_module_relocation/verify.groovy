7a0a846c20421247e41d7814119503bbe7f19dfa
Robert Scholte <rfscholte@apache.org>
2017-08-19 11:42:02 +0000

    [MSHADE-258] RemappingClassAdapter is deprecated and throws an exception with ASM 6.0 beta Provide IT
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1805503 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-shade-plugin/src/it/MSHADE-258_module_relocation/verify.groovy
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+def buildLog = new File ( basedir , "build.log" )
+assert buildLog.text.contains( "[WARNING] Discovered module-info.class. Shading will break its strong encapsulation." )
+
+def jarFile = new java.util.jar.JarFile( new File( basedir, "target/mshade-258-1.0-SNAPSHOT.jar" ) )
+try 
+{
+  assert null == jarFile.getJarEntry( "module-info.class" )
+}
+finally 
+{
+  jarFile.close()
+}
