3036d87b59caaf9ba59a441c05fb73362a302e51
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-09-08 19:46:30 +0000

    Commented out test which actually does not work.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1807812 13f79535-47bb-0310-9956-ffa450edef68

--- maven-jlink-plugin/src/test/java/org/apache/maven/plugins/jlink/AbsractJLinkMojoTest.java
+++ maven-jlink-plugin/src/test/java/org/apache/maven/plugins/jlink/AbsractJLinkMojoTest.java
@@ -23,17 +23,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Collections;
 
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import org.codehaus.plexus.util.cli.CommandLineException;
-import org.codehaus.plexus.util.cli.Commandline;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -115,25 +109,25 @@ public class AbsractJLinkMojoTest
         assertThat( result ).isEqualTo( "A,B" );
     }
 
-    @Test
-    public void xxx()
-        throws MojoExecutionException, IOException, CommandLineException
-    {
-        Process p = mock( Process.class );
-
-        String b = "Error occured";
-        byte[] bytes = b.getBytes();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        baos.write( bytes );
-        
-        when (p.getOutputStream()).thenReturn( baos );
-        
-        Commandline cmd = mock( Commandline.class );
-        when (cmd.execute()).thenReturn( p );
-        
-        File outputDirectory = mock( File.class );
-
-        mojoMock.executeCommand( cmd, outputDirectory );
-
-    }
+//    @Test
+//    public void xxx()
+//        throws MojoExecutionException, IOException, CommandLineException
+//    {
+//        Process p = mock( Process.class );
+//
+//        String b = "Error occured";
+//        byte[] bytes = b.getBytes();
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        baos.write( bytes );
+//        
+//        when (p.getOutputStream()).thenReturn( baos );
+//        
+//        Commandline cmd = mock( Commandline.class );
+//        when (cmd.execute()).thenReturn( p );
+//        
+//        File outputDirectory = mock( File.class );
+//
+//        mojoMock.executeCommand( cmd, outputDirectory );
+//
+//    }
 }
