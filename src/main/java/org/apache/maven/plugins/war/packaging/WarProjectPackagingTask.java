860114355b3145c640a17523eac6d26d308fa1b8
Michael Osipov <michaelo@apache.org>
2017-09-26 21:45:18 +0000

    [MWAR-407] Binary files are modified during web.xml filtering; revert MWAR-404
    This is a regression caused by MWAR-404 and therefore needs to be reverted for now.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk/maven-war-plugin@1809790 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/plugins/war/packaging/WarProjectPackagingTask.java
+++ src/main/java/org/apache/maven/plugins/war/packaging/WarProjectPackagingTask.java
@@ -170,14 +170,13 @@ public class WarProjectPackagingTask
         else if ( !context.getWebappSourceDirectory().getAbsolutePath().equals( context.getWebappDirectory().getPath() ) )
         {
             context.getLog().info( "Copying webapp resources [" + context.getWebappSourceDirectory() + "]" );
-            context.getLog().debug( "isFilteringDeploymentDescriptors: [" + context.isFilteringDeploymentDescriptors() + "]" );
             final PathSet sources =
                 getFilesToIncludes( context.getWebappSourceDirectory(), context.getWebappSourceIncludes(),
                                     context.getWebappSourceExcludes(), context.isWebappSourceIncludeEmptyDirectories() );
 
             try
             {
-                 copyFiles( id, context, context.getWebappSourceDirectory(), sources, context.isFilteringDeploymentDescriptors() );
+                copyFiles( id, context, context.getWebappSourceDirectory(), sources, false );
             }
             catch ( IOException e )
             {
