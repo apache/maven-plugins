9a8fe56149c6bf0ebe8243aba7e9099c3711a7f5
Herve Boutemy <hboutemy@apache.org>
2017-10-21 03:02:35 +0000

    [MPOM-169] removed hardcoded rat version
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1812804 13f79535-47bb-0310-9956-ffa450edef68

--- rat-report.sh
+++ rat-report.sh
@@ -15,14 +15,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-FOLDERS=`find . -maxdepth 1 -type d -not -path "./.svn" -and -not -path "."`
-MVN="/home/build/tools/maven/apache-maven-3.1.1/bin/mvn"
-RAT_REPORT="${MVN} org.apache.rat:apache-rat-plugin:0.11:check -Drat.ignoreErrors=true -Drat.excludeSubProjects=false"
-for i in $FOLDERS; do
-  echo "Checking $i"
-  cd $i
-  $RAT_REPORT >../$i.log
-  cd ..
+RAT_REPORT="mvn apache-rat:check -Drat.ignoreErrors=true -Drat.excludeSubProjects=false"
+for i in *; do
+  if [ -e $i/pom.xml ]; then
+    echo "Checking $i"
+    cd $i
+    $RAT_REPORT >../$i.log
+    cd ..
+  fi
 done;
 # Printout the reports only first 19 lines which contain the usefull information.
 head -n19 `find . -maxdepth 3 -type f -name "rat.txt"`
